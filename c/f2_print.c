b904b4b1602f22552377ee3d6127a022bbe4730b
neptune <neptune@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-02-19 18:39:23 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@74 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- c/f2_print.c
+++ c/f2_print.c
@@ -772,11 +772,9 @@ f2ptr f2__write_pretty(f2ptr cause, f2ptr stream, f2ptr exp, int recursion_depth
 						  ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num, available_width - width, subexp_size, try_wide, wide_success, show_slot_causes, use_ansi_colors, use_html); width += subexp_size[0]; height += subexp_size[1];}
 	  } else if (f2primobject__is_bug(exp, cause)) {
 	    int subexp_size[2];
-	    if (try_wide) {f2__write__space(cause, stream, use_html); width ++;} else {f2__write__line_break(cause, stream, use_html); width = 0; height ++; int i; for (i = 0; i < indent_space_num + width; i++) {f2__write__space(cause, stream, use_html);}}
-	    {f2__write_pretty__slot_key_and_value("type", 5, cause, stream, f2bug__type(exp, cause),                   nil, nil, nil,
+	    if (try_wide) {f2__write__space(cause, stream, use_html); width ++;} else {f2__write__line_break(cause, stream, use_html); width = 0; height ++; int i; for (i = 0; i < indent_space_num + width; i++) {f2__write__space(cause, stream, use_html);}}  
+	    {f2__write_pretty__slot_key_and_value("type", 4, cause, stream, f2bug__type(exp, cause), f2bug__type__tracing_on(exp, cause), f2bug__type__trace(exp, cause), f2bug__type__imagination_frame(exp, cause),
 						  ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num, available_width - width, subexp_size, try_wide, wide_success, show_slot_causes, use_ansi_colors, use_html); width += subexp_size[0]; height += subexp_size[1];}
-	    {f2__write_pretty(cause, stream, f2bug__type(exp, cause),
-			      ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num + width, available_width - width, subexp_size, 1, wide_success, show_slot_causes, use_ansi_colors, use_html); width += subexp_size[0]; height += subexp_size[1];}
 	  } else if (f2primobject__is_hashtable(exp, cause)) {
 	    int subexp_size[2];
 	    if (try_wide) {f2__write__space(cause, stream, use_html); width ++;} else {f2__write__line_break(cause, stream, use_html); width = 0; height ++; int i; for (i = 0; i < indent_space_num + width; i++) {f2__write__space(cause, stream, use_html);}}  

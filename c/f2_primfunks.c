a76b1146d52860401d690b9348cb3c62b7f52f7d
neptune <neptune@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-02-21 18:48:51 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@119 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- c/f2_primfunks.c
+++ c/f2_primfunks.c
@@ -724,6 +724,7 @@ void f2thread__funk(f2ptr thread, f2ptr cause, f2ptr cfunkable, f2ptr args) {
     }
   } else {
     status("[ERROR] f2thread__force_funk error: cfunkable must be funk or metro.");
+    printf("[ERROR] f2thread__force_funk error: cfunkable must be funk or metro."); fflush(stdout);
     f2thread__value__set(thread, cause, f2larva__new(cause, 24));
   }
   

44426633a95948150d98c27d3eee406004136ace
neptune <neptune@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-02-22 19:02:43 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@165 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- c/f2_repl.c
+++ c/f2_repl.c
@@ -23,7 +23,7 @@
 
 int f2__repl(f2ptr cause, f2ptr thread) {
   printf("\nfunk2 warning: garbage collection is disabled in this repl.");
-  f2ptr repl_funk     = f2funk__new(cause, nil, nil, nil, f2cons__new(cause, nil, nil), nil, global_environment(), nil);
+  f2ptr repl_funk     = f2funk__new(cause, nil, nil, nil, f2cons__new(cause, nil, nil), nil, global_environment(), nil, nil);
   f2ptr repl_funk_bcs = f2__compile__funk(cause, environment__safe_lookup_var_value(cause, global_environment(), __funk2.globalenv.trace_all_compiles__symbol), thread, repl_funk);
   f2ptr repl_thread   = f2__thread_serial(cause, thread, f2thread__env(thread, cause), repl_funk, nil);
   while (1) {
@@ -35,7 +35,7 @@ int f2__repl(f2ptr cause, f2ptr thread) {
     } else {
       pause_gc();
       
-      repl_funk     = f2funk__new(cause, nil, nil, nil, f2cons__new(cause, read_exp, nil), read_exp, global_environment(), nil);
+      repl_funk     = f2funk__new(cause, nil, nil, nil, f2cons__new(cause, read_exp, nil), read_exp, global_environment(), nil, nil);
       repl_funk_bcs = f2__compile__funk(cause, environment__safe_lookup_var_value(cause, global_environment(), __funk2.globalenv.trace_all_compiles__symbol), thread, repl_funk);
       if(raw__larvap(repl_funk_bcs, cause)) {
 	f2thread__value__set(thread, cause, repl_funk_bcs);

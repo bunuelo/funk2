c7db57e3478ac3357c2db708bb5fa7d396dc8a6e
neptune <neptune@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-02-22 18:56:52 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@162 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- c/f2_load.c
+++ c/f2_load.c
@@ -41,7 +41,7 @@ f2ptr raw__load(f2ptr cause, f2ptr thread, f2ptr filename) {
   
   try_gc();
   
-  f2ptr load_funk     = f2funk__new(cause, nil, nil, nil, f2cons__new(cause, nil, nil), nil, global_environment(), nil);
+  f2ptr load_funk     = f2funk__new(cause, nil, nil, nil, f2cons__new(cause, nil, nil), nil, global_environment(), nil, nil);
   f2ptr load_funk_bcs = f2__compile__funk(cause, environment__safe_lookup_var_value(cause, global_environment(), __funk2.globalenv.trace_all_compiles__symbol), thread, load_funk);
   f2ptr load_thread   = f2__thread_serial(cause, thread, f2thread__env(thread, cause), load_funk, nil);
   f2ptr read_exp      = nil;
@@ -56,7 +56,7 @@ f2ptr raw__load(f2ptr cause, f2ptr thread, f2ptr filename) {
       } else {
 	pause_gc();
 	
-	load_funk     = f2funk__new(cause, nil, nil, nil, f2cons__new(cause, read_exp, nil), read_exp, global_environment(), nil);
+	load_funk     = f2funk__new(cause, nil, nil, nil, f2cons__new(cause, read_exp, nil), read_exp, global_environment(), nil, nil);
 	load_funk_bcs = f2__compile__funk(cause,
 					  environment__safe_lookup_var_value(cause, global_environment(), __funk2.globalenv.trace_all_compiles__symbol),
 					  thread, load_funk);

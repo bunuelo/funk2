5b19b3eb6d9349764519b78032699f4e8f215007
neptune <neptune@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-02-22 18:53:28 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@158 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- c/f2_primfunks.h
+++ c/f2_primfunks.h
@@ -434,18 +434,18 @@
     def_body; \
   }
 
-#define f2__primcfunk__init__with_c_cfunk_var(name, c_cfunk_var) \
-  f2ptr c_cfunk_var = f2cfunk__new(initial_cause(), def_pcfunk__symbolvar(name), nil, f2pointer__new(initial_cause(), to_ptr(def_pcfunk__funkvar(name))), global_environment()); \
+#define f2__primcfunk__init__with_c_cfunk_var(name, c_cfunk_var, is_funktional) \
+  f2ptr c_cfunk_var = f2cfunk__new(initial_cause(), def_pcfunk__symbolvar(name), nil, f2pointer__new(initial_cause(), to_ptr(def_pcfunk__funkvar(name))), global_environment(), (is_funktional) ? __funk2.globalenv.true__symbol : nil); \
   { \
     def_pcfunk__symbolvar__init(name); \
     environment__add_funkvar_value(initial_cause(), global_environment(), def_pcfunk__symbolvar(name), c_cfunk_var); \
   }
 
-#define f2__primcfunk__init(name) { \
+#define f2__primcfunk__init(is_funktional, name) { \
   f2__primcfunk__init__with_c_cfunk_var(name, temp_c_cfunk_var); \
 }
 
-#define f2__primmetro__init(name) { \
+#define f2__primmetro__init(is_funktional, name) { \
   def_pcfunk__symbolvar__init(name); \
   environment__add_funkvar_value(initial_cause(), global_environment(), def_pcfunk__symbolvar(name), f2metrocfunk__new(initial_cause(), nil, f2pointer__new(initial_cause(), to_ptr(def_pcfunk__metrovar(name))), global_environment())); \
 }

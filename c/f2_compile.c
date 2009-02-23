a1557023bd1e13c5154b1137f21117b57109ddd9
neptune <neptune@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-02-23 23:18:46 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@203 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- c/f2_compile.c
+++ c/f2_compile.c
@@ -439,14 +439,24 @@ f2ptr f2__compile__rawcode(f2ptr simple_cause, bool tracewrap, f2ptr thread, f2p
   bool optimize_subexp_tail_recursion = (f2cons__cdr(exps, cause) == nil) && optimize_tail_recursion;
   
   f2ptr full_bcs = nil;
-  {
-    bool exp__is_funktional = true;
-    full_bcs = raw__compile(cause, tracewrap, thread, f2cons__car(exps, cause), protect_subexp_environment, optimize_subexp_tail_recursion, popped_env_and_return, &exp__is_funktional);
+  do {
+    f2ptr exp                = f2cons__car(exps, cause);
+    bool  exp__is_funktional = true;
+    full_bcs = raw__compile(cause, tracewrap, thread, exp, protect_subexp_environment, optimize_subexp_tail_recursion, popped_env_and_return, &exp__is_funktional);
     if (! exp__is_funktional) {
       if (is_funktional) {
 	*is_funktional = false;
       }
     }
+    if (exp__is_funktional) {
+      printf("\noptimizing funktional beginning of rawcode!");
+      f2__print(cause, exp);
+      full_bcs = nil;
+      exps     = f2cons__cdr(exps, cause);
+    }
+  } while(exps);
+  if (!exps) {
+    return nil;
   }
   if (full_bcs && (! raw__consp(full_bcs, cause))) {return full_bcs;}
   exps = f2cons__cdr(exps, cause);
@@ -461,8 +471,9 @@ f2ptr f2__compile__rawcode(f2ptr simple_cause, bool tracewrap, f2ptr thread, f2p
     optimize_subexp_tail_recursion = (f2cons__cdr(exps, cause) == nil) && optimize_tail_recursion;
     f2ptr exp_bcs = nil;
     {
-      bool exp__is_funktional = true;
-      exp_bcs = raw__compile(cause, tracewrap, thread, f2cons__car(exps, cause), protect_subexp_environment, optimize_subexp_tail_recursion, popped_env_and_return, &exp__is_funktional);
+      f2ptr exp                = f2cons__car(exps, cause);
+      bool  exp__is_funktional = true;
+      exp_bcs = raw__compile(cause, tracewrap, thread, exp, protect_subexp_environment, optimize_subexp_tail_recursion, popped_env_and_return, &exp__is_funktional);
       if (! exp__is_funktional) {
 	if (is_funktional) {
 	  *is_funktional = false;

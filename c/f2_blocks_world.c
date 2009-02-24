b85030ebf55fe86ae6c48e07373404de4c6fa27d
neptune <neptune@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-02-24 23:41:38 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@286 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- c/f2_blocks_world.c
+++ c/f2_blocks_world.c
@@ -208,12 +208,24 @@ f2ptr f2__blocks_world_rectangle__clear_line_above(f2ptr cause, f2ptr this) {
   }
   //s64 lower_left__x = raw__blocks_world__lower_left__x(cause);
   s64 lower_left__y = raw__blocks_world__lower_left__y(cause);
-  s64 rect__x0 = f2blocks_world_rectangle__x0(this, cause);
-  //s64 rect__y0 = f2blocks_world_rectangle__y0(this, cause);
-  s64 rect__x1 = f2blocks_world_rectangle__x1(this, cause);
-  s64 rect__y1 = f2blocks_world_rectangle__y1(this, cause);
-  s64 y1 = lower_left__y - rect__y1 - 1;
-  raw__ansi__stream__rectangle(cause, __funk2.globalenv.stdout_stream, rect__x0 + 10, y1, rect__x1 + 10, y1, ' ');
+
+  f2ptr rect__x0 = f2blocks_world_rectangle__x0(this, cause);
+  //f2ptr rect__y0 = f2blocks_world_rectangle__y0(this, cause);
+  f2ptr rect__x1 = f2blocks_world_rectangle__x1(this, cause);
+  f2ptr rect__y1 = f2blocks_world_rectangle__y1(this, cause);
+  if ((! raw__integerp(rect__x0, cause)) ||
+      (! raw__integerp(rect__y0, cause)) ||
+      (! raw__integerp(rect__x1, cause)) ||
+      (! raw__integerp(rect__y1, cause))) {
+    return f2larva__new(cause, 1);
+  }
+  s64 raw__rect__x0 = f2integer__i(rect__x0, cause);
+  //s64 raw__rect__y0 = f2integer__i(rect__y0, cause);
+  s64 raw__rect__x1 = f2integer__i(rect__x1, cause);
+  s64 raw__rect__y1 = f2integer__i(rect__y1, cause);
+
+  s64 y1 = lower_left__y - raw__rect__y1 - 1;
+  raw__ansi__stream__rectangle(cause, __funk2.globalenv.stdout_stream, raw__rect__x0 + 10, y1, raw__rect__x1 + 10, y1, ' ');
   return nil;
 }
 def_pcfunk1(blocks_world_rectangle__clear_line_above, this, return f2__blocks_world_rectangle__clear_line_above(this_cause, this));
@@ -298,22 +310,40 @@ f2ptr f2__blocks_world_rectangle__overlaps(f2ptr cause, f2ptr this, f2ptr rectan
       (! raw__blocks_world_rectanglep(rectangle, cause))) {
     return f2larva__new(cause, 1);
   }
-  s64 a_x0 = f2blocks_world_rectangle__x0(this,      cause);
-  s64 a_y0 = f2blocks_world_rectangle__y0(this,      cause);
-  s64 a_x1 = f2blocks_world_rectangle__x1(this,      cause);
-  s64 a_y1 = f2blocks_world_rectangle__y1(this,      cause);
-  s64 b_x0 = f2blocks_world_rectangle__x0(rectangle, cause);
-  s64 b_y0 = f2blocks_world_rectangle__y0(rectangle, cause);
-  s64 b_x1 = f2blocks_world_rectangle__x1(rectangle, cause);
-  s64 b_y1 = f2blocks_world_rectangle__y1(rectangle, cause);
-  if ((((b_x0 <= a_x0) && (b_x1 >= a_x1)) ||
-       ((a_x0 <= b_x0) && (a_x1 >= b_x1)) ||
-       ((a_x0 <= b_x0) && (a_x1 >= b_x0)) ||
-       ((a_x0 <= b_x1) && (a_x1 >= b_x1))) &&
-      (((b_y0 <= a_y0) && (b_y1 >= a_y1)) ||
-       ((a_y0 <= b_y0) && (a_y1 >= b_y1)) ||
-       ((a_y0 <= b_y0) && (a_y1 >= b_y0)) ||
-       ((a_y0 <= b_y1) && (a_y1 >= b_y1)))) {
+  f2ptr a_x0 = f2blocks_world_rectangle__x0(this,      cause);
+  f2ptr a_y0 = f2blocks_world_rectangle__y0(this,      cause);
+  f2ptr a_x1 = f2blocks_world_rectangle__x1(this,      cause);
+  f2ptr a_y1 = f2blocks_world_rectangle__y1(this,      cause);
+  f2ptr b_x0 = f2blocks_world_rectangle__x0(rectangle, cause);
+  f2ptr b_y0 = f2blocks_world_rectangle__y0(rectangle, cause);
+  f2ptr b_x1 = f2blocks_world_rectangle__x1(rectangle, cause);
+  f2ptr b_y1 = f2blocks_world_rectangle__y1(rectangle, cause);
+  if ((! raw__integerp(a_x0, cause)) ||
+      (! raw__integerp(a_y0, cause)) ||
+      (! raw__integerp(a_x1, cause)) ||
+      (! raw__integerp(a_y1, cause)) ||
+      (! raw__integerp(b_x0, cause)) ||
+      (! raw__integerp(b_y0, cause)) ||
+      (! raw__integerp(b_x1, cause)) ||
+      (! raw__integerp(b_y1, cause))) {
+    return f2larva__new(cause, 1);
+  }
+  s64 raw_a_x0 = f2integer__i(a_x0, cause);
+  s64 raw_a_y0 = f2integer__i(a_y0, cause);
+  s64 raw_a_x1 = f2integer__i(a_x1, cause);
+  s64 raw_a_y1 = f2integer__i(a_y1, cause);
+  s64 raw_b_x0 = f2integer__i(b_x0, cause);
+  s64 raw_b_y0 = f2integer__i(b_y0, cause);
+  s64 raw_b_x1 = f2integer__i(b_x1, cause);
+  s64 raw_b_y1 = f2integer__i(b_y1, cause);
+  if ((((raw_b_x0 <= raw_a_x0) && (raw_b_x1 >= raw_a_x1)) ||
+       ((raw_a_x0 <= raw_b_x0) && (raw_a_x1 >= raw_b_x1)) ||
+       ((raw_a_x0 <= raw_b_x0) && (raw_a_x1 >= raw_b_x0)) ||
+       ((raw_a_x0 <= raw_b_x1) && (raw_a_x1 >= raw_b_x1))) &&
+      (((raw_b_y0 <= raw_a_y0) && (raw_b_y1 >= raw_a_y1)) ||
+       ((raw_a_y0 <= raw_b_y0) && (raw_a_y1 >= raw_b_y1)) ||
+       ((raw_a_y0 <= raw_b_y0) && (raw_a_y1 >= raw_b_y0)) ||
+       ((raw_a_y0 <= raw_b_y1) && (raw_a_y1 >= raw_b_y1)))) {
     return __funk2.globalenv.true__symbol;
   }
   return nil;

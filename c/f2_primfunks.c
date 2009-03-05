bd130c9112e79af726b474589449179992d776dd
neptune <neptune@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-03-05 23:58:35 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@480 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- c/f2_primfunks.c
+++ c/f2_primfunks.c
@@ -1207,7 +1207,7 @@ f2ptr f2__compile(f2ptr cause, f2ptr thread, f2ptr exp, f2ptr protect_environmen
   bool is_funktional = true;
   return raw__compile(cause, thread, exp, (protect_environment != nil), (protect_environment == nil), NULL, &is_funktional, nil, NULL);
 }
-def_pcfunk3(compile, exp, protect_environment, return f2__compile(this_cause, simple_thread, exp, protect_environment));
+def_pcfunk2(compile, exp, protect_environment, return f2__compile(this_cause, simple_thread, exp, protect_environment));
 
 f2ptr f2__identity(f2ptr cause, f2ptr exp) {return exp;}
 def_pcfunk1(identity, exp, return f2__identity(this_cause, exp));
@@ -1224,7 +1224,7 @@ f2ptr f2__make_funk(f2ptr cause, f2ptr thread, f2ptr bytecode_tracing_on, f2ptr 
 #endif
   return funk;
 }
-def_pcfunk7(make_funk, bytecode_tracing_on, name, args, demetropolized_body, body, bytecodes, is_funktional, return f2__make_funk(this_cause, simple_thread, bytecode_tracing_on, name, args, demetropolized_body, body, bytecodes, is_funktional));
+def_pcfunk6(make_funk, name, args, demetropolized_body, body, bytecodes, is_funktional, return f2__make_funk(this_cause, simple_thread, name, args, demetropolized_body, body, bytecodes, is_funktional));
 
 f2ptr f2__make_metro(f2ptr cause, f2ptr thread, f2ptr bytecode_tracing_on, f2ptr name, f2ptr args, f2ptr demetropolized_body, f2ptr body, f2ptr bytecodes, f2ptr is_funktional) {
   //f2__print_prompt("make-metro args: ", args);
@@ -1241,7 +1241,7 @@ f2ptr f2__make_metro(f2ptr cause, f2ptr thread, f2ptr bytecode_tracing_on, f2ptr
   
   return metro;
 }
-def_pcfunk7(make_metro, bytecode_tracing_on, name, args, demetropolized_body, body, bytecodes, is_funktional, return f2__make_metro(this_cause, simple_thread, bytecode_tracing_on, name, args, demetropolized_body, body, bytecodes, is_funktional));
+def_pcfunk6(make_metro, name, args, demetropolized_body, body, bytecodes, is_funktional, return f2__make_metro(this_cause, simple_thread, name, args, demetropolized_body, body, bytecodes, is_funktional));
 
 f2ptr f2__cfunk__apply(f2ptr cause, f2ptr cfunk, f2ptr thread, f2ptr args) {
   release__assert(raw__cfunkp(cfunk, cause),        nil, "cfunk failed type assertion.");

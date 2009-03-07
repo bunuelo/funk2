07e5f0da978a519a29fded2a9c5dce404ee62480
neptune <neptune@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-03-07 23:58:37 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@639 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- c/f2_memory.c
+++ c/f2_memory.c
@@ -1071,17 +1071,17 @@ f2ptr pool__memblock_f2ptr__try_new(int pool_index, f2size_t byte_num) {
   }
   f2ptr block_f2ptr = ptr_to_f2ptr(pool_index, to_ptr(block)); // this should be the only use of ptr_to_f2ptr in the whole program...
   memory_mutex__unlock(pool_index);
-  u64 computer_id  = f2ptr__computer_id(block_f2ptr);
+  u64 computer_id  = __f2ptr__computer_id(block_f2ptr);
   if (computer_id != 0) {
     status("[ERROR] computer_id must be zero for a local memory allocation.");
     error(nil, "computer_id must be zero for a local memory allocation.");
   }
-  u64 pool_index   = f2ptr__pool_index(block_f2ptr);
+  u64 pool_index   = __f2ptr__pool_index(block_f2ptr);
   if (pool_index >= (((u64)1) << pool_index__bit_num) - 1) {
     status("[ERROR] pool_index is out of range, (0 <= " u64__fstr " <= " u64__fstr ").", pool_index, (((u64)1) << pool_index__bit_num) - 1);
     error(nil, "pool_index is out of range.");
   }
-  pool_address = f2ptr__pool_address(block_f2ptr);
+  pool_address = __f2ptr__pool_address(block_f2ptr);
   if (pool_address >= (((u64)1) << pool_address__bit_num) - 1) {
     status("[ERROR] pool_address is out of range, (0 <= " u64__fstr " <= " u64__fstr ").", pool_address, (((u64)1) << pool_address__bit_num) - 1);
     error(nil, "pool_address is out of range.");

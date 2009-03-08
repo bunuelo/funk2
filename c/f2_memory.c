dcc526c876cf45d56c5ddfa40e75e667d833158e
neptune <neptune@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-03-08 00:00:10 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@641 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- c/f2_memory.c
+++ c/f2_memory.c
@@ -1067,7 +1067,7 @@ f2ptr pool__memblock_f2ptr__try_new(int pool_index, f2size_t byte_num) {
   {
     u64 check_pool_address = __ptr__pool_address(pool_index, block);
     if (check_pool_address >= (((u64)1) << pool_address__bit_num) - 1) {
-      status("pool_address is out of range, (0 <= " u64__fstr " <= " u64__fstr ").", pool_address, (((u64)1) << pool_address__bit_num) - 1);
+      status("pool_address is out of range, (0 <= " u64__fstr " <= " u64__fstr ").", check_pool_address, (((u64)1) << pool_address__bit_num) - 1);
       error(nil, "pool_address is out of range.");
     }
   }

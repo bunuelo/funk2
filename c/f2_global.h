55942167ff8f6d7ac2ae2eec03ffdb9f94fa4bdb
neptune <neptune@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-03-08 01:39:39 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@654 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- c/f2_global.h
+++ c/f2_global.h
@@ -98,12 +98,12 @@ typedef                         u64 pool_address_t;
 //#define scheduler_processor_num 1
 #define ideal_scheduler_processor_num (1<<(processor_num_power - 1))
 
-#if (ideal_scheduler_processor_num > (1<<(pool_index__bit_num - 1)))
-#  define scheduler_processor_num (1<<(pool_index__bit_num - 1))
+#if (ideal_scheduler_processor_num > (1<<(pool_index__bit_num))
+#  define scheduler_processor_num (1<<(pool_index__bit_num))
 #else
 #  define scheduler_processor_num ideal_scheduler_processor_num
 #endif
-
+     
 #define memory_pool_num scheduler_processor_num
 
 

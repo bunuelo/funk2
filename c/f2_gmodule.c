fa9435faf44788162c6a7a91b42e423e9a1d8c79
neptune <neptune@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-03-11 05:36:25 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@735 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- c/f2_gmodule.c
+++ c/f2_gmodule.c
@@ -57,11 +57,11 @@ f2ptr f2__gmodule__build_path(f2ptr cause, f2ptr directory, f2ptr module_name) {
   u8* module_name__str    = alloca(module_name__length + 1);
   f2string__str_copy(module_name, cause, module_name__str);
   module_name__str[module_name__length] = 0;
-  char* new_path_str = raw__gmodule__build_path(directory__str, module_name__str);
+  u8* new_path_str = raw__gmodule__build_path(directory__str, module_name__str);
   if (new_path_str == NULL) {
     return nil;
   }
-  f2ptr new_path = f2string__new(cause, strlen(new_path_str), (u8*)new_path_str);
+  f2ptr new_path = f2string__new(cause, strlen((char*)new_path_str), new_path_str);
   free(new_path_str);
   return new_path;
 }

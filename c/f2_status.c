07b4582decb32e945cd30d5be63fb4c3bbafaf10
bunuelo <bunuelo@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-03-18 20:23:22 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@859 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- c/f2_status.c
+++ c/f2_status.c
@@ -35,6 +35,7 @@ void funk2_status(char* filename, int line_num, char* msg, ...) {
   int trace_fd = open("/tmp/funk2_trace.log", O_CREAT | O_APPEND | O_WRONLY, S_IRWXU | S_IRWXG | S_IRWXO);
   if (trace_fd == -1) {
     printf("[WARNING] funk2_status couldn't open funk2_trace.log");
+    pthread_mutex_unlock(&(__funk2.status.trace_mutex));
     return;
   }
   u64 microseconds_since_1970    = raw__system_microseconds_since_1970();

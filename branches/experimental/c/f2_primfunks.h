dc527fec14d8665a2a93ed365ae077767b8b307a
bunuelo <bunuelo@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-03-21 05:39:30 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@906 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- branches/experimental/c/f2_primfunks.h
+++ branches/experimental/c/f2_primfunks.h
@@ -556,8 +556,8 @@
                                                                                                                          f2symbol__new(initial_cause(), strlen(#arg3), (u8*)#arg3), \
                                                                                                                          f2symbol__new(initial_cause(), strlen(#arg4), (u8*)#arg4), \
                                                                                                                          f2symbol__new(initial_cause(), strlen(#arg5), (u8*)#arg5), \
-                                                                                                                         f2symbol__new(initial_cause(), strlen(#arg5), (u8*)#arg6), \
-                                                                                                                         f2symbol__new(initial_cause(), strlen(#arg6), (u8*)#arg7)))
+                                                                                                                         f2symbol__new(initial_cause(), strlen(#arg6), (u8*)#arg6), \
+                                                                                                                         f2symbol__new(initial_cause(), strlen(#arg7), (u8*)#arg7)))
 
 #define f2__primcfunk__init__with_c_cfunk_var__8_arg_and_rest(name, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, and_rest, c_cfunk_var, is_funktional) \
   f2__primcfunk__init__with_c_cfunk_var__cfunk_args_code(name, c_cfunk_var, is_funktional, f2list9__new(initial_cause(), f2symbol__new(initial_cause(), strlen(#arg0),     (u8*)#arg0) \

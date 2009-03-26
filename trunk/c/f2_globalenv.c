be8c1615c727337caeab3c0d1a0397f4ebcc2cac
bunuelo <bunuelo@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-03-26 16:58:08 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@994 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- trunk/c/f2_globalenv.c
+++ trunk/c/f2_globalenv.c
@@ -180,7 +180,7 @@ void funk2_globalenv__destroy(funk2_globalenv_t* this) {
 
 f2ptr raw__c_source_file__cause__new(f2ptr cause, char* filename) {
   // we should use filename in a source-file cause type...
-  return f2__cause__new(cause, nil, nil, nil, nil, nil, nil, f2mutex__new(cause), nil);
+  return f2__cause__new(cause, nil, nil, nil, nil, nil, nil, nil);
 }
 
 f2ptr              f2_globalenv_c__cause__new(f2ptr cause) {return raw__c_source_file__cause__new(cause, "f2_globalenv.c");}

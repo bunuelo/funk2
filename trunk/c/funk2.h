a8ef8c6534ab1e453e316634807b0c3aef86a677
bunuelo <bunuelo@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-03-27 00:11:21 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@1011 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- trunk/c/funk2.h
+++ trunk/c/funk2.h
@@ -101,6 +101,7 @@
 #include "f2_primfunks__ioctl.h"
 #include "f2_primfunks__locale.h"
 #include "f2_primfunks__memory.h"
+#include "f2_primfunks__string.h"
 #include "f2_primobject__action.h"
 #include "f2_primobject__boolean.h"
 #include "f2_primobject__char_pointer.h"

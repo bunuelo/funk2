a8b7f12916de6e02c7e7402d7f4f10f5b063980c
bunuelo <bunuelo@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-05-11 18:31:50 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@1152 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- trunk/c/f2_dynamic_memory.c
+++ trunk/c/f2_dynamic_memory.c
@@ -24,7 +24,7 @@
 void f2dynamicmemory__init_and_alloc(f2dynamicmemory_t* this, f2size_t byte_num) {
   this->byte_num = byte_num;
   this->ptr      = to_ptr(malloc(byte_num));
-  if (this->ptr == NULL) {
+  if (from_ptr(this->ptr) == NULL) {
     perror("malloc");
     exit(-1);
   }
@@ -37,7 +37,7 @@ void f2dynamicmemory__destroy_and_free(f2dynamicmemory_t* this) {
 }
 
 void f2dynamicmemory__realloc(f2dynamicmemory_t* new_swapmemory, f2dynamicmemory_t* old_swapmemory, f2size_t byte_num) {
-  this->ptr = realloc(from_ptr(this->ptr), byte_num);
+  this->ptr = to_ptr(realloc(from_ptr(this->ptr), byte_num));
   if (from_ptr(this->ptr) == NULL) {
     perror("realloc");
     exit(-1);

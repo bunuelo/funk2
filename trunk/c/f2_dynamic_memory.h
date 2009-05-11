0459602cef05661b997081c3791e51a46b2cb132
bunuelo <bunuelo@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-05-11 18:23:51 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@1146 98e8950a-0dbf-11de-ba4d-252e5d5a8945

---
+++ trunk/c/f2_dynamic_memory.h
@@ -0,0 +1,38 @@
+// 
+// Copyright (c) 2007-2009 Bo Morgan.
+// All rights reserved.
+// 
+// Author: Bo Morgan
+// 
+// Permission to use, copy, modify and distribute this software and its
+// documentation is hereby granted, provided that both the copyright
+// notice and this permission notice appear in all copies of the
+// software, derivative works or modified versions, and any portions
+// thereof, and that both notices appear in supporting documentation.
+// 
+// BO MORGAN ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS" CONDITION.
+// BO MORGAN DISCLAIMS ANY LIABILITY OF ANY KIND FOR ANY DAMAGES
+// WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+// 
+// Bo Morgan requests users of this software to return to bo@mit.edu any
+// improvements or extensions that they make and grant Bo Morgan the
+// rights to redistribute these changes.
+// 
+
+#ifndef F2__DYNAMIC_MEMORY__H
+#define F2__DYNAMIC_MEMORY__H
+
+#include "f2_memory.h"
+#include "f2_archconfig.h"
+
+typedef struct f2dynamicmemory_s {
+  f2size_t byte_num;
+  ptr      ptr;
+} f2dynamicmemory_t;
+
+void f2dynamicmemory__init_and_alloc(f2dynamicmemory_t* this, f2size_t byte_num);
+void f2dynamicmemory__destroy_and_free(f2dynamicmemory_t* this);
+void f2dynamicmemory__realloc(f2dynamicmemory_t* new_swapmemory, f2swapmemory_t* old_swapmemory, f2size_t byte_num);
+
+#endif // F2__DYNAMIC_MEMORY__H
+

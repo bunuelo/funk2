88e90c0003435f7a2856ecd950e8969c55524bec
bunuelo <bunuelo@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-05-19 21:30:18 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@1215 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- extern/link-grammar-4.4.2/link-grammar/sat-solver/fast-sprintf.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef __FAST_SPRINTF_HPP__
-#define __FAST_SPRINTF_HPP__
-
-char* fast_sprintf(char* buffer, int num);
-char* fast_sprintf(char* buffer, const char* str);
-
-#endif
--- extern/link-grammar-4.4.2/link-grammar/sat-solver/variables.cpp
+++
@@ -1,5 +0,0 @@
-#include "variables.hpp"
-
-#ifdef _VARS
-ostream& var_defs_stream = cerr;
-#endif

bac2f0db52ab01f65dbce29256fe672f30ac22ae
bunuelo <bunuelo@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-05-22 21:54:17 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@1316 98e8950a-0dbf-11de-ba4d-252e5d5a8945

---
+++ c/f2_processor_thread.c
@@ -0,0 +1,39 @@
+// 
+// Copyright (c) 2007-2009 Bo Morgan.
+// All rights reserved.
+// 
+// Author: Bo Morgan
+// 
+// Permission to use, copy, modify and distribute this software and its
+// documentation is hereby granted, provided that both the copyright
+// notice and this permission notice appear in all copies of the
+// software, derivative works or modified versions, and any portions
+// thereof, and that both notices appear in supporting documentation.
+// 
+// BO MORGAN ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS" CONDITION.
+// BO MORGAN DISCLAIMS ANY LIABILITY OF ANY KIND FOR ANY DAMAGES
+// WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+// 
+// Bo Morgan requests users of this software to return to bo@mit.edu any
+// improvements or extensions that they make and grant Bo Morgan the
+// rights to redistribute these changes.
+// 
+
+#include "funk2.h"
+
+void funk2_processor_thread__init(funk2_processor_thread_t* this, funk2_processor_thread_function_pointer_t start_function, void* args) {
+  this->start_function = start_function;
+  this->args           = args;
+  int result = pthread_create(&(this->pthread), NULL, this->start_function, args);
+  if (result != 0) {
+    printf("\nfunk2_processor_thread__init: error creating new pthread.\n");
+    perror("pthread_create");
+    exit(-1);
+  }
+}
+
+void funk2_processor_thread__destroy(funk2_processor_thread_t* this) {
+}
+
+
+
---
+++ c/f2_processor_thread.h
@@ -0,0 +1,42 @@
+// 
+// Copyright (c) 2007-2009 Bo Morgan.
+// All rights reserved.
+// 
+// Author: Bo Morgan
+// 
+// Permission to use, copy, modify and distribute this software and its
+// documentation is hereby granted, provided that both the copyright
+// notice and this permission notice appear in all copies of the
+// software, derivative works or modified versions, and any portions
+// thereof, and that both notices appear in supporting documentation.
+// 
+// BO MORGAN ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS" CONDITION.
+// BO MORGAN DISCLAIMS ANY LIABILITY OF ANY KIND FOR ANY DAMAGES
+// WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+// 
+// Bo Morgan requests users of this software to return to bo@mit.edu any
+// improvements or extensions that they make and grant Bo Morgan the
+// rights to redistribute these changes.
+// 
+
+#ifndef F2__PROCESSOR_THREAD__H
+#define F2__PROCESSOR_THREAD__H
+
+typedef void (*funk2_processor_thread_function_pointer_t)(void*);
+
+typedef struct funk2_processor_thread_s {
+  funk2_processor_thread_function_pointer_t start_function;
+  pthread_t                                 pthread;
+} funk2_processor_thread_t;
+
+typedef struct funk2_processor_thread_list_s {
+  funk2_processor_thread_t              processor_thread;
+  struct funk2_processor_thread_list_s* next;
+} funk2_processor_thread_list_t;
+
+void funk2_processor_thread__init(funk2_processor_thread_t* this, funk2_processor_thread_function_pointer_t start_function);
+void funk2_processor_thread__destroy(funk2_processor_thread_t* this);
+
+
+#endif // F2__PROCESSOR_THREAD__H
+

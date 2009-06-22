c0ce4d75f1a54d9364b689ba040fa599da514e27
bunuelo <bunuelo@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-06-22 18:10:52 +0000

    git-svn-id: https://funk2.googlecode.com/svn/funk2@1394 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- c/f2_processor_mutex.c
+++ c/f2_processor_mutex.c
@@ -21,6 +21,10 @@
 
 #include "funk2.h"
 
+void funk2_processor_mutex__error() {
+  printf("\nfunk2_processor_mutex__error here.\n");
+}
+
 void funk2_processor_mutex__init(funk2_processor_mutex_t* this) {
   this->is_initialized   = boolean__true;
   this->is_locked        = boolean__false;
@@ -32,6 +36,7 @@ void funk2_processor_mutex__init(funk2_processor_mutex_t* this) {
 void funk2_processor_mutex__destroy(funk2_processor_mutex_t* this) {
   if (! this->is_initialized) {
     printf("\nfunk2_processor_mutex__destroy error: attempted to destroy uninitialized mutex.\n"); fflush(stdout);
+    funk2_processor_mutex__error();
   }
   this->lock_source_file = "destroyed";
   this->lock_line_num    = 0;
@@ -41,6 +46,7 @@ void funk2_processor_mutex__destroy(funk2_processor_mutex_t* this) {
 funk2_processor_mutex_trylock_result_t funk2_processor_mutex__raw_trylock(funk2_processor_mutex_t* this, const char* lock_source_file, const int lock_line_num) {
   if (! this->is_initialized) {
     printf("\nfunk2_processor_mutex__destroy error: attempted to destroy uninitialized mutex.\n"); fflush(stdout);
+    funk2_processor_mutex__error();
   }
   if (pthread_mutex_trylock(&(this->pthread_mutex)) == 0) {
     this->is_locked        = boolean__true;
@@ -55,6 +61,7 @@ funk2_processor_mutex_trylock_result_t funk2_processor_mutex__raw_trylock(funk2_
 void funk2_processor_mutex__raw_lock(funk2_processor_mutex_t* this, const char* lock_source_file, const int lock_line_num) {
   if (! this->is_initialized) {
     printf("\nfunk2_processor_mutex__destroy error: attempted to destroy uninitialized mutex.\n"); fflush(stdout);
+    funk2_processor_mutex__error();
   }
   while (funk2_processor_mutex__raw_trylock(this, lock_source_file, lock_line_num) != funk2_processor_mutex_trylock_result__success) {
     sched_yield();
@@ -65,6 +72,7 @@ void funk2_processor_mutex__raw_lock(funk2_processor_mutex_t* this, const char* 
 void funk2_processor_mutex__raw_unlock(funk2_processor_mutex_t* this, const char* unlock_source_file, const int unlock_line_num) {
   if (! this->is_initialized) {
     printf("\nfunk2_processor_mutex__destroy error: attempted to destroy uninitialized mutex.\n"); fflush(stdout);
+    funk2_processor_mutex__error();
   }
   this->is_locked = boolean__false;
   pthread_mutex_unlock(&(this->pthread_mutex));

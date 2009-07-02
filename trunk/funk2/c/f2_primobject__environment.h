40897b3c012fb7a630ec5b22e220662d97ce1796
bunuelo <bunuelo@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-07-02 21:14:02 +0000

    debugging (removed all type checking by added "p" as in integerp, replaced with integer__is_type)
    git-svn-id: https://funk2.googlecode.com/svn/funk2@1773 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- trunk/funk2/c/f2_primobject__environment.h
+++ trunk/funk2/c/f2_primobject__environment.h
@@ -34,6 +34,11 @@ extern f2ptr __type_variable_not_defined__symbol;
 
 extern f2ptr __environment__symbol;
 extern f2ptr __current_environment__symbol;
+
+#ifdef DEBUG
+extern f2ptr __environment__last_23_larva_symbol;
+#endif // DEBUG
+
 boolean_t raw__environment__is_type(f2ptr cause, f2ptr this);
 f2ptr f2__environment__is_type(f2ptr cause, f2ptr this);
 f2ptr f2environment__new(f2ptr cause, f2ptr frame, f2ptr parent_env, f2ptr desc);

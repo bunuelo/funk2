9e49d8d5a516b36cacd662eb003fd24942a59e9b
bunuelo <bunuelo@98e8950a-0dbf-11de-ba4d-252e5d5a8945>
2009-07-02 15:34:54 +0000

    debugging (removed all type checking by added "p" as in integerp, replaced with integer__is_type)
    git-svn-id: https://funk2.googlecode.com/svn/funk2@1670 98e8950a-0dbf-11de-ba4d-252e5d5a8945

--- trunk/funk2/c/f2_primfunks.h
+++ trunk/funk2/c/f2_primfunks.h
@@ -869,8 +869,8 @@ f2ptr f2__string__new_raw_c_string(f2ptr cause, f2ptr x);
 boolean_t  raw__cause_is_traced(f2ptr cause);
 f2ptr raw__array__new(f2ptr cause, u64 length);
 f2ptr raw__array__new_copy(f2ptr cause, u64 length, f2ptr init);
-boolean_t raw__array__is_type(f2ptr cause, f2ptr x) {return (raw__simple_array__is_type(cause, x) || raw__traced_array__is_type(cause, x));}
-f2ptr f2__array__is_type(f2ptr cause, f2ptr x) {return f2bool__new(raw__array__is_type(cause, x));}
+boolean_t raw__array__is_type(f2ptr cause, f2ptr x);
+f2ptr f2__array__is_type(f2ptr cause, f2ptr x);
 u64   raw__array__length(f2ptr cause, f2ptr x);
 f2ptr f2__array__length(f2ptr cause, f2ptr x);
 f2ptr raw__array__elt__trace_depth(f2ptr cause, f2ptr this, u64 index, int trace_depth);

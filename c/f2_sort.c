735e7216438d80cdcbdc96016c0e0e3222b63d01
Bo Morgan <neptune@psuche.(none)>
2009-07-05 15:59:51 -0400

    wrote sort_integer_list and added primfunks-sort.fu2

--- c/f2_sort.c
+++ c/f2_sort.c
@@ -21,7 +21,42 @@
 
 #include "funk2.h"
 
+static int f2__sort_integer_list__compare(const void* elt_x_void, const void* elt_y_void) {
+  f2ptr elt_x = *((f2ptr*)elt_x_void);
+  f2ptr elt_y = *((f2ptr*)elt_y_void);
+  if (! raw__integer__is_type(cause, elt_x)) {
+    return -1;
+  }
+  if (! raw__integer__is_type(cause, elt_y)) {
+    return 1;
+  }
+  return f2integer__i(elt_x, cause) - f2integer__i(elt_y, cause);
+}
 
+f2ptr f2__sort_integer_list(f2ptr cause, f2ptr integers) {
+  size_t sort_array__length = (size_t)raw__length(integers);
+  f2ptr* sort_array         = malloc(sizeof(f2ptr));
+  {
+    size_t index = 0;
+    f2ptr iter = integers;
+    while (iter) {
+      f2ptr car = f2cons__car(iter, cause);
+      sort_array[index] = car;
+      index ++;
+      iter = f2cons__cdr(iter, cause);
+    }
+  }
+  qsort(sort_array, sort_array__length, sizeof(u64), &f2__sort_integer_list__compare);
+  f2ptr new_list = nil; 
+  {
+    u64 i;
+    for (i = sort_array__length - 1; i >=  0; i --) {
+      new_list = f2cons__new(cause, sort_array[i], new_list);
+    }
+  }
+  return new_list;
+}
+def_pcfunk1(sort_integer_list, integers, return f2__sort_integer_list(this_cause, integers));
 
 // **
 
@@ -33,6 +68,7 @@ void f2__sort__initialize() {
   
   f2__sort__reinitialize_globalvars();
   
+  f2__primcfunk__init__1(sort_integer_list, this, "sort a list of integers into a new list using the quicksort algorithm.");
   
   resume_gc();
   try_gc();

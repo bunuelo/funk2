cc140c30d0192d134d0ebdc9be9f3e5f22b8d10c
Bo Morgan <neptune@psuche.(none)>
2009-07-21 16:25:36 -0400

    added object-type cfunk to f2_object.c

--- c/f2_object.c
+++ c/f2_object.c
@@ -21,6 +21,63 @@
 
 #include "funk2.h"
 
+f2ptr f2__object__type(f2ptr cause, f2ptr this) {
+  if (! this) {
+    return nil;
+  }
+  ptype_t ptype = f2ptype__raw(this, cause);
+  switch (ptype) {
+  case ptype_free_memory:
+  case ptype_newly_allocated:
+    error(nil, "shouldn't ever see this object ptype.");
+    return nil;
+  case ptype_integer:
+    return f2symbol__new(cause, strlen("integer"), (u8*)"integer");
+  case ptype_double:
+    return f2symbol__new(cause, strlen("double"), (u8*)"double");
+  case ptype_float:
+    return f2symbol__new(cause, strlen("float"), (u8*)"float");
+  case ptype_pointer:
+    return f2symbol__new(cause, strlen("pointer"), (u8*)"pointer");
+  case ptype_gfunkptr:
+    return f2symbol__new(cause, strlen("gfunkptr"), (u8*)"gfunkptr");
+  case ptype_mutex:
+    return f2symbol__new(cause, strlen("mutex"), (u8*)"mutex");
+  case ptype_char:
+    return f2symbol__new(cause, strlen("char"), (u8*)"char");
+  case ptype_string:
+    return f2symbol__new(cause, strlen("string"), (u8*)"string");
+  case ptype_symbol:
+    return f2symbol__new(cause, strlen("symbol"), (u8*)"symbol");
+  case ptype_chunk:
+    return f2symbol__new(cause, strlen("chunk"), (u8*)"chunk");
+  case ptype_simple_array:
+  case ptype_traced_array:
+    if (raw__primobject__is_type(cause, this)) {
+      f2ptr primobject_type_name = f2primobject__type(this, cause);
+      //printf("\nprimobject_type_name: "); f2__print(cause, primobject_type_name); fflush(stdout);
+      if (primobject_type_name == __frame__symbol) {
+	f2ptr test_get_type = frame__lookup_var_value(cause, this, f2symbol__new(cause, strlen("type"), (u8*)"type"), nil);
+	if (test_get_type) {
+	  primobject_type_name = test_get_type;
+	}
+      }
+      return primobject_type_name;
+    } else {
+      if (ptype == ptype_simple_array) {
+	return f2symbol__new(cause, strlen("simple_array"), (u8*)"simple_array");
+      } else if (ptype == ptype_traced_array) {
+	return f2symbol__new(cause, strlen("traced_array"), (u8*)"traced_array");
+      }
+    }
+    return f2larva__new(cause, 1);
+  case ptype_larva:
+    return f2symbol__new(cause, strlen("larva"), (u8*)"larva");
+  }
+  return f2larva__new(cause, 1);
+}
+def_pcfunk1(object__type, this, return f2__object__type(this_cause, this));
+
 f2ptr f2__object__slot__get_funk(f2ptr cause, f2ptr this, f2ptr slot) {
   if (! this) {
     return nil;
@@ -448,6 +505,7 @@ void f2__object__initialize() {
   
   f2__string__reinitialize_globalvars();
   
+  f2__primcfunk__init__1(object__type,               this,       "returns the symbolic type name of the object.");
   f2__primcfunk__init__2(object__slot__get_funk,     this, slot, "returns the slot get funk for the object, i.e. an accessor (e.g. value).");
   f2__primcfunk__init__2(object__slot__set_funk,     this, slot, "returns the slot set funk for the object, i.e. a mutator (e.g. value-set).");
   f2__primcfunk__init__2(object__slot__execute_funk, this, slot, "returns the slot execute funk for the object, i.e. a general executor (e.g. print-value).");

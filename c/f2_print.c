9eb03360105354a30d6bbc52c35c1f24a928f2c8
Bo Morgan <neptune@psuche.(none)>
2009-07-21 13:00:35 -0400

    fixing transframe to print microseconds

--- c/f2_print.c
+++ c/f2_print.c
@@ -850,7 +850,10 @@ f2ptr f2__write_pretty(f2ptr cause, f2ptr stream, f2ptr exp, int recursion_depth
 	  } else if (f2primobject__is_transframe(exp, cause)) {
 	    int subexp_size[2];
 	    if (try_wide) {f2__write__space(cause, stream, use_html); width ++;} else {f2__write__line_break(cause, stream, use_html); width = 0; height ++; int i; for (i = 0; i < indent_space_num + width; i++) {f2__write__space(cause, stream, use_html);}}  
-	    {f2__write_pretty__slot_key_and_value("symbol_old_news", 6, cause, stream, f2transframe__symbol_old_news(exp, cause), f2transframe__symbol_old_news__tracing_on(exp, cause), f2transframe__symbol_old_news__trace(exp, cause), f2transframe__symbol_old_news__imagination_frame(exp, cause),
+	    {f2__write_pretty__slot_key_and_value("microseconds_since_1970", 23, cause, stream, f2transframe__microseconds_since_1970(exp, cause), f2transframe__microseconds_since_1970__tracing_on(exp, cause), f2transframe__microseconds_since_1970__trace(exp, cause), f2transframe__microseconds_since_1970__imagination_frame(exp, cause),
+						  ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num, available_width - width, subexp_size, try_wide, wide_success, show_slot_causes, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];}
+	    if (try_wide) {f2__write__space(cause, stream, use_html); width ++;} else {f2__write__line_break(cause, stream, use_html); width = 0; height ++; int i; for (i = 0; i < indent_space_num + width; i++) {f2__write__space(cause, stream, use_html);}}  
+	    {f2__write_pretty__slot_key_and_value("symbol_old_news",         23, cause, stream, f2transframe__symbol_old_news(exp, cause), f2transframe__symbol_old_news__tracing_on(exp, cause), f2transframe__symbol_old_news__trace(exp, cause), f2transframe__symbol_old_news__imagination_frame(exp, cause),
 						  ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num, available_width - width, subexp_size, try_wide, wide_success, show_slot_causes, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];}
 	  } else if (f2primobject__is_bug(exp, cause)) {
 	    int subexp_size[2];

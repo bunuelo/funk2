f5d0d800e5a34a2acba2817e41dcd5481a72ab27
Bo Morgan <neptune@psuche.(none)>
2009-07-21 21:08:34 -0400

    adding thread sleep_until_time and sleep_for_nanoseconds to thread primobject.

--- c/f2_primobjects.c
+++ c/f2_primobjects.c
@@ -1159,7 +1159,7 @@ def_pcfunk1(thread__last_executed_time, x, return f2__thread__last_executed_time
 f2ptr f2__thread__last_executed_time__set(f2ptr cause, f2ptr this, f2ptr value) {return f2thread__last_executed_time__set(this, cause, value);}
 def_pcfunk2(thread__last_executed_time__set, x, y, return f2__thread__last_executed_time__set(this_cause, x, y));
 
-f2ptr f2__thread__sleep_until_time__get(f2ptr cause, f2ptr this) {return f2thread__sleep_until_time__get(this, cause);}
+f2ptr f2__thread__sleep_until_time__get(f2ptr cause, f2ptr this) {return f2thread__sleep_until_time(this, cause);}
 def_pcfunk1(thread__sleep_until_time__get, x, return f2__thread__sleep_until_time__get(this_cause, x));
 
 f2ptr f2__thread__sleep_until_time__set(f2ptr cause, f2ptr this, f2ptr value) {return f2thread__sleep_until_time__set(this, cause, value);}

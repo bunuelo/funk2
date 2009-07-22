009388f8feba5238243ff7f62ad2303e72e7714b
Bo Morgan <neptune@psuche.(none)>
2009-07-22 08:47:10 -0400

    debugging why threads always allocating from pool 0.

--- c/f2_scheduler.c
+++ c/f2_scheduler.c
@@ -37,7 +37,6 @@ f2ptr f2__global_scheduler__this_processor(f2ptr cause) {
 }
 
 void f2__processor__add_active_thread(f2ptr cause, f2ptr this, f2ptr thread) {
-  printf("\n[adding active thread to processor " s64__fstr "]", f2integer__i(f2processor__pool_index(this, cause), cause)); fflush(stdout);
   f2ptr active_threads_mutex = f2processor__active_threads_mutex(this, cause);
   f2mutex__lock(active_threads_mutex, cause);
   f2ptr active_threads       = f2processor__active_threads(this, cause);
@@ -87,10 +86,12 @@ f2ptr f2__scheduler__processor_with_fewest_threads(f2ptr cause, f2ptr scheduler)
   u64   max_length    = 0;
   f2ptr max_processor = nil;
   u64 i;
+  printf("\nprocessor thread list:"); fflush(stdout);
   for (i = 0; i < processors__length; i ++) {
     f2ptr processor = raw__array__elt(cause, processors, i);
     f2ptr active_threads = f2processor__active_threads(processor, cause);
     u64 threads__length = raw__length(cause, active_threads);
+    printf("\n  processor pool_index=" s64__fstr " active_thread_num=" u64__fstr ".", f2integer__i(f2processor__pool_index(processor, cause), cause), threads__length); fflush(stdout);
     if (threads__length + 1 > max_length) {
       max_length = threads__length + 1; // avoids -1 assignment for u64
       max_processor = processor;
@@ -106,6 +107,7 @@ void f2__global_scheduler__add_thread_serial(f2ptr cause, f2ptr thread) {
 
 void f2__scheduler__add_thread_to_least_used_processor(f2ptr cause, f2ptr this, f2ptr thread) {
   f2ptr processor = f2__scheduler__processor_with_fewest_threads(cause, this);
+  printf("\n[adding thread to least used processor " s64__fstr "]", f2integer__i(f2processor__pool_index(processor, cause), cause)); fflush(stdout);
   f2__processor__add_active_thread(cause, processor, thread);
 }
 

0943cfaf7a4ce5d6e4afdf9f20c49afaa48d0f74
Bo Morgan <neptune@psuche.(none)>
2009-07-24 11:37:04 -0400

    adding should_enlarge_memory_pool to polite garbage collector

--- c/f2_memory.c
+++ c/f2_memory.c
@@ -1008,7 +1008,7 @@ ptr find_or_create_free_splittable_memblock_and_unfree(int pool_index, f2size_t 
   __ptypes_please_wait_for_gc_to_take_place                                         = boolean__true;
   if (pthread_self() == __funk2.memory.memory_handling_thread) {
     funk2_memory_t* memory = (&__funk2.memory);
-    pool__change_total_memory_available(index, memory->pool[pool_index].total_global_memory + (memory->pool[pool_index].total_global_memory >> 3) + memory->pool[pool_index].should_enlarge_memory_now__need_at_least_byte_num);
+    pool__change_total_memory_available(pool_index, memory->pool[pool_index].total_global_memory + (memory->pool[pool_index].total_global_memory >> 3) + memory->pool[pool_index].should_enlarge_memory_now__need_at_least_byte_num);
     __funk2.memory.pool[pool_index].should_enlarge_memory_now__need_at_least_byte_num = 0;
     __funk2.memory.pool[pool_index].should_enlarge_memory_now                         = boolean__false;
     __ptypes_please_wait_for_gc_to_take_place                                         = boolean__false;

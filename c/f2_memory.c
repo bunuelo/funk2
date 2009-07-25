eb917f8a20866a8d13684cc85b36a2733f8b954b
Bo Morgan <neptune@psuche.(none)>
2009-07-25 13:04:58 -0400

    reorganizing and simplifying f2_memory

--- c/f2_memory.c
+++ c/f2_memory.c
@@ -791,7 +791,7 @@ u8 garbage_collect(int pool_index, f2size_t goal_free_block_byte_num) {
   int try_generation_num = 1;
   while (try_generation_num <= maximum_generation_num && (! did_something)) {
     status("garbage_collect try_generation_num=%d, maximum_generation_num=%d", try_generation_num, maximum_generation_num);
-    memblock_t* max_size_block = (memblock_t*)rbt_tree__maximum(&(__funk2.memory.pool[pool_index].free_memory_tree));
+    //memblock_t* max_size_block = (memblock_t*)rbt_tree__maximum(&(__funk2.memory.pool[pool_index].free_memory_tree));
     //if (max_size_block && memblock__byte_num(max_size_block) >= goal_free_block_byte_num) {
     //  break;
     //}

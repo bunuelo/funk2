73c97bbc70c1e3300e6ced52e824a6a14e9fd322
Bo Morgan <neptune@psuche.(none)>
2009-07-26 12:02:57 -0400

    reorganizing and simplifying f2_memory

--- c/f2_memory.c
+++ c/f2_memory.c
@@ -517,7 +517,7 @@ void funk2_gc_touch_circle_buffer__touch_all_referenced_from_block(funk2_gc_touc
 	{
 	  char str[1024];
 	  sprintf(str, "unknown type (%d) of block in garbage collector.",
-		  (int)(block->ptype), to_ptr(block)));
+		  (int)(block->ptype), to_ptr(block));
 	  error(nil, str);
 	}
       }

945a6a1dbd1140b98b61ca9c1d4a3685c99529bb
Bo Morgan <neptune@psuche.(none)>
2009-07-28 09:32:51 -0400

    debugging reentance count not returning to zero (adding f2_protected_alloc_array)

---
+++ c/f2_protected_alloc_array.c
@@ -0,0 +1,64 @@
+// 
+// Copyright (c) 2007-2008 Bo Morgan.
+// All rights reserved.
+// 
+// Author: Bo Morgan
+// 
+// Permission to use, copy, modify and distribute this software and its
+// documentation is hereby granted, provided that both the copyright
+// notice and this permission notice appear in all copies of the
+// software, derivative works or modified versions, and any portions
+// thereof, and that both notices appear in supporting documentation.
+// 
+// BO MORGAN ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS" CONDITION.
+// BO MORGAN DISCLAIMS ANY LIABILITY OF ANY KIND FOR ANY DAMAGES
+// WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+// 
+// Bo Morgan requests users of this software to return to bo@mit.edu any
+// improvements or extensions that they make and grant Bo Morgan the
+// rights to redistribute these changes.
+// 
+
+#include "funk2.h"
+
+void funk2_protected_alloc_array__init(funk2_protected_alloc_array_t* this) {
+  this->used_num = 0;
+  this->length   = 1024;
+  this->protected_alloc_array = (f2ptr*)f2__malloc(sizeof(f2ptr) * this->length);
+  u64 i;
+  for (i = 0; i < this->length; i ++) {
+    this->data[i] = nil;
+  }
+  this->reentrance_count = 0;
+}
+
+void funk2_protected_alloc_array__destroy(funk2_protected_alloc_array_t* this) {
+  free(this->data);
+}
+
+void funk2_protected_alloc_array__add_protected_alloc_f2ptr(funk2_protected_alloc_array_t* this, f2ptr exp) {
+  this->data[this->used_num] = exp;
+  this->used_num ++;
+  if (this->used_num >= this->length) {
+    u64 old_length = this->length;
+    this->length <<= 1;
+    status("funk2_protected_alloc_array__add_protected_alloc_f2ptr: doubling size of protected_alloc_array from " u64__fstr " to " u64__fstr " f2ptrs.", old_length, this->length);
+    this->data = from_ptr(f2__new_alloc(to_ptr(this->data), sizeof(f2ptr) * old_length, sizeof(f2ptr) * this->length));
+  }
+}
+
+void funk2_protected_alloc_array__signal_enter_protected_region(funk2_protected_alloc_array_t* this) {
+  this->reentrance_count ++;
+}
+
+void funk2_protected_alloc_array__signal_exit_protected_region(funk2_protected_alloc_array_t* this) {
+  if (this->reentrance_count == 0) {
+    error(nil, "funk2_protected_alloc_array__signal_exit_protected_region error: bytecode reentrance underflow.");
+  }
+  this->reentrance_count --;
+  if (this->reentrance_count == 0) {
+    // protected counter is back to zero so reset used_num to zero.
+    this->used_num = 0;
+  }
+}
+
---
+++ c/f2_protected_alloc_array.h
@@ -0,0 +1,39 @@
+// 
+// Copyright (c) 2007-2008 Bo Morgan.
+// All rights reserved.
+// 
+// Author: Bo Morgan
+// 
+// Permission to use, copy, modify and distribute this software and its
+// documentation is hereby granted, provided that both the copyright
+// notice and this permission notice appear in all copies of the
+// software, derivative works or modified versions, and any portions
+// thereof, and that both notices appear in supporting documentation.
+// 
+// BO MORGAN ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS" CONDITION.
+// BO MORGAN DISCLAIMS ANY LIABILITY OF ANY KIND FOR ANY DAMAGES
+// WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+// 
+// Bo Morgan requests users of this software to return to bo@mit.edu any
+// improvements or extensions that they make and grant Bo Morgan the
+// rights to redistribute these changes.
+// 
+
+#ifndef F2__PROTECTED_ALLOC_ARRAY__H
+#define F2__PROTECTED_ALLOC_ARRAY__H
+
+typedef struct funk2_protected_alloc_array_s {
+  u64    used_num;
+  u64    length;
+  f2ptr* data;
+  u64    reentrance_count;
+} funk2_protected_alloc_array_t;
+
+void funk2_protected_alloc_array__init(funk2_protected_alloc_array_t* this);
+void funk2_protected_alloc_array__destroy(funk2_protected_alloc_array_t* this);
+void funk2_protected_alloc_array__add_protected_alloc_f2ptr(funk2_protected_alloc_array_t* this, f2ptr exp);
+void funk2_protected_alloc_array__signal_enter_protected_region(funk2_protected_alloc_array_t* this);
+void funk2_protected_alloc_array__signal_exit_protected_region(funk2_protected_alloc_array_t* this);
+
+#endif // F2__PROTECTED_ALLOC_ARRAY__H
+

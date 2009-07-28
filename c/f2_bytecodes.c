90d1c3ea6b05a44716eede4a60ebb4e4e77ea028
Bo Morgan <neptune@psuche.(none)>
2009-07-28 15:54:27 -0400

    debugging lockup loop allocating infinite memory within one bytecode

--- c/f2_bytecodes.c
+++ c/f2_bytecodes.c
@@ -1183,11 +1183,11 @@ int f2__thread__bytecode__type_var__mutate(f2ptr thread, f2ptr bytecode, f2ptr t
   
   f2ptr env        = f2thread__env(thread, cause);
   f2ptr value      = f2thread__value(thread, cause);
-  f2ptr new__value = environment__type_var_value__set(cause, env, type, var, value);
-  if (raw__symbol__eq(cause, new__value, __funk2.primobject__frame.type_variable_not_defined__symbol)) {
+  f2ptr new_value = environment__type_var_value__set(cause, env, type, var, value);
+  if (raw__symbol__eq(cause, new_value, __funk2.primobject__frame.type_variable_not_defined__symbol)) {
     new_value = f2larva__new(cause, 23);
   }
-  f2thread__value__set(thread, cause, new__value);
+  f2thread__value__set(thread, cause, new_value);
   return 0;
 }
 

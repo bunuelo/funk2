7200dc8a1a25a7f4fee8b58c4c6907b2951f5028
Bo Morgan <neptune@psuche.(none)>
2009-07-29 14:54:08 -0400

    praise the lord.  new f2_gc_touch_circle_buffer seems to eliminate memory lockup problems...

--- c/f2_scheduler.c
+++ c/f2_scheduler.c
@@ -230,6 +230,9 @@ f2ptr f2processor__execute_next_bytecodes(f2ptr processor, f2ptr cause) {
 	}
 	if (! thread_needs_sleep) {
 	  int pool_index = f2integer__i(f2processor__pool_index(processor, cause), cause);
+	  if (funk2_memorypool__in_protected_region(&(this->pool[pool_index]))) {
+	    error(nil, "f2processor__execute_next_bytecodes error: should not be in protected memory region at this point.");
+	  }
 	  release__assert(pool_index == this_processor_thread__pool_index(), thread, "f2processor__execute_next_bytecodes: pool_index != this_processor_thread__pool_index().");
 	  f2ptr popped_thread = __funk2.operating_system.processor_thread__current_thread[pool_index];
 	  __funk2.operating_system.processor_thread__current_thread[pool_index] = thread;

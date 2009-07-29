a522a2dda9aae7c844429e936af45d69f7be603b
Bo Morgan <neptune@psuche.(none)>
2009-07-29 13:08:16 -0400

    reorganizing and simplifying f2_memory

--- c/f2_memorypool.h
+++ c/f2_memorypool.h
@@ -79,7 +79,6 @@ u8                funk2_memorypool__defragment_free_memory_blocks_in_place(funk2
 u8                funk2_memorypool__free_all_gc_untouched_blocks_from_generation(funk2_memorypool_t* this, int generation_num);
 void              funk2_memorypool__increment_generation(funk2_memorypool_t* this);
 funk2_memblock_t* funk2_memorypool__find_splittable_free_block_and_unfree(funk2_memorypool_t* this, f2size_t byte_num);
-void              funk2_memorypool__touch_all_referenced_from_block(funk2_memorypool_t* this, ptr block);
 void              funk2_memorypool__touch_all_referenced_from_f2ptr(funk2_memorypool_t* this, f2ptr exp);
 void              funk2_memorypool__touch_all_referenced_from_pool_generation(funk2_memorypool_t* this, int touch_generation_num);
 void              funk2_memorypool__touch_all_protected_alloc_arrays(funk2_memorypool_t* this);

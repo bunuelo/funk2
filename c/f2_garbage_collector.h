4aa839176d957cbdb68a8b3817b56b3484525ace
Bo Morgan <neptune@psuche.(none)>
2009-08-01 18:55:03 -0400

    adding tricolor garbage collector in f2_garbage_collector

--- c/f2_garbage_collector.h
+++ c/f2_garbage_collector.h
@@ -45,6 +45,7 @@ void  funk2_garbage_collector__destroy(funk2_garbage_collector_t* this);
 void  funk2_garbage_collector__know_of_used_exp_mutation(funk2_garbage_collector_t* this, f2ptr exp);
 void  funk2_garbage_collector__know_of_no_more_references(funk2_garbage_collector_t* this, f2ptr exp);
 void  funk2_garbage_collector__touch_all_roots(funk2_garbage_collector_t* this);
+void  funk2_garbage_collector__touch_f2ptr(funk2_garbage_collector_t* this, f2ptr exp);
 void  funk2_garbage_collector__collect_garbage(funk2_garbage_collector_t* this);
 void  funk2_garbage_collector__signal_enter_protected_region(funk2_garbage_collector_t* this);
 void  funk2_garbage_collector__signal_exit_protected_region(funk2_garbage_collector_t* this);

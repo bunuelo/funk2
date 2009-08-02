ed3540b129ec0ebd813143eb8790a41569874c54
Bo Morgan <neptune@psuche.(none)>
2009-08-01 20:40:57 -0400

    adding tricolor garbage collector in f2_garbage_collector

--- c/f2_garbage_collector_pool.h
+++ c/f2_garbage_collector_pool.h
@@ -142,8 +142,8 @@ void      funk2_garbage_collector_pool__know_of_used_exp_other_no_more_reference
 void      funk2_garbage_collector_pool__know_of_used_exp_self_protected_f2ptr( funk2_garbage_collector_pool_t* this, f2ptr exp); // assumes called by self  user thread
 void      funk2_garbage_collector_pool__know_of_used_exp_other_protected_f2ptr(funk2_garbage_collector_pool_t* this, f2ptr exp); // assumes called by other user thread
 void      funk2_garbage_collector_pool__flush_other_knowledge(funk2_garbage_collector_pool_t* this);
-void      funk2_garbage_collector_pool__blacken_grey_nodes__user_process(funk2_garbage_collector_pool_t* this);
-void      funk2_garbage_collector_pool__grey_from_other_nodes__user_process(funk2_garbage_collector_pool_t* this);
+void      funk2_garbage_collector_pool__blacken_grey_nodes(funk2_garbage_collector_pool_t* this);
+void      funk2_garbage_collector_pool__grey_from_other_nodes(funk2_garbage_collector_pool_t* this);
 
 #endif // F2__GARBAGE_COLLECTOR_POOL__H
 

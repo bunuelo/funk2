ab7146d59aae985ffbe2762f3cc3c7b2d175e460
Bo Morgan <neptune@psuche.(none)>
2009-08-01 20:46:32 -0400

    adding tricolor garbage collector in f2_garbage_collector

--- c/f2_user_thread_controller.c
+++ c/f2_user_thread_controller.c
@@ -275,6 +275,8 @@ void funk2_user_thread_controller__user_wait_politely(funk2_user_thread_controll
     else if    (this->touch_all_referenced_from_pool_generation.start) {funk2_user_thread_controller__touch_all_referenced_from_pool_generation__user_process(      &(this->touch_all_referenced_from_pool_generation));}
     else if             (this->touch_all_protected_alloc_arrays.start) {funk2_user_thread_controller__touch_all_protected_alloc_arrays__user_process(                        &(this->touch_all_protected_alloc_arrays));}
     else if (this->free_all_gc_untouched_blocks_from_generation.start) {funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation__user_process(&(this->free_all_gc_untouched_blocks_from_generation));}
+    else if                           (this->blacken_grey_nodes.start) {funk2_user_thread_controller__blacken_grey_nodes__user_process(                                                    &(this->blacken_grey_nodes));}
+    else if                        (this->grey_from_other_nodes.start) {funk2_user_thread_controller__grey_from_other_nodes__user_process(                                              &(this->grey_from_other_nodes));}
     f2__sleep(1);
     sched_yield();
   }

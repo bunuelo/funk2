970dc5ff78e2c08f5dbcfbaa04a8918aadf8f9c7
Bo Morgan <neptune@psuche.(none)>
2009-08-04 21:00:25 -0400

    optimizing free_whiteness in gc.

--- c/f2_garbage_collector.c
+++ c/f2_garbage_collector.c
@@ -161,7 +161,7 @@ void funk2_garbage_collector__signal_exit_protected_region(funk2_garbage_collect
 }
 
 void funk2_garbage_collector__touch_never_delete_list(funk2_garbage_collector_t* this) {
-  status("funk2_garbage_collector: touch_never_delete_list.");
+  status("funk2_garbage_collector: touch_never_delete_list length=" u64__fstr ".", this->never_delete_list.used_num);
   u64 i;
   for (i = 0; i < this->never_delete_list.used_num; i++) {
     funk2_garbage_collector__touch_f2ptr(this, this->never_delete_list.data[i]);

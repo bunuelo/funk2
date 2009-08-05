dd9159fb189b5efc26772aca1e056042d5ae55de
Bo Morgan <neptune@psuche.(none)>
2009-08-05 18:30:41 -0400

    moving global vars in f2_reader to struct

--- c/f2_print.c
+++ c/f2_print.c
@@ -467,7 +467,7 @@ f2ptr f2__write_pretty(f2ptr cause, f2ptr stream, f2ptr exp, int recursion_depth
 		int subexp_size[2];
 		f2__write_pretty(cause, stream, f2cons__car(f2cons__cdr(exp, cause), cause), ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num + width, available_width - width, subexp_size, 1, wide_success, show_slot_causes, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];
 	      } else if (car == __funk2.globalenv.funkvar__symbol) {
-		if (stream) {raw__stream__writef(cause, stream, "%c", __funktion_char);} width ++;
+		if (stream) {raw__stream__writef(cause, stream, "%c", f2char__ch(__funk2.reader.char__funktion, cause));} width ++;
 		int subexp_size[2];
 		f2__write_pretty(cause, stream, f2cons__car(f2cons__cdr(exp, cause), cause), ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num + width, available_width - width, subexp_size, 1, wide_success, show_slot_causes, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];
 	      } else {

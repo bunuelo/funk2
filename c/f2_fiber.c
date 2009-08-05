a5b3af4441f4ba60e5a23af4b444e030ce831f7f
Bo Morgan <neptune@psuche.(none)>
2009-08-05 12:27:56 -0400

    debugging one processor being stuck with protected memory forever.

--- c/f2_fiber.c
+++ c/f2_fiber.c
@@ -91,7 +91,7 @@ f2ptr __fiber__environment_critics__symbol = -1;
 
 boolean_t f2__fiber__execute_next_bytecode(f2ptr cause, f2ptr fiber) {
   boolean_t bytecode_is_yield = boolean__false;
-  funk2_garbage_collector__signal_enter_protected_region(&(__funk2.garbage_collector));
+  pause_gc();
   {  
     debug__assert(raw__fiber__is_type(nil, fiber), nil, "fiber type assertion failed.");
     debug__assert((! cause) || raw__cause__is_type(nil, cause), nil, "fiber type assertion failed.");
@@ -104,7 +104,7 @@ boolean_t f2__fiber__execute_next_bytecode(f2ptr cause, f2ptr fiber) {
     
     bytecode_is_yield = f2__fiber__execute_bytecode(cause, fiber, bytecode);
   }
-  funk2_garbage_collector__signal_exit_protected_region(&(__funk2.garbage_collector));
+  resume_gc();
   return bytecode_is_yield;
 }
 

8fab6621e70bebf6cae230d112a8e8930141269b
Bo Morgan <neptune@psuche.(none)>
2009-08-13 16:53:34 -0400

    organizing f2_reader to convert to fu2 code

--- c/f2_user_thread_controller.c
+++ c/f2_user_thread_controller.c
@@ -21,79 +21,6 @@
 
 #include "funk2.h"
 
-/*
-// funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation
-
-void funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation__init(funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation_t* this) {
-  this->start = boolean__false;
-  funk2_processor_mutex__init(&(this->done_mutex));
-}
-
-void funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation__destroy(funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation_t* this) {
-}
-
-void funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation__signal_execute(funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation_t* this, int generation_num) {
-  this->generation_num = generation_num;
-  this->done_count     = 0;
-  this->everyone_done  = boolean__false;
-  this->start          = boolean__true;
-  while (this->done_count < memory_pool_num) {
-    sched_yield();
-    f2__sleep(1);
-  }
-  this->start         = boolean__false;
-  this->everyone_done = boolean__true;
-}
-
-void funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation__user_process(funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation_t* this) {
-  //int pool_index = this_processor_thread__pool_index();
-  //funk2_memorypool__clear_all_gc_touch_flags_before_generation(&(__funk2.memory.pool[pool_index]), this->generation_num);
-  funk2_processor_mutex__lock(&(this->done_mutex));
-  this->done_count ++;
-  funk2_processor_mutex__unlock(&(this->done_mutex));
-  while (! (this->everyone_done)) {
-    sched_yield();
-    f2__sleep(1);
-  }
-}
-
-
-// funk2_user_thread_controller__touch_all_referenced_from_pool_generation
-
-void funk2_user_thread_controller__touch_all_referenced_from_pool_generation__init(funk2_user_thread_controller__touch_all_referenced_from_pool_generation_t* this) {
-  this->start = boolean__false;
-  funk2_processor_mutex__init(&(this->done_mutex));
-}
-
-void funk2_user_thread_controller__touch_all_referenced_from_pool_generation__destroy(funk2_user_thread_controller__touch_all_referenced_from_pool_generation_t* this) {
-}
-
-void funk2_user_thread_controller__touch_all_referenced_from_pool_generation__signal_execute(funk2_user_thread_controller__touch_all_referenced_from_pool_generation_t* this, int generation_num) {
-  this->generation_num = generation_num;
-  this->done_count     = 0;
-  this->everyone_done  = boolean__false;
-  this->start          = boolean__true;
-  while (this->done_count < memory_pool_num) {
-    sched_yield();
-    f2__sleep(1);
-  }
-  this->start         = boolean__false;
-  this->everyone_done = boolean__true;
-}
-
-void funk2_user_thread_controller__touch_all_referenced_from_pool_generation__user_process(funk2_user_thread_controller__touch_all_referenced_from_pool_generation_t* this) {
-  //int pool_index = this_processor_thread__pool_index();
-  //funk2_memorypool__touch_all_referenced_from_pool_generation(&(__funk2.memory.pool[pool_index]), this->generation_num);
-  funk2_processor_mutex__lock(&(this->done_mutex));
-  this->done_count ++;
-  funk2_processor_mutex__unlock(&(this->done_mutex));
-  while (! (this->everyone_done)) {
-    sched_yield();
-    f2__sleep(1);
-  }
-}
-*/
-
 // funk2_user_thread_controller__touch_all_protected_alloc_arrays
 
 void funk2_user_thread_controller__touch_all_protected_alloc_arrays__init(funk2_user_thread_controller__touch_all_protected_alloc_arrays_t* this) {
@@ -127,45 +54,6 @@ void funk2_user_thread_controller__touch_all_protected_alloc_arrays__user_proces
     f2__sleep(1);
   }
 }
-
-/*
-// funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation
-
-void funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation__init(funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation_t* this) {
-  this->start = boolean__false;
-  funk2_processor_mutex__init(&(this->done_mutex));
-}
-
-void funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation__destroy(funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation_t* this) {
-}
-
-boolean_t funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation__signal_execute(funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation_t* this, int generation_num) {
-  this->generation_num = generation_num;
-  this->done_count     = 0;
-  this->everyone_done  = boolean__false;
-  this->start          = boolean__true;
-  while (this->done_count < memory_pool_num) {
-    sched_yield();
-    f2__sleep(1);
-  }
-  this->start         = boolean__false;
-  this->everyone_done = boolean__true;
-  return this->did_something;
-}
-
-void funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation__user_process(funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation_t* this) {
-  //int pool_index = this_processor_thread__pool_index();
-  boolean_t did_something = boolean__false; //funk2_memorypool__free_all_gc_untouched_blocks_from_generation(&(__funk2.memory.pool[pool_index]), this->generation_num);
-  funk2_processor_mutex__lock(&(this->done_mutex));
-  this->done_count ++;
-  this->did_something |= did_something;
-  funk2_processor_mutex__unlock(&(this->done_mutex));
-  while (! (this->everyone_done)) {
-    sched_yield();
-    f2__sleep(1);
-  }
-}
-*/
 
 // funk2_user_thread_controller__blacken_grey_nodes
 
@@ -283,10 +171,7 @@ void funk2_user_thread_controller__init(funk2_user_thread_controller_t* this) {
   this->waiting_count = 0;
   funk2_processor_mutex__init(&(this->waiting_count_mutex));
 
-  //funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation__init(&(this->clear_all_gc_touch_flags_before_generation));
-  //funk2_user_thread_controller__touch_all_referenced_from_pool_generation__init(&(this->touch_all_referenced_from_pool_generation));
   funk2_user_thread_controller__touch_all_protected_alloc_arrays__init(&(this->touch_all_protected_alloc_arrays));
-  //funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation__init(&(this->free_all_gc_untouched_blocks_from_generation));
   funk2_user_thread_controller__blacken_grey_nodes__init(&(this->blacken_grey_nodes));
   funk2_user_thread_controller__grey_from_other_nodes__init(&(this->grey_from_other_nodes));
   funk2_user_thread_controller__free_whiteness__init(&(this->free_whiteness));
@@ -295,10 +180,7 @@ void funk2_user_thread_controller__init(funk2_user_thread_controller_t* this) {
 void funk2_user_thread_controller__destroy(funk2_user_thread_controller_t* this) {
   funk2_processor_mutex__destroy(&(this->waiting_count_mutex));
   
-  //funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation__destroy(&(this->clear_all_gc_touch_flags_before_generation));
-  //funk2_user_thread_controller__touch_all_referenced_from_pool_generation__destroy(&(this->touch_all_referenced_from_pool_generation));
   funk2_user_thread_controller__touch_all_protected_alloc_arrays__destroy(&(this->touch_all_protected_alloc_arrays));
-  //funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation__destroy(&(this->free_all_gc_untouched_blocks_from_generation));
   funk2_user_thread_controller__blacken_grey_nodes__destroy(&(this->blacken_grey_nodes));
   funk2_user_thread_controller__grey_from_other_nodes__destroy(&(this->grey_from_other_nodes));
   funk2_user_thread_controller__free_whiteness__destroy(&(this->free_whiteness));
@@ -316,14 +198,10 @@ void funk2_user_thread_controller__user_wait_politely(funk2_user_thread_controll
   this->waiting_count ++;
   funk2_processor_mutex__unlock(&(this->waiting_count_mutex));
   while (this->please_wait || funk2_processor_mutex__trylock(&(this->waiting_count_mutex))) {
-    //if        (this->clear_all_gc_touch_flags_before_generation.start) {funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation__user_process(    &(this->clear_all_gc_touch_flags_before_generation));}
-    //else if    (this->touch_all_referenced_from_pool_generation.start) {funk2_user_thread_controller__touch_all_referenced_from_pool_generation__user_process(      &(this->touch_all_referenced_from_pool_generation));}
-    //else 
-    if                  (this->touch_all_protected_alloc_arrays.start) {funk2_user_thread_controller__touch_all_protected_alloc_arrays__user_process(                        &(this->touch_all_protected_alloc_arrays));}
-    //else if (this->free_all_gc_untouched_blocks_from_generation.start) {funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation__user_process(&(this->free_all_gc_untouched_blocks_from_generation));}
-    else if                           (this->blacken_grey_nodes.start) {funk2_user_thread_controller__blacken_grey_nodes__user_process(                                                    &(this->blacken_grey_nodes));}
-    else if                        (this->grey_from_other_nodes.start) {funk2_user_thread_controller__grey_from_other_nodes__user_process(                                              &(this->grey_from_other_nodes));}
-    else if                               (this->free_whiteness.start) {funk2_user_thread_controller__free_whiteness__user_process(                                                            &(this->free_whiteness));}
+    if                  (this->touch_all_protected_alloc_arrays.start) {funk2_user_thread_controller__touch_all_protected_alloc_arrays__user_process(&(this->touch_all_protected_alloc_arrays));}
+    else if                           (this->blacken_grey_nodes.start) {funk2_user_thread_controller__blacken_grey_nodes__user_process(              &(this->blacken_grey_nodes));}
+    else if                        (this->grey_from_other_nodes.start) {funk2_user_thread_controller__grey_from_other_nodes__user_process(           &(this->grey_from_other_nodes));}
+    else if                               (this->free_whiteness.start) {funk2_user_thread_controller__free_whiteness__user_process(                  &(this->free_whiteness));}
     f2__sleep(1);
     sched_yield();
   }
@@ -337,21 +215,9 @@ void funk2_user_thread_controller__user_check_wait_politely(funk2_user_thread_co
   }
 }
 
-//void funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation(funk2_user_thread_controller_t* this, int generation_num) {
-//  funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation__signal_execute(&(this->clear_all_gc_touch_flags_before_generation), generation_num);
-//}
-
-//void funk2_user_thread_controller__touch_all_referenced_from_pool_generation(funk2_user_thread_controller_t* this, int generation_num) {
-//  funk2_user_thread_controller__touch_all_referenced_from_pool_generation__signal_execute(&(this->touch_all_referenced_from_pool_generation), generation_num);
-//}
-
 void funk2_user_thread_controller__touch_all_protected_alloc_arrays(funk2_user_thread_controller_t* this) {
   funk2_user_thread_controller__touch_all_protected_alloc_arrays__signal_execute(&(this->touch_all_protected_alloc_arrays));
 }
-
-//boolean_t funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation(funk2_user_thread_controller_t* this, int generation_num) {
-//  return funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation__signal_execute(&(this->free_all_gc_untouched_blocks_from_generation), generation_num);
-//}
 
 void funk2_user_thread_controller__blacken_grey_nodes(funk2_user_thread_controller_t* this) {
   funk2_user_thread_controller__blacken_grey_nodes__signal_execute(&(this->blacken_grey_nodes));
--- c/f2_user_thread_controller.h
+++ c/f2_user_thread_controller.h
@@ -22,38 +22,6 @@
 #ifndef F2__USER_THREAD_CONTROLLER__H
 #define F2__USER_THREAD_CONTROLLER__H
 
-/*
-// funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation
-
-typedef struct funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation_s {
-  boolean_t               start;
-  int                     generation_num;
-  funk2_processor_mutex_t done_mutex;
-  s64                     done_count;
-  boolean_t               everyone_done;
-} funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation_t;
-
-void funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation__init(funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation_t* this);
-void funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation__destroy(funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation_t* this);
-void funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation__signal_execute(funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation_t* this, int generation_num);
-void funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation__user_process(funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation_t* this);
-
-// funk2_user_thread_controller__touch_all_referenced_from_pool_generation
-
-typedef struct funk2_user_thread_controller__touch_all_referenced_from_pool_generation_s {
-  boolean_t               start;
-  int                     generation_num;
-  funk2_processor_mutex_t done_mutex;
-  s64                     done_count;
-  boolean_t               everyone_done;
-} funk2_user_thread_controller__touch_all_referenced_from_pool_generation_t;
-
-void funk2_user_thread_controller__touch_all_referenced_from_pool_generation__init(funk2_user_thread_controller__touch_all_referenced_from_pool_generation_t* this);
-void funk2_user_thread_controller__touch_all_referenced_from_pool_generation__destroy(funk2_user_thread_controller__touch_all_referenced_from_pool_generation_t* this);
-void funk2_user_thread_controller__touch_all_referenced_from_pool_generation__signal_execute(funk2_user_thread_controller__touch_all_referenced_from_pool_generation_t* this, int generation_num);
-void funk2_user_thread_controller__touch_all_referenced_from_pool_generation__user_process(funk2_user_thread_controller__touch_all_referenced_from_pool_generation_t* this);
-*/
-
 // funk2_user_thread_controller__touch_all_protected_alloc_arrays
 
 typedef struct funk2_user_thread_controller__touch_all_protected_alloc_arrays_s {
@@ -67,24 +35,6 @@ void funk2_user_thread_controller__touch_all_protected_alloc_arrays__init(funk2_
 void funk2_user_thread_controller__touch_all_protected_alloc_arrays__destroy(funk2_user_thread_controller__touch_all_protected_alloc_arrays_t* this);
 void funk2_user_thread_controller__touch_all_protected_alloc_arrays__signal_execute(funk2_user_thread_controller__touch_all_protected_alloc_arrays_t* this);
 void funk2_user_thread_controller__touch_all_protected_alloc_arrays__user_process(funk2_user_thread_controller__touch_all_protected_alloc_arrays_t* this);
-
-/*
-// funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation
-
-typedef struct funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation_s {
-  boolean_t               start;
-  int                     generation_num;
-  funk2_processor_mutex_t done_mutex;
-  s64                     done_count;
-  boolean_t               everyone_done;
-  boolean_t               did_something;
-} funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation_t;
-
-void      funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation__init(funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation_t* this);
-void      funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation__destroy(funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation_t* this);
-boolean_t funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation__signal_execute(funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation_t* this, int generation_num);
-void      funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation__user_process(funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation_t* this);
-*/
 
 // funk2_user_thread_controller__blacken_grey_nodes
 
@@ -137,10 +87,7 @@ typedef struct funk2_user_thread_controller_s {
   boolean_t                                                                    please_wait;
   s64                                                                          waiting_count;
   funk2_processor_mutex_t                                                      waiting_count_mutex;
-  //funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation_t   clear_all_gc_touch_flags_before_generation;
-  //funk2_user_thread_controller__touch_all_referenced_from_pool_generation_t    touch_all_referenced_from_pool_generation;
   funk2_user_thread_controller__touch_all_protected_alloc_arrays_t             touch_all_protected_alloc_arrays;
-  //funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation_t free_all_gc_untouched_blocks_from_generation;
   funk2_user_thread_controller__blacken_grey_nodes_t                           blacken_grey_nodes;
   funk2_user_thread_controller__grey_from_other_nodes_t                        grey_from_other_nodes;
   funk2_user_thread_controller__free_whiteness_t                               free_whiteness;
@@ -151,10 +98,7 @@ void      funk2_user_thread_controller__destroy(funk2_user_thread_controller_t* 
 void      funk2_user_thread_controller__wait_for_all_user_threads_to_wait(funk2_user_thread_controller_t* this);
 void      funk2_user_thread_controller__user_wait_politely(funk2_user_thread_controller_t* this);
 void      funk2_user_thread_controller__user_check_wait_politely(funk2_user_thread_controller_t* this);
-//void      funk2_user_thread_controller__clear_all_gc_touch_flags_before_generation(funk2_user_thread_controller_t* this, int generation_num);
-//void      funk2_user_thread_controller__touch_all_referenced_from_pool_generation(funk2_user_thread_controller_t* this, int generation_num);
 void      funk2_user_thread_controller__touch_all_protected_alloc_arrays(funk2_user_thread_controller_t* this);
-//boolean_t funk2_user_thread_controller__free_all_gc_untouched_blocks_from_generation(funk2_user_thread_controller_t* this, int generation_num);
 void      funk2_user_thread_controller__blacken_grey_nodes(funk2_user_thread_controller_t* this);
 void      funk2_user_thread_controller__grey_from_other_nodes(funk2_user_thread_controller_t* this);
 void      funk2_user_thread_controller__free_whiteness(funk2_user_thread_controller_t* this);

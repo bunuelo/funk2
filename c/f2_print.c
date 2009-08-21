5566a5f902cb3cce3c870e4bf1109fad942c43c7
Bo Morgan <neptune@psuche.(none)>
2009-08-21 15:25:47 -0400

    adding events for larva to report extra info for creating bug objects.  (adding parents to primobject_types for creating larva_events as a child object of events)

--- c/f2_print.c
+++ c/f2_print.c
@@ -1161,11 +1161,13 @@ f2ptr f2__write_pretty(f2ptr cause, f2ptr fiber, f2ptr stream, f2ptr exp, int re
 	    if (try_wide) {f2__write__space(cause, stream, use_html); width ++;} else {f2__write__line_break(cause, stream, use_html); width = 0; height ++; int i; for (i = 0; i < indent_space_num + width; i++) {f2__write__space(cause, stream, use_html);}}  
 	    {f2__write_pretty__slot_key_and_value("end_time",    15, cause, fiber, stream, f2action_event__end_time(exp, cause), f2action_event__end_time__tracing_on(exp, cause), f2action_event__end_time__trace(exp, cause), f2action_event__end_time__imagination_frame(exp, cause),
 						  ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num, available_width - width, subexp_size, try_wide, wide_success, show_slot_causes, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];}
+	    /*
 	  } else if (f2primobject__is__time(exp, cause)) {
 	    int subexp_size[2];
 	    if (try_wide) {f2__write__space(cause, stream, use_html); width ++;} else {f2__write__line_break(cause, stream, use_html); width = 0; height ++; int i; for (i = 0; i < indent_space_num + width; i++) {f2__write__space(cause, stream, use_html);}}  
 	    {f2__write_pretty__slot_key_and_value("nanoseconds_since_1970",    15, cause, fiber, stream, f2time__nanoseconds_since_1970(exp, cause), f2time__nanoseconds_since_1970__tracing_on(exp, cause), f2time__nanoseconds_since_1970__trace(exp, cause), f2time__nanoseconds_since_1970__imagination_frame(exp, cause),
 						  ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num, available_width - width, subexp_size, try_wide, wide_success, show_slot_causes, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];}
+	    */
 	  } else {
 	    f2ptr type_name = f2primobject__type(exp, cause);
 	    f2ptr primobject_type = f2__lookup_type(cause, type_name);

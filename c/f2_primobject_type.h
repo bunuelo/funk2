e307a6b1b7a00887722e1f15403d5d53fe5410e7
Bo Morgan <neptune@psuche.(none)>
2009-08-23 13:33:58 -0400

    adding events for larva to report extra info for creating bug objects.  (creating larva_events as a child object of events [adding compound_object to primobjects])

--- c/f2_primobject_type.h
+++ c/f2_primobject_type.h
@@ -23,13 +23,8 @@
 #define F2__PRIMOBJECT_TYPE__H
 
 f2ptr f2__primobject_type__new(f2ptr cause, f2ptr parents);
-f2ptr f2__primobject_type__add_slot(f2ptr cause, f2ptr this, f2ptr name, f2ptr get_funk, f2ptr set_funk, f2ptr execute_funk);
-f2ptr f2__primobject_type__lookup_slot_get_funk(f2ptr cause, f2ptr this, f2ptr name);
-f2ptr f2__primobject_type__lookup_slot_set_funk(f2ptr cause, f2ptr this, f2ptr name);
-f2ptr f2__primobject_type__lookup_slot_execute_funk(f2ptr cause, f2ptr this, f2ptr name);
-f2ptr f2__primobject_type__get_funk__slot_names(f2ptr cause, f2ptr this);
-f2ptr f2__primobject_type__set_funk__slot_names(f2ptr cause, f2ptr this);
-f2ptr f2__primobject_type__execute_funk__slot_names(f2ptr cause, f2ptr this);
+f2ptr f2__primobject_type__add_slot_type(f2ptr cause, f2ptr this, f2ptr slot_type, f2ptr slot_name, f2ptr funkable);
+f2ptr f2__primobject_type__type_funk__slot_names(f2ptr cause, f2ptr this, f2ptr type_name);
 
 // **
 

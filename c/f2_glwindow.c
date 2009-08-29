2a99bf9ecddb64ea867fb4b91c8f250a9832457d
Bo Morgan <neptune@psuche.(none)>
2009-08-29 12:59:43 -0400

    adding constant title to glwindow

--- c/f2_glwindow.c
+++ c/f2_glwindow.c
@@ -78,7 +78,7 @@ void funk2_glwindow__init(funk2_glwindow_t* this, u8* title, int width, int heig
 }
 
 void funk2_glwindow__destroy(funk2_glwindow_t* this) {
-  funk2_glwindow__hide(this);
+  funk2_glwindow__hide(this, nil);
   f2__free(to_ptr(this->title));
 }
 

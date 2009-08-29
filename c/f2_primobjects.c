6b778d4ef4bdbe3ba23793fac8635cbef9d66d4d
Bo Morgan <neptune@psuche.(none)>
2009-08-29 16:25:04 -0400

    adding macros for simplifying primobjects

--- c/f2_primobjects.c
+++ c/f2_primobjects.c
@@ -1484,7 +1484,7 @@ boolean_t raw__scheduler__is_type(f2ptr cause, f2ptr x) {
 #ifdef F2__PRIMOBJECT__TYPE_CHECK
   if (cause && (! raw__cause__is_type(nil, cause))) {error(nil, "cause is not cause.");}
 #endif // F2__PRIMOBJECT__TYPE_CHECK
-  return (raw__primobject__is_type(cause, x) && f2primobject__is_scheduler(x, cause));
+  return (raw__primobject__is_type(cause, x) && f2primobject__is__scheduler(x, cause));
 }
 f2ptr f2__scheduler__is_type(f2ptr cause, f2ptr x) {return f2bool__new(raw__scheduler__is_type(cause, x));}
 def_pcfunk1(scheduler__is_type, x, return f2__scheduler__is_type(this_cause, x));

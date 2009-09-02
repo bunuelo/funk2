533e4f15fd88cc3300ae74cb348ffa027db0690d
Bo Morgan <neptune@psuche.(none)>
2009-09-02 14:25:43 -0400

    adding physical_objects.[ch] to glwindow

---
+++ c/f2_physical_objects.c
@@ -0,0 +1,101 @@
+// 
+// Copyright (c) 2007-2009 Bo Morgan.
+// All rights reserved.
+// 
+// Author: Bo Morgan
+// 
+// Permission to use, copy, modify and distribute this software and its
+// documentation is hereby granted, provided that both the copyright
+// notice and this permission notice appear in all copies of the
+// software, derivative works or modified versions, and any portions
+// thereof, and that both notices appear in supporting documentation.
+// 
+// BO MORGAN ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS" CONDITION.
+// BO MORGAN DISCLAIMS ANY LIABILITY OF ANY KIND FOR ANY DAMAGES
+// WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+// 
+// Bo Morgan requests users of this software to return to bo@mit.edu any
+// improvements or extensions that they make and grant Bo Morgan the
+// rights to redistribute these changes.
+// 
+
+#include "funk2.h"
+
+// physical_rotation
+
+def_primobject_1_slot(physical_rotation, array);
+
+f2ptr f2__physical_rotation__new(f2ptr cause, f2ptr array) {return f2physical_rotation__new(cause, array);}
+def_pcfunk1(physical_rotation__new, array, return f2__physical_rotation__new(this_cause, array));
+
+f2ptr f2__physical_rotation__new_identity(f2ptr cause) {
+  f2ptr array = raw__array__new(cause, 9);
+  raw__array__elt__set(cause, array, 0, f2double__new(cause, 1));
+  raw__array__elt__set(cause, array, 1, f2double__new(cause, 0));
+  raw__array__elt__set(cause, array, 2, f2double__new(cause, 0));
+  raw__array__elt__set(cause, array, 3, f2double__new(cause, 0));
+  raw__array__elt__set(cause, array, 4, f2double__new(cause, 1));
+  raw__array__elt__set(cause, array, 5, f2double__new(cause, 0));
+  raw__array__elt__set(cause, array, 6, f2double__new(cause, 0));
+  raw__array__elt__set(cause, array, 7, f2double__new(cause, 0));
+  raw__array__elt__set(cause, array, 8, f2double__new(cause, 1));
+  return f2__physical_rotation__new(cause, array);
+}
+def_pcfunk0(physical_rotation__new_identity, return f2__physical_rotation__new_identity(this_cause));
+
+
+// physical_position
+
+def_primobject_3_slot(physical_position, x, y, z);
+
+f2ptr f2__physical_position__new(f2ptr cause, f2ptr x, f2ptr y, f2ptr z) {
+  double x__d; if (raw__double__is_type(cause, x)) {x__d = (double)f2double__d(x, cause);} else if (raw__integer__is_type(cause, x)) {x__d = (double)f2integer__i(x, cause);} else {return f2larva__new(cause, 1);}
+  double y__d; if (raw__double__is_type(cause, y)) {y__d = (double)f2double__d(y, cause);} else if (raw__integer__is_type(cause, y)) {y__d = (double)f2integer__i(y, cause);} else {return f2larva__new(cause, 1);}
+  double z__d; if (raw__double__is_type(cause, z)) {z__d = (double)f2double__d(z, cause);} else if (raw__integer__is_type(cause, z)) {z__d = (double)f2integer__i(z, cause);} else {return f2larva__new(cause, 1);}
+  return f2physical_position__new(cause, f2double__new(cause, x__d), f2double__new(cause, y__d), f2double__new(cause, z__d));
+}
+def_pcfunk3(physical_position__new, x, y, z, return f2__physical_position__new(this_cause, x, y, z));
+
+
+// physical_object
+
+def_primobject_3_slot(physical_object, position, rotation, shape);
+
+f2ptr f2__physical_object__new(f2ptr cause, f2ptr position, f2ptr rotation, f2ptr shape) {return f2physical_object__new(cause, position, rotation, shape);}
+def_pcfunk3(physical_object__new, position, rotation, shape, return f2__physical_object__new(this_cause, position, rotation, shape));
+
+
+// physical_scene
+
+def_primobject_1_slot(physical_scene, physical_objects);
+
+f2ptr f2__physical_scene__new(f2ptr cause, f2ptr physical_objects) {return f2physical_scene__new(cause, physical_objects);}
+def_pcfunk1(physical_scene__new, physical_objects, return f2__physical_scene__new(this_cause, physical_objects));
+
+
+// **
+
+void f2__physical_objects__reinitialize_globalvars() {
+  f2ptr cause = initial_cause();
+  
+  __physical_rotation__symbol = new__symbol(cause, "physical_rotation");
+  __physical_position__symbol = new__symbol(cause, "physical_position");
+  __physical_object__symbol   = new__symbol(cause, "physical_object");
+  __physical_scene__symbol    = new__symbol(cause, "physical_scene");
+}
+
+void f2__physical_objects__initialize() {
+  funk2_module_registration__add_module(&(__funk2.module_registration), "physical_objects", "", &f2__physical_objects__reinitialize_globalvars);
+  
+  f2__physical_objects__reinitialize_globalvars();
+  
+  f2ptr cause = initial_cause();
+  
+  initialize_primobject_1_slot(physical_rotation, array);
+  f2__primcfunk__init__0(physical_rotation__new_identity, "glwindow cfunk declared in f2_glwindow.c");
+  
+  initialize_primobject_3_slot(physical_position, x, y, z);
+  initialize_primobject_3_slot(physical_object, position, rotation, shape);
+  initialize_primobject_1_slot(physical_scene, physical_objects);
+}
+
---
+++ c/f2_physical_objects.h
@@ -0,0 +1,54 @@
+// 
+// Copyright (c) 2007-2009 Bo Morgan.
+// All rights reserved.
+// 
+// Author: Bo Morgan
+// 
+// Permission to use, copy, modify and distribute this software and its
+// documentation is hereby granted, provided that both the copyright
+// notice and this permission notice appear in all copies of the
+// software, derivative works or modified versions, and any portions
+// thereof, and that both notices appear in supporting documentation.
+// 
+// BO MORGAN ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS" CONDITION.
+// BO MORGAN DISCLAIMS ANY LIABILITY OF ANY KIND FOR ANY DAMAGES
+// WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+// 
+// Bo Morgan requests users of this software to return to bo@mit.edu any
+// improvements or extensions that they make and grant Bo Morgan the
+// rights to redistribute these changes.
+// 
+
+#ifndef F2__PHYSICAL_OBJECTS__TYPES__H
+#define F2__PHYSICAL_OBJECTS__TYPES__H
+
+#include "f2_primobjects.h"
+
+typedef struct funk2_object_type__physical_rotation__slot_s funk2_object_type__physical_rotation__slot_t;
+declare_object_type_1_slot(physical_rotation, array, );
+
+typedef struct funk2_object_type__physical_position__slot_s funk2_object_type__physical_position__slot_t;
+declare_object_type_3_slot(physical_position, x, y, z, );
+
+typedef struct funk2_object_type__physical_object__slot_s funk2_object_type__physical_object__slot_t;
+declare_object_type_3_slot(physical_object, position, rotation, shape, );
+
+typedef struct funk2_object_type__physical_scene__slot_s funk2_object_type__physical_scene__slot_t;
+declare_object_type_1_slot(physical_scene, physical_objects, );
+
+
+#endif // F2__PHYSICAL_OBJECTS__TYPES__H
+
+#ifndef F2__PHYSICAL_OBJECTS__H
+#define F2__PHYSICAL_OBJECTS__H
+
+declare_primobject_1_slot(physical_rotation, array);
+declare_primobject_3_slot(physical_position, x, y, z);
+declare_primobject_3_slot(physical_object, position, rotation, shape);
+declare_primobject_1_slot(physical_scene, physical_objects);
+
+// **
+
+void f2__physical_objects__initialize() {
+
+#endif // F2__PHYSICAL_OBJECTS__H

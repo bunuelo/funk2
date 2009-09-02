cf3c76ea980d6e4b634ae6d6d27551c279edfcbe
Bo Morgan <neptune@psuche.(none)>
2009-09-02 14:26:46 -0400

    adding physical_objects.[ch] to glwindow

--- c/funk2.c
+++ c/funk2.c
@@ -99,6 +99,7 @@ void f2__initialize() {
   f2__garbage_collector__initialize();
   f2__frame_objects__initialize();
   f2__glwindow__initialize();
+  f2__physical_objects__initialize();
 }
 
 #define u64_large_prime ((u64)12764787846358441471ull)

677b0bbb6364fc61f223e3438c51c5eda86b3643
Bo Morgan <neptune@psuche.(none)>
2009-09-03 11:50:10 -0400

    adding rendering of physical_scene

--- c/f2_glwindow.c
+++ c/f2_glwindow.c
@@ -421,7 +421,7 @@ void funk2_glwindow__draw_scene(funk2_glwindow_t* this, f2ptr cause) {
     raw__opengl__glRotatef(cause, this->rotate_angle, 1,1,0.5);
     if (raw__physical_scene__is_type(cause, value)) {
       f2ptr physical_scene = value;
-      opengl__render_physical_scene(this, cause, physical_scene);
+      opengl__render_physical_scene(cause, physical_scene);
     } else if (raw__larva__is_type(cause, value)) {
       raw__gl_set_material_color(cause, 1,0,0,1);
       raw__draw_gl_cube(cause);

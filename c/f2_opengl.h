bc4eee914a03031f2e09a20caafa675f3725ee65
Bo Morgan <neptune@psuche.(none)>
2009-09-18 18:11:03 -0400

    adding fonts

--- c/f2_opengl.h
+++ c/f2_opengl.h
@@ -67,6 +67,7 @@ struct funk2_opengl_s {
   GLuint(*       glGenLists     )(GLsizei range);
   void(*         glDeleteLists  )(GLuint list, GLsizei range);
   void(*         glPushAttrib   )(GLbitfield mask);
+  void(*         glListBase     )(GLuint base);
   
   // The following functions are included in libGL.so, but are
   // probably not on non-X environments, so we'll need to deal with
@@ -168,6 +169,7 @@ void   raw__opengl__glDisable(f2ptr cause, GLenum cap);
 GLuint raw__opengl__glGenLists(f2ptr cause, GLsizei range);
 void   raw__opengl__glDeleteLists(f2ptr cause, GLuint list, GLsizei range);
 void   raw__opengl__glPushAttrib(f2ptr cause, GLbitfield mask);
+void   raw__opengl__glListBase(f2ptr cause, GLuint base);
 
 void         raw__opengl__glXSwapBuffers(f2ptr cause, Display* dpy, GLXDrawable drawable);
 Bool         raw__opengl__glXMakeCurrent(f2ptr cause, Display* dpy, GLXDrawable drawable, GLXContext ctx);

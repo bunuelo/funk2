11ad4b4351b031880d6931ce8b0cb146abc2edb9
Bo Morgan <neptune@psuche.(none)>
2009-09-18 15:59:41 -0400

    removing non-mipmapped textures from glwindow

--- c/f2_glwindow.c
+++ c/f2_glwindow.c
@@ -209,16 +209,9 @@ boolean_t funk2_opengl_texture__load_gl_texture_from_bmp(funk2_opengl_texture_t*
     this->width  = image->width;
     this->height = image->height;
     raw__opengl__glBindTexture(cause, GL_TEXTURE_2D, this->texture_id);
-    boolean_t mipmapped = boolean__true;
-    if (! mipmapped) {
-      raw__opengl__glTexParameteri(cause, GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-      raw__opengl__glTexParameteri(cause, GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-      raw__opengl__glTexImage2D(cause, GL_TEXTURE_2D, 0, 4, image->width, image->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image->data);
-    } else {
-      raw__opengl__glTexParameteri(cause, GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-      raw__opengl__glTexParameteri(cause, GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-      raw__openglu__gluBuild2DMipmaps(cause, GL_TEXTURE_2D, 4, image->width, image->height, GL_RGBA, GL_UNSIGNED_BYTE, image->data);
-    }
+    raw__opengl__glTexParameteri(cause, GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    raw__opengl__glTexParameteri(cause, GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    raw__openglu__gluBuild2DMipmaps(cause, GL_TEXTURE_2D, 4, image->width, image->height, GL_RGBA, GL_UNSIGNED_BYTE, image->data);
   }
   if (image) {
     if (image->data) {

86940e05ce057e1d4a7bf53f30cf1198fc4dac7e
Bo Morgan <neptune@psuche.(none)>
2009-09-24 12:29:10 -0400

    adding transform to physical_person

--- c/f2_glwindow.c
+++ c/f2_glwindow.c
@@ -928,6 +928,10 @@ void funk2_glwindow__render_relative_physical_place(funk2_glwindow_t* this, f2pt
   raw__opengl__glPopMatrix(cause);
 }
 
+void funk2_glwindow__render_physical_place(funk2_glwindow_t* this, f2ptr cause, f2ptr physical_place) {
+  funk2_glwindow__render_relative_physical_place(this, , nil, physical_place);
+}
+
 void funk2_glwindow__render_physical_object(funk2_glwindow_t* this, f2ptr cause, f2ptr physical_object) {
   funk2_glwindow__render_relative_physical_object(this, cause, nil, physical_object);
 }

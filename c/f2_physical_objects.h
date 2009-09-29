b43c76db7c2eea572581f1fd68758e9d885fea3b
Bo Morgan <neptune@psuche.(none)>
2009-09-28 20:54:34 -0400

    adding part_frame to physical_object

--- c/f2_physical_objects.h
+++ c/f2_physical_objects.h
@@ -34,7 +34,7 @@ typedef struct funk2_object_type__physical_transform__slot_s funk2_object_type__
 declare_object_type_3_slot(physical_transform, position, rotation, scale, );
 
 typedef struct funk2_object_type__physical_object__slot_s funk2_object_type__physical_object__slot_t;
-declare_object_type_3_slot(physical_object, transform, text, texture_name, );
+declare_object_type_4_slot(physical_object, transform, text, texture_name, part_frame, );
 
 typedef struct funk2_object_type__physical_place__slot_s funk2_object_type__physical_place__slot_t;
 declare_object_type_2_slot(physical_place, transform, thing, );
@@ -54,6 +54,7 @@ declare_primobject_1_slot(physical_rotation, array);
 declare_primobject_3_slot(physical_position, x, y, z);
 declare_primobject_3_slot(physical_transform, position, rotation, scale);
 declare_primobject_3_slot(physical_object, transform, text, texture_name);
+declare_primobject_3_slot(physical_object, transform, text, texture_name, part_frame);
 declare_primobject_2_slot(physical_place, transform, thing);
 declare_primobject_8_slot(physical_person, transform, body, torso_clothing_place, leg_clothing_place, left_foot_clothing_place, right_foot_clothing_place, left_hand_object_place, right_hand_object_place);
 declare_primobject_2_slot(physical_scene, background_texture, physical_things);

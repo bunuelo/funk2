77ab2d30b5be8e881734b098a0fee1628c047c92
Bo Morgan <neptune@psuche.(none)>
2009-10-01 13:50:13 -0400

    added physical_texture to f2_physical_objects

--- c/f2_glwindow.c
+++ c/f2_glwindow.c
@@ -746,7 +746,10 @@ GLfloat funk2_glwindow__get_render_font_width(funk2_glwindow_t* this, f2ptr caus
   raw__opengl__glDisable(cause, GL_TEXTURE_2D);
   
   raw__gl_set_material_color(cause, 1, 0, 0, 0);
+  
   GLfloat start_raster_position[4];
+  GLfloat end_raster_position[4];
+  
   raw__opengl__glPushMatrix(cause);
   {
     raw__opengl__glRasterPos3f(cause, 0, 0, 0);
@@ -754,11 +757,10 @@ GLfloat funk2_glwindow__get_render_font_width(funk2_glwindow_t* this, f2ptr caus
     raw__opengl__glGetFloatv(cause, GL_CURRENT_RASTER_POSITION, start_raster_position);
     
     funk2_opengl_font__printf(&(this->fixed_font), cause, "%s", temp_str);
+  
+    raw__opengl__glGetFloatv(cause, GL_CURRENT_RASTER_POSITION, end_raster_position);
   }
   raw__opengl__glPopMatrix(cause);
-  
-  GLfloat end_raster_position[4];
-  raw__opengl__glGetFloatv(cause, GL_CURRENT_RASTER_POSITION, end_raster_position);
   
   return 2.0 * ((GLfloat)(end_raster_position[0] - start_raster_position[0]) / (GLfloat)(this->width));
 }

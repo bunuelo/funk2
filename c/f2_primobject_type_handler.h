0296a1718db8e4f3d9505c8e547c6e33aa1be4bf
Bo Morgan <neptune@psuche.(none)>
2009-10-05 14:23:10 -0400

    changing old physical_object to physical_primtype_object

--- c/f2_primobject_type_handler.h
+++ c/f2_primobject_type_handler.h
@@ -99,7 +99,7 @@ struct funk2_primobject_object_types_s {
   funk2_object_type__physical_position__slot_t  primobject_type_physical_position;
   funk2_object_type__physical_transform__slot_t primobject_type_physical_transform;
   funk2_object_type__physical_texture__slot_t   primobject_type_physical_texture;
-  funk2_object_type__physical_object__slot_t    primobject_type_physical_object;
+  funk2_object_type__physical_primtype_object__slot_t    primobject_type_physical_primtype_object;
   funk2_object_type__physical_place__slot_t     primobject_type_physical_place;
   funk2_object_type__physical_scene__slot_t     primobject_type_physical_scene;
   funk2_object_type__agent__slot_t              primobject_type_agent;

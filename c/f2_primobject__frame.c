e14f654f73955660558cd1123c979c3da2944d20
Bo Morgan <neptune@psuche.(none)>
2009-10-05 20:28:44 -0400

    adding frame copy function.

--- c/f2_primobject__frame.c
+++ c/f2_primobject__frame.c
@@ -187,7 +187,7 @@ def_pcfunk1(frame__funkvar__slot_names, this, return f2__frame__funkvar__slot_na
 
 f2ptr f2__frame__copy(f2ptr cause, f2ptr this, f2ptr source) {
   if ((! raw__frame__is_type(cause, this)) ||
-      (! raw__frame__is_type(cause, source)) ||) {
+      (! raw__frame__is_type(cause, source))) {
     return f2larva__new(cause, 1);
   }
   frame__iteration(cause, source, type_slot_name, slot_name, slot_value, 

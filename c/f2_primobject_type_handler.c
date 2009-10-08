ef9c467bb871e5942d0fe66eeaa59e867ef7b475
Bo Morgan <neptune@psuche.(none)>
2009-10-08 08:31:19 -0400

    adding object_lattice primobject

--- c/f2_primobject_type_handler.c
+++ c/f2_primobject_type_handler.c
@@ -117,6 +117,7 @@ void funk2_primobject_type_handler__add_builtin_primobjects(funk2_primobject_typ
   {char* type_name = "physical_place";     funk2_primobject_type_handler__add_type(this, cause, f2symbol__new(cause, strlen(type_name), (u8*)type_name),     f2physical_place__primobject_type__new(cause));}
   {char* type_name = "physical_scene";     funk2_primobject_type_handler__add_type(this, cause, f2symbol__new(cause, strlen(type_name), (u8*)type_name),     f2physical_scene__primobject_type__new(cause));}
   {char* type_name = "agent";              funk2_primobject_type_handler__add_type(this, cause, f2symbol__new(cause, strlen(type_name), (u8*)type_name),              f2agent__primobject_type__new(cause));}
+  {char* type_name = "object_lattice";     funk2_primobject_type_handler__add_type(this, cause, f2symbol__new(cause, strlen(type_name), (u8*)type_name),     f2object_lattice__primobject_type__new(cause));}
 }
 
 void funk2_primobject_type_handler__add_builtin_frame_objects(funk2_primobject_type_handler_t* this, f2ptr cause) {
--- c/f2_primobject_type_handler.h
+++ c/f2_primobject_type_handler.h
@@ -30,6 +30,7 @@
 #include "f2_primobject__circular_buffer.h"
 #include "f2_physical_objects.h"
 #include "f2_agent.h"
+#include "f2_object_lattice.h"
 
 typedef struct funk2_primobject_type_handler_s {
   funk2_processor_mutex_t type_hash_mutex;
@@ -102,6 +103,7 @@ struct funk2_primobject_object_types_s {
   funk2_object_type__physical_place__slot_t     primobject_type_physical_place;
   funk2_object_type__physical_scene__slot_t     primobject_type_physical_scene;
   funk2_object_type__agent__slot_t              primobject_type_agent;
+  funk2_object_type__object_lattice__slot_t     primobject_type_object_lattice;
 };
 
 // **

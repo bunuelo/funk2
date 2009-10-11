a18c1a59ca531561b8f46fb2beb4c01ccb5b11e3
Bo Morgan <neptune@psuche.(none)>
2009-10-11 00:12:39 -0400

    working on object_lattice (switching adding array elt accessors to scan)

--- c/f2_object_lattice.c
+++ c/f2_object_lattice.c
@@ -46,8 +46,8 @@ void object_lattice__scan_and_incorporate_leafs__object_slot_helper(f2ptr cause,
   f2ptr this                         = raw__array__elt(cause, aux_data, 2);
   f2ptr start_nanoseconds_since_1970 = raw__array__elt(cause, aux_data, 3);
   {
-    printf("\nobject_slot:");
-    f2__print(cause, fiber, slot_name);
+    //printf("\nobject_slot:");
+    //f2__print(cause, fiber, slot_name);
     f2ptr slot_funk = f2__object__slot__type_funk(cause, object, __funk2.globalenv.get__symbol, slot_name);
     f2ptr slot_value = f2__force_funk_apply(cause, fiber, slot_funk, f2cons__new(cause, object, nil));
     //f2__print(cause, fiber, slot_value);
@@ -61,8 +61,8 @@ void object_lattice__scan_and_incorporate_leafs__frame_slot_helper(f2ptr cause, 
   f2ptr this                         = raw__array__elt(cause, aux_data, 2);
   f2ptr start_nanoseconds_since_1970 = raw__array__elt(cause, aux_data, 3);
   {
-    printf("\nframe_slot:");
-    f2__print(cause, fiber, slot_name);
+    //printf("\nframe_slot:");
+    //f2__print(cause, fiber, slot_name);
     f2ptr slot_value = f2__frame__lookup_var_value(cause, object, slot_name, nil);
     //f2__print(cause, fiber, slot_value);
     f2__object_lattice__scan_and_incorporate_leafs(cause, fiber, this, slot_value, start_nanoseconds_since_1970);

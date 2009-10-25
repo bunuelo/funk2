c2dc8f204f246d1dcd742d36266441282097db27
Bo Morgan <neptune@psuche.(none)>
2009-10-25 16:55:32 -0400

    added f2_cause

--- c/f2_cause.c
+++ c/f2_cause.c
@@ -163,6 +163,7 @@ void f2__cause__reinitialize_globalvars() {
 void f2__cause__initialize() {
   funk2_module_registration__add_module(&(__funk2.module_registration), "cause", "", &f2__cause__reinitialize_globalvars);
   f2__cause__reinitialize_globalvars();
+  f2ptr cause = initial_cause();
   
   //--
   

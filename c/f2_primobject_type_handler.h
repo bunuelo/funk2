032b08737dbcbb092b814d212a97bc99785fed0d
Bo Morgan <neptune@psuche.(none)>
2009-10-29 19:37:34 -0400

    added f2_bug.

--- c/f2_primobject_type_handler.h
+++ c/f2_primobject_type_handler.h
@@ -28,6 +28,7 @@
 #include "f2_primobject__set.h"
 #include "f2_primobject__list.h"
 #include "f2_primobject__circular_buffer.h"
+#include "f2_bug.h"
 #include "f2_physical_objects.h"
 #include "f2_agent.h"
 #include "f2_object_lattice.h"

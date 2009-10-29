12c694bc747352cae5392331715cd2d7e47e5695
Bo Morgan <neptune@psuche.(none)>
2009-10-29 15:12:52 -0400

    making list object print like a conslist

--- c/f2_print.c
+++ c/f2_print.c
@@ -895,17 +895,18 @@ f2ptr f2__write_pretty(f2ptr cause, f2ptr fiber, f2ptr stream, f2ptr exp, int re
 	    {f2__write_pretty__slot_key_and_value("bin_array",     13, cause, fiber, stream, f2ptypehash__bin_array(exp, cause),       f2ptypehash__bin_array__tracing_on(exp, cause), f2ptypehash__bin_array__trace(exp, cause), f2ptypehash__bin_array__imagination_frame(exp, cause),
 						  ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num, available_width - width, subexp_size, try_wide, wide_success, show_slot_causes, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];}
 	    */
-	    //} else if (f2primobject__is_list(exp, cause)) {
-	    //f2ptr cons_cells = f2__list__cons_cells(cause, exp);
-	    //{
-	    //  int subexp_size[2];
-	    //  f2__write_pretty(cause, fiber, stream, cons_cells, recursion_depth, indent_space_num + width, available_width - width, subexp_size, 1, wide_success, 0, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];
-	    //}
+	  } else if (f2primobject__is_list(exp, cause)) {
+	    f2ptr cons_cells = f2__list__cons_cells(cause, exp);
+	    {
+	      int subexp_size[2];
+	      f2__write_pretty(cause, fiber, stream, cons_cells,
+			       ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num + width, available_width - width, subexp_size, 1, wide_success, 0, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];
+	    }
 	  } else if (f2primobject__is_frame(exp, cause)) {
 	    int   max_type_name_length = 0;
 	    int   max_slot_name_length = 0;
 	    
-	    f2ptr type_ptypehash                    = f2frame__type_ptypehash(exp, cause);
+	    f2ptr type_ptypehash = f2frame__type_ptypehash(exp, cause);
 	    
 	    boolean_t need_to_print_type_besides_basic_variable = boolean__false; // if this is false, we can omit printing "variable" before each frame variable key.
 	    

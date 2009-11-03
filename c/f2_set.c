dcd03941485a19ed8e8149a21e5eec5db76f6077
Bo Morgan <neptune@psuche.(none)>
2009-11-03 15:13:53 -0500

    debugging [doc scheduler:global_scheduler], which breaks garbage collection node colors at random times, usually during printing. (found scheduler cause returns noisy values, found assert_failed in grey tricolor gc set)

--- c/f2_set.c
+++ c/f2_set.c
@@ -125,9 +125,12 @@ void* funk2_set__mapc(funk2_set_t* this, void(* mapc_funk)(funk2_set_element_t e
   void*              return_value = NULL;
   u64 i;
   for (i = 0; i < bin_num; i ++) {
-    while (bin[i]) {
+    funk2_set_node_t* iter = bin[i];
+    while (iter) {
+      funk2_set_node_t* next = iter->next;
       funk2_set_element_t element = bin[i]->element;
       (*mapc_funk)(element, user_data, &stop, &return_value);
+      iter = next;
       if (stop) {
 	return return_value;
       }

c4ca344ae9b991954e2283ecdda0ab60d9225075
Bo Morgan <neptune@psuche.(none)>
2009-11-21 16:07:38 -0500

    working on userifying textures for glwindow

--- c/f2_glwindow.c
+++ c/f2_glwindow.c
@@ -1205,9 +1205,10 @@ def_pcfunk0(glwindow__destroy, return f2__glwindow__destroy(this_cause));
 
 boolean_t raw__glwindow__load_texture(f2ptr cause, u8* name, u8* filename) {
 #if defined(F2__GLWINDOW__SUPPORTED)
-  funk2_glwindow__load_texture(&(__funk2.glwindow), cause, name, filename);
+  return funk2_glwindow__load_texture(&(__funk2.glwindow), cause, name, filename);
 #else
   status("glwindow not supported.");
+  return boolean__true; // failure
 #endif
 }
 

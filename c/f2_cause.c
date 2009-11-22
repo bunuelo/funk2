32c561e8f85252af6636002a25420d815e373c7b
Bo Morgan <neptune@psuche.(none)>
2009-11-22 13:58:46 -0500

    adding bytecode_funk_event_hash to cause primobject

--- c/f2_cause.c
+++ c/f2_cause.c
@@ -218,6 +218,19 @@ f2ptr f2cause__primobject_type__new_aux(f2ptr cause) {
 }
 
 
+f2ptr f2__bytecode_funk_callback__create_and_hash_event(f2ptr cause, f2ptr fiber, f2ptr bytecode, f2ptr funk, f2ptr args) {
+  printf("\nbytecode_funk_callback__create_and_hash_event here.");
+  return nil;
+}
+def_pcfunk4(bytecode_funk_callback__create_and_hash_event, fiber, bytecode, funk, args, return f2__bytecode_funk_callback__create_and_hash_event(this_cause, fiber, bytecode, funk, args));
+
+f2ptr f2__bytecode_endfunk_callback__finish_and_unhash_event(f2ptr cause, f2ptr fiber, f2ptr bytecode, f2ptr value, f2ptr funk) {
+  printf("\nbytecode_funk_callback__finish_and_unhash_event here.");
+  return nil;
+}
+def_pcfunk4(bytecode_funk_callback__finish_and_unhash_event, fiber, bytecode, value, funk, return f2__bytecode_funk_callback__finish_and_unhash_event(this_cause, fiber, bytecode, value, funk));
+
+
 
 // **
 
@@ -258,6 +271,8 @@ void f2__cause__initialize() {
   {char* symbol_str = "define-funk"; __funk2.globalenv.object_type.primobject.primobject_type_cause.define__funk__symbol = f2symbol__new(cause, strlen(symbol_str), (u8*)symbol_str);}
   {f2__primcfunk__init__with_c_cfunk_var__3_arg(cause__define__funk, this, var, value, cfunk, 0, "primobject_type funktion (defined in f2_primobjects.c)"); __funk2.globalenv.object_type.primobject.primobject_type_cause.define__funk__funk = never_gc(cfunk);}
   
+  f2__primcfunk__init__4(bytecode_funk_callback__create_and_hash_event, fiber, bytecode, funk, args, "(cfunk defined in f2_cause.c)");
+  f2__primcfunk__init__4(bytecode_funk_callback__finish_and_unhash_event, fiber, bytecode, value, funk, "(cfunk defined in f2_cause.c)");
   
 }
 

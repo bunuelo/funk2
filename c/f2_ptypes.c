36d1fdd7f241165605569c66702281932fd5f44a
Bo Morgan <neptune@psuche.(none)>
2010-01-07 13:32:26 -0500

    adding mutate_funks callback to traced_array__elt__set

--- c/f2_ptypes.c
+++ c/f2_ptypes.c
@@ -1901,7 +1901,7 @@ f2ptr pfunk2__f2traced_array__elt__set__trace_depth(f2ptr this, u64 index, f2ptr
       if (mutate_funks) {
 	f2ptr funkable_iter = mutate_funks;
 	f2ptr fiber         = f2__scheduler__processor_thread_current_fiber(this_processor_thread__pool_index());
-	f2ptr args          = f2cons__new__trace_depth(cause, value, f2cons__new__trace_depth(cause, old_value, trace_depth - 1), trace_depth - 1);
+	f2ptr args          = f2cons__new__trace_depth(cause, value, f2cons__new__trace_depth(cause, old_value, nil, trace_depth - 1), trace_depth - 1);
 	while (funkable_iter) {
 	  f2ptr funkable = f2cons__car__trace_depth(funkable_iter, cause, trace_depth - 1);
 	  f2ptr mutate_funk_value = f2__force_funk_apply(cause, fiber, funkable, args);

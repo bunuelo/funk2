1b2ff466e547420e44c3d499b7273b00b2ce9ad3
Bo Morgan <neptune@psuche.(none)>
2010-01-13 11:21:29 -0500

    adding perception lattice

--- c/f2_perception_lattice.c
+++ c/f2_perception_lattice.c
@@ -61,6 +61,51 @@ f2ptr f2__perception_graph__new_from_string(f2ptr cause, f2ptr string) {
 }
 def_pcfunk1(perception_graph__new_from_string, string, return f2__perception_graph__new_from_string(this_cause, string));
 
+f2ptr raw__string__append_char(f2ptr cause, f2ptr this, u8 ch) {
+  u64 string__length = f2string__length(string, cause);
+  u8* string_copy = (u8*)from_ptr(f2__malloc(string__length + 1));
+  f2string__str_copy(string, cause, string_copy);
+  string_copy[string__length] = ch;
+  return f2string__new(cause, string__length + 1, string_copy);
+}
+
+f2ptr f2__perception_graph__to_string(f2ptr cause, f2ptr this) {
+  if (! raw__perception_graph__is_type(cause, this)) {
+    return f2larva__new(cause, 1);
+  }
+  f2ptr edges = f2__perception_graph__edges(cause, this);
+  f2ptr leftmost_edge = nil;
+  {
+    f2ptr iter = edges;
+    while (iter) {
+      leftmost_edge = iter;
+      iter = f2__perception_graph_edge__left_node(cause, iter);
+    }
+  }
+  f2ptr string = new__string(cause, "");
+  if (leftmost_edge) {
+    f2ptr left_node = f2__perception_graph_edge__left_node(cause, leftmost_edge);
+    if (! raw__char__is_type(cause, left_node)) {
+      return f2larva__new(cause, 1);
+    }
+    u8 ch = f2char__ch(left_node, cause);
+    string = f2__string__append_char(cause, string, ch);
+  }
+  {
+    f2ptr iter = leftmost_edge;
+    while (iter) {
+      f2ptr right_node = f2__perception_graph_edge__right_node(cause, iter);
+      if (! raw__char__is_type(cause, right_node)) {
+	return f2larva__new(cause, 1);
+      }
+      u8 ch = f2char__ch(right_node, cause);
+      string = f2__string__append_char(cause, string, ch);
+    }
+  }
+  return string;
+}
+def_pcfunk1(perception_graph__to_string, this, return f2__perception_graph__to_string(this_cause, this));
+
 // **
 
 void f2__perception_lattice__reinitialize_globalvars() {
@@ -72,7 +117,6 @@ void f2__perception_lattice__reinitialize_globalvars() {
   __perception_graph__symbol      = new__symbol(cause, "perception_graph");
 }
 
-
 void f2__perception_lattice__initialize() {
   f2__perception_lattice__reinitialize_globalvars();
   f2ptr cause = initial_cause();
@@ -83,6 +127,7 @@ void f2__perception_lattice__initialize() {
   // perception_graph
   initialize_primobject_1_slot(perception_graph, edges);
   
-  f2__primcfunk__init__1(perception_graph__new_from_string, string, "creates a perception_graph of characters from a string.");
+  f2__primcfunk__init__1(perception_graph__new_from_string, string, "creates a perception_graph of characters from a string.  (function used for debugging graph matching)");
+  f2__primcfunk__init__1(perception_graph__to_string, this, "creates a string from a perception_graph made from a string.  (function used for debugging graph matching)");
 }
 

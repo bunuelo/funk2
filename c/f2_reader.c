94c31b4dad85b9d6049c766cc477ff3514ac3d03
Bo Morgan <neptune@psuche.(none)>
2010-01-20 16:15:06 -0500

    working on unsigned integer division

--- c/f2_reader.c
+++ c/f2_reader.c
@@ -887,6 +887,21 @@ f2ptr f2__stream__try_read_number(f2ptr cause, f2ptr stream) {
 	whole_decimal_digit --;
       }
     }
+    f2ptr whole_decimal_value__largeinteger = raw__largeinteger__new(cause, 0);
+    {
+      f2ptr ten__largeinteger = raw__largeinteger__new(cause, 10);
+      s64 whole_decimal_digit = whole_decimal_length - 1;
+      iter = whole_decimal_start;
+      while (whole_decimal_digit >= 0) {
+	f2ptr power_i__largeinteger = raw__largeinteger__new(cause, 1);
+	{s64 k; for (k = 0; k < whole_decimal_digit; k ++) {power_i__largeinteger = raw__largeinteger__multiply(cause, power_i__largeinteger, ten__largeinteger);}}
+	f2ptr read_ch = f2cons__car(iter, cause);
+	f2ptr char_value__largeinteger = raw__largeinteger__new(cause, (s64)raw__char__decimal_digit_value(cause, read_ch));
+	whole_decimal_value__largeinteger = raw__largeinteger__add(cause, whole_decimal_value__largeinteger, (raw__largeinteger__multiply(cause, char_value__largeinteger, power_i__largeinteger)));
+	iter = f2cons__cdr(iter, cause);
+	whole_decimal_digit --;
+      }
+    }
     s64 part_decimal_denomenator = 1;
     {s64 k; for (k = 0; k < part_decimal_length; k ++) {part_decimal_denomenator *= 10;}}
     s64 part_decimal_value = 0;
@@ -906,8 +921,19 @@ f2ptr f2__stream__try_read_number(f2ptr cause, f2ptr stream) {
       double d = ((is_negative ? -1.0 : 1.0) * (((double)whole_decimal_value) + (((double)part_decimal_value) / ((double)part_decimal_denomenator))));
       return f2double__new(cause, d);
     } else {
-      s64 i = ((is_negative ? ((s64)-1) : ((s64)1)) * ((s64)whole_decimal_value));
-      return f2integer__new(cause, i);
+      f2ptr sup_integer_value__largeinteger = raw__largeinteger__bitshift_left(cause, raw__largeinteger__new(cause, 1), 63);
+      f2ptr min_integer_value__largeinteger = raw__largeinteger__bitshift_left(cause, raw__largeinteger__new(cause, 1), 63);
+      if ((  raw__largeinteger__less_than(cause, whole_decimal_value__largeinteger, sup_integer_value__largeinteger)) &&
+	  (! raw__largeinteger__less_than(cause, whole_decimal_value__largeinteger, min_integer_value__largeinteger))) {
+	s64 i = ((is_negative ? ((s64)-1) : ((s64)1)) * ((s64)whole_decimal_value));
+	return f2integer__new(cause, i);
+      } else {
+	if (is_negative) {
+	  return raw__largeinteger__negative(cause, whole_decimal_value__largeinteger);
+	} else {
+	  return whole_decimal_value__largeinteger;
+	}
+      }
     }
   }
   return __funk2.reader.could_not_read_type_exception;

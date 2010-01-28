ec35c0a6e86b74e062c0796a555dc320b0906d44
Bo Morgan <neptune@psuche.(none)>
2010-01-28 14:37:40 -0500

    adding object-equals_hash_value and object-eq_hash_value

--- c/f2_globalenv.h
+++ c/f2_globalenv.h
@@ -113,10 +113,11 @@ typedef struct funk2_globalenv_s {
   f2ptr set__symbol;
   f2ptr execute__symbol;
   
-  f2ptr hash_value__symbol;
+  f2ptr eq_hash_value__symbol;
+  f2ptr equals_hash_value__symbol;
   
-  funk2_object_types_t            object_type;
-  funk2_blocks_world_t            blocks_world;
+  funk2_object_types_t object_type;
+  funk2_blocks_world_t blocks_world;
   
 } funk2_globalenv_t;
 

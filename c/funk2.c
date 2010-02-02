22b73d48bae78a22cadf27eacc66dd1b371449e6
Bo Morgan <neptune@psuche.(none)>
2010-02-02 13:41:04 -0500

    adding equals and equals_hash_value to hash (changing f2__print to not require fiber, adding f2__fiber__print)

--- c/funk2.c
+++ c/funk2.c
@@ -225,7 +225,7 @@ void funk2__init(funk2_t* this, int argc, char** argv) {
 	// if we can't find a user-friendly load, then use this basic hardcoded one for compiling the user-friendly one.
 	load_funk = environment__lookup_funkvar_value(cause, global_environment(), f2symbol__new(cause, strlen("primfunk:load"), (u8*)"primfunk:load"));
 	if (raw__larva__is_type(cause, load_funk)) {
-	  f2__print(cause, nil, global_environment());
+	  f2__fiber__print(cause, nil, global_environment());
 	  error(nil, "funk2 main (raw__funkable__is_type(load_funk)) assertion failed.");
 	}
       }

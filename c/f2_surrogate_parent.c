7ff218bb5183b4f4be44e3612ad04985855e9571
Bo Morgan <neptune@psuche.(none)>
2010-02-17 09:26:27 -0500

    adding surrogate parent to allow us to fork new processes to avoid pthread+libc incompatibilities.

--- c/f2_surrogate_parent.c
+++ c/f2_surrogate_parent.c
@@ -42,8 +42,8 @@ void funk2_pipe__write(funk2_pipe_t* this, void* data, f2size_t byte_count) {
   f2size_t bytes_written_count = write(this->write_file_descriptor, data, byte_count);
   if (bytes_written_count != byte_count) {
     const char* error_message = "funk2_pipe__write error: couldn't write data.";
-    printf("\n%s\n", error_message);
-    error(nil, error_message);
+    printf("\n%s\n", (char*)error_message);
+    error(nil, (char*)error_message);
   }
 }
 

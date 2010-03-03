8fea2edb998d319933e344571579da414505a2d6
Bo Morgan <neptune@psuche.(none)>
2010-03-02 21:11:22 -0500

    working on loop_free equals_hash_value

--- c/f2_array.c
+++ c/f2_array.c
@@ -176,6 +176,9 @@ f2ptr raw__array__equals_hash_value__loop_free(f2ptr cause, f2ptr this, f2ptr no
       f2ptr this__subexp = raw__array__elt(cause, this, index);
       if (this__subexp) {
 	f2ptr subexp__hash_value = f2__object__equals_hash_value__loop_free(cause, this__subexp, node_hash);
+	if (raw__larva__is_type(cause, subexp__hash_value)) {
+	  return subexp__hash_value;
+	}
 	if (! raw__integer__is_type(cause, subexp__hash_value)) {
 	  return f2larva__new(cause, 4);
 	}

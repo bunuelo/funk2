49352f42efbb7dc0f5b198d5e540e3d2d23d179b
Bo Morgan <neptune@psuche.(none)>
2010-03-20 14:48:39 -0400

    working on trans

--- c/f2_print.c
+++ c/f2_print.c
@@ -930,10 +930,10 @@ f2ptr f2__write_pretty(f2ptr cause, f2ptr fiber, f2ptr stream, f2ptr exp, int re
 	  } else if (f2primobject__is__trans(exp, cause)) {
 	    int subexp_size[2];
 	    if (try_wide) {f2__write__space(cause, stream, use_html); width ++;} else {f2__write__line_break(cause, stream, use_html); width = 0; height ++; int i; for (i = 0; i < indent_space_num + width; i++) {f2__write__space(cause, stream, use_html);}}  
-	    {f2__write_pretty__slot_key_and_value("remove", 6, cause, fiber, stream, f2trans__nodes(exp, cause), f2trans__nodes__tracing_on(exp, cause), f2trans__nodes__trace(exp, cause), f2trans__nodes__imagination_frame(exp, cause),
+	    {f2__write_pretty__slot_key_and_value("remove", 6, cause, fiber, stream, f2trans__remove(exp, cause), f2trans__remove__tracing_on(exp, cause), f2trans__remove__trace(exp, cause), f2trans__remove__imagination_frame(exp, cause),
 						  ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num, available_width - width, subexp_size, try_wide, wide_success, show_slot_causes, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];}
 	    if (try_wide) {f2__write__space(cause, stream, use_html); width ++;} else {f2__write__line_break(cause, stream, use_html); width = 0; height ++; int i; for (i = 0; i < indent_space_num + width; i++) {f2__write__space(cause, stream, use_html);}}  
-	    {f2__write_pretty__slot_key_and_value("add",    6, cause, fiber, stream, f2trans__edges(exp, cause), f2trans__edges__tracing_on(exp, cause), f2trans__edges__trace(exp, cause), f2trans__edges__imagination_frame(exp, cause),
+	    {f2__write_pretty__slot_key_and_value("add",    6, cause, fiber, stream, f2trans__add(exp, cause), f2trans__add__tracing_on(exp, cause), f2trans__add__trace(exp, cause), f2trans__add__imagination_frame(exp, cause),
 						  ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num, available_width - width, subexp_size, try_wide, wide_success, show_slot_causes, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];}
 	  } else if (f2primobject__is_list(exp, cause)) {
 	    f2ptr cons_cells = f2list__cons_cells(exp, cause);

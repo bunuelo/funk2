84e1e852ba3105de9e93c685922ac0b9ea59b384
Bo Morgan <neptune@psuche.(none)>
2010-03-22 18:36:12 -0400

    redoing graph representation to use ptypehash instead of all lists.

--- c/f2_perception_lattice.h
+++ c/f2_perception_lattice.h
@@ -36,7 +36,7 @@ declare_object_type_3_slot(graph_edge_type, label, left_node_hash, right_node_ha
 
 // graph
 typedef struct funk2_object_type__graph__slot_s funk2_object_type__graph__slot_t;
-declare_object_type_4_slot(graph, is_rooted, root, node_label_hash, edge_type_label_hash
+declare_object_type_4_slot(graph, is_rooted, root, node_label_hash, edge_type_label_hash,
 			   f2ptr equals__symbol;
 			   f2ptr equals__funk;
 			   f2ptr equals_hash_value__symbol;

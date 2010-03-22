9a3c9a05436bb68de962e461a578803328f3c1ae
Bo Morgan <neptune@psuche.(none)>
2010-03-22 18:28:57 -0400

    redoing graph representation to use ptypehash instead of all lists.

--- c/f2_perception_lattice.h
+++ c/f2_perception_lattice.h
@@ -24,19 +24,19 @@
 
 // graph_node
 typedef struct funk2_object_type__graph_node__slot_s funk2_object_type__graph_node__slot_t;
-declare_object_type_2_slot(graph_node, label, in_edge_hash, out_edge_hash, );
+declare_object_type_3_slot(graph_node, label, in_edge_hash, out_edge_hash, );
 
 // graph_edge
 typedef struct funk2_object_type__graph_edge__slot_s funk2_object_type__graph_edge__slot_t;
-declare_object_type_2_slot(graph_edge, label, left_node, right_node, );
+declare_object_type_3_slot(graph_edge, label, left_node, right_node, );
 
 // graph_edge_type
 typedef struct funk2_object_type__graph_edge_type__slot_s funk2_object_type__graph_edge_type__slot_t;
-declare_object_type_2_slot(graph_edge_type, label, left_node_hash, right_node_hash, );
+declare_object_type_3_slot(graph_edge_type, label, left_node_hash, right_node_hash, );
 
 // graph
 typedef struct funk2_object_type__graph__slot_s funk2_object_type__graph__slot_t;
-declare_object_type_4_slot(graph, is_rooted, root, node_label_hash, edge_label_hash, edge_type_label_hash
+declare_object_type_4_slot(graph, is_rooted, root, node_label_hash, edge_type_label_hash
 			   f2ptr equals__symbol;
 			   f2ptr equals__funk;
 			   f2ptr equals_hash_value__symbol;
@@ -66,16 +66,16 @@ declare_object_type_2_slot(trans, remove, add,
 #include "f2_primobjects.h"
 
 // graph_node
-declare_primobject_2_slot(graph_node, label, in_edge_hash, out_edge_hash);
+declare_primobject_3_slot(graph_node, label, in_edge_hash, out_edge_hash);
 
 // graph_edge
-declare_primobject_2_slot(graph_edge, label, left_node, right_node);
+declare_primobject_3_slot(graph_edge, label, left_node, right_node);
 
 // graph_edge_type
-declare_primobject_2_slot(graph_edge_type, label, left_node, right_node);
+declare_primobject_3_slot(graph_edge_type, label, left_node, right_node);
 
 // graph
-declare_primobject_3_slot(graph, is_rooted, root, node_edge_hash_node_hash);
+declare_primobject_3_slot(graph, is_rooted, root, node_label_hash, edge_type_label_hash);
 
 f2ptr      f2__graph__new(f2ptr cause);
 f2ptr      f2__graph__equals_hash_value(f2ptr cause, f2ptr this);

e802f3ad9a6df768ad4d78ed2fa7eb81a0ce387b
Bo Morgan <neptune@psuche.(none)>
2010-03-23 23:11:04 -0400

    redoing graph representation to use ptypehash instead of all lists.

--- c/f2_print.c
+++ c/f2_print.c
@@ -926,14 +926,28 @@ f2ptr f2__write_pretty(f2ptr cause, f2ptr fiber, f2ptr stream, f2ptr exp, int re
 	      {f2__write_pretty__slot_key_and_value("root", 5, cause, fiber, stream, f2graph__root(exp, cause), f2graph__root__tracing_on(exp, cause), f2graph__root__trace(exp, cause), f2graph__root__imagination_frame(exp, cause),
 						    ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num, available_width - width, subexp_size, try_wide, wide_success, show_slot_causes, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];}
 	    }
-	    // should iterate through nodes and for each node iterate through edges.  (and be cute about it)
-	    //   ...but for now print the hairy mess.
-	    if (try_wide) {f2__write__space(cause, stream, use_html); width ++;} else {f2__write__line_break(cause, stream, use_html); width = 0; height ++; int i; for (i = 0; i < indent_space_num + width; i++) {f2__write__space(cause, stream, use_html);}}  
-	    {f2__write_pretty__slot_key_and_value("node_label_hash",      20, cause, fiber, stream, f2graph__node_label_hash(exp, cause), f2graph__node_label_hash__tracing_on(exp, cause), f2graph__node_label_hash__trace(exp, cause), f2graph__node_label_hash__imagination_frame(exp, cause),
-						  ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num, available_width - width, subexp_size, try_wide, wide_success, show_slot_causes, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];}
-	    if (try_wide) {f2__write__space(cause, stream, use_html); width ++;} else {f2__write__line_break(cause, stream, use_html); width = 0; height ++; int i; for (i = 0; i < indent_space_num + width; i++) {f2__write__space(cause, stream, use_html);}}  
-	    {f2__write_pretty__slot_key_and_value("edge_type_label_hash", 20, cause, fiber, stream, f2graph__edge_type_label_hash(exp, cause), f2graph__edge_type_label_hash__tracing_on(exp, cause), f2graph__edge_type_label_hash__trace(exp, cause), f2graph__edge_type_label_hash__imagination_frame(exp, cause),
-						  ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num, available_width - width, subexp_size, try_wide, wide_success, show_slot_causes, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];}
+	    graph__edge__iteration(cause, exp, edge,
+				   f2ptr edge__label       = f2__graph_edge__label(     cause, edge);
+				   f2ptr left_node         = f2__graph_edge__left_node( cause, edge);
+				   f2ptr left_node__label  = f2__graph_node__label(     cause, left_node);
+				   f2ptr right_node        = f2__graph_edge__right_node(cause, edge);
+				   f2ptr right_node__label = f2__graph_node__label(     cause, right_node);
+				   
+				   if (try_wide) {f2__write__space(cause, stream, use_html); width ++;} else {f2__write__line_break(cause, stream, use_html); width = 0; height ++; int i; for (i = 0; i < indent_space_num + width; i++) {f2__write__space(cause, stream, use_html);}}  
+				   {
+				     int subexp_size[2];
+				     f2ptr short_list = f2cons__new(cause, edge__label, f2cons__new(cause, left_node__label, f2cons__new(cause, right_node__label, nil)));
+				     f2__write_pretty(cause, fiber, stream, short_list,
+						      ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num + width, available_width - width, subexp_size, 1, wide_success, show_slot_causes, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];
+				   }
+				   );
+	    
+	    //if (try_wide) {f2__write__space(cause, stream, use_html); width ++;} else {f2__write__line_break(cause, stream, use_html); width = 0; height ++; int i; for (i = 0; i < indent_space_num + width; i++) {f2__write__space(cause, stream, use_html);}}  
+	    //{f2__write_pretty__slot_key_and_value("node_label_hash",      20, cause, fiber, stream, f2graph__node_label_hash(exp, cause), f2graph__node_label_hash__tracing_on(exp, cause), f2graph__node_label_hash__trace(exp, cause), f2graph__node_label_hash__imagination_frame(exp, cause),
+	    //					  ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num, available_width - width, subexp_size, try_wide, wide_success, show_slot_causes, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];}
+	    //if (try_wide) {f2__write__space(cause, stream, use_html); width ++;} else {f2__write__line_break(cause, stream, use_html); width = 0; height ++; int i; for (i = 0; i < indent_space_num + width; i++) {f2__write__space(cause, stream, use_html);}}  
+	    //{f2__write_pretty__slot_key_and_value("edge_type_label_hash", 20, cause, fiber, stream, f2graph__edge_type_label_hash(exp, cause), f2graph__edge_type_label_hash__tracing_on(exp, cause), f2graph__edge_type_label_hash__trace(exp, cause), f2graph__edge_type_label_hash__imagination_frame(exp, cause),
+	    //					  ((recursion_depth == -1) ? recursion_depth : (recursion_depth - 1)), indent_space_num, available_width - width, subexp_size, try_wide, wide_success, show_slot_causes, use_ansi_colors, use_html, brief_mode); width += subexp_size[0]; height += subexp_size[1];}
 	  } else if (f2primobject__is__trans(exp, cause)) {
 	    int subexp_size[2];
 	    if (try_wide) {f2__write__space(cause, stream, use_html); width ++;} else {f2__write__line_break(cause, stream, use_html); width = 0; height ++; int i; for (i = 0; i < indent_space_num + width; i++) {f2__write__space(cause, stream, use_html);}}  

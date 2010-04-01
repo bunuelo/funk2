32c3df6f47b8e901fc3e113303f07932e0bb107c
Bo Morgan <neptune@psuche.(none)>
2010-04-01 13:57:05 -0400

    working on keeping track of line numbers for streams

--- c/f2_ptypes.h
+++ c/f2_ptypes.h
@@ -470,7 +470,7 @@ f2ptr f2larva__primobject_type__new(f2ptr cause);
 u32   raw__larva__larva_type(f2ptr cause, f2ptr this);
 f2ptr  f2__larva__larva_type(f2ptr cause, f2ptr this);
 
-u32   raw__larva__bug(f2ptr cause, f2ptr this);
+f2ptr raw__larva__bug(f2ptr cause, f2ptr this);
 f2ptr  f2__larva__bug(f2ptr cause, f2ptr this);
 
 boolean_t raw__larva__is_type(f2ptr cause, f2ptr exp);

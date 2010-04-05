ed8080039343ec1f279dbd53b27f4e8e22fb5ed1
Bo Morgan <neptune@psuche.(none)>
2010-04-05 19:23:25 -0400

    adding list and conslist to graphviz-label

--- c/f2_graphviz.c
+++ c/f2_graphviz.c
@@ -81,6 +81,30 @@ f2ptr f2__graphviz__exp__as__label(f2ptr cause, f2ptr exp) {
   if (raw__symbol__is_type(cause, exp)) {
     return f2__exp__as__string(cause, exp);
   }
+  if (raw__cons__is_type(cause, exp)) {
+    f2ptr stringlist = nil;
+    {
+      f2ptr iter = exp;
+      while (iter) {
+	f2ptr element = f2__cons__car(cause, iter);
+	f2ptr new_cons = f2cons__new(cause, f2__graphviz__exp__as__label(cause, element), nil);
+	if (stringlist) {
+	  f2__cons__cdr__set(cause, stringlist, new_cons);
+	} else {
+	  stringlist = new_cons;
+	}
+	iter = f2__cons__cdr(cause, iter);
+      }
+    }
+    return f2__stringlist__concat(cause, f2list3__new(cause,
+						      new__string(cause, "["),
+						      f2__stringlist__intersperse(cause, stringlist, new__string(cause, " ")),
+						      new__string(cause, "]")));
+  }
+  if (raw__list__is_type(cause, exp)) {
+    f2ptr cons_cells = f2__list__cons_cells(cause, exp);
+    return f2__graphviz__exp__as__label(cause, cons_cells);
+  }
   if (raw__graph_variable__is_type(cause, exp)) {
     return f2__stringlist__concat(cause, f2list3__new(cause,
 						      new__string(cause, "<font color=\"#CF0000\">?"),

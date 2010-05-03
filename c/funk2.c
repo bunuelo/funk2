9f67335c815e25436558a9dcff10bef92b37506c
Bo Morgan <neptune@psuche.(none)>
2010-05-03 18:40:09 -0400

    debugging bootstrap problem on Windows

--- c/funk2.c
+++ c/funk2.c
@@ -350,17 +350,26 @@ void funk2__destroy(funk2_t* this) {
 }
 
 boolean_t funk2__handle(funk2_t* this) {
+  status("funk2-handle: peer command server handling clients.");
   funk2_peer_command_server__handle_clients(&(this->peer_command_server));  
+  status("funk2-handle: peer command server flushing command input buffer.");
   funk2_peer_command_server__flush_command_input_buffer(&(__funk2.peer_command_server), 1);
+  status("funk2-handle: node handler handling nodes.");
   funk2_node_handler__handle_nodes(&(this->node_handler));
+  status("funk2-handle: handling memory.");
   funk2_memory__handle(&(this->memory));
+  status("funk2-handle: handling garbage collection.");
   funk2_garbage_collector__handle(&(this->garbage_collector));
+  status("funk2-handle: management thread handling user threads.");
   funk2_management_thread__handle_user_threads(&(this->management_thread));
+  status("funk2-handle: handling cpu.");
   funk2_cpu__handle(&(this->cpu));
+  status("funk2-handle: handling surrogate parent.");
   funk2_surrogate_parent__handle(&(this->surrogate_parent));
   //funk2_event_router__handle_input_events(&(this->event_router));
   //printf("\nYour parent is here."); fflush(stdout);
   // very primitive global reflection might go here if necessary... (maybe handle global process signals?)
+  status("funk2-handle: done handling.");
   raw__fast_spin_sleep_yield();
   return boolean__false; // should return true if we did something.
 }

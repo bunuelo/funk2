9a6227b11ac894e80a537e820df0cabf935a6f89
Bo Morgan <neptune@psuche.(none)>
2010-05-08 18:15:24 -0400

    adding event_graph_last_event to cause object

--- c/f2_cause.c
+++ c/f2_cause.c
@@ -207,7 +207,7 @@ f2ptr f2__cause__get_event_graph__thread_unsafe(f2ptr cause, f2ptr this) {
 }
 
 f2ptr f2__cause__add_graph_event__funk(f2ptr cause, f2ptr this, f2ptr fiber, f2ptr bytecode, f2ptr funk, f2ptr args) {
-  if (cause == nil) {
+  if (this == nil) {
     return nil;
   }
   f2ptr event_graph_mutex = f2__cause__event_graph_mutex(cause, this);
@@ -230,7 +230,7 @@ f2ptr f2__cause__add_graph_event__funk(f2ptr cause, f2ptr this, f2ptr fiber, f2p
 def_pcfunk5(cause__add_graph_event__funk, this, fiber, bytecode, funk, args, return f2__cause__add_graph_event__funk(this_cause, this, fiber, bytecode, funk, args));
 
 f2ptr f2__cause__add_graph_event__endfunk(f2ptr cause, f2ptr this, f2ptr fiber, f2ptr bytecode, f2ptr value, f2ptr funk) {
-  if (cause == nil) {
+  if (this == nil) {
     return nil;
   }
   f2ptr event_graph_mutex = f2__cause__event_graph_mutex(cause, this);
@@ -253,7 +253,7 @@ f2ptr f2__cause__add_graph_event__endfunk(f2ptr cause, f2ptr this, f2ptr fiber, 
 def_pcfunk5(cause__add_graph_event__endfunk, this, fiber, bytecode, value, funk, return f2__cause__add_graph_event__endfunk(this_cause, this, fiber, bytecode, value, funk));
 
 f2ptr f2__cause__add_graph_event__branch(f2ptr cause, f2ptr this, f2ptr fiber, f2ptr bytecode, f2ptr program_counter, f2ptr branch_program_counter, f2ptr value) {
-  if (cause == nil) {
+  if (this == nil) {
     return nil;
   }
   f2ptr event_graph_mutex = f2__cause__event_graph_mutex(cause, this);

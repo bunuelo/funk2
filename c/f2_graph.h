d404667b868ed9f48408603eaf9df9d476b4b1fb
Bo Morgan <bo@mit.edu>
2010-07-01 12:51:31 -0400

    working on bruno_decomposition_lattice-subgraph_isomorphisms

--- c/f2_graph.h
+++ c/f2_graph.h
@@ -211,6 +211,8 @@ f2ptr      f2__graph__equals_hash_value(f2ptr cause, f2ptr this);
 boolean_t raw__graph__remove_edge(f2ptr cause, f2ptr this, f2ptr edge_label, f2ptr left_node_label, f2ptr right_node_label);
 f2ptr      f2__graph__remove_edge(f2ptr cause, f2ptr this, f2ptr edge_label, f2ptr left_node_label, f2ptr right_node_label);
 f2ptr      f2__graph__add_edge(f2ptr cause, f2ptr this, f2ptr edge_label, f2ptr left_label_node, f2ptr right_node_label);
+f2ptr     raw__graph__contains_edge(f2ptr cause, f2ptr this, f2ptr edge_label, f2ptr left_node_label, f2ptr right_node_label);
+f2ptr      f2__graph__contains_edge(f2ptr cause, f2ptr this, f2ptr edge_label, f2ptr left_node_label, f2ptr right_node_label);
 f2ptr     raw__graph__nodes(f2ptr cause, f2ptr this);
 f2ptr      f2__graph__nodes(f2ptr cause, f2ptr this);
 boolean_t raw__graph__has_nodes(f2ptr cause, f2ptr this);

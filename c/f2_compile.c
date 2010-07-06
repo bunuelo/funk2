f048256ad3c4778d1e741bf16c32646c0d5acc19
Bo Morgan <bo@mit.edu>
2010-07-06 07:51:13 -0400

    working on isis_world-test.  (DEBUGGING THREADS)

--- c/f2_compile.c
+++ c/f2_compile.c
@@ -521,9 +521,10 @@ f2ptr f2__compile__rawcode(f2ptr simple_cause, f2ptr fiber, f2ptr exps, boolean_
     protect_subexp_environment     = (f2cons__cdr(exps, cause) != nil) || protect_environment;
     optimize_subexp_tail_recursion = (f2cons__cdr(exps, cause) == nil) && optimize_tail_recursion;
     
-    f2ptr exp_bcs = nil;
-    boolean_t  exp__is_funktional = boolean__true;
-    f2ptr next = nil;
+    f2ptr     exp_bcs = nil;
+    //boolean_t exp__is_funktional = boolean__true; // was this way, debugging...
+    boolean_t exp__is_funktional = boolean__false;
+    f2ptr     next = nil;
     do {
       f2ptr exp = f2cons__car(exps, cause);
       next      = f2cons__cdr(exps, cause);

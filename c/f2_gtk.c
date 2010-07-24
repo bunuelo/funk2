4abbafa52aa65068eb823bd910a55f6c190547b8
Bo Morgan <bo@mit.edu>
2010-07-24 17:51:33 -0400

    adding gtk_main thread to f2_gtk

--- c/f2_gtk.c
+++ c/f2_gtk.c
@@ -21,9 +21,25 @@
 
 #include "funk2.h"
 
+#if defined(F2__GTK__SUPPORTED)
+
+
+// funk2_gtk_widget
+
+void funk2_gtk_widget__init(funk2_gtk_widget_t* this, u8* name, GtkWidget* gtk_widget) {
+  u64 name__length = strlen((char*)name);
+  this->name = (u8*)f2__malloc(name__length);
+  memcpy(this->name, name, name__length + 1);
+  this->gtk_widget = gtk_widget;
+}
+
+void funk2_gtk_widget__destroy(funk2_gtk_widget_t* this) {
+  f2__free(this->name);
+}
+
+
 // funk2_gtk
 
-#if defined(F2__GTK__SUPPORTED)
 void funk2_gtk__thread__start_function(funk2_gtk_t* this) {
   funk2_processor_mutex__lock(&(this->main_thread__mutex));
   if (this->main_thread__active) {
@@ -56,6 +72,8 @@ void* funk2_gtk__thread__start_function__helper(void* ptr) {
 void funk2_gtk__init(funk2_gtk_t* this, int* argv, char*** argc) {
   gtk_init(argv, argc);
   
+  this->widgets = NULL;
+  
   funk2_processor_mutex__init(&(this->main_thread__mutex));
   this->main_thread__active = boolean__false;
   this->main_thread         = funk2_processor_thread_handler__add_new_processor_thread(&(__funk2.processor_thread_handler), &funk2_gtk__thread__start_function__helper, (void*)this);
@@ -64,6 +82,35 @@ void funk2_gtk__init(funk2_gtk_t* this, int* argv, char*** argc) {
 void funk2_gtk__destroy(funk2_gtk_t* this) {
   funk2_processor_mutex__destroy(&(this->main_thread__mutex));
 }
+
+void funk2_gtk__add_widget(funk2_gtk_t* this, funk2_gtk_widget_t* widget) {
+  funk2_gtk_widget_cons_t* cons = (funk2_gtk_widget_cons_t*)f2__malloc(sizeof(funk2_gtk_widget_cons_t));
+  cons->widget = widget;
+  cons->next   = this->widgets;
+  this->widgets = cons;
+}
+
+funk2_gtk_widget_t* funk2_gtk__lookup_widget(funk2_gtk_t* this, u8* name) {
+  funk2_gtk_widget_cons_t* iter = this->widgets;
+  while (iter) {
+    funk2_gtk_widget_t* widget = iter->widget;
+    if (strcmp(this->name, name) == 0) {
+      return widget;
+    }
+    iter = iter->next;
+  }
+  return NULL;
+}
+
+funk2_gtk_widget_t* funk2_gtk__window__new(funk2_gtk_t* this, u8* name) {
+  GtkWidget           window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  funk2_gtk_widget_t* widget = (funk2_gtk_widget_t*)f2__malloc(sizeof(funk2_gtk_widget_t));
+  funk2_gtk_widget__init(widget, name, window);
+  funk2_gtk__add_widget(this, widget);
+  return widget;
+}
+
+
 #endif // F2__GTK__SUPPORTED
 
 
@@ -83,6 +130,28 @@ f2ptr f2__gtk__is_supported(f2ptr cause) {
 def_pcfunk0(gtk__is_supported, return f2__gtk__is_supported(this_cause));
 
 
+f2ptr raw__gtk__window__new(f2ptr cause, f2ptr name) {
+#if defined(F2__GTK__SUPPORTED)
+  u64 name__length = raw__symbol__length(cause, name);
+  u8* name__str    = (u8*)f2__malloc(name__length + 1);
+  raw__symbol__str_copy(cause, name, name__str);
+  name__str[name__length] = 0;
+  funk2_gtk__window__new(&(__funk2.gtk), name__str);
+  return name;
+#else
+  return nil;
+#endif
+}
+
+f2ptr f2__gtk__window__new(f2ptr cause, f2ptr name) {
+  if (! raw__symbol__is_type(cause, name)) {
+    return f2larva__new(cause, 1, nil);
+  }
+  return raw__gtk__window__new(cause, name);
+}
+def_pcfunk1(gtk__window__new, name, return f2__gtk__window__new(this_cause, name));
+
+
 // **
 
 void f2__gtk__reinitialize_globalvars() {
@@ -95,7 +164,8 @@ void f2__gtk__initialize() {
   
   f2__string__reinitialize_globalvars();
   
-  f2__primcfunk__init__0(gtk__is_supported, "Returns true if GIMP ToolKit (GTK) support has been compiled into this version of Funk2.");
+  f2__primcfunk__init__0(gtk__is_supported,       "Returns true if GIMP ToolKit (GTK) support has been compiled into this version of Funk2.");
+  f2__primcfunk__init__1(gtk__window__new,  name, "If GIMP TookKit (GTK) is supported, returns the name of a new window widget.");
   
 }
 
--- c/f2_gtk.h
+++ c/f2_gtk.h
@@ -24,11 +24,26 @@
 
 #include "f2_processor_thread.h"
 
+typedef struct funk2_gtk_widget_s funk2_gtk_widget_t;
+struct funk2_gtk_widget_s {
+  u8*        name;
+#if defined(F2__GTK__SUPPORTED)
+  GtkWidget* gtk_widget;
+#endif // F2__GTK__SUPPORTED
+};
+
+typedef struct funk2_gtk_widget_cons_s funk2_gtk_widget_cons_t;
+struct funk2_gtk_widget_cons_s {
+  funk2_gtk_widget_t*             widget;
+  struct funk2_gtk_widget_cons_s* next;
+};
+
 typedef struct funk2_gtk_s funk2_gtk_t;
 struct funk2_gtk_s {
   funk2_processor_mutex_t   main_thread__mutex;
   boolean_t                 main_thread__active;
   funk2_processor_thread_t* main_thread;
+  funk2_gtk_widget_cons_t*  widgets;
 };
 
 #endif // F2__GTK__TYPES__H
@@ -43,6 +58,13 @@ struct funk2_gtk_s {
 
 void funk2_gtk__init(funk2_gtk_t* this, int* argv, char*** argc);
 void funk2_gtk__destroy(funk2_gtk_t* this);
+
+// funk2_gtk_widget
+
+#ifdef F2__GTK__SUPPORTED
+void funk2_gtk_widget__init(funk2_gtk_widget_t* this, char* name, GtkWidget* widget);
+void funk2_gtk_widget__destroy(funk2_gtk_widget_t* this);
+#endif // F2__GTK__SUPPORTED
 
 // **
 

bd284bfa504f5bdebd7fcf805302af0739be4109
Bo Morgan <bo@mit.edu>
2010-08-01 21:53:04 -0400

    working on isis_world-test

--- c/f2_time.c
+++ c/f2_time.c
@@ -131,7 +131,8 @@ void f2__sleep(int microseconds) {
 
 void raw__spin_sleep_yield() {
   sched_yield();
-  //f2__sleep(1000);
+  //f2__sleep(10000);
+  f2__sleep(1);
 }
 
 void raw__fast_spin_sleep_yield() {

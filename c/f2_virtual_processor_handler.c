0f86314f7e60e05aeaeb9f79e88b2e55a5006528
Bo Morgan <bo@mit.edu>
2010-08-12 17:30:22 -0400

    adding virtual_processor_handler

--- c/f2_virtual_processor_handler.c
+++ c/f2_virtual_processor_handler.c
@@ -21,16 +21,16 @@
 
 #include "funk2.h"
 
-void funk2_virtual_processor_handler__init(funk2_virtual_processor_handler_t* this, u64 virtual_processor_thread_count) {
-  status("initializing virtual_processor_handler with " u64__fstr " virtual processor threads.", virtual_processor_thread_count);
-  this->virtual_processor_thread_count = virtual_processor_thread_count;
-  this->virtual_processor_thread       = (funk2_virtual_processor_thread_t**)from_ptr(f2__malloc(sizeof(funk2_virtual_processor_thread_t*) * virtual_processor_thread_count));
+void funk2_virtual_processor_handler__init(funk2_virtual_processor_handler_t* this, u64 virtual_processor_count) {
+  status("initializing virtual_processor_handler with " u64__fstr " virtual processor threads.", virtual_processor_count);
+  this->virtual_processor_count = virtual_processor_count;
+  this->virtual_processor       = (funk2_virtual_processor_t**)from_ptr(f2__malloc(sizeof(funk2_virtual_processor_t*) * virtual_processor_count));
   {
     s64 index;
-    for (index = 0; index < virtual_processor_thread_count; index ++) {
-      funk2_virtual_processor_thread_t* virtual_processor_thread = (funk2_virtual_processor_thread_t*)from_ptr(f2__malloc(sizeof(funk2_virtual_processor_thread_t)));
-      funk2_virtual_processor_thread__init(virtual_processor_thread);
-      this->virtual_processor_thread[index] = virtual_processor_thread;
+    for (index = 0; index < virtual_processor_count; index ++) {
+      funk2_virtual_processor_t* virtual_processor = (funk2_virtual_processor_t*)from_ptr(f2__malloc(sizeof(funk2_virtual_processor_t)));
+      funk2_virtual_processor__init(virtual_processor);
+      this->virtual_processor[index] = virtual_processor;
     }
   }
 }
@@ -40,11 +40,11 @@ void funk2_virtual_processor_handler__destroy(funk2_virtual_processor_handler_t*
   status("destroying virtual_processor_handler.");
   {
     s64 index;
-    for (index = 0; index < this->virtual_processor_thread_count; index ++) {
-      funk2_virtual_processor_thread__destroy(this->virtual_processor_thread[index]);
+    for (index = 0; index < this->virtual_processor_count; index ++) {
+      funk2_virtual_processor__destroy(this->virtual_processor[index]);
     }
   }
-  f2__free(to_ptr(this->virtual_processor_thread));
+  f2__free(to_ptr(this->virtual_processor));
 }
 
 

68c318ffa94c07ffc9351c9de646db0f5b8be54f
Bo Morgan <bo@mit.edu>
2010-08-16 16:33:15 -0400

    adding f2_virtual_processor_thread.[ch]

--- c/f2_virtual_processor_thread.c
+++ c/f2_virtual_processor_thread.c
@@ -47,9 +47,9 @@ void* funk2_virtual_processor_thread__start_function(void* args) {
 	did_something = funk2_virtual_processor__execute_next_bytecodes(virtual_processor, this);
       }
     } else {
-      funk2_processor_mutex__lock(&(virtual_processor->free_virtual_processor_thread_count_mutex));
+      //funk2_processor_mutex__lock(&(virtual_processor->free_virtual_processor_thread_count_mutex));
       s64 free_virtual_processor_thread_count = virtual_processor->free_virtual_processor_thread_count;
-      funk2_processor_mutex__unlock(&(virtual_processor->free_virtual_processor_thread_count_mutex));
+      //funk2_processor_mutex__unlock(&(virtual_processor->free_virtual_processor_thread_count_mutex));
       //if (free_virtual_processor_thread_count > 8) {
       //	funk2_virtual_processor__know_of_one_less_free_virtual_processor_thread(virtual_processor);
       //	funk2_virtual_processor_thread__unassign_from_virtual_processor(this);

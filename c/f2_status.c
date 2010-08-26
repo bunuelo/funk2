59b1f1de1812251c1dfb945f1e701317fe42ad58
Bo Morgan <bo@mit.edu>
2010-08-26 13:05:38 -0400

    debugging total_free_memory misalignment bug.

--- c/f2_status.c
+++ c/f2_status.c
@@ -55,7 +55,7 @@ void funk2_status(char* filename, int line_num, char* msg, ...) {
   u64 seconds      = seconds_since_1970      - (minutes_since_1970         * 60);
   u64 nanoseconds  = nanoseconds_since_1970  - (seconds_since_1970         * nanoseconds_per_second);
   char temp_msg2[2048];
-  sprintf(temp_msg2, "\n[%-36s %5d] tid=0x" "%-16" x64__fstr_without_percent " 0x%X_%02d_%02d_%02d." nano_decimal_fraction__fstr " funk2 status: %s", filename, line_num, (u64)to_ptr(self_thread), (int)earth_rotations_since_1970, (int)hours, (int)minutes, (int)seconds, (u64)nanoseconds, temp_msg);
+  sprintf(temp_msg2, "\n[%-40s %5d] tid=0x" "%-16" x64__fstr_without_percent " 0x%X_%02d_%02d_%02d." nano_decimal_fraction__fstr " funk2 status: %s", filename, line_num, (u64)to_ptr(self_thread), (int)earth_rotations_since_1970, (int)hours, (int)minutes, (int)seconds, (u64)nanoseconds, temp_msg);
   size_t size_to_write = strlen(temp_msg2) + 1;
   size_t size_written  = write(trace_fd, temp_msg2, size_to_write);
   if (size_written != size_to_write) {

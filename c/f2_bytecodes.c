d7ad186551fe17b6d236c056661497fd3252ad2e
Bo Morgan <bo@mit.edu>
2010-09-01 13:07:46 -0400

    working macro-bytecodes for super-speed

--- c/f2_bytecodes.c
+++ c/f2_bytecodes.c
@@ -1068,14 +1068,18 @@ int f2__fiber__bytecode__copy__value_reg__return_reg(f2ptr fiber, f2ptr bytecode
   return 0;
 }
 
+int f2__fiber__bytecode__copy__value_reg__iter_reg__no_increment_pc_reg(f2ptr cause, f2ptr fiber, f2ptr bytecode) {
+  f2fiber__iter__set(fiber, cause, f2fiber__value(fiber, cause));
+  return 0;
+}
+
 int f2__fiber__bytecode__copy__value_reg__iter_reg(f2ptr fiber, f2ptr bytecode) {
   bytecode_status("bytecode copy beginning.");
   f2ptr cause = f2fiber__cause_reg(fiber, nil);
   
   f2__fiber__increment_pc(fiber, cause);
   
-  f2fiber__iter__set(fiber, cause, f2fiber__value(fiber, cause));
-  return 0;
+  return f2__fiber__bytecode__copy__value_reg__iter_reg__no_increment_pc_reg(cause, fiber, bytecode);
 }
 
 int f2__fiber__bytecode__copy__value_reg__program_counter_reg(f2ptr fiber, f2ptr bytecode) {

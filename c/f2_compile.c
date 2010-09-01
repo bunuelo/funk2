d38b485bd995d81a724bbb79573f9b33d3da83b8
Bo Morgan <bo@mit.edu>
2010-09-01 13:17:22 -0400

    working macro-bytecodes for super-speed

--- c/f2_compile.c
+++ c/f2_compile.c
@@ -444,8 +444,10 @@ f2ptr f2__compile__funk(f2ptr simple_cause, f2ptr fiber, f2ptr funk) {
     
     // BYTECODE -- block_exit_and_no_pop
     
-    iter = f2__list_cdr__set(cause, iter, f2__compile__endfunk(cause, funk));
-    iter = f2__list_cdr__set(cause, iter, f2__compile__copy_return_to_pc(cause));
+    //iter = f2__list_cdr__set(cause, iter, f2__compile__endfunk(cause, funk));
+    //iter = f2__list_cdr__set(cause, iter, f2__compile__copy_return_to_pc(cause));
+    
+    iter = f2__list_cdr__set(cause, iter, f2__compile__block_exit_and_no_pop(cause, funk));
     
   }
   

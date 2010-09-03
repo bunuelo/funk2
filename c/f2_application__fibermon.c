5080bb3d00d9e0325f3ff2e03b45ebca757fcc1b
Bo Morgan <bo@mit.edu>
2010-09-03 16:40:19 -0400

    adding f2_application__fibermon.[ch]

--- c/f2_application__fibermon.c
+++ c/f2_application__fibermon.c
@@ -39,7 +39,7 @@ f2ptr f2__fibermon_fiber__construct_fast(f2ptr cause, f2ptr this) {
   f2__frame__add_var_value(cause, this, new__symbol(cause, "hbox"),                               raw__gtk__hbox__new(cause, f2integer__new(cause, 2)));
   f2__frame__add_var_value(cause, this, new__symbol(cause, "progress_bar"),                       raw__gtk__progress_bar__new(cause));
   f2__frame__add_var_value(cause, this, new__symbol(cause, "table"),                              raw__gtk__table__new(cause, f2integer__new(cause, 12), f2integer__new(cause, 2), nil));
-  f2__frame__add_var_value(cause, this, new__symbol(cause, "table_labels"),                       f2__array__new(cause, f2list2__new(f2integer__new(cause, 12), f2integer__new(cause, 2))));
+  f2__frame__add_var_value(cause, this, new__symbol(cause, "table_labels"),                       f2__array__new(cause, f2list2__new(cause, f2integer__new(cause, 12), f2integer__new(cause, 2))));
   return this;
 }
 

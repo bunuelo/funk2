0f67172f6868f5d16db6dfa5f36fafe85feb9242
Bo Morgan <bo@mit.edu>
2010-09-03 16:40:00 -0400

    adding f2_application__fibermon.[ch]

--- c/f2_gtk.h
+++ c/f2_gtk.h
@@ -118,6 +118,244 @@ void funk2_gtk__add_callback(funk2_gtk_t* this, funk2_gtk_callback_t* callback);
 void funk2_gtk__add_callback_event(funk2_gtk_t* this, funk2_gtk_callback_t* callback);
 
 
+// user-space funktions
+
+boolean_t raw__gtk__is_supported(f2ptr cause);
+  f2ptr f2__gtk__is_supported(f2ptr cause);
+
+
+// window
+
+f2ptr raw__gtk__window__new(f2ptr cause);
+f2ptr f2__gtk__window__new(f2ptr cause);
+f2ptr raw__gtk__window__set_title(f2ptr cause, f2ptr window, f2ptr title);
+f2ptr f2__gtk__window__set_title(f2ptr cause, f2ptr window, f2ptr title);
+f2ptr raw__gtk__window__set_default_size(f2ptr cause, f2ptr window, f2ptr width, f2ptr height);
+f2ptr f2__gtk__window__set_default_size(f2ptr cause, f2ptr window, f2ptr width, f2ptr height);
+
+
+  // vbox
+
+f2ptr raw__gtk__vbox__new(f2ptr cause, f2ptr spacing);
+f2ptr f2__gtk__vbox__new(f2ptr cause, f2ptr spacing);
+
+
+  // hbox
+
+f2ptr raw__gtk__hbox__new(f2ptr cause, f2ptr spacing);
+f2ptr f2__gtk__hbox__new(f2ptr cause, f2ptr spacing);
+
+
+  // button
+
+f2ptr raw__gtk__button__new_with_label(f2ptr cause, f2ptr label);
+f2ptr f2__gtk__button__new_with_label(f2ptr cause, f2ptr label);
+
+
+  // entry
+
+f2ptr raw__gtk__entry__new(f2ptr cause);
+f2ptr f2__gtk__entry__new(f2ptr cause);
+f2ptr raw__gtk__entry__get_text(f2ptr cause, f2ptr widget);
+f2ptr f2__gtk__entry__get_text(f2ptr cause, f2ptr widget);
+
+
+  // scrolled_window
+
+f2ptr raw__gtk__scrolled_window__new(f2ptr cause);
+f2ptr f2__gtk__scrolled_window__new(f2ptr cause);
+f2ptr raw__gtk__scrolled_window__add_with_viewport(f2ptr cause, f2ptr scrolled_window, f2ptr child);
+f2ptr f2__gtk__scrolled_window__add_with_viewport(f2ptr cause, f2ptr scrolled_window, f2ptr child);
+
+
+  // text_view
+
+f2ptr raw__gtk__text_view__new(f2ptr cause);
+f2ptr f2__gtk__text_view__new(f2ptr cause);
+f2ptr raw__gtk__text_view__get_buffer(f2ptr cause, f2ptr widget);
+f2ptr f2__gtk__text_view__get_buffer(f2ptr cause, f2ptr widget);
+
+
+// container
+
+f2ptr raw__gtk__container__add(f2ptr cause, f2ptr widget, f2ptr add_widget);
+f2ptr f2__gtk__container__add(f2ptr cause, f2ptr widget, f2ptr add_widget);
+f2ptr raw__gtk__container__remove(f2ptr cause, f2ptr widget, f2ptr remove_widget);
+f2ptr f2__gtk__container__remove(f2ptr cause, f2ptr widget, f2ptr remove_widget);
+
+
+// expose_event
+
+f2ptr raw__gtk__expose_event__signal_connect(f2ptr cause, f2ptr widget, f2ptr funk, f2ptr args);
+f2ptr f2__gtk__expose_event__signal_connect(f2ptr cause, f2ptr widget, f2ptr funk, f2ptr args);
+
+
+// works for 'clicked' event but not 'expose_event'
+
+f2ptr raw__gtk__signal_connect(f2ptr cause, f2ptr widget, f2ptr signal_name, f2ptr funk, f2ptr args);
+f2ptr f2__gtk__signal_connect(f2ptr cause, f2ptr widget, f2ptr signal_name, f2ptr funk, f2ptr args);
+
+
+// widget
+
+f2ptr raw__gtk__widget__show_all(f2ptr cause, f2ptr widget);
+f2ptr f2__gtk__widget__show_all(f2ptr cause, f2ptr widget);
+f2ptr raw__gtk__widget__hide_all(f2ptr cause, f2ptr widget);
+f2ptr f2__gtk__widget__hide_all(f2ptr cause, f2ptr widget);
+f2ptr raw__gtk__widget__set_size_request(f2ptr cause, f2ptr widget, f2ptr width, f2ptr height);
+f2ptr f2__gtk__widget__set_size_request(f2ptr cause, f2ptr widget, f2ptr width, f2ptr height);
+f2ptr raw__gtk__widget__get_visible(f2ptr cause, f2ptr widget);
+f2ptr f2__gtk__widget__get_visible(f2ptr cause, f2ptr widget);
+f2ptr raw__gtk__widget__destroy(f2ptr cause, f2ptr widget);
+f2ptr raw__gtk__widget__connect_hide_on_delete(f2ptr cause, f2ptr widget);
+f2ptr f2__gtk__widget__connect_hide_on_delete(f2ptr cause, f2ptr widget);
+
+
+// beginning of GtkWidget drawing fuctions, which are not really GtkWidget functions in the GTK library.
+
+f2ptr raw__gtk__widget__queue_draw_area(f2ptr cause, f2ptr widget, f2ptr x, f2ptr y, f2ptr width, f2ptr height);
+f2ptr f2__gtk__widget__queue_draw_area(f2ptr cause, f2ptr widget, f2ptr x, f2ptr y, f2ptr width, f2ptr height);
+f2ptr raw__gtk__widget__draw_arc(f2ptr cause, f2ptr widget, f2ptr filled, f2ptr x, f2ptr y, f2ptr width, f2ptr height, f2ptr angle1, f2ptr angle2);
+f2ptr f2__gtk__widget__draw_arc(f2ptr cause, f2ptr widget, f2ptr filled, f2ptr x, f2ptr y, f2ptr width, f2ptr height, f2ptr angle1, f2ptr angle2);
+f2ptr raw__gtk__widget__draw_rectangle(f2ptr cause, f2ptr widget, f2ptr filled, f2ptr x, f2ptr y, f2ptr width, f2ptr height);
+f2ptr f2__gtk__widget__draw_rectangle(f2ptr cause, f2ptr widget, f2ptr filled, f2ptr x, f2ptr y, f2ptr width, f2ptr height);
+
+
+// misc (GtkMisc)
+
+f2ptr raw__gtk__misc__set_alignment(f2ptr cause, f2ptr misc, f2ptr xalign, f2ptr yalign);
+f2ptr f2__gtk__misc__set_alignment(f2ptr cause, f2ptr misc, f2ptr xalign, f2ptr yalign);
+
+
+// box
+
+f2ptr raw__gtk__box__pack_start(f2ptr cause, f2ptr widget, f2ptr child_widget, f2ptr expand, f2ptr fill, f2ptr padding);
+f2ptr f2__gtk__box__pack_start(f2ptr cause, f2ptr box, f2ptr child_widget, f2ptr expand, f2ptr fill, f2ptr padding);
+
+  // text_buffer
+
+f2ptr raw__gtk__text_buffer__get_start_iter(f2ptr cause, f2ptr text_buffer);
+f2ptr f2__gtk__text_buffer__get_start_iter(f2ptr cause, f2ptr text_buffer);
+f2ptr raw__gtk__text_buffer__select_range(f2ptr cause, f2ptr text_buffer, f2ptr range);
+f2ptr f2__gtk__text_buffer__select_range(f2ptr cause, f2ptr text_buffer, f2ptr range);
+f2ptr raw__gtk__text_buffer__get_text(f2ptr cause, f2ptr text_buffer);
+f2ptr f2__gtk__text_buffer__get_text(f2ptr cause, f2ptr text_buffer);
+f2ptr raw__gtk__text_buffer__set_text(f2ptr cause, f2ptr text_buffer, f2ptr text);
+f2ptr f2__gtk__text_buffer__set_text(f2ptr cause, f2ptr text_buffer, f2ptr text);
+f2ptr raw__gtk__text_iter__forward_search(f2ptr cause, f2ptr text_iter, f2ptr text);
+f2ptr f2__gtk__text_iter__forward_search(f2ptr cause, f2ptr text_iter, f2ptr text);
+
+
+// paned
+
+f2ptr raw__gtk__paned__pack1(f2ptr cause, f2ptr paned, f2ptr child, f2ptr resize, f2ptr shrink);
+f2ptr f2__gtk__paned__pack1(f2ptr cause, f2ptr paned, f2ptr child, f2ptr resize, f2ptr shrink);
+f2ptr raw__gtk__paned__pack2(f2ptr cause, f2ptr paned, f2ptr child, f2ptr resize, f2ptr shrink);
+f2ptr f2__gtk__paned__pack2(f2ptr cause, f2ptr paned, f2ptr child, f2ptr resize, f2ptr shrink);
+f2ptr raw__gtk__paned__set_position(f2ptr cause, f2ptr paned, f2ptr position);
+f2ptr f2__gtk__paned__set_position(f2ptr cause, f2ptr paned, f2ptr position);
+
+
+// vpaned
+
+f2ptr raw__gtk__vpaned__new(f2ptr cause);
+f2ptr f2__gtk__vpaned__new(f2ptr cause);
+
+
+// hpaned
+
+f2ptr raw__gtk__hpaned__new(f2ptr cause);
+f2ptr f2__gtk__hpaned__new(f2ptr cause);
+
+
+// progress_bar
+
+f2ptr raw__gtk__progress_bar__new(f2ptr cause);
+f2ptr f2__gtk__progress_bar__new(f2ptr cause);
+f2ptr raw__gtk__progress_bar__set_fraction(f2ptr cause, f2ptr this, f2ptr fraction);
+f2ptr f2__gtk__progress_bar__set_fraction(f2ptr cause, f2ptr this, f2ptr fraction);
+f2ptr raw__gtk__progress_bar__set_text(f2ptr cause, f2ptr this, f2ptr text);
+f2ptr f2__gtk__progress_bar__set_text(f2ptr cause, f2ptr this, f2ptr text);
+f2ptr raw__gtk__progress_bar__set_orientation(f2ptr cause, f2ptr this, f2ptr orientation);
+f2ptr f2__gtk__progress_bar__set_orientation(f2ptr cause, f2ptr this, f2ptr orientation);
+f2ptr raw__gtk__progress_bar__pulse(f2ptr cause, f2ptr this);
+f2ptr f2__gtk__progress_bar__pulse(f2ptr cause, f2ptr this);
+f2ptr raw__gtk__progress_bar__set_pulse_step(f2ptr cause, f2ptr this, f2ptr fraction);
+f2ptr f2__gtk__progress_bar__set_pulse_step(f2ptr cause, f2ptr this, f2ptr fraction);
+
+
+// notebook
+
+f2ptr raw__gtk__notebook__new(f2ptr cause);
+f2ptr f2__gtk__notebook__new(f2ptr cause);
+f2ptr raw__gtk__notebook__append_page(f2ptr cause, f2ptr notebook, f2ptr child, f2ptr tab_label);
+f2ptr f2__gtk__notebook__append_page(f2ptr cause, f2ptr notebook, f2ptr child, f2ptr tab_label);
+f2ptr raw__gtk__notebook__prepend_page(f2ptr cause, f2ptr notebook, f2ptr child, f2ptr tab_label);
+f2ptr f2__gtk__notebook__prepend_page(f2ptr cause, f2ptr notebook, f2ptr child, f2ptr tab_label);
+f2ptr raw__gtk__notebook__insert_page(f2ptr cause, f2ptr notebook, f2ptr child, f2ptr tab_label, f2ptr position);
+f2ptr f2__gtk__notebook__insert_page(f2ptr cause, f2ptr notebook, f2ptr child, f2ptr tab_label, f2ptr position);
+f2ptr raw__gtk__notebook__remove_page(f2ptr cause, f2ptr notebook, f2ptr position);
+f2ptr f2__gtk__notebook__remove_page(f2ptr cause, f2ptr notebook, f2ptr position);
+f2ptr raw__gtk__notebook__get_current_page(f2ptr cause, f2ptr notebook);
+f2ptr f2__gtk__notebook__get_current_page(f2ptr cause, f2ptr notebook);
+f2ptr raw__gtk__notebook__set_scrollable(f2ptr cause, f2ptr notebook, f2ptr scrollable);
+f2ptr f2__gtk__notebook__set_scrollable(f2ptr cause, f2ptr notebook, f2ptr scrollable);
+
+
+// label
+
+f2ptr raw__gtk__label__new(f2ptr cause, f2ptr text);
+f2ptr f2__gtk__label__new(f2ptr cause, f2ptr text);
+f2ptr raw__gtk__label__set_text(f2ptr cause, f2ptr label, f2ptr text);
+f2ptr f2__gtk__label__set_text(f2ptr cause, f2ptr label, f2ptr text);
+f2ptr raw__gtk__label__set_selectable(f2ptr cause, f2ptr label, f2ptr selectable);
+f2ptr f2__gtk__label__set_selectable(f2ptr cause, f2ptr label, f2ptr selectable);
+
+
+// drawing_area
+
+f2ptr raw__gtk__drawing_area__new(f2ptr cause);
+f2ptr f2__gtk__drawing_area__new(f2ptr cause);
+
+
+// table
+
+f2ptr raw__gtk__table__new(f2ptr cause, f2ptr rows, f2ptr columns, f2ptr homogenous);
+f2ptr f2__gtk__table__new(f2ptr cause, f2ptr rows, f2ptr columns, f2ptr homogenous);
+f2ptr raw__gtk__table__attach(f2ptr cause, f2ptr table, f2ptr child, f2ptr left_attach, f2ptr right_attach, f2ptr top_attach, f2ptr bottom_attach, f2ptr xpadding, f2ptr ypadding);
+f2ptr f2__gtk__table__attach(f2ptr cause, f2ptr table, f2ptr child, f2ptr left_attach, f2ptr right_attach, f2ptr top_attach, f2ptr bottom_attach, f2ptr xpadding, f2ptr ypadding);
+
+
+// frame
+
+f2ptr raw__gtk__frame__new(f2ptr cause, f2ptr label);
+f2ptr f2__gtk__frame__new(f2ptr cause, f2ptr label);
+
+
+// menu_bar
+
+f2ptr raw__gtk__menu_bar__new(f2ptr cause);
+f2ptr f2__gtk__menu_bar__new(f2ptr cause);
+f2ptr raw__gtk__menu_bar__append(f2ptr cause, f2ptr menu_bar, f2ptr append_widget);
+f2ptr f2__gtk__menu_bar__append(f2ptr cause, f2ptr menu_bar, f2ptr append_widget);
+
+
+// menu_item
+
+f2ptr raw__gtk__menu_item__new(f2ptr cause, f2ptr label);
+f2ptr f2__gtk__menu_item__new(f2ptr cause, f2ptr label);
+f2ptr raw__gtk__menu_item__set_submenu(f2ptr cause, f2ptr widget, f2ptr submenu);
+f2ptr f2__gtk__menu_item__set_submenu(f2ptr cause, f2ptr widget, f2ptr submenu);
+
+
+// menu
+
+f2ptr raw__gtk__menu__new(f2ptr cause);
+f2ptr f2__gtk__menu__new(f2ptr cause);
+f2ptr raw__gtk__menu__append(f2ptr cause, f2ptr menu, f2ptr append_widget);
+f2ptr f2__gtk__menu__append(f2ptr cause, f2ptr menu, f2ptr append_widget);
+
+
 // **
 
 void f2__gtk__initialize();

0ec4cbf653d3d00e69312989d03c2f33fa818b02
Bo Morgan <bo@mit.edu>
2010-09-08 17:44:46 -0400

    adding f2_terminal_print.[ch]

--- c/f2_frame_objects.h
+++ c/f2_frame_objects.h
@@ -464,75 +464,75 @@
   def_pcfunk17(frame_object__funk__pcfunkvar(name, new), slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14, slot_15, slot_16, slot_17, return f2__frame_object__funk__funkvar(name, new)(this_cause, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14, slot_15, slot_16, slot_17))
   
 
-#define def_f2__frame__object__new__0_slot(name, body)			\
+#define def_f2__frame__object__new__0_slot(name)			\
   def_f2__frame__object__new__0_slot__macro(name,			\
 					    def_f2__frame__object__new__0_slot__body())
   
-#define def_f2__frame__object__new__1_slot(name, slot_1, body)		\
+#define def_f2__frame__object__new__1_slot(name, slot_1)		\
   def_f2__frame__object__new__1_slot__macro(name, slot_1,		\
 					    def_f2__frame__object__new__1_slot__body(slot_1))
   
-#define def_f2__frame__object__new__2_slot(name, slot_1, slot_2, body)	\
+#define def_f2__frame__object__new__2_slot(name, slot_1, slot_2)	\
   def_f2__frame__object__new__2_slot__macro(name, slot_1, slot_2,	\
 					    def_f2__frame__object__new__2_slot__body(slot_1, slot_2))
   
-#define def_f2__frame__object__new__3_slot(name, slot_1, slot_2, slot_3, body) \
+#define def_f2__frame__object__new__3_slot(name, slot_1, slot_2, slot_3) \
   def_f2__frame__object__new__3_slot__macro(name, slot_1, slot_2, slot_3, \
 					    def_f2__frame__object__new__3_slot__body(slot_1, slot_2, slot_3))
   
-#define def_f2__frame__object__new__4_slot(name, slot_1, slot_2, slot_3, slot_4, body) \
+#define def_f2__frame__object__new__4_slot(name, slot_1, slot_2, slot_3, slot_4) \
   def_f2__frame__object__new__4_slot__macro(name, slot_1, slot_2, slot_3, slot_4,  \
 					    def_f2__frame__object__new__4_slot__body(slot_1, slot_2, slot_3, slot_4))
   
-#define def_f2__frame__object__new__5_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, body) \
+#define def_f2__frame__object__new__5_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5) \
   def_f2__frame__object__new__5_slot__macro(name, slot_1, slot_2, slot_3, slot_4, slot_5, \
 					    def_f2__frame__object__new__5_slot__body(slot_1, slot_2, slot_3, slot_4, slot_5))
   
-#define def_f2__frame__object__new__6_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, body) \
+#define def_f2__frame__object__new__6_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6) \
   def_f2__frame__object__new__6_slot__macro(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, \
 					    def_f2__frame__object__new__6_slot__body(slot_1, slot_2, slot_3, slot_4, slot_5, slot_6))
   
-#define def_f2__frame__object__new__7_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, body) \
+#define def_f2__frame__object__new__7_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7) \
   def_f2__frame__object__new__7_slot__macro(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7,  \
 					    def_f2__frame__object__new__7_slot__body(slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7))
   
-#define def_f2__frame__object__new__8_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, body) \
+#define def_f2__frame__object__new__8_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8) \
   def_f2__frame__object__new__8_slot__macro(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, \
 					    def_f2__frame__object__new__8_slot__body(slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8))
   
-#define def_f2__frame__object__new__9_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, body) \
+#define def_f2__frame__object__new__9_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9) \
   def_f2__frame__object__new__9_slot__macro(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9,  \
 					    def_f2__frame__object__new__9_slot__body(slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9))
   
-#define def_f2__frame__object__new__10_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, body) \
+#define def_f2__frame__object__new__10_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10) \
   def_f2__frame__object__new__10_slot__macro(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, \
 					     def_f2__frame__object__new__10_slot__body(slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10))
   
-#define def_f2__frame__object__new__11_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, body) \
+#define def_f2__frame__object__new__11_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11) \
   def_f2__frame__object__new__11_slot__macro(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, \
 					     def_f2__frame__object__new__11_slot__body(slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11))
   
-#define def_f2__frame__object__new__12_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, body) \
+#define def_f2__frame__object__new__12_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12) \
   def_f2__frame__object__new__12_slot__macro(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12,  \
 					     def_f2__frame__object__new__12_slot__body(slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12))
   
-#define def_f2__frame__object__new__13_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, body) \
+#define def_f2__frame__object__new__13_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13) \
   def_f2__frame__object__new__13_slot__macro(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, \
 					     def_f2__frame__object__new__13_slot__body(slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13))
   
-#define def_f2__frame__object__new__14_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14, body) \
+#define def_f2__frame__object__new__14_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14) \
   def_f2__frame__object__new__14_slot__macro(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14,	\
 					     def_f2__frame__object__new__14_slot__body(slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14))
   
-#define def_f2__frame__object__new__15_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14, slot_15, body) \
+#define def_f2__frame__object__new__15_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14, slot_15) \
   def_f2__frame__object__new__15_slot__macro(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14,	slot_15,  \
 					     def_f2__frame__object__new__15_slot__body(slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14, slot_15))
   
-#define def_f2__frame__object__new__16_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14, slot_15, slot_16, body) \
+#define def_f2__frame__object__new__16_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14, slot_15, slot_16) \
   def_f2__frame__object__new__16_slot__macro(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14,	slot_15, slot_16, \
 					     def_f2__frame__object__new__16_slot__body(slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14, slot_15, slot_16))
   
-#define def_f2__frame__object__new__17_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14, slot_15, slot_16, slot_17, body) \
+#define def_f2__frame__object__new__17_slot(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14, slot_15, slot_16, slot_17) \
   def_f2__frame__object__new__17_slot__macro(name, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14,	slot_15, slot_16, slot_17, \
 					     def_f2__frame__object__new__17_slot__body(slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10, slot_11, slot_12, slot_13, slot_14, slot_15, slot_16, slot_17))
   

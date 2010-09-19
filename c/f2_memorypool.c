dbf87c9946f2b552ca0f222e6b80d2d64896d2cd
Bo Morgan <bo@mit.edu>
2010-09-19 13:49:34 -0400

    working on terminal_print

--- c/f2_memorypool.c
+++ c/f2_memorypool.c
@@ -245,7 +245,8 @@ u8 funk2_memorypool__defragment_free_memory_blocks_in_place(funk2_memorypool_t* 
 	funk2_memblock_t* iter                     = (funk2_memblock_t*)(((u8*)segment_first_free_block) + funk2_memblock__byte_num(((funk2_memblock_t*)segment_first_free_block)));
 	while ((iter < end_of_blocks) && (! (iter->used))) {
 	  if (funk2_memblock__byte_num(iter) == 0) {
-	    error(nil, "funk2_memorypool__defragment_free_memory_blocks_in_place error: memblock__byte_num = 0.");
+	    status(nil, "funk2_memorypool__defragment_free_memory_blocks_in_place WARNING: memblock__byte_num = 0.");
+	    break;
 	  }
 	  funk2_memblock_t* next = (funk2_memblock_t*)(((u8*)iter) + funk2_memblock__byte_num(iter));
 	  if (funk2_hash__contains(&blocks_to_defragment, (u64)iter)) {

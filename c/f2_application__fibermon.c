8a8144fef3f7bf0209031e15b6f24bfd129f4671
Bo Morgan <bo@mit.edu>
2010-09-19 15:07:15 -0400

    working on fibermon

--- c/f2_application__fibermon.c
+++ c/f2_application__fibermon.c
@@ -179,12 +179,16 @@ f2ptr f2__fibermon_fiber__redraw_fast(f2ptr cause, f2ptr this) {
 			     f2__stringlist__concat(cause, f2list2__new(cause, f2__exp__as__string(cause, f2__number__multiplied_by(cause, ((this__execution_efficiency != nil) ? this__execution_efficiency : f2integer__new(cause, 0)), f2double__new(cause, 100.0))),
 									new__string(cause, "%"))));
   }
-  // paint menu_bar as pink if fiber has locked-up since last redraw.
-  if ((this__bytecodes_per_second != nil) &&
-      (f2__number__is_numerically_equal_to(cause, this__bytecodes_per_second, f2integer__new(cause, 0)) != nil) &&
-      raw__mutex__is_locked(cause, f2__fiber__execute_mutex(cause, this__fiber))) {
+  if        ((this__bytecodes_per_second != nil) &&
+	     (f2__number__is_numerically_equal_to(cause, this__bytecodes_per_second, f2integer__new(cause, 0)) != nil) &&
+	     raw__mutex__is_locked(cause, f2__fiber__execute_mutex(cause, this__fiber))) {
+    // paint menu_bar as pink if fiber has locked-up since last redraw.
     f2__gtk__widget__modify_bg(cause, this__menu_bar, new__symbol(cause, "normal"), f2__gdk__rgb_color__new(cause, f2double__new(cause, 1.0), f2double__new(cause, 0.75), f2double__new(cause, 0.75)));
+  } else if (f2fiber__sleep_until_time(fiber, cause) != nil) {
+    // paint menu_bar as black if fiber is asleep.
+    f2__gtk__widget__modify_bg(cause, this__menu_bar, new__symbol(cause, "normal"), f2__gdk__rgb_color__new(cause, f2double__new(cause, 0.0), f2double__new(cause, 0.0), f2double__new(cause, 0.0)));
   } else {
+    // paint menu_bar as the default theme color if fiber is in none of the above states.
     f2__gtk__widget__modify_bg(cause, this__menu_bar, new__symbol(cause, "normal"), nil);
   }
   return nil;

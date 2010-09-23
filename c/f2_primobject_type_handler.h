91ed80b97562136712d5a93ae4edd6203d2013bd
Bo Morgan <bo@mit.edu>
2010-09-23 10:47:33 -0400

    adding c/f2_natural_language.[ch]

--- c/f2_primobject_type_handler.h
+++ c/f2_primobject_type_handler.h
@@ -152,7 +152,7 @@ struct funk2_primobject_object_types_s {
   // frame objects
   funk2_object_type__terminal_print_frame__slot_t             primobject_type_terminal_print_frame;
   funk2_object_type__knowledge__slot_t                        primobject_type_knowledge;
-  funk2_object_type__natural_language__slot_t                 primobject_type_natural_language;
+  funk2_object_type__sentence__slot_t                         primobject_type_sentence;
 };
 
 // **

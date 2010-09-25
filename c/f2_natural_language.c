22a56af793b81f8ef18c509952195579ec9eb57e
Bo Morgan <bo@mit.edu>
2010-09-25 13:23:01 -0400

    adding noun word

--- c/f2_natural_language.c
+++ c/f2_natural_language.c
@@ -264,6 +264,23 @@ f2ptr f2ordinal_word__primobject_type__new_aux(f2ptr cause) {
 }
 
 
+// preposition_word
+
+def_frame_object__global__3_slot(preposition_word, symbol, combination_first, combination_second);
+
+f2ptr f2__preposition_word__new(f2ptr cause, f2ptr symbol, f2ptr combination_first, f2ptr combination_second) {
+  return f2preposition_word__new(cause, symbol, combination_first, combination_second);
+}
+def_pcfunk3(preposition_word__new, symbol, combination_first, combination_second, return f2__preposition_word__new(this_cause, symbol, combination_first, combination_second));
+
+
+f2ptr f2preposition_word__primobject_type__new_aux(f2ptr cause) {
+  f2ptr this = f2preposition_word__primobject_type__new(cause);
+  {char* slot_name = "new"; f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.execute__symbol, new__symbol(cause, slot_name), __funk2.globalenv.object_type.primobject.primobject_type_preposition_word.new__funk);}
+  return this;
+}
+
+
 
 
 // **
@@ -391,6 +408,14 @@ void f2__natural_language__initialize() {
   
   {char* symbol_str = "new"; __funk2.globalenv.object_type.primobject.primobject_type_ordinal_word.new__symbol = f2symbol__new(cause, strlen(symbol_str), (u8*)symbol_str);}
   {f2__primcfunk__init__with_c_cfunk_var__1_arg(ordinal_word__new, symbol, cfunk, 0, "");  __funk2.globalenv.object_type.primobject.primobject_type_ordinal_word.new__funk = never_gc(cfunk);}
+  
+  
+  // preposition_word
+  
+  init_frame_object__3_slot(preposition_word, symbol, combination_first, combination_second);
+  
+  {char* symbol_str = "new"; __funk2.globalenv.object_type.primobject.primobject_type_preposition_word.new__symbol = f2symbol__new(cause, strlen(symbol_str), (u8*)symbol_str);}
+  {f2__primcfunk__init__with_c_cfunk_var__3_arg(preposition_word__new, symbol, combination_first, combination_second, cfunk, 0, "");  __funk2.globalenv.object_type.primobject.primobject_type_preposition_word.new__funk = never_gc(cfunk);}
   
   
 }
--- c/f2_natural_language.h
+++ c/f2_natural_language.h
@@ -148,6 +148,15 @@ struct funk2_object_type__ordinal_word__slot_s {
 };
 
 
+// preposition_word
+
+typedef struct funk2_object_type__preposition_word__slot_s funk2_object_type__preposition_word__slot_t;
+struct funk2_object_type__preposition_word__slot_s {
+  f2ptr new__symbol;
+  f2ptr new__funk;
+};
+
+
 #endif // F2__NATURAL_LANGUAGE__TYPES__H
 
 #ifndef F2__NATURAL_LANGUAGE__H
@@ -251,6 +260,13 @@ f2ptr f2number_determiner_word__primobject_type__new_aux(f2ptr cause);
 declare_frame_object_1_slot(ordinal_word, symbol);
 
 f2ptr f2ordinal_word__primobject_type__new_aux(f2ptr cause);
+
+
+// preposition_word
+
+declare_frame_object_3_slot(preposition_word, symbol, combination_first, combination_second);
+
+f2ptr f2preposition_word__primobject_type__new_aux(f2ptr cause);
 
 
 // **

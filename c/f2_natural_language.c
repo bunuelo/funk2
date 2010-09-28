507b661c6dc0e8eb3b7018ca4c8a38d5a7b6d44e
Bo Morgan <bo@mit.edu>
2010-09-28 15:33:09 -0400

    working on natural_language

--- c/f2_natural_language.c
+++ c/f2_natural_language.c
@@ -157,7 +157,7 @@ f2ptr f2adjective_group__primobject_type__new_aux(f2ptr cause) {
 
 // verb_group
 
-def_frame_object__global__11_slot(verb_group, symbols, imperative, en, ing, to, todel, tense, be, passive, negative, not_only_auxiliary);
+def_frame_object__global__12_slot(verb_group, symbols, imperative, en, ing, to, todel, tense, be, passive, negative, auxiliary, not_only_auxiliary);
 
 f2ptr f2__verb_group__new(f2ptr cause) {
   f2ptr symbols            = nil;
@@ -170,8 +170,9 @@ f2ptr f2__verb_group__new(f2ptr cause) {
   f2ptr be                 = nil;
   f2ptr passive            = nil;
   f2ptr negative           = nil;
+  f2ptr auxiliary          = nil;
   f2ptr not_only_auxiliary = nil;
-  return f2verb_group__new(cause, symbols, imperative, en, ing, to, todel, tense, be, passive, negative, not_only_auxiliary);
+  return f2verb_group__new(cause, symbols, imperative, en, ing, to, todel, tense, be, passive, negative, auxiliary, not_only_auxiliary);
 }
 def_pcfunk0(verb_group__new, return f2__verb_group__new(this_cause));
 
@@ -610,7 +611,7 @@ void f2__natural_language__initialize() {
   
   // verb_group
   
-  init_frame_object__11_slot(verb_group, symbols, imperative, en, ing, to, todel, tense, be, passive, negative, not_only_auxiliary);
+  init_frame_object__12_slot(verb_group, symbols, imperative, en, ing, to, todel, tense, be, passive, negative, auxiliary, not_only_auxiliary);
   
   {char* symbol_str = "new"; __funk2.globalenv.object_type.primobject.primobject_type_verb_group.new__symbol = f2symbol__new(cause, strlen(symbol_str), (u8*)symbol_str);}
   {f2__primcfunk__init__with_c_cfunk_var__0_arg(verb_group__new, cfunk, 0, ""); __funk2.globalenv.object_type.primobject.primobject_type_verb_group.new__funk = never_gc(cfunk);}

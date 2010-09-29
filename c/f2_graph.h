c0b5d8c14d5e2acbfc9e2504124a5a930f70683a
Bo Morgan <bo@mit.edu>
2010-09-29 13:14:45 -0400

    changing bruno_graph to graph

--- c/f2_graph.h
+++ c/f2_graph.h
@@ -32,7 +32,10 @@ declare_object_type_3_slot(graph_edge, label, left_node, right_node, );
 
 // graph
 typedef struct funk2_object_type__graph__slot_s funk2_object_type__graph__slot_t;
-declare_object_type_5_slot(graph, node_set, edge_set, nodes_label_hash, edges_label_hash_right_node_hash_left_node_hash, edges_label_hash_left_node_hash_right_node_hash, );
+declare_object_type_5_slot(graph, node_set, edge_set, nodes_label_hash, edges_label_hash_right_node_hash_left_node_hash, edges_label_hash_left_node_hash_right_node_hash,
+			   f2ptr terminal_print_with_frame__symbol;
+			   f2ptr terminal_print_with_frame__funk;
+			   );
 
 // graph_isomorphism
 typedef struct funk2_object_type__graph_isomorphism__slot_s funk2_object_type__graph_isomorphism__slot_t;

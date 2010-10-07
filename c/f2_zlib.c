999a325c7b9d2a11441ebc44f8c3615a989a4857
Bo Morgan <bo@mit.edu>
2010-10-07 18:48:36 -0400

    fixing compressing/decompression/saving/loading memorypool images larger than 32 bits

--- c/f2_zlib.c
+++ c/f2_zlib.c
@@ -26,6 +26,68 @@
 
 
 
+/* Compress from file source to file dest until EOF on source.
+   def() returns Z_OK on success, Z_MEM_ERROR if memory could not be
+   allocated for processing, Z_STREAM_ERROR if an invalid compression
+   level is supplied, Z_VERSION_ERROR if the version of zlib.h and the
+   version of the library linked do not match, or Z_ERRNO if there is
+   an error reading or writing the files. */
+int def(FILE *source, FILE *dest, int level)
+{
+    int ret, flush;
+    unsigned have;
+    z_stream strm;
+    unsigned char in[CHUNK];
+    unsigned char out[CHUNK];
+
+    /* allocate deflate state */
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+    ret = deflateInit(&strm, level);
+    if (ret != Z_OK)
+        return ret;
+
+    /* compress until end of file */
+    do {
+        strm.avail_in = fread(in, 1, CHUNK, source);
+        if (ferror(source)) {
+            (void)deflateEnd(&strm);
+            return Z_ERRNO;
+        }
+        flush = feof(source) ? Z_FINISH : Z_NO_FLUSH;
+        strm.next_in = in;
+
+        /* run deflate() on input until output buffer not full, finish
+           compression if all of source has been read in */
+        do {
+            strm.avail_out = CHUNK;
+            strm.next_out = out;
+            ret = deflate(&strm, flush);    /* no bad return value */
+            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+            have = CHUNK - strm.avail_out;
+            if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+                (void)deflateEnd(&strm);
+                return Z_ERRNO;
+            }
+        } while (strm.avail_out == 0);
+        assert(strm.avail_in == 0);     /* all input will be used */
+
+        /* done when last data in file processed */
+    } while (flush != Z_FINISH);
+    assert(ret == Z_STREAM_END);        /* stream will be complete */
+
+    /* clean up and return */
+    (void)deflateEnd(&strm);
+    return Z_OK;
+}
+
+
+
+
+
+
+
 // *dest_length returns length of src_data after compression.
 // dest_data can be NULL.
 boolean_t zlib__deflate(u8* dest_data, u64* dest_length, u8* src_data, u64 src_length) {
@@ -58,18 +120,22 @@ boolean_t zlib__deflate(u8* dest_data, u64* dest_length, u8* src_data, u64 src_l
     
     zlib_stream.next_in = src_data + src_index;
     
-    do {
-      zlib_stream.avail_out = ZLIB_CHUNK;
-      zlib_stream.next_out = out_buffer;
-      zlib_result = deflate(&zlib_stream, Z_FINISH);
-      assert(zlib_result != Z_STREAM_ERROR);
-      byte_num = ZLIB_CHUNK - zlib_stream.avail_out;
-      if (dest_data) {
-	memcpy(dest_data + dest_index, out_buffer, byte_num);
-      }
-      dest_index += byte_num;
-    } while (zlib_stream.avail_out == 0);
-    assert(zlib_stream.avail_in == 0); // all input used
+    {
+      u64 out_used = 0;
+      do {
+	zlib_stream.avail_out = ZLIB_CHUNK - out_used;
+	zlib_stream.next_out  = out_buffer + out_used;
+	zlib_result = deflate(&zlib_stream, Z_FINISH);
+	assert(zlib_result != Z_STREAM_ERROR);
+	byte_num = ZLIB_CHUNK - zlib_stream.avail_out;
+	if (dest_data) {
+	  memcpy(dest_data + dest_index, out_buffer, byte_num);
+	}
+	dest_index += byte_num;
+	out_used   += byte_num;
+      } while (zlib_stream.avail_out == 0);
+      assert(zlib_stream.avail_in == 0); // all input used
+    }
     
     if (available_input > ZLIB_CHUNK) {
       available_input -= ZLIB_CHUNK;

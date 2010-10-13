5b092708dcdbef73f7a9085a7928b4b85aff78ea
Bo Morgan <bo@mit.edu>
2010-10-13 12:25:49 -0400

    working on socket

--- c/f2_reader.h
+++ c/f2_reader.h
@@ -140,6 +140,7 @@ struct funk2_reader_s {
   f2ptr char__escape_gfunkptr;
   f2ptr char__string_quote;
   f2ptr char__string_escape_newline;
+  f2ptr char__string_escape_telnet_newline;
   f2ptr char__string_escape_return;
   f2ptr char__string_escape_tab;
   f2ptr char__string_escape_backspace;

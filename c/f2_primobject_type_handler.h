0a843e6055b0e99f2466061ed5b65b4e04c402c5
Bo Morgan <bo@mit.edu>
2010-10-19 17:55:01 -0400

    working on core_extension_funk

--- c/f2_primobject_type_handler.h
+++ c/f2_primobject_type_handler.h
@@ -51,6 +51,7 @@
 #include "f2_natural_language.h"
 #include "f2_partial_order.h"
 #include "f2_dlfcn.h"
+#include "f2_core_extension.h"
 
 typedef struct funk2_primobject_type_handler_s {
   funk2_processor_mutex_t type_hash_mutex;

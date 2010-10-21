873765b006f9ac54028b5ddd03a6894f4c7e0029
Bo Morgan <bo@mit.edu>
2010-10-21 14:08:53 -0400

    working on causality

--- extension/causality/causality.c
+++ extension/causality/causality.c
@@ -26,6 +26,7 @@
 f2ptr f2__causality_type__new(f2ptr cause) {
   f2ptr type = f2__primobject_type__new(cause, f2list1__new(cause, new__symbol(cause, "frame")));
   f2__primobject_type__add_slot_type(cause, type, new__symbol(cause, "execute"), new__symbol(cause, "terminal_print_with_frame"), new__core_extension_funk(cause, CAUSALITY_DYLIB, causality__terminal_print_with_frame));
+  f2__primobject_type__add_slot_type(cause, type, new__symbol(cause, "get"),     new__symbol(cause, "cause_set"),                 new__core_extension_funk(cause, CAUSALITY_DYLIB, causality__cause_set));
   return type;
 }
 
@@ -45,12 +46,18 @@ f2ptr f2__causality__new(f2ptr cause) {
 }
 export_cefunk0(causality__new, 0, "");
 
+f2ptr f2__causality__cause_set(f2ptr cause, f2ptr this) {
+  return f2__frame__lookup_var_value(cause, this, new__symbol(cause, "cause_set"), f2larva__new(cause, 1324, nil));
+}
+export_cefunk1(causality__cause_set, this, 0, "");
+
 f2ptr f2__causality__terminal_print_with_frame(f2ptr cause, f2ptr this, f2ptr terminal_print_frame) {
   f2ptr print_as_frame_hash = raw__terminal_print_frame__print_as_frame_hash(cause, terminal_print_frame);
   f2ptr frame               = raw__ptypehash__lookup(cause, print_as_frame_hash, this);
   if (frame == nil) {
     frame = f2__frame__new(cause, f2list2__new(cause,
-					       new__symbol(cause, "print_object_type"), new__symbol(cause, "causality")));
+					       new__symbol(cause, "print_object_type"), new__symbol(cause, "causality"),
+					       new__symbol(cause, "cause_set"),         f2__causality__cause_set(cause, this)));
     f2__ptypehash__add(cause, print_as_frame_hash, this, frame);
   }
   return raw__frame__terminal_print_with_frame(cause, frame, terminal_print_frame);

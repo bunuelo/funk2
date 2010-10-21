dd218ee3279822cc1c39bc2b53d2f5976b8d0c75
Bo Morgan <bo@mit.edu>
2010-10-21 12:24:27 -0400

    working on core_extension_funk

--- c/f2_core_extension.h
+++ c/f2_core_extension.h
@@ -62,7 +62,7 @@ struct funk2_object_type__core_extension_funk__slot_s {
 #include "f2_primfunks.h"
 
 
-#define def_cefunk_define_args(name, define_correct_arg_num, define_args, body) \
+#define def_basic_cefunk_define_args(name, define_correct_arg_num, define_args, body) \
   def_pcfunk__prototype__declare(core_extension_funk__##name) {		\
     f2ptr cause            = simple_cause;				\
     u64   correct_arg_num  = define_correct_arg_num;			\
@@ -75,62 +75,122 @@ struct funk2_object_type__core_extension_funk__slot_s {
   }
 
 
-#define def_cefunk_define_arg_iter(name, arg_name)			\
+#define def_basic_cefunk_define_arg_iter(name, arg_name)			\
   if (simple_args_iter == nil) {return f2__argument_number_check_failure__larva__new(cause, new__symbol(cause, #name), correct_arg_num, simple_args);} \
   if (! raw__cons__is_type(cause, simple_args_iter)) {return f2larva__new(cause, 1, nil);} \
   f2ptr arg_name = f2cons__car(simple_args_iter, cause);		\
   simple_args_iter = f2cons__cdr(simple_args_iter, cause)
 
 
-#define def_cefunk_define_arg0_iter(name)
+#define def_basic_cefunk_define_arg0_iter(name)
 
-#define def_cefunk_define_arg1_iter(name, arg1) \
-  def_cefunk_define_arg0_iter(name);		\
-  def_cefunk_define_arg_iter(name, arg1)
+#define def_basic_cefunk_define_arg1_iter(name, arg1) \
+  def_basic_cefunk_define_arg0_iter(name);		\
+  def_basic_cefunk_define_arg_iter(name, arg1)
 
-#define def_cefunk_define_arg2_iter(name, arg1, arg2)	\
-  def_cefunk_define_arg1_iter(name, arg1);		\
-  def_cefunk_define_arg_iter(name, arg2)
+#define def_basic_cefunk_define_arg2_iter(name, arg1, arg2)	\
+  def_basic_cefunk_define_arg1_iter(name, arg1);		\
+  def_basic_cefunk_define_arg_iter(name, arg2)
 
-#define def_cefunk_define_arg3_iter(name, arg1, arg2, arg3)	\
-  def_cefunk_define_arg2_iter(name, arg1, arg2);		\
-  def_cefunk_define_arg_iter(name, arg3)
+#define def_basic_cefunk_define_arg3_iter(name, arg1, arg2, arg3)	\
+  def_basic_cefunk_define_arg2_iter(name, arg1, arg2);		\
+  def_basic_cefunk_define_arg_iter(name, arg3)
 
-#define def_cefunk_define_arg4_iter(name, arg1, arg2, arg3, arg4)	\
-  def_cefunk_define_arg3_iter(name, arg1, arg2, arg3);			\
-  def_cefunk_define_arg_iter(name, arg4)
-
-
-#define def_cefunk0(name, body)						\
-  def_cefunk_define_args(name, 0, def_cefunk_define_arg0_iter(name), body);
-
-#define def_cefunk1(name, arg1, body)					\
-  def_cefunk_define_args(name, 1, def_cefunk_define_arg1_iter(name, arg1), body);
-
-#define def_cefunk2(name, arg1, arg2, body)				\
-  def_cefunk_define_args(name, 2, def_cefunk_define_arg2_iter(name, arg1, arg2), body);
-
-#define def_cefunk3(name, arg1, arg2, arg3, body)			\
-  def_cefunk_define_args(name, 3, def_cefunk_define_arg3_iter(name, arg1, arg2, arg3), body);
-
-#define def_cefunk4(name, arg1, arg2, arg3, arg4, body)			\
-  def_cefunk_define_args(name, 4, def_cefunk_define_arg4_iter(name, arg1, arg2, arg3, arg4), body);
+#define def_basic_cefunk_define_arg4_iter(name, arg1, arg2, arg3, arg4)	\
+  def_basic_cefunk_define_arg3_iter(name, arg1, arg2, arg3);			\
+  def_basic_cefunk_define_arg_iter(name, arg4)
 
 
-#define export_cefunk0(name) def_cefunk0(name, \
-					 return f2__##name(cause));
+#define def_basic_cefunk0(name, body)						\
+  def_basic_cefunk_define_args(name, 0, def_basic_cefunk_define_arg0_iter(name), body);
 
-#define export_cefunk1(name) def_cefunk1(name, arg1, \
-					 return f2__##name(cause, arg1));
+#define def_basic_cefunk1(name, arg1, body)					\
+  def_basic_cefunk_define_args(name, 1, def_basic_cefunk_define_arg1_iter(name, arg1), body);
 
-#define export_cefunk2(name) def_cefunk2(name, arg1, arg2, \
-					 return f2__##name(cause, arg1, arg2));
+#define def_basic_cefunk2(name, arg1, arg2, body)				\
+  def_basic_cefunk_define_args(name, 2, def_basic_cefunk_define_arg2_iter(name, arg1, arg2), body);
 
-#define export_cefunk3(name) def_cefunk3(name, arg1, arg2, arg3, \
-					 return f2__##name(cause, arg1, arg2, arg3));
+#define def_basic_cefunk3(name, arg1, arg2, arg3, body)			\
+  def_basic_cefunk_define_args(name, 3, def_basic_cefunk_define_arg3_iter(name, arg1, arg2, arg3), body);
 
-#define export_cefunk4(name) def_cefunk4(name, arg1, arg2, arg3, arg4,	\
-					 return f2__##name(cause, arg1, arg2, arg3, arg4));
+#define def_basic_cefunk4(name, arg1, arg2, arg3, arg4, body)			\
+  def_basic_cefunk_define_args(name, 4, def_basic_cefunk_define_arg4_iter(name, arg1, arg2, arg3, arg4), body);
+
+
+#define export_basic_cefunk0(name) def_basic_cefunk0(name, \
+						     return f2__##name(cause));
+
+#define export_basic_cefunk1(name) def_basic_cefunk1(name, arg1, \
+						     return f2__##name(cause, arg1));
+
+#define export_basic_cefunk2(name) def_basic_cefunk2(name, arg1, arg2, \
+						     return f2__##name(cause, arg1, arg2));
+
+#define export_basic_cefunk3(name) def_basic_cefunk3(name, arg1, arg2, arg3, \
+						     return f2__##name(cause, arg1, arg2, arg3));
+
+#define export_basic_cefunk4(name) def_basic_cefunk4(name, arg1, arg2, arg3, arg4,	\
+						     return f2__##name(cause, arg1, arg2, arg3, arg4));
+
+
+
+#define def_cefunk0__args(name)						\
+  def_pcfunk__prototype__declare(core_extension_funk__##name##__args) {return nil;}
+
+#define def_cefunk1__args(name, arg1)					\
+  def_pcfunk__prototype__declare(core_extension_funk__##name##__args) {return f2list1__new(simple_cause, new__symbol(cause, #arg1));}
+
+#define def_cefunk2__args(name, arg1, arg2)				\
+  def_pcfunk__prototype__declare(core_extension_funk__##name##__args) {return f2list2__new(simple_cause, new__symbol(cause, #arg1), new__symbol(cause, #arg2));}
+
+#define def_cefunk3__args(name, arg1, arg2, arg3)				\
+  def_pcfunk__prototype__declare(core_extension_funk__##name##__args) {return f2list3__new(simple_cause, new__symbol(cause, #arg1), new__symbol(cause, #arg2), new__symbol(cause, #arg3));}
+
+#define def_cefunk4__args(name, arg1, arg2, arg3, arg4)			\
+  def_pcfunk__prototype__declare(core_extension_funk__##name##__args) {return f2list4__new(simple_cause, new__symbol(cause, #arg1), new__symbol(cause, #arg2), new__symbol(cause, #arg3), new__symbol(cause, #arg4));}
+
+
+#define def_cefunk__is_funktional(name, is_funktional)			\
+  def_pcfunk__prototype__declare(core_extension_funk__##name##__is_funktional) {return f2bool__new(cause, is_funktional);}
+
+
+#define def_cefunk__documentation(name, documentation_string)		\
+  def_pcfunk__prototype__declare(core_extension_funk__##name##__documentation) {return new__string(cause, documentation_string);}
+
+
+
+#define export_cefunk_without_args(name, is_funktional, documentation_string) \
+  export_basic_cefunk0(name##__args);				  \
+  def_cefunk__is_funktional(name, is_funktional)		  \
+  export_basic_cefunk0(name##__is_funktional);			  \
+  def_cefunk__documentation(name, documentation_string)		  \
+  export_basic_cefunk0(name##__documentation);
+
+
+#define export_cefunk0(name, is_funktional, documentation_string)	\
+  export_basic_cefunk0(name);						\
+  def_cefunk0_args(name)						\
+  export_cefunk_without_args(name, is_funktional, documentation_string)
+
+#define export_cefunk1(name, arg1, is_funktional, documentation_string)	\
+  export_basic_cefunk1(name, arg1);					\
+  def_cefunk1_args(name, arg1)						\
+  export_cefunk_without_args(name, is_funktional, documentation_string)
+
+#define export_cefunk2(name, arg1, arg2, is_funktional, documentation_string) \
+  export_basic_cefunk2(name, arg1, arg2);				\
+  def_cefunk2_args(name, arg1, arg2)					\
+  export_cefunk_without_args(name, is_funktional, documentation_string)
+
+#define export_cefunk3(name, arg1, arg2, arg3, is_funktional, documentation_string) \
+  export_basic_cefunk3(name, arg1, arg2, arg3);				\
+  def_cefunk3_args(name, arg1, arg2, arg3)				\
+  export_cefunk_without_args(name, is_funktional, documentation_string)
+
+#define export_cefunk4(name, arg1, arg2, arg3, arg4, is_funktional, documentation_string) \
+  export_basic_cefunk4(name, arg1, arg2, arg3, arg4);			\
+  def_cefunk4_args(name, arg1, arg2, arg3, arg4)			\
+  export_cefunk_without_args(name, is_funktional, documentation_string)
 
 
 declare_frame_object_6_slot(core_extension_funk, filename, name, cname, args_cname, is_funktional_cname, documentation_cname);
--- extension/fibermon/fibermon.c
+++ extension/fibermon/fibermon.c
@@ -21,11 +21,6 @@
 
 #include "../../c/funk2.h"
 
-f2ptr f2__fibermon__bytes__to_memory_string__documentation(f2ptr cause) {
-  return new__string(cause, "yay!");
-}
-export_cefunk0(fibermon__bytes__to_memory_string__documentation);
-
 f2ptr f2__fibermon__bytes__to_memory_string(f2ptr cause, f2ptr this) {
   f2ptr i = object__get_0(cause, this, "as-integer");
   if (! raw__integer__is_type(cause, i)) {
@@ -41,7 +36,7 @@ f2ptr f2__fibermon__bytes__to_memory_string(f2ptr cause, f2ptr this) {
   else if (i__i < (1024ull * 1024ull * 1024ull * 1024ull)) {return f2__stringlist__concat(cause, f2list2__new(cause, f2__exp__as__string(cause, f2double__new(cause, (double)i__i /          (1024.0 * 1024.0 * 1024.0))), new__string(cause, "G")));}
   else                                                     {return f2__stringlist__concat(cause, f2list2__new(cause, f2__exp__as__string(cause, f2double__new(cause, (double)i__i / (1024.0 * 1024.0 * 1024.0 * 1024.0))), new__string(cause, "T")));}
 }
-export_cefunk1(fibermon__bytes__to_memory_string);
+export_cefunk1(fibermon__bytes__to_memory_string, this, 0, "");
 
 
 f2ptr f2__fibermon__nanoseconds__to_time_string(f2ptr cause, f2ptr this) {
@@ -61,7 +56,7 @@ f2ptr f2__fibermon__nanoseconds__to_time_string(f2ptr cause, f2ptr this) {
   else if (i__i < (24ull * 60ull * 60ull * 1000ull * 1000ull * 1000ull)) {return f2__stringlist__concat(cause, f2list2__new(cause, f2__exp__as__string(cause, f2double__new(cause, (double)i__i /        (60.0 * 60.0 * 1000.0 * 1000.0 * 1000.0))), new__string(cause, "h")));}
   else                                                                   {return f2__stringlist__concat(cause, f2list2__new(cause, f2__exp__as__string(cause, f2double__new(cause, (double)i__i / (24.0 * 60.0 * 60.0 * 1000.0 * 1000.0 * 1000.0))), new__string(cause, "d")));}
 }
-export_cefunk1(fibermon__nanoseconds__to_time_string);
+export_cefunk1(fibermon__nanoseconds__to_time_string, this, 0, "");
 
 
 f2ptr f2__fibermon_fiber__construct_fast(f2ptr cause, f2ptr this) {
@@ -187,7 +182,7 @@ f2ptr f2__fibermon_fiber__construct_fast(f2ptr cause, f2ptr this) {
   
   return this;
 }
-export_cefunk1(fibermon_fiber__construct_fast);
+export_cefunk1(fibermon_fiber__construct_fast, this, 0, "");
 
 
 f2ptr f2__fibermon_fiber__redraw_fast(f2ptr cause, f2ptr this) {
@@ -259,7 +254,7 @@ f2ptr f2__fibermon_fiber__redraw_fast(f2ptr cause, f2ptr this) {
   }
   return nil;
 }
-export_cefunk1(fibermon_fiber__redraw_fast);
+export_cefunk1(fibermon_fiber__redraw_fast, this, 0, "");
 
 
 f2ptr f2__fibermon_fiber__recompute_statistics_fast(f2ptr cause, f2ptr this) {
@@ -290,7 +285,7 @@ f2ptr f2__fibermon_fiber__recompute_statistics_fast(f2ptr cause, f2ptr this) {
   }
   return nil;
 }
-export_cefunk1(fibermon_fiber__recompute_statistics_fast);
+export_cefunk1(fibermon_fiber__recompute_statistics_fast, this, 0, "");
 
 
 f2ptr f2__fibermon_processor__construct_fast(f2ptr cause, f2ptr this) {
@@ -327,7 +322,7 @@ f2ptr f2__fibermon_processor__construct_fast(f2ptr cause, f2ptr this) {
   f2__gtk__container__add(cause, this__frame, this__vbox);
   return nil;
 }
-export_cefunk1(fibermon_processor__construct_fast);
+export_cefunk1(fibermon_processor__construct_fast, this, 0, "");
 
 
 f2ptr f2__fibermon_processor__redraw_fast(f2ptr cause, f2ptr this) {
@@ -354,7 +349,7 @@ f2ptr f2__fibermon_processor__redraw_fast(f2ptr cause, f2ptr this) {
   }
   return nil;
 }
-export_cefunk1(fibermon_processor__redraw_fast);
+export_cefunk1(fibermon_processor__redraw_fast, this, 0, "");
 
 
 f2ptr f2__fibermon_processor__recompute_statistics_fast(f2ptr cause, f2ptr this) {
@@ -388,6 +383,6 @@ f2ptr f2__fibermon_processor__recompute_statistics_fast(f2ptr cause, f2ptr this)
   f2__frame__add_var_value(cause, this, new__symbol(cause, "total_used_memory"), this__total_used_memory);
   return nil;
 }
-export_cefunk1(fibermon_processor__recompute_statistics_fast);
+export_cefunk1(fibermon_processor__recompute_statistics_fast, this, 0, "");
 
 

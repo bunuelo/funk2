c9c186aaf635e1aea55be614ad3fb327ef36e631
Bo Morgan <bo@mit.edu>
2010-10-25 10:32:30 -0400

    fixing printing of larva to include bug

--- c/f2_ptypes.c
+++ c/f2_ptypes.c
@@ -4246,10 +4246,10 @@ f2ptr raw__larva__terminal_print_with_frame(f2ptr cause, f2ptr this, f2ptr termi
   f2ptr print_as_frame_hash = raw__terminal_print_frame__print_as_frame_hash(cause, terminal_print_frame);
   f2ptr frame               = raw__ptypehash__lookup(cause, print_as_frame_hash, this);
   if (frame == nil) {
-    frame = f2__frame__new(cause, f2list10__new(cause,
-						new__symbol(cause, "print_object_type"), new__symbol(cause, "larva"),
-						new__symbol(cause, "larva_type"),        f2__larva__larva_type(cause, this),
-						new__symbol(cause, "bug"),               f2__larva__bug(       cause, this)));
+    frame = f2__frame__new(cause, f2list6__new(cause,
+					       new__symbol(cause, "print_object_type"), new__symbol(cause, "larva"),
+					       new__symbol(cause, "larva_type"),        f2__larva__larva_type(cause, this),
+					       new__symbol(cause, "bug"),               f2__larva__bug(       cause, this)));
     f2__ptypehash__add(cause, print_as_frame_hash, this, frame);
   }
   return raw__frame__terminal_print_with_frame(cause, frame, terminal_print_frame);

7e2b47b6dc5ccd0a91a8cb504de5e66ac7805626
Bo Morgan <bo@mit.edu>
2010-11-13 19:59:29 -0500

    adding movie saving to isismon

--- extension/movie/movie.c
+++ extension/movie/movie.c
@@ -205,6 +205,9 @@ f2ptr raw__libavcodec__video_chunk__new_from_image_sequence(f2ptr cause, f2ptr i
 	    double green = ((double)raw__chunk__bit8__elt(cause, rgba_data, ((y * c->width) + x) << 2) + 1) / 255.0;
 	    double blue  = ((double)raw__chunk__bit8__elt(cause, rgba_data, ((y * c->width) + x) << 2) + 2) / 255.0;
 	    double Y = 0.299 * red + 0.587 * green + 0.114 * blue;
+	    if (x == 0) {
+	      printf("\nY=%f", Y);
+	    }
 	    picture->data[0][y * picture->linesize[0] + x] = (u8)(Y * 255.0);
 	  }
 	}
@@ -233,6 +236,9 @@ f2ptr raw__libavcodec__video_chunk__new_from_image_sequence(f2ptr cause, f2ptr i
 	    double Y = 0.299 * red + 0.587 * green + 0.114 * blue;
 	    double U = (blue - Y) * 0.565;
 	    double V = (red  - Y) * 0.713;
+	    if (ix == 0) {
+	      printf("\nU=%f, V=%f", U, V);
+	    }
 	    picture->data[1][iy * picture->linesize[1] + ix] = (u8)(127.5 + U * 255.0);
 	    picture->data[2][iy * picture->linesize[2] + ix] = (u8)(127.5 + V * 255.0);
 	  }

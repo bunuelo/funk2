4a42f2457ed563fde5add7f6f534d4cb077dd5a9
Bo Morgan <bo@mit.edu>
2010-11-17 16:05:33 -0500

    adding core extension frame_ball

--- extension/frame_ball/frame_ball.c
+++ extension/frame_ball/frame_ball.c
@@ -22,9 +22,89 @@
 #include "../../c/funk2.h"
 
 
+boolean_t raw__frame__ball_size_is_less_than_with_ptypehash(f2ptr cause, f2ptr this, f2ptr ptypehash, s64 maximum_size, s64* exact_size) {
+  if (maximum_size <= 0) {
+    return boolean__false;
+  }
+  f2ptr already_counted_this_frame = raw__ptypehash__lookup(cause, ptypehash, f2bool__new(boolean__true));
+  if (already_counted_this_frame != nil) {
+    *exact_size = 0;
+    return boolean__true;
+  }
+  raw__ptypehash__add(cause, ptypehash, this, this);
+  // frame nodes count as one.
+  s64 this_size = 1;
+  frame__iteration(cause, this, type_slot_name, slot_name, slot_value,
+		   if (raw__frame__is_type(cause, slot_value)) {
+		     s64 slot_value__exact_size;
+		     if (raw__frame__ball_size_is_less_than(cause, slot_value, ptypehash, maximum_size - this_size, &slot_value__exact_size)) {
+		       this_size += slot_value__exact_size;
+		     } else {
+		       return boolean__false;
+		     }
+		   } else {
+		     // non-frame leaf nodes count as one.
+		     this_size ++;
+		   }
+		   if (this_size >= maximum_size) {
+		     return boolean__false;
+		   }
+		   );
+  *exact_size = this_size;
+  return boolean__true;
+}
+
+
+boolean_t raw__frame__ball_size_is_less_than(f2ptr cause, f2ptr this, s64 maximum_size, s64* possibly_null_exact_size) {
+  f2ptr     ptypehash = f2__ptypehash__new(cause);
+  s64       exact_size;
+  boolean_t is_less_than = raw__frame__ball_size_is_less_than_with_ptypehash(cause, this, ptypehash, maximum_size, exact_size);
+  if (possibly_null_exact_size != NULL) {
+    *possibly_null_exact_size = exact_size;
+  }
+  return is_less_than;
+}
+
+
+#define baller_frame_height 1000
+
+boolean_t raw__frame__wishes_to_be_a_baller(f2ptr cause, f2ptr this) {
+  return raw__frame__ball_size_is_less_than(cause, this, baller_frame_height, NULL);
+}
+
+// this function is dangerous.  make sure you know this recursion won't become self-recursive (infinite).
+f2ptr raw__frame__copy_recursively_with_ptypehash(f2ptr cause, f2ptr this, f2ptr ptypehash) {
+  f2ptr previous_copy_of_this_frame = raw__ptypehash__lookup(cause, ptypehash, this);
+  if (previous_copy_of_this_frame != nil) {
+    return previous_copy_of_this_frame;
+  }
+  f2ptr frame = f2__frame__new(cause, nil);
+  raw__ptypehash__add(cause, ptypehash, this, frame);
+  frame__iteration(cause, this, type_slot_name, slot_name, slot_value,
+		   if (raw__frame__is_type(cause, slot_value)) {
+		     f2ptr slot_value_copy = raw__frame__copy_recursively_with_ptypehash(cause, slot_value, ptypehash);
+		     raw__frame__add_type_var_value(cause, frame, type_slot_name, slot_name, slot_value_copy);
+		   } else {
+		     raw__frame__add_type_var_value(cause, frame, type_slot_name, slot_name, slot_value);
+		   }
+		   );
+  return frame;
+}
+
+// this function is dangerous.  make sure you know this recursion won't become self-recursive (infinite).
+f2ptr raw__frame__copy_recursively(f2ptr cause, f2ptr this) {
+  return raw__frame__copy_recursively_with_ptypehash(cause, this, f2__ptypehash__new(cause));
+}
+
+
 // frame_ball
 
-f2ptr raw__frame_ball__new(f2ptr cause, f2ptr root_frame) {
+f2ptr raw__frame_ball__new(f2ptr cause, f2ptr user_root_frame) {
+  // we first test the size of the recursive structure is smaller (shorter) than baller_frame_height. (see Skee Lo's "I wish I was a Baller")
+  if (! raw__frame__wished_to_be_a_baller(cause, user_root_frame)) {
+    return f2larva__new(cause, 444, nil);
+  }
+  f2ptr root_frame = raw__frame__copy_recursively(cause, user_root_frame);
   return f2__frame__new(cause, f2list4__new(cause,
 					    new__symbol(cause, "type"),       new__symbol(cause, "frame_ball"),
 					    new__symbol(cause, "root_frame"), root_frame));

a1e6612a9701fa4d92a8b1d371a5967e908063d4
Bo Morgan <bo@mit.edu>
2010-11-17 06:49:59 -0500

    cleaning up graph_matching code

--- c/f2_primobject_type_handler.h
+++ c/f2_primobject_type_handler.h
@@ -149,7 +149,7 @@ struct funk2_primobject_object_types_s {
   funk2_object_type__graph_isomorphism__slot_t                primobject_type_graph_isomorphism;
   funk2_object_type__graph_decomposition_lattice_node__slot_t primobject_type_graph_decomposition_lattice_node;
   funk2_object_type__graph_decomposition_lattice__slot_t      primobject_type_graph_decomposition_lattice;
-  funk2_object_type__edit_sequence__slot_t                    primobject_type_edit_sequence;
+  funk2_object_type__graph_edit_sequence__slot_t              primobject_type_graph_edit_sequence;
   funk2_object_type__traced_mutex__slot_t                     primobject_type_traced_mutex;
   funk2_object_type__fiber_stack_trace__slot_t                primobject_type_fiber_stack_trace;
   funk2_object_type__fiber_stack_trace_block__slot_t          primobject_type_fiber_stack_trace_block;

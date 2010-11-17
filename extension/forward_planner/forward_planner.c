fd05127ab5f32c1383d48a414cdb966c55fac376
Bo Morgan <bo@mit.edu>
2010-11-17 14:44:20 -0500

    adding core extension forward_planner

--- extension/forward_planner/forward_planner.c
+++ extension/forward_planner/forward_planner.c
@@ -22,6 +22,91 @@
 #include "../../c/funk2.h"
 
 
+f2ptr raw__forward_planner__new(f2ptr cause, f2ptr action_set) {
+  return f2__frame__new(cause, f2list4__new(cause,
+					    new__symbol(cause, "type"),       new__symbol(cause, "forward_planner"),
+					    new__symbol(cause, "action_set"), action_set));
+}
+
+f2ptr f2__forward_planner__new(f2ptr cause, f2ptr action_set) {
+  if (! raw__chunk__is_type(cause, action_set)) {
+    return f2larva__new(cause, 1, nil);
+  }
+  return raw__forward_planner__new(cause, action_set);
+}
+export_cefunk1(forward_planner__new, images, 0, "Returns a new forward_planner object.");
+
+
+boolean_t raw__forward_planner__is_type(f2ptr cause, f2ptr thing) {
+  if (! raw__frame__is_type(cause, thing)) {
+    return boolean__false;
+  }
+  f2ptr this_type_name_symbol = new__symbol(cause, "forward_planner");
+  f2ptr thing_type_name       = f2__frame__lookup_var_value(cause, thing, new__symbol(cause, "type"), nil);
+  if (raw__eq(cause, this_type_name_symbol, thing_type_name)) {
+    return boolean__true;
+  }
+  f2ptr thing_type = f2__lookup_type(cause, thing_type_name);
+  if (raw__primobject_type__has_parent_type(cause, thing_type, this_type_name_symbol)) {
+    return boolean__true;
+  }
+  return boolean__false;
+}
+
+f2ptr f2__forward_planner__is_type(f2ptr cause, f2ptr thing) {
+  return f2bool__new(raw__forward_planner__is_type(cause, thing));
+}
+export_cefunk1(forward_planner__is_type, thing, 0, "Returns whether or not thing is of type forward_planner.");
+
+
+f2ptr raw__forward_planner__type(f2ptr cause, f2ptr this) {
+  return f2__object__type(cause, this);
+}
+
+f2ptr f2__forward_planner__type(f2ptr cause, f2ptr this) {
+  if (! raw__forward_planner__is_type(cause, this)) {
+    return f2larva__new(cause, 1, nil);
+  }
+  return raw__forward_planner__type(cause, this);
+}
+export_cefunk1(forward_planner__type, thing, 0, "Returns the specific type of object that this forward_planner is.");
+
+
+f2ptr raw__forward_planner__action_set(f2ptr cause, f2ptr this) {
+  return f2__frame__lookup_var_value(cause, this, new__symbol(cause, "action_set"), nil);
+}
+
+f2ptr f2__forward_planner__action_set(f2ptr cause, f2ptr this) {
+  if (! raw__forward_planner__is_type(cause, this)) {
+    return f2larva__new(cause, 1, nil);
+  }
+  return raw__forward_planner__action_set(cause, this);
+}
+export_cefunk1(forward_planner__action_set, thing, 0, "Returns the action_set of the forward_planner.");
+
+
+f2ptr raw__forward_planner__action_set__set(f2ptr cause, f2ptr this, f2ptr value) {
+  return f2__frame__add_var_value(cause, this, new__symbol(cause, "action_set"), value);
+}
+
+f2ptr f2__forward_planner__action_set__set(f2ptr cause, f2ptr this, f2ptr value) {
+  if (! raw__forward_planner__is_type(cause, this)) {
+    return f2larva__new(cause, 1, nil);
+  }
+  return raw__forward_planner__action_set__set(cause, this, value);
+}
+export_cefunk2(forward_planner__action_set__set, thing, value, 0, "Sets the action_set of the forward_planner.");
+
+
+f2ptr f2__forward_planner_type__new(f2ptr cause) {
+  f2ptr this = f2__primobject_type__new(cause, f2list1__new(cause, new__symbol(cause, "frame")));
+  {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.execute__symbol, new__symbol(cause, "new"),        f2__core_extension_funk__new(cause, new__symbol(cause, "forward_planner"), new__symbol(cause, "forward_planner__new")));}
+  {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.execute__symbol, new__symbol(cause, "is_type"),    f2__core_extension_funk__new(cause, new__symbol(cause, "forward_planner"), new__symbol(cause, "forward_planner__is_type")));}
+  {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.get__symbol,     new__symbol(cause, "type"),       f2__core_extension_funk__new(cause, new__symbol(cause, "forward_planner"), new__symbol(cause, "forward_planner__type")));}
+  {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.get__symbol,     new__symbol(cause, "action_set"), f2__core_extension_funk__new(cause, new__symbol(cause, "forward_planner"), new__symbol(cause, "forward_planner__action_set")));}
+  {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.set__symbol,     new__symbol(cause, "action_set"), f2__core_extension_funk__new(cause, new__symbol(cause, "forward_planner"), new__symbol(cause, "forward_planner__action_set__set")));}
+  return this;
+}
 
 
 // **
@@ -32,6 +117,7 @@ f2ptr f2__forward_planner__core_extension_ping(f2ptr cause) {
 export_cefunk0(forward_planner__core_extension_ping, 0, "");
 
 f2ptr f2__forward_planner__core_extension_initialize(f2ptr cause) {
+  f2__add_type(cause, new__symbol(cause, "forward_planner"), f2__forward_planner_type__new(cause));
   printf("\nforward_planner initialized."); fflush(stdout);
   return nil;
 }

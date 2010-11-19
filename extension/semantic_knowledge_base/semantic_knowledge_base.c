9a061c834ce2febfec8f5f2c864c99cbfa997bec
Bo Morgan <bo@mit.edu>
2010-11-18 21:41:45 -0500

    merging with IsisWorld isisobj branch (fixing semantic_knowledge_base first)

--- extension/semantic_knowledge_base/semantic_knowledge_base.c
+++ extension/semantic_knowledge_base/semantic_knowledge_base.c
@@ -302,7 +302,7 @@ f2ptr raw__semantic_frame__lookup(f2ptr cause, f2ptr this, f2ptr key_type, f2ptr
   f2ptr frame                = raw__semantic_frame__frame(cause, this);
   f2ptr key_type__object_key = raw__semantic_realm__object_key(cause, realm, key_type);
   f2ptr key__object_key      = raw__semantic_realm__object_key(cause, realm, key);
-  return raw__frame__lookup_type_var_value(cause, frame, key_type__object_key, key__object_key);
+  return raw__frame__lookup_type_var_value(cause, frame, key_type__object_key, key__object_key, nil);
 }
 
 f2ptr f2__semantic_frame__lookup(f2ptr cause, f2ptr this, f2ptr key_type, f2ptr key) {

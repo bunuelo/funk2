964ace82854968db7d393f3258ceb014f0de04c7
Bo Morgan <bo@mit.edu>
2010-11-30 10:03:10 -0500

    adding meta_semantic_knowledge_base core extension

--- extension/semantic_knowledge_base/semantic_knowledge_base.c
+++ extension/semantic_knowledge_base/semantic_knowledge_base.c
@@ -966,6 +966,31 @@ f2ptr f2__semantic_frame__as__graphviz_label(f2ptr cause, f2ptr this) {
 export_cefunk1(semantic_frame__as__graphviz_label, this, 0, "Simply returns the string 'semantic_frame' for rendering semantic frames in graphviz.");
 
 
+
+f2ptr raw__semantic_frame__terminal_print_with_frame(f2ptr cause, f2ptr this, f2ptr terminal_print_frame) {
+  f2ptr print_as_frame_hash = raw__terminal_print_frame__print_as_frame_hash(cause, terminal_print_frame);
+  f2ptr frame               = raw__ptypehash__lookup(cause, print_as_frame_hash, this);
+  if (frame == nil) {
+    frame = f2__frame__new(cause, f2list8__new(cause,
+					       new__symbol(cause, "print_object_type"), f2__type(cause, this),
+					       new__symbol(cause, "time"),              raw__semantic_frame__time(cause, this),
+					       new__symbol(cause, "event_type"),        raw__semantic_frame__event_type(cause, this),
+					       new__symbol(cause, "semantic_frame"),    raw__semantic_frame__semantic_frame(cause, this)));
+    f2__ptypehash__add(cause, print_as_frame_hash, this, frame);
+  }
+  return raw__frame__terminal_print_with_frame(cause, frame, terminal_print_frame);
+}
+
+f2ptr f2__semantic_frame__terminal_print_with_frame(f2ptr cause, f2ptr this, f2ptr terminal_print_frame) {
+  if (! raw__semantic_frame__is_type(cause, this)) {
+    return f2larva__new(cause, 1, nil);
+  }
+  return raw__semantic_frame__terminal_print_with_frame(cause, this, terminal_print_frame);
+}
+export_cefunk2(semantic_frame__terminal_print_with_frame, this, terminal_print_frame, 0, "");
+
+
+
 f2ptr f2__semantic_frame_type__new(f2ptr cause) {
   f2ptr this = f2__primobject_type__new(cause, f2list1__new(cause, new__symbol(cause, "frame")));
   {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.execute__symbol, new__symbol(cause, "new"),                      f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_knowledge_base"), new__symbol(cause, "semantic_frame__new")));}

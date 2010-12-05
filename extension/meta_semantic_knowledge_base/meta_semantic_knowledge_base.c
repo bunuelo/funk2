df1f99fae55634ef3cbf60fac3bfb22dda9be18b
Bo Morgan <bo@mit.edu>
2010-12-05 15:55:12 -0500

    working on as-dot_code for combinations of semantic and meta-semantic graphs.

--- extension/meta_semantic_knowledge_base/meta_semantic_knowledge_base.c
+++ extension/meta_semantic_knowledge_base/meta_semantic_knowledge_base.c
@@ -398,11 +398,12 @@ export_cefunk1(relationship_meta_semantic_object__as__graphviz_label, this, 0, "
 
 
 f2ptr raw__relationship_meta_semantic_object__as__graphviz_edge_code(f2ptr cause, f2ptr this) {
+  f2ptr edge_label           = raw__relationship_meta_semantic_object__label(cause, this);
   f2ptr semantic_frame       = raw__relationship_meta_semantic_object__source(cause, this);
   f2ptr value                = raw__relationship_meta_semantic_object__target(cause, this);
   f2ptr semantic_frame__name = f2__graphviz__exp__as__name(cause, semantic_frame);
   f2ptr value__name          = f2__graphviz__exp__as__name(cause, value);
-  return f2__graphviz__raw_labelled_edge(cause, this, semantic_frame__name, value__name);
+  return f2__graphviz__raw_labelled_edge(cause, edge_label, semantic_frame__name, value__name);
 }
 
 f2ptr f2__relationship_meta_semantic_object__as__graphviz_edge_code(f2ptr cause, f2ptr this) {

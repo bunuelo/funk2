aa52fb5a2edd1bc1f434ce999c114f25aee96937
Bo Morgan <bo@mit.edu>
2010-12-23 12:26:22 -0500

    adding lick core extension

---
+++ extension/lick/lick.h
@@ -0,0 +1,47 @@
+// 
+// Copyright (c) 2007-2010 Bo Morgan.
+// All rights reserved.
+// 
+// Author: Bo Morgan
+// 
+// Permission to use, copy, modify and distribute this software and its
+// documentation is hereby granted, provided that both the copyright
+// notice and this permission notice appear in all copies of the
+// software, derivative works or modified versions, and any portions
+// thereof, and that both notices appear in supporting documentation.
+// 
+// BO MORGAN ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS" CONDITION.
+// BO MORGAN DISCLAIMS ANY LIABILITY OF ANY KIND FOR ANY DAMAGES
+// WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+// 
+// Bo Morgan requests users of this software to return to bo@mit.edu any
+// improvements or extensions that they make and grant Bo Morgan the
+// rights to redistribute these changes.
+// 
+
+#ifndef F2__EXTENSION__LICK__H
+#define F2__EXTENSION__LICK__H
+
+#include "../../c/funk2.h"
+
+f2ptr     raw__lick__new       (f2ptr cause, f2ptr width, f2ptr height, f2ptr rgba_data);
+f2ptr      f2__lick__new       (f2ptr cause, f2ptr width, f2ptr height, f2ptr rgba_data);
+boolean_t raw__lick__is_type   (f2ptr cause, f2ptr thing);
+f2ptr      f2__lick__is_type   (f2ptr cause, f2ptr thing);
+f2ptr     raw__lick__type      (f2ptr cause, f2ptr this);
+f2ptr      f2__lick__type      (f2ptr cause, f2ptr this);
+f2ptr     raw__lick__width     (f2ptr cause, f2ptr this);
+f2ptr      f2__lick__width     (f2ptr cause, f2ptr this);
+f2ptr     raw__lick__width__set(f2ptr cause, f2ptr this, f2ptr value);
+f2ptr      f2__lick__width__set(f2ptr cause, f2ptr this, f2ptr value);
+
+f2ptr f2__lick_type__new(f2ptr cause);
+
+// **
+
+f2ptr f2__lick__core_extension_ping(f2ptr cause);
+f2ptr f2__lick__core_extension_initialize(f2ptr cause);
+f2ptr f2__lick__core_extension_destroy(f2ptr cause);
+
+#endif // F2__EXTENSION__LICK__H
+

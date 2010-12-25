ff02afbe11b9922891e79d2828e0323e9d33e758
Bo Morgan <bo@mit.edu>
2010-12-25 11:39:48 -0500

    adding lick core extension

--- extension/lick/lick.c
+++ extension/lick/lick.c
@@ -741,7 +741,7 @@ f2ptr raw__array__lick_chunk__remember_with_notes(f2ptr cause, f2ptr lick_chunk,
   {
     s64 index;
     for (index = 0; index < array__length; index ++) {
-      s64   unique_identifier = raw__chunk__bit64__elt(cause, chunk, index << 3);
+      f2ptr unique_identifier = f2integer__new(cause, raw__chunk__bit64__elt(cause, chunk, index << 3));
       f2ptr lick_note         = raw__lick_note__new(cause, unique_identifier);
       raw__array__elt__set(cause, array, index, lick_note);
     }

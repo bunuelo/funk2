98336d5f0240cfcb44bb2b4eba5a41c5764dcd74
Bo Morgan <bo@mit.edu>
2010-12-27 15:59:03 -0500

    adding equals_hash core extension

--- extension/lick/lick.h
+++ extension/lick/lick.h
@@ -24,6 +24,37 @@
 
 #include "../../c/funk2.h"
 
+
+// lick_chunk
+
+f2ptr     raw__lick_chunk__new                              (f2ptr cause, f2ptr type_name, f2ptr lick_note, f2ptr chunk);
+f2ptr      f2__lick_chunk__new                              (f2ptr cause, f2ptr type_name, f2ptr lick_note, f2ptr chunk);
+boolean_t raw__lick_chunk__is_type                          (f2ptr cause, f2ptr thing);
+f2ptr      f2__lick_chunk__is_type                          (f2ptr cause, f2ptr thing);
+f2ptr     raw__lick_chunk__type                             (f2ptr cause, f2ptr this);
+f2ptr      f2__lick_chunk__type                             (f2ptr cause, f2ptr this);
+f2ptr     raw__lick_chunk__type_name                        (f2ptr cause, f2ptr this);
+f2ptr      f2__lick_chunk__type_name                        (f2ptr cause, f2ptr this);
+f2ptr     raw__lick_chunk__type_name__set                   (f2ptr cause, f2ptr this, f2ptr value);
+f2ptr      f2__lick_chunk__type_name__set                   (f2ptr cause, f2ptr this, f2ptr value);
+f2ptr     raw__lick_chunk__lick_note                        (f2ptr cause, f2ptr this);
+f2ptr      f2__lick_chunk__lick_note                        (f2ptr cause, f2ptr this);
+f2ptr     raw__lick_chunk__lick_note__set                   (f2ptr cause, f2ptr this, f2ptr value);
+f2ptr      f2__lick_chunk__lick_note__set                   (f2ptr cause, f2ptr this, f2ptr value);
+f2ptr     raw__lick_chunk__chunk                            (f2ptr cause, f2ptr this);
+f2ptr      f2__lick_chunk__chunk                            (f2ptr cause, f2ptr this);
+f2ptr     raw__lick_chunk__chunk__set                       (f2ptr cause, f2ptr this, f2ptr value);
+f2ptr      f2__lick_chunk__chunk__set                       (f2ptr cause, f2ptr this, f2ptr value);
+f2ptr     raw__lick_chunk__unlick_with_notes                (f2ptr cause, f2ptr this, f2ptr object_note_hash);
+f2ptr      f2__lick_chunk__unlick_with_notes                (f2ptr cause, f2ptr this, f2ptr object_note_hash);
+f2ptr     raw__lick_chunk__unlick_replace_notes_with_objects(f2ptr cause, f2ptr this, f2ptr object_note_hash);
+f2ptr      f2__lick_chunk__unlick_replace_notes_with_objects(f2ptr cause, f2ptr this, f2ptr object_note_hash);
+
+f2ptr f2__lick_chunk_type__new(f2ptr cause);
+
+
+// lick
+
 f2ptr     raw__lick__new                  (f2ptr cause, f2ptr root_note, f2ptr chunk_note_hash, f2ptr current_size);
 f2ptr      f2__lick__new                  (f2ptr cause, f2ptr object);
 boolean_t raw__lick__is_type              (f2ptr cause, f2ptr thing);

6abbfe837b9a9789d37dc203a9698caac82e26ac
Bo Morgan <bo@mit.edu>
2011-01-12 19:22:41 -0500

    adding semantic_action_event core extension

--- extension/semantic_event/semantic_event.c
+++ extension/semantic_event/semantic_event.c
@@ -78,32 +78,6 @@ f2ptr f2__semantic_event__type(f2ptr cause, f2ptr this) {
 export_cefunk1(semantic_event__type, thing, 0, "Returns the specific type of object that this semantic_event is.");
 
 
-f2ptr raw__semantic_event__property__start_time__add(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "start_time"), that);
-}
-
-f2ptr f2__semantic_event__property__start_time__add(f2ptr cause, f2ptr this, f2ptr that) {
-  if (! raw__semantic_event__is_type(cause, this)) {
-    return f2larva__new(cause, 1, nil);
-  }
-  return raw__semantic_event__property__start_time__add(cause, this, that);
-}
-export_cefunk2(semantic_event__property__start_time__add, this, that, 0, "");
-
-
-f2ptr raw__semantic_event__property__start_time__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__remove(cause, this, new__symbol(cause, "property"), new__symbol(cause, "start_time"), that);
-}
-
-f2ptr f2__semantic_event__property__start_time__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  if (! raw__semantic_event__is_type(cause, this)) {
-    return f2larva__new(cause, 1, nil);
-  }
-  return raw__semantic_event__property__start_time__remove(cause, this, that);
-}
-export_cefunk2(semantic_event__property__start_time__remove, this, that, 0, "");
-
-
 f2ptr raw__semantic_event__property__action_name__add(f2ptr cause, f2ptr this, f2ptr that) {
   return raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "action_name"), that);
 }
@@ -130,43 +104,13 @@ f2ptr f2__semantic_event__property__action_name__remove(f2ptr cause, f2ptr this,
 export_cefunk2(semantic_event__property__action_name__remove, this, that, 0, "");
 
 
-f2ptr raw__semantic_event__property__result__add(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "result"), that);
-}
-
-f2ptr f2__semantic_event__property__result__add(f2ptr cause, f2ptr this, f2ptr that) {
-  if (! raw__semantic_event__is_type(cause, this)) {
-    return f2larva__new(cause, 1, nil);
-  }
-  return raw__semantic_event__property__result__add(cause, this, that);
-}
-export_cefunk2(semantic_event__property__result__add, this, that, 0, "");
-
-
-f2ptr raw__semantic_event__property__result__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__remove(cause, this, new__symbol(cause, "property"), new__symbol(cause, "result"), that);
-}
-
-f2ptr f2__semantic_event__property__result__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  if (! raw__semantic_event__is_type(cause, this)) {
-    return f2larva__new(cause, 1, nil);
-  }
-  return raw__semantic_event__property__result__remove(cause, this, that);
-}
-export_cefunk2(semantic_event__property__result__remove, this, that, 0, "");
-
-
 f2ptr f2__semantic_event_type__new(f2ptr cause) {
   f2ptr this = f2__primobject_type__new(cause, f2list1__new(cause, new__symbol(cause, "semantic_object")));
   {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.execute__symbol,     new__symbol(cause, "new"),                  f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event"), new__symbol(cause, "semantic_event__new")));}
   {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.execute__symbol,     new__symbol(cause, "is_type"),              f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event"), new__symbol(cause, "semantic_event__is_type")));}
   {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.get__symbol,         new__symbol(cause, "type"),                 f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event"), new__symbol(cause, "semantic_event__type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-add"),    new__symbol(cause, "property-start_time"),  f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event"), new__symbol(cause, "semantic_event__property__start_time__add")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-remove"), new__symbol(cause, "property-start_time"),  f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event"), new__symbol(cause, "semantic_event__property__start_time__remove")));}
   {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-add"),    new__symbol(cause, "property-action_name"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event"), new__symbol(cause, "semantic_event__property__action_name__add")));}
   {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-remove"), new__symbol(cause, "property-action_name"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event"), new__symbol(cause, "semantic_event__property__action_name__remove")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-add"),    new__symbol(cause, "property-result"),      f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event"), new__symbol(cause, "semantic_event__property__result__add")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-remove"), new__symbol(cause, "property-result"),      f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event"), new__symbol(cause, "semantic_event__property__result__remove")));}
   return this;
 }
 

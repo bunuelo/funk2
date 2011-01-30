bc9951ec7b383cf0e3f14f37bd24df7888834ca9
Bo Morgan <bo@mit.edu>
2011-01-30 13:17:16 -0500

    removing relationship- and property- prefix for semantic slot names

--- extension/semantic_object/semantic_object.c
+++ extension/semantic_object/semantic_object.c
@@ -78,67 +78,67 @@ f2ptr f2__semantic_object__type(f2ptr cause, f2ptr this) {
 export_cefunk1(semantic_object__type, thing, 0, "Returns the specific type of object that this semantic_object is.");
 
 
-f2ptr raw__semantic_object__property__noun__add(f2ptr cause, f2ptr this, f2ptr that) {
+f2ptr raw__semantic_object__noun__add(f2ptr cause, f2ptr this, f2ptr that) {
   return raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "noun"), that);
 }
 
-f2ptr f2__semantic_object__property__noun__add(f2ptr cause, f2ptr this, f2ptr that) {
+f2ptr f2__semantic_object__noun__add(f2ptr cause, f2ptr this, f2ptr that) {
   if (! raw__semantic_object__is_type(cause, this)) {
     return f2larva__new(cause, 1, nil);
   }
-  return raw__semantic_object__property__noun__add(cause, this, that);
+  return raw__semantic_object__noun__add(cause, this, that);
 }
-export_cefunk2(semantic_object__property__noun__add, this, that, 0, "");
+export_cefunk2(semantic_object__noun__add, this, that, 0, "");
 
 
-f2ptr raw__semantic_object__property__noun__remove(f2ptr cause, f2ptr this, f2ptr that) {
+f2ptr raw__semantic_object__noun__remove(f2ptr cause, f2ptr this, f2ptr that) {
   return raw__semantic_frame__remove(cause, this, new__symbol(cause, "property"), new__symbol(cause, "noun"), that);
 }
 
-f2ptr f2__semantic_object__property__noun__remove(f2ptr cause, f2ptr this, f2ptr that) {
+f2ptr f2__semantic_object__noun__remove(f2ptr cause, f2ptr this, f2ptr that) {
   if (! raw__semantic_object__is_type(cause, this)) {
     return f2larva__new(cause, 1, nil);
   }
-  return raw__semantic_object__property__noun__remove(cause, this, that);
+  return raw__semantic_object__noun__remove(cause, this, that);
 }
-export_cefunk2(semantic_object__property__noun__remove, this, that, 0, "");
+export_cefunk2(semantic_object__noun__remove, this, that, 0, "");
 
 
-f2ptr raw__semantic_object__property__classifier__add(f2ptr cause, f2ptr this, f2ptr that) {
+f2ptr raw__semantic_object__classifier__add(f2ptr cause, f2ptr this, f2ptr that) {
   return raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "classifier"), that);
 }
 
-f2ptr f2__semantic_object__property__classifier__add(f2ptr cause, f2ptr this, f2ptr that) {
+f2ptr f2__semantic_object__classifier__add(f2ptr cause, f2ptr this, f2ptr that) {
   if (! raw__semantic_object__is_type(cause, this)) {
     return f2larva__new(cause, 1, nil);
   }
-  return raw__semantic_object__property__classifier__add(cause, this, that);
+  return raw__semantic_object__classifier__add(cause, this, that);
 }
-export_cefunk2(semantic_object__property__classifier__add, this, that, 0, "");
+export_cefunk2(semantic_object__classifier__add, this, that, 0, "");
 
 
-f2ptr raw__semantic_object__property__classifier__remove(f2ptr cause, f2ptr this, f2ptr that) {
+f2ptr raw__semantic_object__classifier__remove(f2ptr cause, f2ptr this, f2ptr that) {
   return raw__semantic_frame__remove(cause, this, new__symbol(cause, "property"), new__symbol(cause, "classifier"), that);
 }
 
-f2ptr f2__semantic_object__property__classifier__remove(f2ptr cause, f2ptr this, f2ptr that) {
+f2ptr f2__semantic_object__classifier__remove(f2ptr cause, f2ptr this, f2ptr that) {
   if (! raw__semantic_object__is_type(cause, this)) {
     return f2larva__new(cause, 1, nil);
   }
-  return raw__semantic_object__property__classifier__remove(cause, this, that);
+  return raw__semantic_object__classifier__remove(cause, this, that);
 }
-export_cefunk2(semantic_object__property__classifier__remove, this, that, 0, "");
+export_cefunk2(semantic_object__classifier__remove, this, that, 0, "");
 
 
 f2ptr f2__semantic_object_type__new(f2ptr cause) {
   f2ptr this = f2__primobject_type__new(cause, f2list1__new(cause, new__symbol(cause, "semantic_frame")));
-  {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.execute__symbol,     new__symbol(cause, "new"),                 f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_object"), new__symbol(cause, "semantic_object__new")));}
-  {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.execute__symbol,     new__symbol(cause, "is_type"),             f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_object"), new__symbol(cause, "semantic_object__is_type")));}
-  {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.get__symbol,         new__symbol(cause, "type"),                f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_object"), new__symbol(cause, "semantic_object__type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-add"),    new__symbol(cause, "property-noun"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_object"), new__symbol(cause, "semantic_object__property__noun__add")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-remove"), new__symbol(cause, "property-noun"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_object"), new__symbol(cause, "semantic_object__property__noun__remove")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-add"),    new__symbol(cause, "property-classifier"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_object"), new__symbol(cause, "semantic_object__property__classifier__add")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-remove"), new__symbol(cause, "property-classifier"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_object"), new__symbol(cause, "semantic_object__property__classifier__remove")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"),         new__symbol(cause, "new"),        f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_object"), new__symbol(cause, "semantic_object__new")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"),         new__symbol(cause, "is_type"),    f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_object"), new__symbol(cause, "semantic_object__is_type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),             new__symbol(cause, "type"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_object"), new__symbol(cause, "semantic_object__type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-add"),    new__symbol(cause, "noun"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_object"), new__symbol(cause, "semantic_object__noun__add")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-remove"), new__symbol(cause, "noun"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_object"), new__symbol(cause, "semantic_object__noun__remove")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-add"),    new__symbol(cause, "classifier"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_object"), new__symbol(cause, "semantic_object__classifier__add")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-remove"), new__symbol(cause, "classifier"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_object"), new__symbol(cause, "semantic_object__classifier__remove")));}
   return this;
 }
 

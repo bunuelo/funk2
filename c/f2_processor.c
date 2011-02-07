9676666443067745c02c6c4320469a47d57b9391
Bo Morgan <bo@mit.edu>
2011-02-07 16:19:54 -0500

    organizing scheduler, adding f2_processor and moving processor from f2_scheduler to f2_processor.

--- c/f2_processor.c
+++ c/f2_processor.c
@@ -353,7 +353,9 @@ f2ptr f2processor__execute_next_bytecodes(f2ptr processor, f2ptr cause) {
 	  funk2_operating_system__pop_current_fiber(&(__funk2.operating_system), pool_index);
 	}
       } else { // (fiber__paused)
-	f2processor__newly_paused_fibers__set(cause, processor, f2cons__new(cause, fiber, f2processor__newly_paused_fibers(cause, processor)));
+	pause_gc();
+	f2processor__newly_paused_fibers__set(processor, cause, f2cons__new(cause, fiber, f2processor__newly_paused_fibers(processor, cause)));
+	resume_gc();
       }
       f2mutex__unlock(f2fiber__execute_mutex(fiber, cause), cause);
     } else {

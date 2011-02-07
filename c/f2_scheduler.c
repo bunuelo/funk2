0db12ba3847cf4684a1d0210a4ea88df4ef41535
Bo Morgan <bo@mit.edu>
2011-02-07 13:31:45 -0500

    organizing scheduler, adding f2_processor and moving processor from f2_scheduler to f2_processor.

--- c/f2_scheduler.c
+++ c/f2_scheduler.c
@@ -245,14 +245,6 @@ void f2__scheduler__initialize() {
   f2ptr cause = f2_scheduler_c__cause__new(initial_cause());
   f2__scheduler__initialize_global_symbolic_vars();
   
-  // processor
-  
-  initialize_primobject_11_slot(processor, scheduler, processor_thread, active_fibers_mutex, active_fibers, active_fibers_iter, active_fibers_prev, active_fibers_next, sleeping_fibers_mutex, sleeping_fibers, pool_index, desc);
-  
-  {char* symbol_str = "terminal_print_with_frame"; __funk2.globalenv.object_type.primobject.primobject_type_processor.terminal_print_with_frame__symbol = f2symbol__new(cause, strlen(symbol_str), (u8*)symbol_str);}
-  {f2__primcfunk__init__with_c_cfunk_var__2_arg(processor__terminal_print_with_frame, this, terminal_print_frame, cfunk, 0, "primobject_type funktion (defined in f2_primobjects.c)"); __funk2.globalenv.object_type.primobject.primobject_type_processor.terminal_print_with_frame__funk = never_gc(cfunk);}
-  
-  
   // scheduler
   
   initialize_primobject_1_slot(scheduler, processors);

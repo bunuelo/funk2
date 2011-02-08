d2365a879206c05c096831ec02432a7b714bfb79
Bo Morgan <bo@mit.edu>
2011-02-08 12:18:33 -0500

    organizing scheduler, adding f2_processor and moving processor from f2_scheduler to f2_processor.

--- c/f2_compile.c
+++ c/f2_compile.c
@@ -964,7 +964,7 @@ f2ptr raw__apply_funk(f2ptr simple_cause, f2ptr fiber, f2ptr funk, f2ptr args) {
   
   //f2fiber__keep_undead__set(new_fiber, cause, __true__symbol);
   //printf ("\nCompile fiber created: 0x%X", (uint)new_fiber); fflush(stdout);
-  raw__scheduler__complete_fiber(cause, new_fiber);
+  f2__global_scheduler__complete_fiber(cause, new_fiber);
   
   f2ptr value = f2fiber__value(new_fiber, cause);
   f2fiber__keep_undead__set(new_fiber, cause, nil);

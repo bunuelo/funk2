bb48f71cb388388f3968e5f1cdc0962ef1a5a03c
Bo Morgan <bo@mit.edu>
2011-02-12 16:46:11 -0500

    adding fiber_trigger as core primobject

--- c/f2_primobject__fiber_trigger.c
+++ c/f2_primobject__fiber_trigger.c
@@ -23,7 +23,7 @@
 
 // fiber_trigger
 
-def_primobject_0_slot(fiber_trigger);
+def_primobject_1_slot(fiber_trigger, test);
 
 f2ptr raw__fiber_trigger__new(f2ptr cause) {
   return f2fiber_trigger__new(cause);
@@ -56,7 +56,7 @@ void f2__primobject_fiber_trigger__initialize() {
   
   // fiber_trigger
   
-  initialize_primobject_0_slot(fiber_trigger);
+  initialize_primobject_1_slot(fiber_trigger, test);
   
   {char* symbol_str = "new"; __funk2.globalenv.object_type.primobject.primobject_type_fiber_trigger.new__symbol = f2symbol__new(cause, strlen(symbol_str), (u8*)symbol_str);}
   {f2__primcfunk__init__with_c_cfunk_var__0_arg(fiber_trigger__new, cfunk, 0, "primobject_type funktion (defined in f2_primobjects.c)"); __funk2.globalenv.object_type.primobject.primobject_type_fiber_trigger.new__funk = never_gc(cfunk);}
--- c/f2_primobject__fiber_trigger.h
+++ c/f2_primobject__fiber_trigger.h
@@ -25,7 +25,7 @@
 // fiber_trigger
 
 typedef struct funk2_object_type__fiber_trigger__slot_s funk2_object_type__fiber_trigger__slot_t;
-declare_object_type_0_slot(fiber_trigger,
+declare_object_type_1_slot(fiber_trigger, test,
 			   );
 
 #endif // F2__PRIMOBJECT__FIBER_TRIGGER__TYPES__H
@@ -37,7 +37,7 @@ declare_object_type_0_slot(fiber_trigger,
 
 // fiber_trigger
 
-declare_primobject_0_slot(fiber_trigger);
+declare_primobject_1_slot(fiber_trigger, test);
 
 f2ptr raw__fiber_trigger__new(f2ptr cause);
 f2ptr  f2__fiber_trigger__new(f2ptr cause);

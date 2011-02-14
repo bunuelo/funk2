ff2301e1e21313d3af8d87655c2058f4a1a1ed2c
Bo Morgan <bo@mit.edu>
2011-02-14 17:59:43 -0500

    working on cairo

--- extension/cairo/cairo.c
+++ extension/cairo/cairo.c
@@ -21,17 +21,24 @@
 
 #include "cairo.h"
 
+
+f2ptr f2__cairo_not_supported_larva__new(f2ptr cause) {
+  return f2larva__new(cause, 1243, f2__bug__new(cause, f2integer__new(cause, 1243), f2__frame__new(cause, f2list2__new(cause, new__symbol(cause, "bug_type"), new__symbol(cause, "cairo_not_supported")))));
+}
+
+
+
 // cairo
 
-f2ptr raw__cairo__new(f2ptr cause) {
-  f2ptr cairo_pointer = nil;
+f2ptr raw__cairo__new(f2ptr cause, f2ptr cairo_pointer) {
   return f2__frame__new(cause, f2list4__new(cause,
 					    new__symbol(cause, "type"),          new__symbol(cause, "cairo"),
 					    new__symbol(cause, "cairo_pointer"), cairo_pointer));
 }
 
 f2ptr f2__cairo__new(f2ptr cause) {
-  return raw__cairo__new(cause);
+  f2ptr cairo_pointer = nil;
+  return raw__cairo__new(cause, cairo_pointer);
 }
 export_cefunk0(cairo__new, 0, "Returns a new cairo object.");
 
@@ -109,19 +116,84 @@ f2ptr f2__cairo_type__new(f2ptr cause) {
 
 
 
+#if defined(F2__CAIRO_SUPPORTED)
+// cairo_format
+
+boolean_t raw__cairo_format__is_type(f2ptr cause, f2ptr object) {
+  return (raw__eq(cause, object, new__symbol(cause, "ARGB32")) ||
+	  raw__eq(cause, object, new__symbol(cause, "RGB24")) ||
+	  raw__eq(cause, object, new__symbol(cause, "A8")) ||
+	  raw__eq(cause, object, new__symbol(cause, "A1")) ||
+	  raw__eq(cause, object, new__symbol(cause, "RGB16_565")));
+}
+
+cairo_format_t raw__cairo_format__as__cairo_format_t(f2ptr cause, f2ptr this) {
+  if (raw__eq(cause, object, new__symbol(cause, "ARGB32"))) {
+    return CAIRO_FORMAT_ARGB32;
+  } else if (raw__eq(cause, object, new__symbol(cause, "RGB24"))) {
+    return CAIRO_FORMAT_RGB24;
+  } else if (raw__eq(cause, object, new__symbol(cause, "A8"))) {
+    return CAIRO_FORMAT_A8;
+  } else if (raw__eq(cause, object, new__symbol(cause, "A1"))) {
+    return CAIRO_FORMAT_A1;
+  } else if (raw__eq(cause, object, new__symbol(cause, "RGB16_565"))) {
+    return CAIRO_FORMAT_RGB16_565;
+  }
+  return CAIRO_FORMAT_INVALID;
+}
+#endif // F2__CAIRO_SUPPORTED
+
 // cairo_image_surface
 
-f2ptr raw__cairo_image_surface__new(f2ptr cause) {
-  f2ptr cairo_image_surface_pointer = nil;
+f2ptr f2cairo_image_surface__new(f2ptr cause, f2ptr cairo_image_surface_pointer) {
   return f2__frame__new(cause, f2list4__new(cause,
 					    new__symbol(cause, "type"),          new__symbol(cause, "cairo_image_surface"),
 					    new__symbol(cause, "cairo_image_surface_pointer"), cairo_image_surface_pointer));
 }
 
-f2ptr f2__cairo_image_surface__new(f2ptr cause) {
-  return raw__cairo_image_surface__new(cause);
+f2ptr raw__cairo_image_surface__new(f2ptr cause, f2ptr format, f2ptr width, f2ptr height) {
+#if defined(F2__CAIRO_SUPPORTED)
+  cairo_format_t   cairo_format  = raw__cairo_format__as__cairo_format_t(cause, format);
+  s64              width__i      = f2integer__i(width,  cause);
+  s64              height__i     = f2integer__i(height, cause);
+  cairo_surface_t* cairo_surface = cairo_image_surface_create(cairo_format,
+							      width__i,
+							      height__i);
+  cairo_status_t status = cairo_surface_status(cairo_surface);
+  if (status != CAIRO_STATUS_SUCCESS) {
+    f2ptr cairo_status = nil;
+    switch(status) {
+    case CAIRO_STATUS_NULL_POINTER:    cauro_status = new__symbol(cause, "CAIRO_STATUS_NULL_POINTER");    break;
+    case CAIRO_STATUS_NO_MEMORY:       cauro_status = new__symbol(cause, "CAIRO_STATUS_NO_MEMORY");       break;
+    case CAIRO_STATUS_READ_ERROR:      cauro_status = new__symbol(cause, "CAIRO_STATUS_READ_ERROR");      break;
+    case CAIRO_STATUS_INVALID_CONTENT: cauro_status = new__symbol(cause, "CAIRO_STATUS_INVALID_CONTENT"); break;
+    case CAIRO_STATUS_INVALID_FORMAT:  cauro_status = new__symbol(cause, "CAIRO_STATUS_INVALID_FORMAT");  break;
+    case CAIRO_STATUS_INVALID_VISUAL:  cauro_status = new__symbol(cause, "CAIRO_STATUS_INVALID_VISUAL");  break;
+    }
+    cauro_surface_destroy(cairo_surface);
+    return f2larva__new(cause, 44321, f2__bug__new(cause, f2integer__new(cause, 44321), f2__frame__new(cause, f2list10__new(cause,
+															    new__symbol(cause, "bug_type"),     new__symbol(cause, "failure_to_create_cairo_surface"),
+															    new__symbol(cause, "cairo_status"), cairo_status,
+															    new__symbol(cause, "format"),       format,
+															    new__symbol(cause, "width"),        width,
+															    new__symbol(cause, "height"),       height))));
+  }
+  f2ptr cairo_image_surface_pointer = f2pointer__new(cause, to_ptr(cairo_surface));
+  return raw__cairo_image_surface__new(cause, cairo_image_surface_pointer);
+#else
+  return f2__cairo_not_supported_larva__new(cause);
+#endif // F2__CAIRO_SUPPORTED
 }
-export_cefunk0(cairo_image_surface__new, 0, "Returns a new cairo_image_surface object.");
+
+f2ptr f2__cairo_image_surface__new(f2ptr cause, f2ptr format, f2ptr width, f2ptr height) {
+  if ((! raw__cairo_format__is_type(cause, format)) ||
+      (! raw__integer__is_type(cause, width)) ||
+      (! raw__integer__is_type(cause, height))) {
+    return f2larva__new(cause, 1, nil);
+  }
+  return raw__cairo_image_surface__new(cause, format, width, height);
+}
+export_cefunk3(cairo_image_surface__new, format, width, height, 0, "Returns a new cairo_image_surface object.  Given format must be one of the following symbols: ARGB32, RGB24, A8, A1, RGB16_565.");
 
 
 boolean_t raw__cairo_image_surface__is_type(f2ptr cause, f2ptr thing) {

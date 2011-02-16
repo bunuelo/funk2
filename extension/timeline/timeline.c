961158f1581558b0e2e0324c843b69bce8978034
Bo Morgan <bo@mit.edu>
2011-02-16 16:42:39 -0500

    adding timeline-test example package

--- extension/timeline/timeline.c
+++ extension/timeline/timeline.c
@@ -138,6 +138,12 @@ f2ptr f2__timeline__core_extension__initialize(f2ptr cause) {
     }
   }
   {
+    f2ptr result = f2__force_funk_apply(cause, f2__this__fiber(cause), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event"), new__symbol(cause, "semantic_event__core_extension__ping")), nil);
+    if (raw__larva__is_type(cause, result)) {
+      return result;
+    }
+  }
+  {
     f2ptr result = f2__force_funk_apply(cause, f2__this__fiber(cause), f2__core_extension_funk__new(cause, new__symbol(cause, "cairo"), new__symbol(cause, "cairo__core_extension__ping")), nil);
     if (raw__larva__is_type(cause, result)) {
       return result;

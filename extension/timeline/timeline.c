41be17a7838c4de9e9fb006a7e43485866e1ecff
Bo Morgan <bo@mit.edu>
2011-02-16 17:02:06 -0500

    adding timeline-test example package

--- extension/timeline/timeline.c
+++ extension/timeline/timeline.c
@@ -158,13 +158,29 @@ f2ptr f2__timeline__contains_timeline_event(f2ptr cause, f2ptr this, f2ptr timel
 export_cefunk2(timeline__contains_timeline_event, this, timeline_event, 0, "Returns whether or not a timeline contains a timeline_event.");
 
 
+f2ptr raw__timeline__timeline_events(f2ptr cause, f2ptr this) {
+  f2ptr timeline_event_set = raw__timeline__timeline_event_set(cause, this);
+  f2ptr timeline_events    = raw__set__elements(cause, timeline_event_set);
+  return timeline_events;
+}
+
+f2ptr f2__timeline__timeline_events(f2ptr cause, f2ptr this) {
+  if (! raw__timeline__is_type(cause, this)) {
+    return f2larva__new(cause, 1, nil);
+  }
+  return raw__timeline__timeline_events(cause, this);
+}
+export_cefunk1(timeline__timeline_events, this, 0, "Returns a new list of the timeline_events contained within this timeline.");
+
+
+
 f2ptr raw__timeline__terminal_print_with_frame(f2ptr cause, f2ptr this, f2ptr terminal_print_frame) {
   f2ptr print_as_frame_hash = raw__terminal_print_frame__print_as_frame_hash(cause, terminal_print_frame);
   f2ptr frame               = raw__ptypehash__lookup(cause, print_as_frame_hash, this);
   if (frame == nil) {
     frame = f2__frame__new(cause, f2list2__new(cause,
 					       new__symbol(cause, "print_object_type"),  new__symbol(cause, "timeline"),
-					       new__symbol(cause, "timeline_event_set"), raw__timeline__timeline_event_set(cause, this)));
+					       new__symbol(cause, "timeline_events"),    raw__timeline__timeline_events(cause, this)));
     f2__ptypehash__add(cause, print_as_frame_hash, this, frame);
   }
   return raw__frame__terminal_print_with_frame(cause, frame, terminal_print_frame);
@@ -185,6 +201,7 @@ f2ptr f2__timeline_type__new_aux(f2ptr cause) {
   {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "add_timeline_event"),        f2__core_extension_funk__new(cause, new__symbol(cause, "timeline"), new__symbol(cause, "timeline__add_timeline_event")));}
   {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "remove_timeline_event"),     f2__core_extension_funk__new(cause, new__symbol(cause, "timeline"), new__symbol(cause, "timeline__remove_timeline_event")));}
   {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "contains_timeline_event"),   f2__core_extension_funk__new(cause, new__symbol(cause, "timeline"), new__symbol(cause, "timeline__contains_timeline_event")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "timeline_events"),           f2__core_extension_funk__new(cause, new__symbol(cause, "timeline"), new__symbol(cause, "timeline__timeline_events")));}
   {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "terminal_print_with_frame"), f2__core_extension_funk__new(cause, new__symbol(cause, "timeline"), new__symbol(cause, "timeline__terminal_print_with_frame")));}
   return this;
 }

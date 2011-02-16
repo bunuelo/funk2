b7c2c78ba0c04d543bbb4f4db040c4c99936e513
Bo Morgan <bo@mit.edu>
2011-02-16 18:53:47 -0500

    adding timeline-test example package

--- extension/timeline/timeline.c
+++ extension/timeline/timeline.c
@@ -177,10 +177,13 @@ f2ptr raw__timeline__cairo_render(f2ptr cause, f2ptr this, f2ptr cairo_context) 
   //f2ptr timeline_event_set = raw__timeline__timeline_event_set(cause, this);
   raw__cairo_context__set_source_rgba(cause, cairo_context, 0, 0, 0, 1);
   raw__cairo_context__set_line_width( cause, cairo_context, 0.001);
+  // box
   raw__cairo_context__rel_line_to(    cause, cairo_context,  1,  0);
   raw__cairo_context__rel_line_to(    cause, cairo_context,  0,  1);
   raw__cairo_context__rel_line_to(    cause, cairo_context, -1,  0);
   raw__cairo_context__rel_line_to(    cause, cairo_context,  0, -1);
+  // slant
+  raw__cairo_context__rel_line_to(    cause, cairo_context,  1,  1);
   raw__cairo_context__stroke(         cause, cairo_context);
   return nil;
 }

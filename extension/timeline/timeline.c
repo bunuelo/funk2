4f3b5c3299df263a99cade785aa1dfc7624b4c1c
Bo Morgan <bo@mit.edu>
2011-02-17 14:43:35 -0500

    adding timeline-test example package

--- extension/timeline/timeline.c
+++ extension/timeline/timeline.c
@@ -88,7 +88,7 @@ void raw__cairo_context__render_outlined_rounded_box(f2ptr cause,
   raw__cairo_context__move_to(        cause, this, x0 + corner_radius, y0);
   raw__cairo_context__arc(            cause, this, x0 + corner_radius, y0 + corner_radius, corner_radius, M_PI, 3 * M_PI / 2);
   raw__cairo_context__rel_line_to(    cause, this, dx - (corner_radius * 2), 0);
-  raw__cairo_context__arc(            cause, this, x0 + dx - corner_radius, y0 + dy + corner_radius, corner_radius, 3 * M_PI / 2, 2 * M_PI);
+  raw__cairo_context__arc(            cause, this, x0 + dx - corner_radius, y0 + corner_radius, corner_radius, 3 * M_PI / 2, 2 * M_PI);
   raw__cairo_context__rel_line_to(    cause, this, 0, dy - (corner_radius * 2));
   raw__cairo_context__arc(            cause, this, x0 + dx - corner_radius, y0 + dy - corner_radius, corner_radius, 0, M_PI / 2);
   raw__cairo_context__rel_line_to(    cause, this, -(dx - (corner_radius * 2)), 0);

a566d9636e3f26604ee46e127d9a8cd1997deaef
Bo Morgan <bo@mit.edu>
2011-02-20 16:08:31 -0500

    removing timeline dependency on semantic knowledge

--- extension/timeline/timeline.c
+++ extension/timeline/timeline.c
@@ -503,17 +503,17 @@ f2ptr raw__timeline__cairo_render(f2ptr cause, f2ptr this, f2ptr cairo_context) 
 		     f2ptr start_time = raw__timeline_event__start_time(cause, event);
 		     f2ptr end_time   = raw__timeline_event__end_time(  cause, event);
 		     if ((minimum_time == nil) ||
-			 f2__is_greater_than(cause, start_time, minimum_time)) {
+			 (f2__is_less_than(cause, start_time, minimum_time) != nil)) {
 		       minimum_time = start_time;
 		     }
 		     if ((maximum_time == nil) ||
-			 f2__is_less_than(cause, end_time, maximum_time)) {
+			 (f2__is_greater_than(cause, end_time, maximum_time) != nil)) {
 		       maximum_time = end_time;
 		     }
 		     );
     }
     raw__timeline__minimum_time__set(cause, this, minimum_time);
-    raw__timeline__minimum_time__set(cause, this, maximum_time);
+    raw__timeline__maximum_time__set(cause, this, maximum_time);
   }
   f2ptr connected_sets = nil;
   // find connected sets of timeline_events

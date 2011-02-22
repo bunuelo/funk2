f8f7029cc0da27cd6f326e5bdabafde3b9c27d7e
Bo Morgan <bo@mit.edu>
2011-02-22 14:58:57 -0500

    incorporating timeline images into isismon

--- extension/semantic_event/semantic_event.c
+++ extension/semantic_event/semantic_event.c
@@ -253,6 +253,9 @@ f2ptr raw__semantic_knowledge_base__as__timeline(f2ptr cause, f2ptr this) {
 	      if ((absolute_start_time != nil) &&
 		  (absolute_end_time   != nil)) {
 		f2ptr timeline_event = f2__timeline_event__new(cause, name, absolute_start_time, absolute_end_time);
+		if (raw__larva__is_type(cause, timeline_event)) {
+		  return timeline_event;
+		}
 		raw__ptypehash__add(cause, timeline_event_semantic_event_hash, semantic_event, timeline_event);
 	      }
 	    }

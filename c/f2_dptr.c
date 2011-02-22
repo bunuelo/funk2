82aeb277526c65e31b271d3fd389d0fbb620cd46
Bo Morgan <bo@mit.edu>
2011-02-21 19:50:38 -0500

    removing timeline dependency on semantic knowledge

--- c/f2_dptr.c
+++ c/f2_dptr.c
@@ -25,40 +25,48 @@
 
 void funk2_dptr__init(funk2_dptr_t* this, f2ptr p, f2ptr tracing_on, f2ptr trace, f2ptr imagination_frame, f2ptr mutate_funks, f2ptr read_funks) {
   this->p                 = p;
+#if defined(F2__USE_TRACED_DPTRS)
   this->tracing_on        = tracing_on;
   this->trace             = trace;
   this->imagination_frame = imagination_frame;
   this->mutate_funks      = mutate_funks;
   this->read_funks        = read_funks;
+#endif // F2__USE_TRACED_DPTRS
 }
 
 boolean_t funk2_dptr__check_all_memory_pointers_valid_in_memory(funk2_dptr_t* this, funk2_memory_t* memory) {
   boolean_t found_invalid = boolean__false;
   found_invalid |= (! funk2_memory__is_reasonably_valid_used_funk2_memblock_ptr(memory, __f2ptr_to_ptr(this->p)));
+#if defined(F2__USE_TRACED_DPTRS)
   found_invalid |= (! funk2_memory__is_reasonably_valid_used_funk2_memblock_ptr(memory, __f2ptr_to_ptr(this->tracing_on)));
   found_invalid |= (! funk2_memory__is_reasonably_valid_used_funk2_memblock_ptr(memory, __f2ptr_to_ptr(this->trace)));
   found_invalid |= (! funk2_memory__is_reasonably_valid_used_funk2_memblock_ptr(memory, __f2ptr_to_ptr(this->imagination_frame)));
   found_invalid |= (! funk2_memory__is_reasonably_valid_used_funk2_memblock_ptr(memory, __f2ptr_to_ptr(this->mutate_funks)));
   found_invalid |= (! funk2_memory__is_reasonably_valid_used_funk2_memblock_ptr(memory, __f2ptr_to_ptr(this->read_funks)));
+#endif // F2__USE_TRACED_DPTRS
   return found_invalid;
 }
 
 void funk2_dptr__decrement_reference_counts(funk2_dptr_t* this, funk2_garbage_collector_t* garbage_collector) {
   funk2_memblock__decrement_reference_count((funk2_memblock_t*)from_ptr(__f2ptr_to_ptr(this->p)),                 this->p,                 garbage_collector);
+#if defined(F2__USE_TRACED_DPTRS)
   funk2_memblock__decrement_reference_count((funk2_memblock_t*)from_ptr(__f2ptr_to_ptr(this->tracing_on)),        this->tracing_on,        garbage_collector);
   funk2_memblock__decrement_reference_count((funk2_memblock_t*)from_ptr(__f2ptr_to_ptr(this->trace)),             this->trace,             garbage_collector);
   funk2_memblock__decrement_reference_count((funk2_memblock_t*)from_ptr(__f2ptr_to_ptr(this->imagination_frame)), this->imagination_frame, garbage_collector);
   funk2_memblock__decrement_reference_count((funk2_memblock_t*)from_ptr(__f2ptr_to_ptr(this->mutate_funks)),      this->mutate_funks,      garbage_collector);
   funk2_memblock__decrement_reference_count((funk2_memblock_t*)from_ptr(__f2ptr_to_ptr(this->read_funks)),        this->read_funks,        garbage_collector);
+#endif // F2__USE_TRACED_DPTRS
 }
 
 void funk2_dptr__grey_referenced_elements(funk2_dptr_t* this, funk2_garbage_collector_pool_t* garbage_collector_pool, int pool_index) {
   funk2_garbage_collector_pool__grey_maybe_other_element(garbage_collector_pool, pool_index, this->p);
+#if defined(F2__USE_TRACED_DPTRS)
   funk2_garbage_collector_pool__grey_maybe_other_element(garbage_collector_pool, pool_index, this->tracing_on);
   funk2_garbage_collector_pool__grey_maybe_other_element(garbage_collector_pool, pool_index, this->trace);
   funk2_garbage_collector_pool__grey_maybe_other_element(garbage_collector_pool, pool_index, this->imagination_frame);
   funk2_garbage_collector_pool__grey_maybe_other_element(garbage_collector_pool, pool_index, this->mutate_funks);
   funk2_garbage_collector_pool__grey_maybe_other_element(garbage_collector_pool, pool_index, this->read_funks);
+#endif // F2__USE_TRACED_DPTRS
 }
 
 

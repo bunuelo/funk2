0100f492c1513821102f59e7b077f24b8b2b7de9
Bo Morgan <bo@mit.edu>
2011-02-23 19:38:05 -0500

    working on broken timeline edges

--- extension/timeline/timeline.c
+++ extension/timeline/timeline.c
@@ -102,12 +102,11 @@ void raw__cairo_context__render_outlined_rounded_box_with_broken_right(f2ptr cau
   double corner_radius = maximum_corner_radius;
   raw__cairo_context__move_to(        cause, this, x0, y0 + corner_radius);
   raw__cairo_context__arc(            cause, this, x0 + corner_radius, y0 + corner_radius, corner_radius, M_PI, 3 * M_PI / 2);
-  raw__cairo_context__rel_line_to(    cause, this, dx - corner_radius - (dy / 4), 0);
-  raw__cairo_context__rel_line_to(    cause, this,  (dy / 4), (dy / 4));
-  raw__cairo_context__rel_line_to(    cause, this, -(dy / 4), (dy / 4));
-  raw__cairo_context__rel_line_to(    cause, this,  (dy / 4), (dy / 4));
-  raw__cairo_context__rel_line_to(    cause, this, -(dy / 4), (dy / 4));
-  raw__cairo_context__rel_line_to(    cause, this, -(dx - corner_radius - (dy / 4)), 0);
+  raw__cairo_context__rel_line_to(    cause, this, dx - corner_radius - (dy / 3), 0);
+  raw__cairo_context__rel_line_to(    cause, this,  (dy / 3), (dy / 3));
+  raw__cairo_context__rel_line_to(    cause, this, -(dy / 3), (dy / 3));
+  raw__cairo_context__rel_line_to(    cause, this,  (dy / 3), (dy / 3));
+  raw__cairo_context__rel_line_to(    cause, this, -(dx - corner_radius), 0);
   raw__cairo_context__arc(            cause, this, x0 + corner_radius, y0 + dy - corner_radius, corner_radius, M_PI / 2, M_PI);
   raw__cairo_context__rel_line_to(    cause, this, 0, -(dy - (corner_radius * 2)));
   raw__cairo_context__close_path(     cause, this);

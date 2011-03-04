1c756e994ad0580e327b53a63fe5f6f098f27dea
Bo Morgan <bo@mit.edu>
2011-03-03 23:04:47 -0500

    adding assert_argument_type

--- c/f2_compile.c
+++ c/f2_compile.c
@@ -1228,10 +1228,9 @@ void dont_know_how_to_compile() {
 }
 
 f2ptr f2__compile__cons_exp(f2ptr simple_cause, f2ptr fiber, f2ptr exp, boolean_t protect_environment, boolean_t optimize_tail_recursion, boolean_t* popped_env_and_return, boolean_t* is_funktional, f2ptr local_variables, boolean_t* is_locally_funktional) {
-  assert_argument_type(fiber, fiber);
-  
   release__assert(__funk2.compile.f2__compile__cons_exp__symbol != -1, nil, "__funk2.compile.f2__compile__cons_exp__symbol not yet defined.");
   f2ptr cause = f2cause__compiled_from__new(simple_cause, __funk2.compile.f2__compile__cons_exp__symbol, f2cons__new(simple_cause, exp, nil));
+  assert_argument_type(fiber, fiber);
   
   f2ptr car = f2cons__car(exp, cause);
   //f2ptr funkvar_value = environment__lookup_funkvar_value(cause, f2fiber__env(fiber, cause), car);

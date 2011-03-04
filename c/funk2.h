4b6db8fddd8995d467f16d1980756c1914de820d
Bo Morgan <bo@mit.edu>
2011-03-04 12:55:21 -0500

    adding assert_argument_type

--- c/funk2.h
+++ c/funk2.h
@@ -107,7 +107,6 @@
 #include "f2_primobject__stream.h"
 #include "f2_primobject__tensor.h"
 #include "f2_primobject__dynamic_library.h"
-#include "f2_primobject__semantic_graph.h"
 #include "f2_primobject__text_buffer.h"
 #include "f2_primobject__traced_mutex.h"
 #include "f2_primobject_type.h"

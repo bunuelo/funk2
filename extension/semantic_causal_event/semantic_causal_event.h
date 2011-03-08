e53b39346773e131a195b613dd43c9b242ac96ef
Bo Morgan <bo@mit.edu>
2011-03-08 17:46:28 -0500

    adding semantic_causal_object core extension

--- extension/semantic_causal_event/semantic_causal_event.h
+++ extension/semantic_causal_event/semantic_causal_event.h
@@ -23,6 +23,7 @@
 #define F2__EXTENSION__SEMANTIC_CAUSAL_EVENT__H
 
 #include "../semantic_event/semantic_event.h"
+#include "../semantic_causal_object/semantic_causal_object.h"
 #include "../timeline/timeline.h"
 
 

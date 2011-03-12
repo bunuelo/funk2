282f4172489ec64afcffa22dcaf49c69ed224cdc
Bo Morgan <bo@mit.edu>
2011-03-12 18:17:00 -0500

    changing mutex and traced_mutex to cmutex and traced_cmutex

--- c/f2_memory.c
+++ c/f2_memory.c
@@ -59,12 +59,12 @@ void funk2_memory__handle(funk2_memory_t* this) {
 void funk2_memory__print_gc_stats(funk2_memory_t* this) {
   int pool_index;
   for(pool_index = 0; pool_index < memory_pool_num; pool_index++) {
-    funk2_memorypool__memory_cmutex__lock(&(this->pool[pool_index]));
+    funk2_memorypool__memory_mutex__lock(&(this->pool[pool_index]));
     status("pool[%d].total_global_memory  = " f2size_t__fstr, pool_index, this->pool[pool_index].total_global_memory);
     status("pool[%d].total_free_memory()  = " f2size_t__fstr, pool_index, funk2_memorypool__total_free_memory(&(__funk2.memory.pool[pool_index])));
     status("pool[%d].total_used_memory()  = " f2size_t__fstr, pool_index, funk2_memorypool__total_used_memory(&(__funk2.memory.pool[pool_index])));
     status("pool[%d].next_unique_block_id = %u", pool_index, this->pool[pool_index].next_unique_block_id);
-    funk2_memorypool__memory_cmutex__unlock(&(this->pool[pool_index]));
+    funk2_memorypool__memory_mutex__unlock(&(this->pool[pool_index]));
   }
 }
 
@@ -624,7 +624,7 @@ void funk2_memory__memory_test(funk2_memory_t* this) {
 
 f2ptr f2__memory__assert_valid(f2ptr cause) {
   f2ptr return_result = nil;
-  funk2_processor_mutex__lock(&(__funk2.garbage_collector.do_collection_cmutex));
+  funk2_processor_mutex__lock(&(__funk2.garbage_collector.do_collection_mutex));
   int pool_index;
   for (pool_index = 0; pool_index < memory_pool_num; pool_index ++) {
     f2ptr result = raw__memorypool__assert_valid(cause, pool_index);
@@ -634,7 +634,7 @@ f2ptr f2__memory__assert_valid(f2ptr cause) {
     }
   }
  f2__memory__assert_valid__return_goto:
-  funk2_processor_mutex__unlock(&(__funk2.garbage_collector.do_collection_cmutex));
+  funk2_processor_mutex__unlock(&(__funk2.garbage_collector.do_collection_mutex));
   return return_result;
 }
 

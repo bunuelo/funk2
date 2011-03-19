9f79b229129881a4765c7656d6d3464126391550
Bo Morgan <bo@mit.edu>
2011-03-19 17:09:32 -0400

    adding logic and math bytecodes

--- c/f2_optimize.c
+++ c/f2_optimize.c
@@ -122,10 +122,11 @@ f2ptr raw__optimize_context__terminal_print_with_frame(f2ptr cause, f2ptr this, 
   f2ptr print_as_frame_hash = raw__terminal_print_frame__print_as_frame_hash(cause, terminal_print_frame);
   f2ptr frame               = raw__ptypehash__lookup(cause, print_as_frame_hash, this);
   if (frame == nil) {
-    frame = f2__frame__new(cause, f2list6__new(cause,
+    frame = f2__frame__new(cause, f2list8__new(cause,
 					       new__symbol(cause, "print_object_type"), new__symbol(cause, "optimize_context"),
+					       new__symbol(cause, "graph"),                    f2__optimize_context__graph(                   cause, this),
 					       new__symbol(cause, "node_fiber_register_hash"), f2__optimize_context__node_fiber_register_hash(cause, this),
-					       new__symbol(cause, "graph"),                    f2__optimize_context__graph(                   cause, this)));
+					       new__symbol(cause, "node_variable_hash"),       f2__optimize_context__node_variable_hash(      cause, this)));
     f2__ptypehash__add(cause, print_as_frame_hash, this, frame);
   }
   return raw__frame__terminal_print_with_frame(cause, frame, terminal_print_frame);

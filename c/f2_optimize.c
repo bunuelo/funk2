c41c08a34ae8009f094a8b61ab170e38f2695874
Bo Morgan <bo@mit.edu>
2011-03-19 12:43:47 -0400

    adding logic and math bytecodes

--- c/f2_optimize.c
+++ c/f2_optimize.c
@@ -21,9 +21,45 @@
 
 #include "funk2.h"
 
+f2ptr raw__argument_data_node__new(f2ptr cause, f2ptr name) {
+  f2ptr argument_data = raw__array__new(cause, 2);
+  raw__array__elt__set(cause, this, 0, new__symbol(cause, "argument_data"));
+  raw__array__elt__set(cause, this, 1, name);
+  return raw__graph_node__new(cause, argument_data);
+}
+
+boolean_t raw__argument_data_node__is_type(f2ptr cause, f2ptr object) {
+  if (! raw__graph_node__is_type(cause, object)) {
+    return boolean__false;
+  }
+  f2ptr data = raw__graph_node__label(cause, object);
+  return ((data != nil) &&
+	  raw__array__is_type(cause, data) &&
+	  (raw__array__length(cause, data) == 2) &&
+	  raw__eq(cause, raw__array__elt(cause, data, 0), new__symbol(cause, "argument_data")));
+}
+
+f2ptr raw__argument_data_node__name(f2ptr cause, f2ptr this) {
+  return raw__array__elt(cause, raw__graph_node__label(cause, this), 1);
+}
+
 f2ptr raw__funk__optimize(f2ptr cause, f2ptr this) {
-  f2ptr args           = f2__funk__args(          cause, this);
-  f2ptr body_bytecodes = f2__funk__body_bytecodes(cause, this);
+  f2ptr args               = f2__funk__args(          cause, this);
+  f2ptr body_bytecodes     = f2__funk__body_bytecodes(cause, this);
+  f2ptr graph              = f2__graph__new(cause);
+  f2ptr node_argument_hash = f2__ptypehash__new(cause);
+  {
+    f2ptr iter = args;
+    while (iter != nil) {
+      f2ptr argument = f2__cons__car(cause, iter);
+      {
+	f2ptr argument_data_node = f2__argument_data_node__new(cause, argument);
+	raw__graph__add_node(cause, graph, argument_data_node);
+	raw__ptypehash__add(cause, node_argument_hash, argument, argument_data_node);
+      }
+      iter = f2__cons__cdr(cause, iter);
+    }
+  }
   {
     f2ptr iter = body_bytecodes;
     while (iter != nil) {
@@ -34,7 +70,7 @@ f2ptr raw__funk__optimize(f2ptr cause, f2ptr this) {
       iter = f2__cons__cdr(cause, iter);
     }
   }
-  return body_bytecodes;
+  return graph;
 }
 
 f2ptr f2__funk__optimize(f2ptr cause, f2ptr this) {

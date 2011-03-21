d962aa91006a240aabe313c172d22350b91604f6
Bo Morgan <bo@mit.edu>
2011-03-21 15:03:34 -0400

    working on f2_optimize.c

--- c/f2_optimize.c
+++ c/f2_optimize.c
@@ -879,12 +879,12 @@ f2ptr raw__optimize_context__terminal_print_with_frame(f2ptr cause, f2ptr this, 
   f2ptr print_as_frame_hash = raw__terminal_print_frame__print_as_frame_hash(cause, terminal_print_frame);
   f2ptr frame               = raw__ptypehash__lookup(cause, print_as_frame_hash, this);
   if (frame == nil) {
-    frame = f2__frame__new(cause, f2list4__new(cause,
-					       new__symbol(cause, "print_object_type"), new__symbol(cause, "optimize_context"),
-					       new__symbol(cause, "initial_fiber"),      f2__optimize_context__initial_fiber(     cause, this),
-					       new__symbol(cause, "active_fiber_set"),   f2__optimize_context__active_fiber_set(  cause, this),
-					       new__symbol(cause, "branched_fiber_set"), f2__optimize_context__branched_fiber_set(cause, this),
-					       new__symbol(cause, "finished_fiber_set"), f2__optimize_context__finished_fiber_set(cause, this)));
+    frame = f2__frame__new(cause, f2list10__new(cause,
+						new__symbol(cause, "print_object_type"), new__symbol(cause, "optimize_context"),
+						new__symbol(cause, "initial_fiber"),      f2__optimize_context__initial_fiber(     cause, this),
+						new__symbol(cause, "active_fiber_set"),   f2__optimize_context__active_fiber_set(  cause, this),
+						new__symbol(cause, "branched_fiber_set"), f2__optimize_context__branched_fiber_set(cause, this),
+						new__symbol(cause, "finished_fiber_set"), f2__optimize_context__finished_fiber_set(cause, this)));
     f2__ptypehash__add(cause, print_as_frame_hash, this, frame);
   }
   return raw__frame__terminal_print_with_frame(cause, frame, terminal_print_frame);

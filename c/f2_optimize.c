f785dd4306e485798c83d146c4a41d143d6d6b7d
Bo Morgan <bo@mit.edu>
2011-03-27 19:28:43 -0400

    working on optimize

--- c/f2_optimize.c
+++ c/f2_optimize.c
@@ -739,8 +739,6 @@ f2ptr raw__optimize_data__compile__less_than(f2ptr cause, f2ptr this) {
 
 
 f2ptr raw__optimize_data__compile_new_bytecodes_for_value(f2ptr cause, f2ptr this) {
-  f2ptr optimize_context = f2__optimize_data__optimize_context(cause, this);
-  f2ptr defined_data_set = f2__optimize_context__defined_data_set(cause, optimize_context);
   f2ptr full_bcs = nil;
   f2ptr iter_bcs = nil;
   {

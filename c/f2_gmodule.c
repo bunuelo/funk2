b13c76c4258b568dcdea52d7f86fe6e18b509a5c
Bo Morgan <bo@mit.edu>
2011-04-05 16:24:30 -0400

    working on is_funktional variable definition for cfunk

--- c/f2_gmodule.c
+++ c/f2_gmodule.c
@@ -202,13 +202,13 @@ void f2__gmodule__initialize() {
   
   f2__gmodule__reinitialize_globalvars();
   
-  f2__funktional_primcfunk__init__0(gmodule__supported, "");
-  f2__primcfunk__init__2(           gmodule__build_path, directory, module_name, "");
-  f2__primcfunk__init__2(           gmodule__open, filename, flags, "");
-  f2__primcfunk__init__2(           gmodule__symbol, module, symbol_name, "");
-  f2__primcfunk__init__1(           gmodule__name, module, "");
-  f2__primcfunk__init__1(           gmodule__make_resident, module, "");
-  f2__primcfunk__init__1(           gmodule__close, module, "");
-  f2__primcfunk__init__0(           gmodule__error, "");
+  f2__primcfunk__init__0(gmodule__supported, "");
+  f2__primcfunk__init__2(gmodule__build_path, directory, module_name, "");
+  f2__primcfunk__init__2(gmodule__open, filename, flags, "");
+  f2__primcfunk__init__2(gmodule__symbol, module, symbol_name, "");
+  f2__primcfunk__init__1(gmodule__name, module, "");
+  f2__primcfunk__init__1(gmodule__make_resident, module, "");
+  f2__primcfunk__init__1(gmodule__close, module, "");
+  f2__primcfunk__init__0(gmodule__error, "");
 }
 

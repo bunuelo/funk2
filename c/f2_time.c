d7edd842484aa5305e6e36fff8e471e51910ca32
Bo Morgan <bo@mit.edu>
2011-04-05 17:05:29 -0400

    working on is_funktional variable definition for cfunk

--- c/f2_time.c
+++ c/f2_time.c
@@ -620,7 +620,7 @@ void f2__time__initialize() {
   initialize_primobject_funk__0_arg(time, timezone,                       "Returns the timezone or name or abbreviation.");
   
   {char* symbol_str = "terminal_print_with_frame"; __funk2.globalenv.object_type.primobject.primobject_type_time.terminal_print_with_frame__symbol = f2symbol__new(cause, strlen(symbol_str), (u8*)symbol_str);}
-  {f2__primcfunk__init__with_c_cfunk_var__2_arg(time__terminal_print_with_frame, this, terminal_print_frame, cfunk, 0, "primobject_type funktion (defined in f2_primobjects.c)"); __funk2.globalenv.object_type.primobject.primobject_type_time.terminal_print_with_frame__funk = never_gc(cfunk);}
+  {f2__primcfunk__init__with_c_cfunk_var__2_arg(time__terminal_print_with_frame, this, terminal_print_frame, cfunk, "primobject_type funktion (defined in f2_primobjects.c)"); __funk2.globalenv.object_type.primobject.primobject_type_time.terminal_print_with_frame__funk = never_gc(cfunk);}
   
   
 }

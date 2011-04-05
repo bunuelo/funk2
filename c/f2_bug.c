d9dc5abc2a715a6e9b6858a134a86edc1bc2ab96
Bo Morgan <bo@mit.edu>
2011-04-05 16:09:04 -0400

    working on is_funktional variable definition for cfunk

--- c/f2_bug.c
+++ c/f2_bug.c
@@ -127,7 +127,7 @@ void f2__bug__initialize() {
   initialize_primobject_2_slot(bug, bug_type, frame);
   
   {char* symbol_str = "terminal_print_with_frame"; __funk2.globalenv.object_type.primobject.primobject_type_bug.terminal_print_with_frame__symbol = f2symbol__new(cause, strlen(symbol_str), (u8*)symbol_str);}
-  {f2__primcfunk__init__with_c_cfunk_var__2_arg(bug__terminal_print_with_frame, this, terminal_print_frame, cfunk, 0, "primobject_type funktion (defined in f2_primobjects.c)"); __funk2.globalenv.object_type.primobject.primobject_type_bug.terminal_print_with_frame__funk = never_gc(cfunk);}
+  {f2__primcfunk__init__with_c_cfunk_var__2_arg(bug__terminal_print_with_frame, this, terminal_print_frame, cfunk, "primobject_type funktion (defined in f2_primobjects.c)"); __funk2.globalenv.object_type.primobject.primobject_type_bug.terminal_print_with_frame__funk = never_gc(cfunk);}
   
   f2__primcfunk__init__1(bug__pretty_print, this, "Prints a bug object for human readability, suppressing large objects from printing in their entirety.");
   

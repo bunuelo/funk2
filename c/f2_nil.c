336f72451e9ea0b3b97eaa0e823d3ab95824516e
Bo Morgan <bo@mit.edu>
2011-04-05 16:25:43 -0400

    working on is_funktional variable definition for cfunk

--- c/f2_nil.c
+++ c/f2_nil.c
@@ -77,9 +77,9 @@ void f2__nil__initialize() {
   f2ptr cause = initial_cause();
   
   {char* str = "as-string"; __funk2.globalenv.object_type.nil_type.as__string__symbol = f2symbol__new(cause, strlen(str), (u8*)str);}
-  {f2__primcfunk__init__with_c_cfunk_var__1_arg(nil__as__string, this, cfunk, 1, "returns the string '[]'."); __funk2.globalenv.object_type.nil_type.as__string__funk = never_gc(cfunk);}
+  {f2__primcfunk__init__with_c_cfunk_var__1_arg(nil__as__string, this, cfunk, "returns the string '[]'."); __funk2.globalenv.object_type.nil_type.as__string__funk = never_gc(cfunk);}
   {char* str = "terminal_print_with_frame"; __funk2.globalenv.object_type.nil_type.terminal_print_with_frame__symbol = f2symbol__new(cause, strlen(str), (u8*)str);}
-  {f2__primcfunk__init__with_c_cfunk_var__2_arg(nil__terminal_print_with_frame, this, terminal_print_frame, cfunk, 1, "prints the nil value using the given terminal_print_frame object."); __funk2.globalenv.object_type.nil_type.terminal_print_with_frame__funk = never_gc(cfunk);}
+  {f2__primcfunk__init__with_c_cfunk_var__2_arg(nil__terminal_print_with_frame, this, terminal_print_frame, cfunk, "prints the nil value using the given terminal_print_frame object."); __funk2.globalenv.object_type.nil_type.terminal_print_with_frame__funk = never_gc(cfunk);}
   
 }
 

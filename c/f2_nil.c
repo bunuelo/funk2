26dbe76a1989cd1b951cddbc4552fefef9a9576c
Bo Morgan <bo@mit.edu>
2011-04-06 16:08:33 -0400

    adding documentation string to def_pcfunk and def_pcmetro

--- c/f2_nil.c
+++ c/f2_nil.c
@@ -33,7 +33,9 @@ f2ptr f2__nil__as__string(f2ptr cause, f2ptr this) {
   assert_argument_type(nil, this);
   return raw__nil__as__string(cause, this);
 }
-def_pcfunk1(nil__as__string, this, return f2__nil__as__string(this_cause, this));
+def_pcfunk1(nil__as__string, this,
+	    "",
+	    return f2__nil__as__string(this_cause, this));
 
 
 f2ptr raw__nil__terminal_print_with_frame(f2ptr cause, f2ptr this, f2ptr terminal_print_frame) {
@@ -53,7 +55,9 @@ f2ptr f2__nil__terminal_print_with_frame(f2ptr cause, f2ptr this, f2ptr terminal
   assert_argument_type(terminal_print_frame, terminal_print_frame);
   return raw__nil__terminal_print_with_frame(cause, this, terminal_print_frame);
 }
-def_pcfunk2(nil__terminal_print_with_frame, this, terminal_print_frame, return f2__nil__terminal_print_with_frame(this_cause, this, terminal_print_frame));
+def_pcfunk2(nil__terminal_print_with_frame, this, terminal_print_frame,
+	    "",
+	    return f2__nil__terminal_print_with_frame(this_cause, this, terminal_print_frame));
 
 
 f2ptr f2nil__primobject_type__new(f2ptr cause) {

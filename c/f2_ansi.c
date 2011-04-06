6c5f0ae4116b2d548c2d6164efa4db4fb7a32ecc
Bo Morgan <bo@mit.edu>
2011-04-06 15:28:58 -0400

    adding documentation string to def_pcfunk and def_pcmetro

--- c/f2_ansi.c
+++ c/f2_ansi.c
@@ -25,7 +25,9 @@ f2ptr f2__ansi__stream__begin_command(f2ptr cause, f2ptr stream) {
   raw__stream__writef(cause, stream, "%c[", 27);
   return nil;
 }
-def_pcfunk1(ansi__stream__begin_command, stream, return f2__ansi__stream__begin_command(this_cause, stream));
+def_pcfunk1(ansi__stream__begin_command, stream,
+	    "",
+	    return f2__ansi__stream__begin_command(this_cause, stream));
 
 void raw__ansi__stream__print_code(f2ptr cause, f2ptr stream, int code) {
   f2__cmutex__lock(cause, f2__stream__cmutex(cause, stream));
@@ -41,7 +43,9 @@ f2ptr f2__ansi__stream__print_code(f2ptr cause, f2ptr stream, f2ptr code) {
   raw__ansi__stream__print_code(cause, stream, raw_code);
   return nil;
 }
-def_pcfunk2(ansi__stream__print_code, stream, code, return f2__ansi__stream__print_code(this_cause, stream, code));
+def_pcfunk2(ansi__stream__print_code, stream, code,
+	    "",
+	    return f2__ansi__stream__print_code(this_cause, stream, code));
 
 
 
@@ -50,203 +54,261 @@ f2ptr f2__ansi__stream__reset(f2ptr cause, f2ptr stream) {
   raw__ansi__stream__print_code(cause, stream, 0);
   return nil;
 }
-def_pcfunk1(ansi__stream__reset, stream, return f2__ansi__stream__reset(this_cause, stream));
+def_pcfunk1(ansi__stream__reset, stream,
+	    "",
+	    return f2__ansi__stream__reset(this_cause, stream));
 
 f2ptr f2__ansi__stream__bold(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 1);
   return nil;
 }
-def_pcfunk1(ansi__stream__bold, stream, return f2__ansi__stream__bold(this_cause, stream));
+def_pcfunk1(ansi__stream__bold, stream,
+	    "",
+	    return f2__ansi__stream__bold(this_cause, stream));
 
 f2ptr f2__ansi__stream__half_bright(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 2);
   return nil;
 }
-def_pcfunk1(ansi__stream__half_bright, stream, return f2__ansi__stream__half_bright(this_cause, stream));
+def_pcfunk1(ansi__stream__half_bright, stream,
+	    "",
+	    return f2__ansi__stream__half_bright(this_cause, stream));
 
 f2ptr f2__ansi__stream__underscore(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 4);
   return nil;
 }
-def_pcfunk1(ansi__stream__underscore, stream, return f2__ansi__stream__underscore(this_cause, stream));
+def_pcfunk1(ansi__stream__underscore, stream,
+	    "",
+	    return f2__ansi__stream__underscore(this_cause, stream));
 
 f2ptr f2__ansi__stream__blink(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 5);
   return nil;
 }
-def_pcfunk1(ansi__stream__blink, stream, return f2__ansi__stream__blink(this_cause, stream));
+def_pcfunk1(ansi__stream__blink, stream,
+	    "",
+	    return f2__ansi__stream__blink(this_cause, stream));
 
 f2ptr f2__ansi__stream__reverse_video(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 7);
   return nil;
 }
-def_pcfunk1(ansi__stream__reverse_video, stream, return f2__ansi__stream__reverse_video(this_cause, stream));
+def_pcfunk1(ansi__stream__reverse_video, stream,
+	    "",
+	    return f2__ansi__stream__reverse_video(this_cause, stream));
 
 f2ptr f2__ansi__stream__normal_intensity(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 22);
   return nil;
 }
-def_pcfunk1(ansi__stream__normal_intensity, stream, return f2__ansi__stream__normal_intensity(this_cause, stream));
+def_pcfunk1(ansi__stream__normal_intensity, stream,
+	    "",
+	    return f2__ansi__stream__normal_intensity(this_cause, stream));
 
 f2ptr f2__ansi__stream__underline_off(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 24);
   return nil;
 }
-def_pcfunk1(ansi__stream__underline_off, stream, return f2__ansi__stream__underline_off(this_cause, stream));
+def_pcfunk1(ansi__stream__underline_off, stream,
+	    "",
+	    return f2__ansi__stream__underline_off(this_cause, stream));
 
 f2ptr f2__ansi__stream__blink_off(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 25);
   return nil;
 }
-def_pcfunk1(ansi__stream__blink_off, stream, return f2__ansi__stream__blink_off(this_cause, stream));
+def_pcfunk1(ansi__stream__blink_off, stream,
+	    "",
+	    return f2__ansi__stream__blink_off(this_cause, stream));
 
 f2ptr f2__ansi__stream__reverse_video_off(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 27);
   return nil;
 }
-def_pcfunk1(ansi__stream__reverse_video_off, stream, return f2__ansi__stream__reverse_video_off(this_cause, stream));
+def_pcfunk1(ansi__stream__reverse_video_off, stream,
+	    "",
+	    return f2__ansi__stream__reverse_video_off(this_cause, stream));
 
 f2ptr f2__ansi__stream__black_foreground(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 30);
   return nil;
 }
-def_pcfunk1(ansi__stream__black_foreground, stream, return f2__ansi__stream__black_foreground(this_cause, stream));
+def_pcfunk1(ansi__stream__black_foreground, stream,
+	    "",
+	    return f2__ansi__stream__black_foreground(this_cause, stream));
 
 f2ptr f2__ansi__stream__red_foreground(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 31);
   return nil;
 }
-def_pcfunk1(ansi__stream__red_foreground, stream, return f2__ansi__stream__red_foreground(this_cause, stream));
+def_pcfunk1(ansi__stream__red_foreground, stream,
+	    "",
+	    return f2__ansi__stream__red_foreground(this_cause, stream));
 
 f2ptr f2__ansi__stream__green_foreground(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 32);
   return nil;
 }
-def_pcfunk1(ansi__stream__green_foreground, stream, return f2__ansi__stream__green_foreground(this_cause, stream));
+def_pcfunk1(ansi__stream__green_foreground, stream,
+	    "",
+	    return f2__ansi__stream__green_foreground(this_cause, stream));
 
 f2ptr f2__ansi__stream__brown_foreground(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 33);
   return nil;
 }
-def_pcfunk1(ansi__stream__brown_foreground, stream, return f2__ansi__stream__brown_foreground(this_cause, stream));
+def_pcfunk1(ansi__stream__brown_foreground, stream,
+	    "",
+	    return f2__ansi__stream__brown_foreground(this_cause, stream));
 
 f2ptr f2__ansi__stream__blue_foreground(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 34);
   return nil;
 }
-def_pcfunk1(ansi__stream__blue_foreground, stream, return f2__ansi__stream__blue_foreground(this_cause, stream));
+def_pcfunk1(ansi__stream__blue_foreground, stream,
+	    "",
+	    return f2__ansi__stream__blue_foreground(this_cause, stream));
 
 f2ptr f2__ansi__stream__magenta_foreground(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 35);
   return nil;
 }
-def_pcfunk1(ansi__stream__magenta_foreground, stream, return f2__ansi__stream__magenta_foreground(this_cause, stream));
+def_pcfunk1(ansi__stream__magenta_foreground, stream,
+	    "",
+	    return f2__ansi__stream__magenta_foreground(this_cause, stream));
 
 f2ptr f2__ansi__stream__cyan_foreground(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 36);
   return nil;
 }
-def_pcfunk1(ansi__stream__cyan_foreground, stream, return f2__ansi__stream__cyan_foreground(this_cause, stream));
+def_pcfunk1(ansi__stream__cyan_foreground, stream,
+	    "",
+	    return f2__ansi__stream__cyan_foreground(this_cause, stream));
 
 f2ptr f2__ansi__stream__white_foreground(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 37);
   return nil;
 }
-def_pcfunk1(ansi__stream__white_foreground, stream, return f2__ansi__stream__white_foreground(this_cause, stream));
+def_pcfunk1(ansi__stream__white_foreground, stream,
+	    "",
+	    return f2__ansi__stream__white_foreground(this_cause, stream));
 
 f2ptr f2__ansi__stream__underscore_with_default_foreground(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 38);
   return nil;
 }
-def_pcfunk1(ansi__stream__underscore_with_default_foreground, stream, return f2__ansi__stream__underscore_with_default_foreground(this_cause, stream));
+def_pcfunk1(ansi__stream__underscore_with_default_foreground, stream,
+	    "",
+	    return f2__ansi__stream__underscore_with_default_foreground(this_cause, stream));
 
 f2ptr f2__ansi__stream__underscore_off_with_default_foreground(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 39);
   return nil;
 }
-def_pcfunk1(ansi__stream__underscore_off_with_default_foreground, stream, return f2__ansi__stream__underscore_off_with_default_foreground(this_cause, stream));
+def_pcfunk1(ansi__stream__underscore_off_with_default_foreground, stream,
+	    "",
+	    return f2__ansi__stream__underscore_off_with_default_foreground(this_cause, stream));
 
 f2ptr f2__ansi__stream__black_background(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 40);
   return nil;
 }
-def_pcfunk1(ansi__stream__black_background, stream, return f2__ansi__stream__black_background(this_cause, stream));
+def_pcfunk1(ansi__stream__black_background, stream,
+	    "",
+	    return f2__ansi__stream__black_background(this_cause, stream));
 
 f2ptr f2__ansi__stream__red_background(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 41);
   return nil;
 }
-def_pcfunk1(ansi__stream__red_background, stream, return f2__ansi__stream__red_background(this_cause, stream));
+def_pcfunk1(ansi__stream__red_background, stream,
+	    "",
+	    return f2__ansi__stream__red_background(this_cause, stream));
 
 f2ptr f2__ansi__stream__green_background(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 42);
   return nil;
 }
-def_pcfunk1(ansi__stream__green_background, stream, return f2__ansi__stream__green_background(this_cause, stream));
+def_pcfunk1(ansi__stream__green_background, stream,
+	    "",
+	    return f2__ansi__stream__green_background(this_cause, stream));
 
 f2ptr f2__ansi__stream__brown_background(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 43);
   return nil;
 }
-def_pcfunk1(ansi__stream__brown_background, stream, return f2__ansi__stream__brown_background(this_cause, stream));
+def_pcfunk1(ansi__stream__brown_background, stream,
+	    "",
+	    return f2__ansi__stream__brown_background(this_cause, stream));
 
 f2ptr f2__ansi__stream__blue_background(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 44);
   return nil;
 }
-def_pcfunk1(ansi__stream__blue_background, stream, return f2__ansi__stream__blue_background(this_cause, stream));
+def_pcfunk1(ansi__stream__blue_background, stream,
+	    "",
+	    return f2__ansi__stream__blue_background(this_cause, stream));
 
 f2ptr f2__ansi__stream__magenta_background(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 45);
   return nil;
 }
-def_pcfunk1(ansi__stream__magenta_background, stream, return f2__ansi__stream__magenta_background(this_cause, stream));
+def_pcfunk1(ansi__stream__magenta_background, stream,
+	    "",
+	    return f2__ansi__stream__magenta_background(this_cause, stream));
 
 f2ptr f2__ansi__stream__cyan_background(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 46);
   return nil;
 }
-def_pcfunk1(ansi__stream__cyan_background, stream, return f2__ansi__stream__cyan_background(this_cause, stream));
+def_pcfunk1(ansi__stream__cyan_background, stream,
+	    "",
+	    return f2__ansi__stream__cyan_background(this_cause, stream));
 
 f2ptr f2__ansi__stream__white_background(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 47);
   return nil;
 }
-def_pcfunk1(ansi__stream__white_background, stream, return f2__ansi__stream__white_background(this_cause, stream));
+def_pcfunk1(ansi__stream__white_background, stream,
+	    "",
+	    return f2__ansi__stream__white_background(this_cause, stream));
 
 f2ptr f2__ansi__stream__default_background(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__ansi__stream__print_code(cause, stream, 49);
   return nil;
 }
-def_pcfunk1(ansi__stream__default_background, stream, return f2__ansi__stream__default_background(this_cause, stream));
+def_pcfunk1(ansi__stream__default_background, stream,
+	    "",
+	    return f2__ansi__stream__default_background(this_cause, stream));
 
 f2ptr f2__ansi__stream__clear_screen(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
@@ -256,7 +318,9 @@ f2ptr f2__ansi__stream__clear_screen(f2ptr cause, f2ptr stream) {
   f2__cmutex__unlock(cause, f2__stream__cmutex(cause, stream));
   return nil;
 }
-def_pcfunk1(ansi__stream__clear_screen, stream, return f2__ansi__stream__clear_screen(this_cause, stream));
+def_pcfunk1(ansi__stream__clear_screen, stream,
+	    "",
+	    return f2__ansi__stream__clear_screen(this_cause, stream));
 
 f2ptr f2__ansi__stream__clear_keyboard_leds(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
@@ -266,7 +330,9 @@ f2ptr f2__ansi__stream__clear_keyboard_leds(f2ptr cause, f2ptr stream) {
   f2__cmutex__unlock(cause, f2__stream__cmutex(cause, stream));
   return nil;
 }
-def_pcfunk1(ansi__stream__clear_keyboard_leds, stream, return f2__ansi__stream__clear_keyboard_leds(this_cause, stream));
+def_pcfunk1(ansi__stream__clear_keyboard_leds, stream,
+	    "",
+	    return f2__ansi__stream__clear_keyboard_leds(this_cause, stream));
 
 f2ptr f2__ansi__stream__set_scroll_lock_led(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
@@ -276,7 +342,9 @@ f2ptr f2__ansi__stream__set_scroll_lock_led(f2ptr cause, f2ptr stream) {
   f2__cmutex__unlock(cause, f2__stream__cmutex(cause, stream));
   return nil;
 }
-def_pcfunk1(ansi__stream__set_scroll_lock_led, stream, return f2__ansi__stream__set_scroll_lock_led(this_cause, stream));
+def_pcfunk1(ansi__stream__set_scroll_lock_led, stream,
+	    "",
+	    return f2__ansi__stream__set_scroll_lock_led(this_cause, stream));
 
 f2ptr f2__ansi__stream__set_num_lock_led(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
@@ -286,7 +354,9 @@ f2ptr f2__ansi__stream__set_num_lock_led(f2ptr cause, f2ptr stream) {
   f2__cmutex__unlock(cause, f2__stream__cmutex(cause, stream));
   return nil;
 }
-def_pcfunk1(ansi__stream__set_num_lock_led, stream, return f2__ansi__stream__set_num_lock_led(this_cause, stream));
+def_pcfunk1(ansi__stream__set_num_lock_led, stream,
+	    "",
+	    return f2__ansi__stream__set_num_lock_led(this_cause, stream));
 
 f2ptr f2__ansi__stream__set_caps_lock_led(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
@@ -296,7 +366,9 @@ f2ptr f2__ansi__stream__set_caps_lock_led(f2ptr cause, f2ptr stream) {
   f2__cmutex__unlock(cause, f2__stream__cmutex(cause, stream));
   return nil;
 }
-def_pcfunk1(ansi__stream__set_caps_lock_led, stream, return f2__ansi__stream__set_caps_lock_led(this_cause, stream));
+def_pcfunk1(ansi__stream__set_caps_lock_led, stream,
+	    "",
+	    return f2__ansi__stream__set_caps_lock_led(this_cause, stream));
 
 void raw__ansi__stream__move_cursor(f2ptr cause, f2ptr stream, int x, int y) {
   if (x < 0 || y < 0) {
@@ -317,14 +389,18 @@ f2ptr f2__ansi__stream__move_cursor(f2ptr cause, f2ptr stream, f2ptr x, f2ptr y)
   raw__ansi__stream__move_cursor(cause, stream, raw_x, raw_y);
   return nil;
 }
-def_pcfunk3(ansi__stream__move_cursor, stream, x, y, return f2__ansi__stream__move_cursor(this_cause, stream, x, y));
+def_pcfunk3(ansi__stream__move_cursor, stream, x, y,
+	    "",
+	    return f2__ansi__stream__move_cursor(this_cause, stream, x, y));
 
 f2ptr f2__ansi__stream__beep(f2ptr cause, f2ptr stream) {
   assert_argument_type(stream, stream);
   raw__stream__writef(cause, stream, "%c", 7);
   return nil;
 }
-def_pcfunk1(ansi__stream__beep, stream, return f2__ansi__stream__beep(this_cause, stream));
+def_pcfunk1(ansi__stream__beep, stream,
+	    "",
+	    return f2__ansi__stream__beep(this_cause, stream));
 
 
 f2ptr raw__ansi__stream__foreground(f2ptr cause, f2ptr stream, ansi_color_t color) {
@@ -355,7 +431,9 @@ f2ptr f2__ansi__stream__foreground(f2ptr cause, f2ptr stream, f2ptr color) {
   ansi_color_t raw_color = (ansi_color_t)f2integer__i(color, cause);
   return raw__ansi__stream__foreground(cause, stream, raw_color);
 }
-def_pcfunk2(ansi__stream__foreground, stream, color, return f2__ansi__stream__foreground(this_cause, stream, color));
+def_pcfunk2(ansi__stream__foreground, stream, color,
+	    "",
+	    return f2__ansi__stream__foreground(this_cause, stream, color));
 
 f2ptr raw__ansi__stream__background(f2ptr cause, f2ptr stream, ansi_color_t color) {
   switch(color) {
@@ -385,7 +463,9 @@ f2ptr f2__ansi__stream__background(f2ptr cause, f2ptr stream, f2ptr color) {
   ansi_color_t raw_color = (ansi_color_t)f2integer__i(color, cause);
   return raw__ansi__stream__background(cause, stream, raw_color);
 }
-def_pcfunk2(ansi__stream__background, stream, color, return f2__ansi__stream__background(this_cause, stream, color));
+def_pcfunk2(ansi__stream__background, stream, color,
+	    "",
+	    return f2__ansi__stream__background(this_cause, stream, color));
 
 void raw__ansi__stream__rectangle(f2ptr cause, f2ptr stream, s64 x0, s64 y0, s64 x1, s64 y1, u8 ch) {
   if (y0 > y1) {u32 swap = y0; y0 = y1; y1 = swap;}
@@ -415,7 +495,9 @@ f2ptr f2__ansi__stream__rectangle(f2ptr cause, f2ptr stream, f2ptr x0, f2ptr y0,
   raw__ansi__stream__rectangle(cause, stream, raw_x0, raw_y0, raw_x1, raw_y1, raw_ch);
   return nil;
 }
-def_pcfunk6(ansi__stream__rectangle, stream, x0, y0, x1, y1, ch, return f2__ansi__stream__rectangle(this_cause, stream, x0, y0, x1, y1, ch));
+def_pcfunk6(ansi__stream__rectangle, stream, x0, y0, x1, y1, ch,
+	    "",
+	    return f2__ansi__stream__rectangle(this_cause, stream, x0, y0, x1, y1, ch));
 
 
 void raw__ansi__stream__bordered_rectangle(f2ptr cause, f2ptr stream, int x0, int y0, int x1, int y1, char background_char) {
@@ -452,7 +534,9 @@ f2ptr f2__ansi__stream__bordered_rectangle(f2ptr cause, f2ptr stream, f2ptr x0, 
   raw__ansi__stream__bordered_rectangle(cause, stream, raw_x0, raw_y0, raw_x1, raw_y1, raw_background_char);
   return nil;
 }
-def_pcfunk6(ansi__stream__bordered_rectangle, stream, x0, y0, x1, y1, background_char, return f2__ansi__stream__bordered_rectangle(this_cause, stream, x0, y0, x1, y1, background_char));
+def_pcfunk6(ansi__stream__bordered_rectangle, stream, x0, y0, x1, y1, background_char,
+	    "",
+	    return f2__ansi__stream__bordered_rectangle(this_cause, stream, x0, y0, x1, y1, background_char));
 
 // **
 

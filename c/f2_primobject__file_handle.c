f4c56929ea942838c74634f84bd7a43464ed899f
Bo Morgan <bo@mit.edu>
2011-04-09 16:23:29 -0400

    adding f2_primobject__file_handle.[ch]

--- c/f2_primobject__file_handle.c
+++ c/f2_primobject__file_handle.c
@@ -79,7 +79,7 @@ void f2__primobject__file_handle__reinitialize_globalvars() {
 void f2__primobject__file_handle__initialize() {
   f2ptr cause = initial_cause();
   
-  funk2_module_registration__add_module(&(__funk2.module_registration), "file_handle", "", &f2__file_handle__reinitialize_globalvars);
+  funk2_module_registration__add_module(&(__funk2.module_registration), "file_handle", "", &f2__primobject__file_handle__reinitialize_globalvars);
   
   f2__file_handle__reinitialize_globalvars();
   

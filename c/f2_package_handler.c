146b3dd5c37bf8b76d750a083578bb52f917b2fb
Bo Morgan <bo@mit.edu>
2011-04-20 12:47:53 -0400

    adding c/f2_package_handler.[ch]

--- c/f2_package_handler.c
+++ c/f2_package_handler.c
@@ -55,7 +55,7 @@ void f2__package_handler__reinitialize_globalvars() {
 void f2__package_handler__initialize() {
   f2__package_handler__reinitialize_globalvars();
   funk2_module_registration__add_module(&(__funk2.module_registration), "package_handler", "", &f2__cause__reinitialize_globalvars);
-  //f2ptr cause = initial_cause();
+  f2ptr cause = initial_cause();
   
   // package_handler
   

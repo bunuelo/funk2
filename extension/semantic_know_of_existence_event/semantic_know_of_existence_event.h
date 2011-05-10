4538822c2e156a3aec1d37cc6e4d20505fa927b9
Bo Morgan <bo@mit.edu>
2011-05-10 16:24:24 -0400

    adding semantic_know_of_existence_event extension

---
+++ extension/semantic_know_of_existence_event/semantic_know_of_existence_event.h
@@ -0,0 +1,69 @@
+// 
+// Copyright (c) 2007-2011 Bo Morgan.
+// All rights reserved.
+// 
+// Author: Bo Morgan
+// 
+// Permission to use, copy, modify and distribute this software and its
+// documentation is hereby granted, provided that both the copyright
+// notice and this permission notice appear in all copies of the
+// software, derivative works or modified versions, and any portions
+// thereof, and that both notices appear in supporting documentation.
+// 
+// BO MORGAN ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS" CONDITION.
+// BO MORGAN DISCLAIMS ANY LIABILITY OF ANY KIND FOR ANY DAMAGES
+// WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+// 
+// Bo Morgan requests users of this software to return to bo@mit.edu any
+// improvements or extensions that they make and grant Bo Morgan the
+// rights to redistribute these changes.
+// 
+
+#ifndef F2__EXTENSION__SEMANTIC_KNOW_OF_EXISTENCE_EVENT__H
+#define F2__EXTENSION__SEMANTIC_KNOW_OF_EXISTENCE_EVENT__H
+
+#include "../semantic_temporal_object/semantic_temporal_object.h"
+
+
+f2ptr     raw__semantic_know_of_existence_event__type_create                                                      (f2ptr cause, f2ptr this, f2ptr semantic_realm, f2ptr action_name);
+f2ptr     raw__semantic_know_of_existence_event__new                                                              (f2ptr cause, f2ptr semantic_realm, f2ptr action_name);
+f2ptr      f2__semantic_know_of_existence_event__new                                                              (f2ptr cause, f2ptr semantic_realm, f2ptr action_name);
+boolean_t raw__semantic_know_of_existence_event__is_type                                                          (f2ptr cause, f2ptr thing);
+f2ptr      f2__semantic_know_of_existence_event__is_type                                                          (f2ptr cause, f2ptr thing);
+f2ptr     raw__semantic_know_of_existence_event__type                                                             (f2ptr cause, f2ptr this);
+f2ptr      f2__semantic_know_of_existence_event__type                                                             (f2ptr cause, f2ptr this);
+f2ptr     raw__semantic_know_of_existence_event__action_name__lookup                                              (f2ptr cause, f2ptr this);
+f2ptr      f2__semantic_know_of_existence_event__action_name__lookup                                              (f2ptr cause, f2ptr this);
+f2ptr     raw__semantic_know_of_existence_event__action_name__add                                                 (f2ptr cause, f2ptr this, f2ptr that);
+f2ptr      f2__semantic_know_of_existence_event__action_name__add                                                 (f2ptr cause, f2ptr this, f2ptr that);
+f2ptr     raw__semantic_know_of_existence_event__action_name__remove                                              (f2ptr cause, f2ptr this, f2ptr that);
+f2ptr      f2__semantic_know_of_existence_event__action_name__remove                                              (f2ptr cause, f2ptr this, f2ptr that);
+f2ptr     raw__semantic_know_of_existence_event__absolute_start_time__lookup                                      (f2ptr cause, f2ptr this);
+f2ptr      f2__semantic_know_of_existence_event__absolute_start_time__lookup                                      (f2ptr cause, f2ptr this);
+f2ptr     raw__semantic_know_of_existence_event__absolute_start_time__add                                         (f2ptr cause, f2ptr this, f2ptr that);
+f2ptr      f2__semantic_know_of_existence_event__absolute_start_time__add                                         (f2ptr cause, f2ptr this, f2ptr that);
+f2ptr     raw__semantic_know_of_existence_event__absolute_start_time__remove                                      (f2ptr cause, f2ptr this, f2ptr that);
+f2ptr      f2__semantic_know_of_existence_event__absolute_start_time__remove                                      (f2ptr cause, f2ptr this, f2ptr that);
+f2ptr     raw__semantic_know_of_existence_event__absolute_end_time__lookup                                        (f2ptr cause, f2ptr this);
+f2ptr      f2__semantic_know_of_existence_event__absolute_end_time__lookup                                        (f2ptr cause, f2ptr this);
+f2ptr     raw__semantic_know_of_existence_event__absolute_end_time__add                                           (f2ptr cause, f2ptr this, f2ptr that);
+f2ptr      f2__semantic_know_of_existence_event__absolute_end_time__add                                           (f2ptr cause, f2ptr this, f2ptr that);
+f2ptr     raw__semantic_know_of_existence_event__absolute_end_time__remove                                        (f2ptr cause, f2ptr this, f2ptr that);
+f2ptr      f2__semantic_know_of_existence_event__absolute_end_time__remove                                        (f2ptr cause, f2ptr this, f2ptr that);
+f2ptr     raw__semantic_know_of_existence_event__update_parent_container_start_and_end_times_with_child_start_time(f2ptr cause, f2ptr this, f2ptr child_start_time);
+f2ptr     raw__semantic_know_of_existence_event__update_parent_container_start_and_end_times_with_child_end_time  (f2ptr cause, f2ptr this, f2ptr child_end_time);
+f2ptr     raw__semantic_know_of_existence_event__update_parent_container_start_and_end_times_with_child_event     (f2ptr cause, f2ptr this, f2ptr child_semantic_know_of_existence_event);
+f2ptr      f2__semantic_know_of_existence_event__update_parent_container_start_and_end_times_with_child_event     (f2ptr cause, f2ptr this, f2ptr child_semantic_know_of_existence_event);
+f2ptr     raw__semantic_know_of_existence_event__update_child_container_start_and_end_times_with_parent_start_time(f2ptr cause, f2ptr this, f2ptr parent_start_time);
+f2ptr     raw__semantic_know_of_existence_event__update_child_container_start_and_end_times_with_parent_end_time  (f2ptr cause, f2ptr this, f2ptr parent_end_time);
+f2ptr     raw__semantic_know_of_existence_event__update_child_container_start_and_end_times_with_parent_event     (f2ptr cause, f2ptr this, f2ptr parent_semantic_know_of_existence_event);
+f2ptr      f2__semantic_know_of_existence_event__update_child_container_start_and_end_times_with_parent_event     (f2ptr cause, f2ptr this, f2ptr parent_semantic_know_of_existence_event);
+
+// **
+
+f2ptr f2__semantic_know_of_existence_event__core_extension__ping(f2ptr cause);
+f2ptr f2__semantic_know_of_existence_event__core_extension__initialize(f2ptr cause);
+f2ptr f2__semantic_know_of_existence_event__core_extension__destroy(f2ptr cause);
+
+#endif // F2__EXTENSION__SEMANTIC_KNOW_OF_EXISTENCE_EVENT__H
+

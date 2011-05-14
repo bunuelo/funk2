b666f8c2e3bc1175ecd43d11be9f0a42a9c9bd1a
Bo Morgan <bo@mit.edu>
2011-05-14 17:34:51 -0400

    adding phenomenal_name to semantic_object and removing visual_phenomenal_name from visual_semantic_object

--- c/f2_time.c
+++ c/f2_time.c
@@ -630,7 +630,7 @@ f2ptr f2time__primobject_type__new_aux(f2ptr cause) {
   {char* slot_name = "day_of_year";               f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.get__symbol,     new__symbol(cause, slot_name), __funk2.globalenv.object_type.primobject.primobject_type_time.day_of_year__funk);}
   {char* slot_name = "is_daylight_savings_time";  f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.get__symbol,     new__symbol(cause, slot_name), __funk2.globalenv.object_type.primobject.primobject_type_time.is_daylight_savings_time__funk);}
   {char* slot_name = "timezone";                  f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.get__symbol,     new__symbol(cause, slot_name), __funk2.globalenv.object_type.primobject.primobject_type_time.timezone__funk);}
-  {char* slot_name = "as-graphviz_label";         f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.execute__symbol, new__symbol(cause, slot_name), __funk2.globalenv.object_type.primobject.primobject_type_time.as__graphviz_label__funk);}
+  {char* slot_name = "as-graphviz_label";         f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.get__symbol,     new__symbol(cause, slot_name), __funk2.globalenv.object_type.primobject.primobject_type_time.as__graphviz_label__funk);}
   {char* slot_name = "terminal_print_with_frame"; f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.execute__symbol, new__symbol(cause, slot_name), __funk2.globalenv.object_type.primobject.primobject_type_time.terminal_print_with_frame__funk);}
   return this;
 }

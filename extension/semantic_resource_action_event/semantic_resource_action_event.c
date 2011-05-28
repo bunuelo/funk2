e524b597d0858b973c88d104caab10061d267ba8
Bo Morgan <bo@mit.edu>
2011-05-28 13:06:59 -0400

    working on reflective_event_knowledge

--- extension/semantic_resource_action_event/semantic_resource_action_event.c
+++ extension/semantic_resource_action_event/semantic_resource_action_event.c
@@ -30,7 +30,6 @@ f2ptr raw__semantic_resource_action_event__new(f2ptr cause, f2ptr semantic_realm
     return this;
   }
   raw__frame__add_var_value(cause, this, new__symbol(cause, "type"), new__symbol(cause, "semantic_resource_action_event"));
-  raw__semantic_frame__add( cause, this, new__symbol(cause, "type"), new__symbol(cause, "name"), new__symbol(cause, "semantic_resource_action_event"));
   return this;
 }
 

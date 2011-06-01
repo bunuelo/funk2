2cb5064b095c11f5107ab0ef7cd24f56cf5ff733
Bo Morgan <bo@mit.edu>
2011-06-01 11:58:42 -0400

    adding test/interval_tree-test

--- c/f2_primobject__interval_tree.c
+++ c/f2_primobject__interval_tree.c
@@ -55,7 +55,7 @@ f2ptr raw__interval_tree__insert(f2ptr cause, f2ptr this, f2ptr element) {
     f2ptr left_value   = assert_value(f2__force_funk_apply(cause, f2__this__fiber(cause), left_value_funk,   f2list1__new(cause, element)));
     f2ptr right_value  = assert_value(f2__force_funk_apply(cause, f2__this__fiber(cause), right_value_funk,  f2list1__new(cause, element)));
     f2ptr center_value = assert_value(f2__force_funk_apply(cause, f2__this__fiber(cause), value_center_funk, f2list2__new(cause, left_value, right_value)));
-    head = f2__interval_tree_node__new(cause, new__symbol(cause, "red"), center_value, left_value_funk, right_value_funk, value_comparison_funk);
+    head = f2__interval_tree_node__new(cause, nil, center_value, left_value_funk, right_value_funk, value_comparison_funk);
     f2__interval_tree__head__set(cause, this, head);
   }
   return raw__interval_tree_node__insert(cause, head, element, left_value_funk, right_value_funk, value_comparison_funk, value_center_funk);
@@ -186,7 +186,7 @@ f2ptr raw__interval_tree_node__simple_insert(f2ptr cause, f2ptr this, f2ptr elem
       f2ptr left_node = f2__interval_tree_node__left_node(cause, this);
       if (left_node == nil) {
 	f2ptr center_value = assert_value(f2__force_funk_apply(cause, f2__this__fiber(cause), value_center_funk, f2list2__new(cause, left_value, right_value)));
-	left_node = f2__interval_tree_node__new(cause, new__symbol(cause, "red"), center_value, left_value_funk, right_value_funk, value_comparison_funk);
+	left_node = f2__interval_tree_node__new(cause, nil, center_value, left_value_funk, right_value_funk, value_comparison_funk);
 	f2__interval_tree_node__parent_node__set(cause, left_node, this);
 	f2__interval_tree_node__left_node__set(  cause, this,      left_node);
       }
@@ -196,7 +196,7 @@ f2ptr raw__interval_tree_node__simple_insert(f2ptr cause, f2ptr this, f2ptr elem
       f2ptr right_node = f2__interval_tree_node__right_node(cause, this);
       if (right_node == nil) {
 	f2ptr center_value = assert_value(f2__force_funk_apply(cause, f2__this__fiber(cause), value_center_funk, f2list2__new(cause, left_value, right_value)));
-	right_node = f2__interval_tree_node__new(cause, new__symbol(cause, "red"), center_value, left_value_funk, right_value_funk, value_comparison_funk);
+	right_node = f2__interval_tree_node__new(cause, nil, center_value, left_value_funk, right_value_funk, value_comparison_funk);
 	f2__interval_tree_node__parent_node__set(cause, right_node, this);
 	f2__interval_tree_node__right_node__set( cause, this,       right_node);
       }

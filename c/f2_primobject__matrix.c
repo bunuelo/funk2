c096c1df14a61519a39ce389cb11babe6393b06b
Bo Morgan <bo@mit.edu>
2011-06-04 16:41:15 -0400

    adding f2_primobject__matrix.[ch]

--- c/f2_primobject__matrix.c
+++ c/f2_primobject__matrix.c
@@ -119,8 +119,8 @@ f2ptr raw__matrix__terminal_print_with_frame(f2ptr cause, f2ptr this, f2ptr term
   f2ptr frame               = raw__ptypehash__lookup(cause, print_as_frame_hash, this);
   if (frame == nil) {
     frame = f2__frame__new(cause, f2list4__new(cause,
-					       new__symbol(cause, "print_object_type"),   new__symbol(cause, "matrix"),
-					       new__symbol(cause, "row_first_ptypehash"), f2__matrix__row_first_ptypehash(cause, this)));
+					       new__symbol(cause, "print_object_type"),    new__symbol(cause, "matrix"),
+					       new__symbol(cause, "column_row_ptypehash"), f2__matrix__column_row_ptypehash(cause, this)));
     f2__ptypehash__add(cause, print_as_frame_hash, this, frame);
   }
   return raw__frame__terminal_print_with_frame(cause, frame, terminal_print_frame);

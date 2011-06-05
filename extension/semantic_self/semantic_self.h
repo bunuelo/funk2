afd85c41bc993c948fa9eb12e8d570528bf2649a
Bo Morgan <bo@mit.edu>
2011-06-05 14:03:33 -0400

    adding semantic_self core extension

--- extension/semantic_self/semantic_self.h
+++ extension/semantic_self/semantic_self.h
@@ -26,9 +26,9 @@
 #include "../semantic_event/semantic_event.h"
 
 
-f2ptr     raw__semantic_self__type_create  (f2ptr cause, f2ptr this, f2ptr semantic_realm, f2ptr time);
-f2ptr     raw__semantic_self__new          (f2ptr cause, f2ptr semantic_realm, f2ptr time);
-f2ptr      f2__semantic_self__new          (f2ptr cause, f2ptr semantic_realm, f2ptr time);
+f2ptr     raw__semantic_self__type_create  (f2ptr cause, f2ptr this, f2ptr semantic_realm);
+f2ptr     raw__semantic_self__new          (f2ptr cause, f2ptr semantic_realm);
+f2ptr      f2__semantic_self__new          (f2ptr cause, f2ptr semantic_realm);
 boolean_t raw__semantic_self__is_type      (f2ptr cause, f2ptr thing);
 f2ptr      f2__semantic_self__is_type      (f2ptr cause, f2ptr thing);
 f2ptr     raw__semantic_self__type         (f2ptr cause, f2ptr this);

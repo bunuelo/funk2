f680f4edf1e7f976ccea922b648452a21c03cf55
Bo Morgan <bo@mit.edu>
2011-06-07 19:08:31 -0400

    optimizing mutex by using trigger instead of sleep.  adding cmutex to fiber_trigger

--- extension/fibermon/fibermon.c
+++ extension/fibermon/fibermon.c
@@ -84,6 +84,8 @@ f2ptr f2__fibermon_fiber__construct_fast(f2ptr cause, f2ptr this) {
   f2__frame__add_var_value(cause, this, new__symbol(cause, "view_menu"), this__view_menu);
   f2ptr this__view_fiber_menu_item = raw__gtk__menu_item__new(cause, new__string(cause, "Fiber"));
   f2__frame__add_var_value(cause, this, new__symbol(cause, "view_fiber_menu_item"), this__view_fiber_menu_item);
+  f2ptr this__view_fiber_value_menu_item = raw__gtk__menu_item__new(cause, new__string(cause, "Fiber Value"));
+  f2__frame__add_var_value(cause, this, new__symbol(cause, "view_fiber_value_menu_item"), this__view_fiber_value_menu_item);
   f2ptr this__view_cause_menu_item = raw__gtk__menu_item__new(cause, new__string(cause, "Cause"));
   f2__frame__add_var_value(cause, this, new__symbol(cause, "view_cause_menu_item"), this__view_cause_menu_item);
   f2ptr this__view_stack_trace_menu_item = raw__gtk__menu_item__new(cause, new__string(cause, "Stack Trace"));
@@ -122,6 +124,7 @@ f2ptr f2__fibermon_fiber__construct_fast(f2ptr cause, f2ptr this) {
   raw__gtk__menu_item__set_submenu(cause, this__command_menu_item, this__command_menu);
   
   raw__gtk__menu__append(cause, this__view_menu, this__view_fiber_menu_item);
+  raw__gtk__menu__append(cause, this__view_menu, this__view_fiber_value_menu_item);
   raw__gtk__menu__append(cause, this__view_menu, this__view_cause_menu_item);
   raw__gtk__menu__append(cause, this__view_menu, this__view_stack_trace_menu_item);
   raw__gtk__menu__append(cause, this__view_menu, this__view_stack_trace_first_block_menu_item);

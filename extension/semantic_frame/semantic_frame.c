ee159325cfddfb1438546b5cd9e9d9696afc1175
Bo Morgan <bo@mit.edu>
2011-06-07 19:23:20 -0400

    optimizing mutex by using trigger instead of sleep.  adding cmutex to fiber_trigger

--- extension/semantic_frame/semantic_frame.c
+++ extension/semantic_frame/semantic_frame.c
@@ -819,40 +819,52 @@ export_cefunk3(object__semantic__remove__apply, this, slot, args, 0, "");
 
 
 f2ptr f2__object__semantic__get(f2ptr cause, f2ptr this, f2ptr slot, f2ptr args) {
-  f2ptr     value_set           = f2__object__semantic__lookup__apply(cause, this, slot, args);
-  boolean_t found_current_value = boolean__false;
-  f2ptr     current_value       = nil;
-  if (value_set != nil) {
-    if (! raw__set__is_type(cause, value_set)) {
-      return f2larva__new(cause, 2734, f2__bug__new(cause, f2integer__new(cause, 2734), f2__frame__new(cause, f2list10__new(cause,
-															    new__symbol(cause, "bug_type"), new__symbol(cause, "object_slot_contains_non_set"),
-															    new__symbol(cause, "funkname"), new__symbol(cause, "object-semantic-get"),
-															    new__symbol(cause, "this"),     this,
-															    new__symbol(cause, "slot"),     slot,
-															    new__symbol(cause, "args"),     args))));
+  assert_argument_type(semantic_frame, this);
+  f2ptr frame_mutate_cmutex = raw__semantic_frame__frame_mutate_cmutex(cause, this);
+  f2ptr result = nil;
+  f2__cmutex__lock(cause, frame_mutate_cmutex);
+  {
+    f2ptr     value_set           = f2__object__semantic__lookup__apply(cause, this, slot, args);
+    boolean_t found_current_value = boolean__false;
+    f2ptr     current_value       = nil;
+    if (value_set != nil) {
+      if (! raw__set__is_type(cause, value_set)) {
+	result = f2larva__new(cause, 2734, f2__bug__new(cause, f2integer__new(cause, 2734), f2__frame__new(cause, f2list10__new(cause,
+																new__symbol(cause, "bug_type"), new__symbol(cause, "object_slot_contains_non_set"),
+																new__symbol(cause, "funkname"), new__symbol(cause, "object-semantic-get"),
+																new__symbol(cause, "this"),     this,
+																new__symbol(cause, "slot"),     slot,
+																new__symbol(cause, "args"),     args))));
+	goto f2__object__semantic__get__unlock_mutex_and_return;
+      }
+      set__iteration(cause, value_set, value,
+		     if (found_current_value) {
+		       result = f2larva__new(cause, 2347, f2__bug__new(cause, f2integer__new(cause, 2346), f2__frame__new(cause, f2list10__new(cause,
+																	       new__symbol(cause, "bug_type"), new__symbol(cause, "object_has_more_than_one_value_in_slot"),
+																	       new__symbol(cause, "funkname"), new__symbol(cause, "object-semantic-get"),
+																	       new__symbol(cause, "this"),     this,
+																	       new__symbol(cause, "slot"),     slot,
+																	       new__symbol(cause, "args"),     args))));
+		       goto f2__object__semantic__get__unlock_mutex_and_return;
+		     }
+		     current_value = value;
+		     found_current_value = boolean__true;
+		     );
     }
-    set__iteration(cause, value_set, value,
-		   if (found_current_value) {
-		     return f2larva__new(cause, 2347, f2__bug__new(cause, f2integer__new(cause, 2346), f2__frame__new(cause, f2list10__new(cause,
-																	   new__symbol(cause, "bug_type"), new__symbol(cause, "object_has_more_than_one_value_in_slot"),
-																	   new__symbol(cause, "funkname"), new__symbol(cause, "object-semantic-get"),
-																	   new__symbol(cause, "this"),     this,
-																	   new__symbol(cause, "slot"),     slot,
-																	   new__symbol(cause, "args"),     args))));
-		   }
-		   current_value = value;
-		   found_current_value = boolean__true;
-		   );
+    if (! found_current_value) {
+      result = f2larva__new(cause, 2347, f2__bug__new(cause, f2integer__new(cause, 2346), f2__frame__new(cause, f2list10__new(cause,
+															      new__symbol(cause, "bug_type"), new__symbol(cause, "object_does_not_have_slot_value"),
+															      new__symbol(cause, "funkname"), new__symbol(cause, "object-semantic-get"),
+															      new__symbol(cause, "this"),     this,
+															      new__symbol(cause, "slot"),     slot,
+															      new__symbol(cause, "args"),     args))));
+      goto f2__object__semantic__get__unlock_mutex_and_return;
+    }
+    result = current_value;
   }
-  if (! found_current_value) {
-    return f2larva__new(cause, 2347, f2__bug__new(cause, f2integer__new(cause, 2346), f2__frame__new(cause, f2list10__new(cause,
-															  new__symbol(cause, "bug_type"), new__symbol(cause, "object_does_not_have_slot_value"),
-															  new__symbol(cause, "funkname"), new__symbol(cause, "object-semantic-get"),
-															  new__symbol(cause, "this"),     this,
-															  new__symbol(cause, "slot"),     slot,
-															  new__symbol(cause, "args"),     args))));
-  }
-  return current_value;
+ f2__object__semantic__get__unlock_mutex_and_return:
+  f2__cmutex__unlock(cause, frame_mutate_cmutex);
+  return result;
 }
 export_cefunk2_and_rest(object__semantic__get, this, slot, args, 0, "");
 

830dabc13cd318c569bdac6c4e6f50d9e5a0cc70
Bo Morgan <bo@mit.edu>
2011-06-19 17:55:52 -0400

    replacing lock with trylock

--- c/f2_virtual_processor_thread.c
+++ c/f2_virtual_processor_thread.c
@@ -193,6 +193,7 @@ void* funk2_virtual_processor_thread__start_function(void* args) {
 	    (line_length == (spinning_virtual_processor_thread_count - 1))) {
 	  funk2_virtual_processor__know_of_one_less_spinning_virtual_processor_thread(virtual_processor);
 	  funk2_virtual_processor_thread__unassign_from_virtual_processor(this);
+	  funk2_virtual_processor__assure_at_least_one_spinning_virtual_processor_thread(virtual_processor);
 	} else {
 	  funk2_virtual_processor_thread__spin_sleep_yield(this);
 	  //f2__nanosleep(line_length * line_length * 10000000);

48646ef9834a12bb1ffd25b7c6faf9cfe0e1bc78
Bo Morgan <bo@mit.edu>
2011-06-19 20:40:36 -0400

    adding back a few changes after optimizing for nautilus

--- c/f2_virtual_processor_thread.c
+++ c/f2_virtual_processor_thread.c
@@ -192,6 +192,7 @@ void* funk2_virtual_processor_thread__start_function(void* args) {
 	if ((spinning_virtual_processor_thread_count > 2) &&
 	    (line_length == (spinning_virtual_processor_thread_count - 1))) {
 	  funk2_virtual_processor__know_of_one_less_spinning_virtual_processor_thread(virtual_processor);
+	  funk2_virtual_processor__know_of_one_less_assigned_virtual_processor_thread(virtual_processor);
 	  funk2_virtual_processor_thread__unassign_from_virtual_processor(this);
 	} else {
 	  funk2_virtual_processor_thread__spin_sleep_yield(this);

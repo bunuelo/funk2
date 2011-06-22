f5aae02d110e63872586b9b6a2dd965cc655bf63
Bo Morgan <bo@mit.edu>
2011-06-22 11:18:17 -0400

    adding concept_version_space core extension

--- extension/concept_version_space/concept_version_space.c
+++ extension/concept_version_space/concept_version_space.c
@@ -113,6 +113,64 @@ f2ptr f2__concept_version_space_example_type__new_aux(f2ptr cause) {
 }
 
 
+// concept_version_space_hypothesis
+
+def_ceframe1(concept_version_space, concept_version_space_hypothesis, value_variable_name_ptypehash);
+
+f2ptr raw__concept_version_space_hypothesis__new(f2ptr cause) {
+  f2ptr value_variable_name_ptypehash = f2__ptypehash__new(cause);
+  return f2concept_version_space_hypothesis__new(cause, value_variable_name_ptypehash);
+}
+
+f2ptr f2__concept_version_space_hypothesis__new(f2ptr cause) {
+  return raw__concept_version_space_hypothesis__new(cause);
+}
+export_cefunk0(concept_version_space_hypothesis__new, 0, "Returns a new concept_version_space_hypothesis object.");
+
+
+f2ptr raw__concept_version_space_hypothesis__add_variable_value(f2ptr cause, f2ptr this, f2ptr variable_name, f2ptr value) {
+  f2ptr value_variable_name_ptypehash = raw__concept_version_space_hypothesis__value_variable_name_ptypehash(cause, this);
+  raw__ptypehash__add(cause, value_variable_name_ptypehash, variable_name, value);
+  return nil;
+}
+
+f2ptr f2__concept_version_space_hypothesis__add_variable_value(f2ptr cause, f2ptr this, f2ptr variable_name, f2ptr value) {
+  return raw__concept_version_space_hypothesis__add_variable_value(cause, this, variable_name, value);
+}
+export_cefunk3(concept_version_space_hypothesis__add_variable_value, this, variable_name, value, 0, "Adds a variable value to this concept_version_space_hypothesis.");
+
+
+f2ptr raw__concept_version_space_hypothesis__terminal_print_with_frame(f2ptr cause, f2ptr this, f2ptr terminal_print_frame) {
+  f2ptr print_as_frame_hash = raw__terminal_print_frame__print_as_frame_hash(cause, terminal_print_frame);
+  f2ptr frame               = raw__ptypehash__lookup(cause, print_as_frame_hash, this);
+  if (frame == nil) {
+    frame = f2__frame__new(cause, f2list2__new(cause,
+					       new__symbol(cause, "print_object_type"), f2__object__type(cause, this)));
+    f2ptr value_variable_name_ptypehash = raw__concept_version_space_hypothesis__value_variable_name_ptypehash(cause, this);
+    ptypehash__iteration(cause, value_variable_name_ptypehash, key, value,
+			 f2__frame__add_var_value(cause, frame, key, value);
+			 );
+    f2__ptypehash__add(cause, print_as_frame_hash, this, frame);
+  }
+  return raw__frame__terminal_print_with_frame(cause, frame, terminal_print_frame);
+}
+
+f2ptr f2__concept_version_space_hypothesis__terminal_print_with_frame(f2ptr cause, f2ptr this, f2ptr terminal_print_frame) {
+  assert_argument_type(concept_version_space_hypothesis,       this);
+  assert_argument_type(terminal_print_frame, terminal_print_frame);
+  return raw__concept_version_space_hypothesis__terminal_print_with_frame(cause, this, terminal_print_frame);
+}
+export_cefunk2(concept_version_space_hypothesis__terminal_print_with_frame, this, terminal_print_frame, 0, "");
+
+
+f2ptr f2__concept_version_space_hypothesis_type__new_aux(f2ptr cause) {
+  f2ptr this = f2__concept_version_space_hypothesis_type__new(cause);
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "add_variable_value"),        f2__core_extension_funk__new(cause, new__symbol(cause, "concept_version_space"), new__symbol(cause, "concept_version_space_hypothesis__add_variable_value")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "terminal_print_with_frame"), f2__core_extension_funk__new(cause, new__symbol(cause, "concept_version_space"), new__symbol(cause, "concept_version_space_hypothesis__terminal_print_with_frame")));}
+  return this;
+}
+
+
 // concept_version_space
 
 def_ceframe1(concept_version_space, concept_version_space, variable_ptypehash);
@@ -164,9 +222,10 @@ f2ptr f2__concept_version_space__core_extension__initialize(f2ptr cause) {
 export_cefunk0(concept_version_space__core_extension__initialize, 0, "");
 
 f2ptr f2__concept_version_space__core_extension__define_types(f2ptr cause) {
-  f2__add_type(cause, new__symbol(cause, "concept_version_space_variable"), f2__concept_version_space_variable_type__new_aux(cause));
-  f2__add_type(cause, new__symbol(cause, "concept_version_space_example"),  f2__concept_version_space_example_type__new_aux(cause));
-  f2__add_type(cause, new__symbol(cause, "concept_version_space"),          f2__concept_version_space_type__new_aux(cause));
+  f2__add_type(cause, new__symbol(cause, "concept_version_space_variable"),   f2__concept_version_space_variable_type__new_aux(cause));
+  f2__add_type(cause, new__symbol(cause, "concept_version_space_example"),    f2__concept_version_space_example_type__new_aux(cause));
+  f2__add_type(cause, new__symbol(cause, "concept_version_space_hypothesis"), f2__concept_version_space_hypothesis_type__new_aux(cause));
+  f2__add_type(cause, new__symbol(cause, "concept_version_space"),            f2__concept_version_space_type__new_aux(cause));
   status("concept_version_space types defined.");
   return nil;
 }

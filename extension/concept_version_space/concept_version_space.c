bb91c5f369a936092dc69481f9cd6c656f462ad3
Bo Morgan <bo@mit.edu>
2011-06-22 22:06:59 -0400

    adding concept_version_space-test to tests.

--- extension/concept_version_space/concept_version_space.c
+++ extension/concept_version_space/concept_version_space.c
@@ -217,8 +217,7 @@ f2ptr raw__concept_version_space_hypothesis__is_consistent_with_or_more_general_
     f2ptr value_variable_name_ptypehash = raw__concept_version_space_hypothesis__value_variable_name_ptypehash(cause, this);
     ptypehash__iteration(cause, value_variable_name_ptypehash, variable_name, value,
 			 f2ptr hypothesis__value = raw__concept_version_space_hypothesis__lookup_variable_value(cause, hypothesis, variable_name);
-    			 if ((raw__eq(cause, value, new__symbol(cause, "-")) &&
-			      (! raw__eq(cause, hypothesis__value, new__symbol(cause, "-")))) ||
+			 if ((! raw__eq(cause, hypothesis__value, new__symbol(cause, "-"))) &&
 			     (! raw__eq(cause, value, hypothesis__value))) {
 			   return f2bool__new(boolean__false);
 			 }

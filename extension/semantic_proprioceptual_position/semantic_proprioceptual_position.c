7d56e13b09de8812b454c0d3334a28aaafc7f26d
Bo Morgan <bo@mit.edu>
2011-06-26 11:12:45 -0400

    adding semantic_proprioceptual_position core extension

--- extension/semantic_proprioceptual_position/semantic_proprioceptual_position.c
+++ extension/semantic_proprioceptual_position/semantic_proprioceptual_position.c
@@ -34,7 +34,9 @@ f2ptr raw__semantic_proprioceptual_position__type_create(f2ptr cause, f2ptr this
       return result;
     }
   }
-  raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "is_occurring"), nil);
+  raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "x"), nil);
+  raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "y"), nil);
+  raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "z"), nil);
   return this;
 }
 
@@ -92,46 +94,83 @@ f2ptr f2__semantic_proprioceptual_position__type(f2ptr cause, f2ptr this) {
 export_cefunk1(semantic_proprioceptual_position__type, thing, 0, "Returns the specific type of object that this semantic_proprioceptual_position is.");
 
 
-f2ptr raw__semantic_proprioceptual_position__is_occurring(f2ptr cause, f2ptr this) {
-  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "is_occurring"));
+f2ptr raw__semantic_proprioceptual_position__x(f2ptr cause, f2ptr this) {
+  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "x"));
 }
 
-f2ptr f2__semantic_proprioceptual_position__is_occurring(f2ptr cause, f2ptr this) {
+f2ptr f2__semantic_proprioceptual_position__x(f2ptr cause, f2ptr this) {
   assert_argument_type(semantic_proprioceptual_position, this);
-  return raw__semantic_proprioceptual_position__is_occurring(cause, this);
+  return raw__semantic_proprioceptual_position__x(cause, this);
 }
-export_cefunk1(semantic_proprioceptual_position__is_occurring, this, 0, "");
+export_cefunk1(semantic_proprioceptual_position__x, this, 0, "");
 
 
-f2ptr raw__semantic_proprioceptual_position__is_occurring__set(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "is_occurring"), that);
+f2ptr raw__semantic_proprioceptual_position__x__set(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "x"), that);
 }
 
-f2ptr f2__semantic_proprioceptual_position__is_occurring__set(f2ptr cause, f2ptr this, f2ptr that) {
+f2ptr f2__semantic_proprioceptual_position__x__set(f2ptr cause, f2ptr this, f2ptr that) {
   assert_argument_type(semantic_proprioceptual_position, this);
-  return raw__semantic_proprioceptual_position__is_occurring__set(cause, this, that);
+  return raw__semantic_proprioceptual_position__x__set(cause, this, that);
 }
-export_cefunk2(semantic_proprioceptual_position__is_occurring__set, this, that, 0, "");
+export_cefunk2(semantic_proprioceptual_position__x__set, this, that, 0, "");
 
 
-f2ptr raw__semantic_proprioceptual_position__is_occurring__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__remove(cause, this, new__symbol(cause, "property"), new__symbol(cause, "is_occurring"), that);
+f2ptr raw__semantic_proprioceptual_position__y(f2ptr cause, f2ptr this) {
+  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "y"));
 }
 
-f2ptr f2__semantic_proprioceptual_position__is_occurring__remove(f2ptr cause, f2ptr this, f2ptr that) {
+f2ptr f2__semantic_proprioceptual_position__y(f2ptr cause, f2ptr this) {
   assert_argument_type(semantic_proprioceptual_position, this);
-  return raw__semantic_proprioceptual_position__is_occurring__remove(cause, this, that);
+  return raw__semantic_proprioceptual_position__y(cause, this);
 }
-export_cefunk2(semantic_proprioceptual_position__is_occurring__remove, this, that, 0, "");
+export_cefunk1(semantic_proprioceptual_position__y, this, 0, "");
+
+
+f2ptr raw__semantic_proprioceptual_position__y__set(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "y"), that);
+}
+
+f2ptr f2__semantic_proprioceptual_position__y__set(f2ptr cause, f2ptr this, f2ptr that) {
+  assert_argument_type(semantic_proprioceptual_position, this);
+  return raw__semantic_proprioceptual_position__y__set(cause, this, that);
+}
+export_cefunk2(semantic_proprioceptual_position__y__set, this, that, 0, "");
+
+
+f2ptr raw__semantic_proprioceptual_position__z(f2ptr cause, f2ptr this) {
+  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "z"));
+}
+
+f2ptr f2__semantic_proprioceptual_position__z(f2ptr cause, f2ptr this) {
+  assert_argument_type(semantic_proprioceptual_position, this);
+  return raw__semantic_proprioceptual_position__z(cause, this);
+}
+export_cefunk1(semantic_proprioceptual_position__z, this, 0, "");
+
+
+f2ptr raw__semantic_proprioceptual_position__z__set(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "z"), that);
+}
+
+f2ptr f2__semantic_proprioceptual_position__z__set(f2ptr cause, f2ptr this, f2ptr that) {
+  assert_argument_type(semantic_proprioceptual_position, this);
+  return raw__semantic_proprioceptual_position__z__set(cause, this, that);
+}
+export_cefunk2(semantic_proprioceptual_position__z__set, this, that, 0, "");
 
 
 f2ptr f2__semantic_proprioceptual_position_type__new(f2ptr cause) {
   f2ptr this = f2__primobject_type__new(cause, f2list1__new(cause, new__symbol(cause, "semantic_object")));
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "new"),          f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_proprioceptual_position"), new__symbol(cause, "semantic_proprioceptual_position__new")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "is_type"),      f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_proprioceptual_position"), new__symbol(cause, "semantic_proprioceptual_position__is_type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "type"),         f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_proprioceptual_position"), new__symbol(cause, "semantic_proprioceptual_position__type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "is_occurring"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_proprioceptual_position"), new__symbol(cause, "semantic_proprioceptual_position__is_occurring")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "is_occurring"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_proprioceptual_position"), new__symbol(cause, "semantic_proprioceptual_position__is_occurring__set")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "new"),     f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_proprioceptual_position"), new__symbol(cause, "semantic_proprioceptual_position__new")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "is_type"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_proprioceptual_position"), new__symbol(cause, "semantic_proprioceptual_position__is_type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "type"),    f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_proprioceptual_position"), new__symbol(cause, "semantic_proprioceptual_position__type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "x"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_proprioceptual_position"), new__symbol(cause, "semantic_proprioceptual_position__x")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "x"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_proprioceptual_position"), new__symbol(cause, "semantic_proprioceptual_position__x__set")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "y"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_proprioceptual_position"), new__symbol(cause, "semantic_proprioceptual_position__y")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "y"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_proprioceptual_position"), new__symbol(cause, "semantic_proprioceptual_position__y__set")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "z"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_proprioceptual_position"), new__symbol(cause, "semantic_proprioceptual_position__z")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "z"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_proprioceptual_position"), new__symbol(cause, "semantic_proprioceptual_position__z__set")));}
   return this;
 }
 

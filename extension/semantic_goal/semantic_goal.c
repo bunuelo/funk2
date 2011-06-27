7e5871c24c13582996eed8b15e8ef0d5461980b4
Bo Morgan <bo@mit.edu>
2011-06-27 14:57:21 -0400

    adding semantic_somatosensation core extension

--- extension/semantic_goal/semantic_goal.c
+++ extension/semantic_goal/semantic_goal.c
@@ -114,17 +114,6 @@ f2ptr f2__semantic_goal__is_occurring__set(f2ptr cause, f2ptr this, f2ptr that) 
 export_cefunk2(semantic_goal__is_occurring__set, this, that, 0, "");
 
 
-f2ptr raw__semantic_goal__is_occurring__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__remove(cause, this, new__symbol(cause, "property"), new__symbol(cause, "is_occurring"), that);
-}
-
-f2ptr f2__semantic_goal__is_occurring__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  assert_argument_type(semantic_goal, this);
-  return raw__semantic_goal__is_occurring__remove(cause, this, that);
-}
-export_cefunk2(semantic_goal__is_occurring__remove, this, that, 0, "");
-
-
 f2ptr f2__semantic_goal_type__new(f2ptr cause) {
   f2ptr this = f2__primobject_type__new(cause, f2list1__new(cause, new__symbol(cause, "semantic_object")));
   {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "new"),          f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_goal"), new__symbol(cause, "semantic_goal__new")));}
--- extension/semantic_somatosensation/semantic_somatosensation.c
+++ extension/semantic_somatosensation/semantic_somatosensation.c
@@ -34,7 +34,8 @@ f2ptr raw__semantic_somatosensation__type_create(f2ptr cause, f2ptr this, f2ptr 
       return result;
     }
   }
-  raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "is_occurring"), nil);
+  raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "left_hand"),  nil);
+  raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "right_hand"), nil);
   return this;
 }
 
@@ -92,46 +93,61 @@ f2ptr f2__semantic_somatosensation__type(f2ptr cause, f2ptr this) {
 export_cefunk1(semantic_somatosensation__type, thing, 0, "Returns the specific type of object that this semantic_somatosensation is.");
 
 
-f2ptr raw__semantic_somatosensation__is_occurring(f2ptr cause, f2ptr this) {
-  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "is_occurring"));
+f2ptr raw__semantic_somatosensation__left_hand(f2ptr cause, f2ptr this) {
+  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "relationship"), new__symbol(cause, "left_hand"));
 }
 
-f2ptr f2__semantic_somatosensation__is_occurring(f2ptr cause, f2ptr this) {
+f2ptr f2__semantic_somatosensation__left_hand(f2ptr cause, f2ptr this) {
   assert_argument_type(semantic_somatosensation, this);
-  return raw__semantic_somatosensation__is_occurring(cause, this);
+  return raw__semantic_somatosensation__left_hand(cause, this);
 }
-export_cefunk1(semantic_somatosensation__is_occurring, this, 0, "");
+export_cefunk1(semantic_somatosensation__left_hand, this, 0, "");
 
 
-f2ptr raw__semantic_somatosensation__is_occurring__set(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "is_occurring"), that);
+f2ptr raw__semantic_somatosensation__left_hand__set(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "relationship"), new__symbol(cause, "left_hand"), that);
 }
 
-f2ptr f2__semantic_somatosensation__is_occurring__set(f2ptr cause, f2ptr this, f2ptr that) {
+f2ptr f2__semantic_somatosensation__left_hand__set(f2ptr cause, f2ptr this, f2ptr that) {
+  assert_argument_type(       semantic_somatosensation,      this);
+  assert_argument_type_or_nil(semantic_somatosensory_object, that);
+  return raw__semantic_somatosensation__left_hand__set(cause, this, that);
+}
+export_cefunk2(semantic_somatosensation__left_hand__set, this, that, 0, "");
+
+
+f2ptr raw__semantic_somatosensation__right_hand(f2ptr cause, f2ptr this) {
+  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "relationship"), new__symbol(cause, "right_hand"));
+}
+
+f2ptr f2__semantic_somatosensation__right_hand(f2ptr cause, f2ptr this) {
   assert_argument_type(semantic_somatosensation, this);
-  return raw__semantic_somatosensation__is_occurring__set(cause, this, that);
+  return raw__semantic_somatosensation__right_hand(cause, this);
 }
-export_cefunk2(semantic_somatosensation__is_occurring__set, this, that, 0, "");
+export_cefunk1(semantic_somatosensation__right_hand, this, 0, "");
 
 
-f2ptr raw__semantic_somatosensation__is_occurring__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__remove(cause, this, new__symbol(cause, "property"), new__symbol(cause, "is_occurring"), that);
+f2ptr raw__semantic_somatosensation__right_hand__set(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "relationship"), new__symbol(cause, "right_hand"), that);
 }
 
-f2ptr f2__semantic_somatosensation__is_occurring__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  assert_argument_type(semantic_somatosensation, this);
-  return raw__semantic_somatosensation__is_occurring__remove(cause, this, that);
+f2ptr f2__semantic_somatosensation__right_hand__set(f2ptr cause, f2ptr this, f2ptr that) {
+  assert_argument_type(       semantic_somatosensation,      this);
+  assert_argument_type_or_nil(semantic_somatosensory_object, that);
+  return raw__semantic_somatosensation__right_hand__set(cause, this, that);
 }
-export_cefunk2(semantic_somatosensation__is_occurring__remove, this, that, 0, "");
+export_cefunk2(semantic_somatosensation__right_hand__set, this, that, 0, "");
 
 
 f2ptr f2__semantic_somatosensation_type__new(f2ptr cause) {
   f2ptr this = f2__primobject_type__new(cause, f2list1__new(cause, new__symbol(cause, "semantic_object")));
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "new"),          f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_somatosensation"), new__symbol(cause, "semantic_somatosensation__new")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "is_type"),      f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_somatosensation"), new__symbol(cause, "semantic_somatosensation__is_type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "type"),         f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_somatosensation"), new__symbol(cause, "semantic_somatosensation__type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "is_occurring"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_somatosensation"), new__symbol(cause, "semantic_somatosensation__is_occurring")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "is_occurring"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_somatosensation"), new__symbol(cause, "semantic_somatosensation__is_occurring__set")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "new"),        f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_somatosensation"), new__symbol(cause, "semantic_somatosensation__new")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "is_type"),    f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_somatosensation"), new__symbol(cause, "semantic_somatosensation__is_type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "type"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_somatosensation"), new__symbol(cause, "semantic_somatosensation__type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "left_hand"),  f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_somatosensation"), new__symbol(cause, "semantic_somatosensation__left_hand")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "left_hand"),  f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_somatosensation"), new__symbol(cause, "semantic_somatosensation__left_hand__set")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "right_hand"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_somatosensation"), new__symbol(cause, "semantic_somatosensation__right_hand")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "right_hand"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_somatosensation"), new__symbol(cause, "semantic_somatosensation__right_hand__set")));}
   return this;
 }
 

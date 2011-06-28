9a5bc0e2eaf916b4afab4ba3972d18dc3b3d6d4a
Bo Morgan <bo@mit.edu>
2011-06-28 13:23:48 -0400

    adding semantic_somatosensation to builtin_reactive_sensory_agency

--- extension/fibermon/fibermon.c
+++ extension/fibermon/fibermon.c
@@ -86,7 +86,7 @@ f2ptr f2__fibermon_fiber__construct_fast(f2ptr cause, f2ptr this) {
   f2__frame__add_var_value(cause, this, new__symbol(cause, "view_fiber_menu_item"), this__view_fiber_menu_item);
   f2ptr this__view_fiber_value_menu_item = raw__gtk__menu_item__new(cause, new__string(cause, "Fiber Value"));
   f2__frame__add_var_value(cause, this, new__symbol(cause, "view_fiber_value_menu_item"), this__view_fiber_value_menu_item);
-  f2ptr this__view_bug_menu_item = raw__gtk__menu_item__new(cause, new__string(cause, "Fiber Value"));
+  f2ptr this__view_bug_menu_item = raw__gtk__menu_item__new(cause, new__string(cause, "Bug"));
   f2__frame__add_var_value(cause, this, new__symbol(cause, "view_bug_menu_item"), this__view_bug_menu_item);
   f2ptr this__view_cause_menu_item = raw__gtk__menu_item__new(cause, new__string(cause, "Cause"));
   f2__frame__add_var_value(cause, this, new__symbol(cause, "view_cause_menu_item"), this__view_cause_menu_item);

bf079a13e313c3cb7c2e04aaf4d13d966e831038
Bo Morgan <bo@mit.edu>
2011-08-09 06:14:01 -0400

    changing semantic_object_type to semantic_object_type_event, semantic_physical_object_type to semantic_physical_object_type_event, semantic_physical_object_type_relation to semantic_physical_object_type_relation_event.

--- extension/semantic_object_type_event/semantic_object_type_event.c
+++ extension/semantic_object_type_event/semantic_object_type_event.c
@@ -28,7 +28,7 @@ f2ptr raw__semantic_object_type_event__type_create(f2ptr cause, f2ptr this, f2pt
   if (! raw__frame__contains_var(cause, this, new__symbol(cause, "type"))) {
     raw__frame__add_var_value(cause, this, new__symbol(cause, "type"), new__symbol(cause, "semantic_object_type_event"));
   }
-  assert_value(raw__semantic_event__type_create(cause, this, semantic_realm, nil)_;
+  assert_value(raw__semantic_event__type_create(cause, this, semantic_realm, nil));
   return this;
 }
 

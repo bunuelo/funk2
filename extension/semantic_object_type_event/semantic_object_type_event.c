a470b809c56cb39f4fe5e72f8009964c16af4ea8
Bo Morgan <bo@mit.edu>
2011-08-09 06:11:28 -0400

    changing semantic_object_type to semantic_object_type_event, semantic_physical_object_type to semantic_physical_object_type_event, semantic_physical_object_type_relation to semantic_physical_object_type_relation_event.

--- extension/semantic_object_type_event/semantic_object_type_event.c
+++ extension/semantic_object_type_event/semantic_object_type_event.c
@@ -28,12 +28,7 @@ f2ptr raw__semantic_object_type_event__type_create(f2ptr cause, f2ptr this, f2pt
   if (! raw__frame__contains_var(cause, this, new__symbol(cause, "type"))) {
     raw__frame__add_var_value(cause, this, new__symbol(cause, "type"), new__symbol(cause, "semantic_object_type_event"));
   }
-  {
-    f2ptr result = raw__semantic_event__type_create(cause, this, semantic_realm, nil);
-    if (raw__larva__is_type(cause, result)) {
-      return result;
-    }
-  }
+  assert_value(raw__semantic_event__type_create(cause, this, semantic_realm, nil)_;
   return this;
 }
 
@@ -42,19 +37,12 @@ f2ptr raw__semantic_object_type_event__new(f2ptr cause, f2ptr semantic_realm) {
   if (raw__larva__is_type(cause, this)) {
     return this;
   }
-  {
-    f2ptr result = raw__semantic_object_type_event__type_create(cause, this, semantic_realm);
-    if (raw__larva__is_type(cause, result)) {
-      return result;
-    }
-  }
+  assert_value(raw__semantic_object_type_event__type_create(cause, this, semantic_realm));
   return this;
 }
 
 f2ptr f2__semantic_object_type_event__new(f2ptr cause, f2ptr semantic_realm) {
-  if (! raw__semantic_realm__is_type(cause, semantic_realm)) {
-    return f2larva__new(cause, 1, nil);
-  }
+  assert_argument_type(semantic_realm, semantic_realm);
   return raw__semantic_object_type_event__new(cause, semantic_realm);
 }
 export_cefunk1(semantic_object_type_event__new, semantic_realm, 0, "Returns a new semantic_object_type_event object.");

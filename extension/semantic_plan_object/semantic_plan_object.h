4ba75596d31734178e73178fc99b32fe885594e3
Bo Morgan <bo@mit.edu>
2011-08-13 19:03:14 -0400

    working on deliberative_plan_agency

--- extension/semantic_plan_object/semantic_plan_object.h
+++ extension/semantic_plan_object/semantic_plan_object.h
@@ -24,6 +24,7 @@
 
 #include "../semantic_object/semantic_object.h"
 
+f2ptr raw__semantic_plan_object__type_create(f2ptr cause, f2ptr this, f2ptr semantic_realm, f2ptr phenomenal_name, f2ptr deliberative_plan);
 
 // **
 

3bfdd289b6cba820590ea96066c93bd5dbc8bca4
Bo Morgan <bo@mit.edu>
2011-10-02 13:17:41 -0700

    working on funk2-webpage (reverting to 8e7a6a0db8c17468100a696e2f956b0841f92f6b)

--- c/funk2.c
+++ c/funk2.c
@@ -157,6 +157,9 @@ void funk2__init(funk2_t* this, int argc, char** argv) {
   status("****************************************************************");
   status("");
   
+  status("  sizeof(funk2_memblock_t) = " u64__fstr, (u64)(sizeof(funk2_memblock_t)));
+  status("");
+  
   funk2_system_processor__print_status(&(__funk2.system_processor));
   funk2_command_line__init(&(this->command_line), argc, argv);
   

53b37f10a254c807b6c72f6895f999c620c7e5a6
Bo Morgan <bo@mit.edu>
2011-10-06 10:50:18 -0700

    working on reducing memory usage.

--- c/f2_garbage_collector_pool.c
+++ c/f2_garbage_collector_pool.c
@@ -630,7 +630,7 @@ void funk2_garbage_collector_pool__grey_referenced_elements(funk2_garbage_collec
   case ptype_chunk:            return;
   case ptype_simple_array: {
     s64 i;
-    f2ptr* iter = (f2ptr*)((ptype_simple_array_block_t*)block)->f2ptr_data;
+    ptype_simple_array_block_slot_t* iter = ((ptype_simple_array_block_t*)block)->slot;
     for (i = ((ptype_simple_array_block_t*)block)->length; i > 0; i --) {
       funk2_garbage_collector_pool__grey_maybe_other_element(this, pool_index, *iter);
       iter ++;

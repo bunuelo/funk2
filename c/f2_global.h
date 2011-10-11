2c62a30184da1ad4903188090ab287717957cc2e
Bo Morgan <bo@mit.edu>
2011-10-11 16:22:27 -0700

    working on block_size bug.

--- c/f2_global.h
+++ c/f2_global.h
@@ -84,8 +84,8 @@ typedef unsigned char boolean_t;
 
 #define error(fiber, str) {fputs("\n*** ", stderr); fputs(str, stderr); fputs(" ***\n", stderr); assert_failed(fiber, __FILE__, __LINE__, str); exit(-1);}
 
-typedef                             u32       pool_address_t;
-#define pool_address__fstr          X32__fstr
+typedef                             u64       pool_address_t;
+#define pool_address__fstr          X64__fstr
 #define pool_address__bit_num        32
 #define pool_address__max_byte_num     ((1ull << pool_address__bit_num) - 1)
 

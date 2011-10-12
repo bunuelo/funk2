4d9f813268be1158a7d187bdbb0bf87822b8e2a7
Bo Morgan <bo@mit.edu>
2011-10-12 14:06:38 -0700

    working on unicode_mode for input streams.

--- c/f2_gtk.c
+++ c/f2_gtk.c
@@ -4944,12 +4944,12 @@ f2ptr raw__gtk__entry__set_text(f2ptr cause, f2ptr entry, f2ptr text) {
     
     assert_g_type(GTK_TYPE_ENTRY, gtk_entry);
     
-    u64 text__length = raw__string__length(cause, text);
-    u8* text__str    = (u8*)from_ptr(f2__malloc(text__length + 1));
-    raw__string__str_copy(cause, text, text__str);
-    text__str[text__length] = 0;
-    funk2_gtk__entry__set_text(&(__funk2.gtk), gtk_entry, text__str);
-    f2__free(to_ptr(text__str));
+    u64 text__utf8_length = raw__string__utf8_length(cause, text);
+    u8* text__utf8_str    = (u8*)from_ptr(f2__malloc(text__utf8_length + 1));
+    raw__string__utf8_str_copy(cause, text, text__utf8_str);
+    text__utf8_str[text__utf8_length] = 0;
+    funk2_gtk__entry__set_text(&(__funk2.gtk), gtk_entry, text__utf8_str);
+    f2__free(to_ptr(text__utf8_str));
     return nil;
   } else {
     return f2__gtk_not_supported_larva__new(cause);

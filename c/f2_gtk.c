cf760e759727cd22f3e2e3a270d103f1a7d360fc
Bo Morgan <bo@mit.edu>
2011-10-12 14:03:36 -0700

    working on unicode_mode for input streams.

--- c/f2_gtk.c
+++ c/f2_gtk.c
@@ -3956,12 +3956,12 @@ f2ptr raw__gtk__text_buffer__set_text(f2ptr cause, f2ptr text_buffer, f2ptr text
     
     assert_g_type(GTK_TYPE_TEXT_BUFFER, gtk_text_buffer);
     
-    u64 text__length = raw__string__length(cause, text);
-    u8* text__str    = (u8*)alloca(text__length + 1);
-    raw__string__str_copy(cause, text, text__str);
-    text__str[text__length] = 0;
+    u64 text__utf8_length = raw__string__utf8_length(cause, text);
+    u8* text__utf8_str    = (u8*)alloca(text__utf8_length + 1);
+    raw__string__utf8_str_copy(cause, text, text__utf8_str);
+    text__utf8_str[text__utf8_length] = 0;
   
-    funk2_gtk__text_buffer__set_text(&(__funk2.gtk), gtk_text_buffer, text__str);
+    funk2_gtk__text_buffer__set_text(&(__funk2.gtk), gtk_text_buffer, text__utf8_str);
     return nil;
   } else {
     return f2__gtk_not_supported_larva__new(cause);

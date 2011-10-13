32c945bd2cafcc31f1efc4b2ed49674f170c6912
Bo Morgan <bo@mit.edu>
2011-10-12 17:07:40 -0700

    working on unicode_mode for input streams.

--- c/f2_partial_order.c
+++ c/f2_partial_order.c
@@ -81,7 +81,7 @@ void f2__partial_order__initialize() {
   // partial_order_node
   initialize_primobject_1_slot(partial_order_node, label);
   
-  {char* symbol_str = "terminal_print_with_frame"; __funk2.globalenv.object_type.primobject.primobject_type_partial_order_node.terminal_print_with_frame__symbol = f2symbol__new(cause, strlen(symbol_str), (u8*)symbol_str);}
+  {char* symbol_str = "terminal_print_with_frame"; __funk2.globalenv.object_type.primobject.primobject_type_partial_order_node.terminal_print_with_frame__symbol = new__symbol(cause, symbol_str);}
   {f2__primcfunk__init__with_c_cfunk_var__2_arg(partial_order_node__terminal_print_with_frame, this, terminal_print_frame, cfunk); __funk2.globalenv.object_type.primobject.primobject_type_partial_order_node.terminal_print_with_frame__funk = never_gc(cfunk);}
   
 }

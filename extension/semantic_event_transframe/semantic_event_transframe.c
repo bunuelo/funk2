501fe7c1b7f52e7e7f71f3d7691f8e194baaab73
Bo Morgan <bo@mit.edu>
2012-01-24 13:14:38 -0800

    working on transframe

--- extension/semantic_event_transframe/semantic_event_transframe.c
+++ extension/semantic_event_transframe/semantic_event_transframe.c
@@ -24,7 +24,7 @@
 
 // semantic_event_transframe
 
-f2ptr raw__semantic_event_transframe__type_create(f2ptr cause, f2ptr this, f2ptr semantic_realm, f2ptr removals, f2ptr additions) {
+f2ptr raw__semantic_event_transframe__type_create(f2ptr cause, f2ptr this, f2ptr semantic_realm) {
   if (! raw__frame__contains_var(cause, this, new__symbol(cause, "type"))) {
     raw__frame__add_var_value(cause, this, new__symbol(cause, "type"), new__symbol(cause, "semantic_event_transframe"));
   }
@@ -34,8 +34,6 @@ f2ptr raw__semantic_event_transframe__type_create(f2ptr cause, f2ptr this, f2ptr
       return result;
     }
   }
-  raw__frame__add_var_value(cause, this, new__symbol(cause, "removals"),  removals);
-  raw__frame__add_var_value(cause, this, new__symbol(cause, "additions"), additions);
   return this;
 }
 
@@ -93,59 +91,71 @@ f2ptr f2__semantic_event_transframe__type(f2ptr cause, f2ptr this) {
 export_cefunk1(semantic_event_transframe__type, thing, 0, "Returns the specific type of object that this semantic_event_transframe is.");
 
 
-f2ptr raw__semantic_event_transframe__removals(f2ptr cause, f2ptr this) {
-  return f2__frame__lookup_var_value(cause, this, new__symbol(cause, "removals"));
+f2ptr raw__semantic_event_transframe__relationship__removal__add(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__add(cause, this, new__symbol(cause, "relationship"), new__symbol(cause, "removal"), that);
 }
 
-f2ptr f2__semantic_event_transframe__removals(f2ptr cause, f2ptr this) {
-  assert_argument_type(semantic_event_transframe, this);
-  return raw__semantic_event_transframe__removals(cause, this);
+f2ptr f2__semantic_event_transframe__relationship__removal__add(f2ptr cause, f2ptr this, f2ptr that) {
+  if ((! raw__semantic_event_transframe__is_type(cause, this)) ||
+      (! raw__semantic_event_transframe__is_type(cause, that))) {
+    return f2larva__new(cause, 1, nil);
+  }
+  return raw__semantic_event_transframe__relationship__removal__add(cause, this, that);
 }
-export_cefunk1(semantic_event_transframe__removals, this, 0, "Returns the removals slot value of this semantic_event_transframe.");
+export_cefunk2(semantic_event_transframe__relationship__removal__add, this, that, 0, "");
 
 
-f2ptr raw__semantic_event_transframe__removals__set(f2ptr cause, f2ptr this, f2ptr value) {
-  return f2__frame__add_var_value(cause, this, new__symbol(cause, "removals"), value);
-}
-
-f2ptr f2__semantic_event_transframe__removals__set(f2ptr cause, f2ptr this, f2ptr value) {
-  assert_argument_type(semantic_event_transframe, this);
-  return raw__semantic_event_transframe__removals__set(cause, this, value);
-}
-export_cefunk2(semantic_event_transframe__removals__set, this, value, 0, "Sets the removals slot value of this semantic_event_transframe.");
-
-
-f2ptr raw__semantic_event_transframe__additions(f2ptr cause, f2ptr this) {
-  return f2__frame__lookup_var_value(cause, this, new__symbol(cause, "additions"));
+f2ptr raw__semantic_event_transframe__relationship__removal__remove(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__remove(cause, this, new__symbol(cause, "relationship"), new__symbol(cause, "removal"), that);
 }
 
-f2ptr f2__semantic_event_transframe__additions(f2ptr cause, f2ptr this) {
-  assert_argument_type(semantic_event_transframe, this);
-  return raw__semantic_event_transframe__additions(cause, this);
+f2ptr f2__semantic_event_transframe__relationship__removal__remove(f2ptr cause, f2ptr this, f2ptr that) {
+  if ((! raw__semantic_event_transframe__is_type(cause, this)) ||
+      (! raw__semantic_event_transframe__is_type(cause, that))) {
+    return f2larva__new(cause, 1, nil);
+  }
+  return raw__semantic_event_transframe__relationship__removal__remove(cause, this, that);
 }
-export_cefunk1(semantic_event_transframe__additions, this, 0, "Returns the additions slot value of this semantic_event_transframe.");
+export_cefunk2(semantic_event_transframe__relationship__removal__remove, this, that, 0, "");
 
 
-f2ptr raw__semantic_event_transframe__additions__set(f2ptr cause, f2ptr this, f2ptr value) {
-  return f2__frame__add_var_value(cause, this, new__symbol(cause, "additions"), value);
+f2ptr raw__semantic_event_transframe__relationship__addition__add(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__add(cause, this, new__symbol(cause, "relationship"), new__symbol(cause, "addition"), that);
 }
 
-f2ptr f2__semantic_event_transframe__additions__set(f2ptr cause, f2ptr this, f2ptr value) {
-  assert_argument_type(semantic_event_transframe, this);
-  return raw__semantic_event_transframe__additions__set(cause, this, value);
+f2ptr f2__semantic_event_transframe__relationship__addition__add(f2ptr cause, f2ptr this, f2ptr that) {
+  if ((! raw__semantic_event_transframe__is_type(cause, this)) ||
+      (! raw__semantic_event_transframe__is_type(cause, that))) {
+    return f2larva__new(cause, 1, nil);
+  }
+  return raw__semantic_event_transframe__relationship__addition__add(cause, this, that);
 }
-export_cefunk2(semantic_event_transframe__additions__set, this, value, 0, "Sets the additions slot value of this semantic_event_transframe.");
+export_cefunk2(semantic_event_transframe__relationship__addition__add, this, that, 0, "");
+
+
+f2ptr raw__semantic_event_transframe__relationship__addition__remove(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__remove(cause, this, new__symbol(cause, "relationship"), new__symbol(cause, "addition"), that);
+}
+
+f2ptr f2__semantic_event_transframe__relationship__addition__remove(f2ptr cause, f2ptr this, f2ptr that) {
+  if ((! raw__semantic_event_transframe__is_type(cause, this)) ||
+      (! raw__semantic_event_transframe__is_type(cause, that))) {
+    return f2larva__new(cause, 1, nil);
+  }
+  return raw__semantic_event_transframe__relationship__addition__remove(cause, this, that);
+}
+export_cefunk2(semantic_event_transframe__relationship__addition__remove, this, that, 0, "");
 
 
 f2ptr f2__semantic_event_transframe__primobject_type__new(f2ptr cause) {
   f2ptr this = f2__primobject_type__new(cause, f2list1__new(cause, new__symbol(cause, "semantic_object")));
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "new"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event_transframe"), new__symbol(cause, "semantic_event_transframe__new")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "is_type"),   f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event_transframe"), new__symbol(cause, "semantic_event_transframe__is_type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "type"),      f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event_transframe"), new__symbol(cause, "semantic_event_transframe__type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "removals"),  f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event_transframe"), new__symbol(cause, "semantic_event_transframe__removals")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "removals"),  f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event_transframe"), new__symbol(cause, "semantic_event_transframe__removals__set")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "additions"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event_transframe"), new__symbol(cause, "semantic_event_transframe__additions")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "additions"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event_transframe"), new__symbol(cause, "semantic_event_transframe__additions__set")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"),         new__symbol(cause, "new"),                   f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event_transframe"), new__symbol(cause, "semantic_event_transframe__new")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"),         new__symbol(cause, "is_type"),               f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event_transframe"), new__symbol(cause, "semantic_event_transframe__is_type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),             new__symbol(cause, "type"),                  f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event_transframe"), new__symbol(cause, "semantic_event_transframe__type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-add"),    new__symbol(cause, "relationship-removal"),  f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event_transframe"), new__symbol(cause, "semantic_event_transframe__relationship__removal__add")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-remove"), new__symbol(cause, "relationship-removal"),  f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event_transframe"), new__symbol(cause, "semantic_event_transframe__relationship__removal__remove")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-add"),    new__symbol(cause, "relationship-addition"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event_transframe"), new__symbol(cause, "semantic_event_transframe__relationship__addition__add")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-remove"), new__symbol(cause, "relationship-addition"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_event_transframe"), new__symbol(cause, "semantic_event_transframe__relationship__addition__remove")));}
   return this;
 }
 

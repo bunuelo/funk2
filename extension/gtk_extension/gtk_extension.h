8039a4ad7a11433805423b73354db991dcb90cc3
Bo Morgan <bo@mit.edu>
2012-02-08 16:34:07 -0800

    working on gtk size_allocate callback

--- extension/gtk_extension/gtk_extension.h
+++ extension/gtk_extension/gtk_extension.h
@@ -41,7 +41,8 @@ enum funk2_gtk_callback_args_type_e {
   funk2_gtk_callback_args_type__key_press,
   funk2_gtk_callback_args_type__response,
   funk2_gtk_callback_args_type__update_preview,
-  funk2_gtk_callback_args_type__value_changed
+  funk2_gtk_callback_args_type__value_changed,
+  funk2_gtk_callback_args_type__size_allocate
 };
 
 struct funk2_gtk_callback_s {

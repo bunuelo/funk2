dedeb0c6a1efa75a42657e5a85c5d96738bb5ae0
Bo Morgan <bo@mit.edu>
2012-02-29 13:23:17 -0800

    working on memory (adding local defragmentation of freed block with next block)

--- c/f2_memorypool.c
+++ c/f2_memorypool.c
@@ -336,14 +336,18 @@ void funk2_memorypool__free_used_block(funk2_memorypool_t* this, funk2_memblock_
 
   // try to join block with next block if next block is also free
   {
-    funk2_memblock_t* next_block    = (funk2_memblock_t*)(((u8*)block) + funk2_memblock__byte_num(block));
-    funk2_memblock_t* end_of_blocks = (funk2_memblock_t*)(((u8*)from_ptr(this->dynamic_memory.ptr)) + this->total_global_memory);
-    if (next_block < end_of_blocks) {
-      if (! next_block->used) {
+    funk2_memblock_t* end_of_blocks      = (funk2_memblock_t*)(((u8*)from_ptr(this->dynamic_memory.ptr)) + this->total_global_memory);
+    boolean_t         done = boolean__false;
+    while (! done) {
+      funk2_memblock_t* next_block = (funk2_memblock_t*)(((u8*)block) + funk2_memblock__byte_num(block));
+      if ((next_block < end_of_blocks) &&
+	  (! next_block->used)) {
 	// remove next block from free memory heap
 	funk2_memorypool__free_memory_heap__remove(this, next_block);
 	// increase the size of this block to include next block
 	funk2_memblock__byte_num(block) += funk2_memblock__byte_num(next_block);
+      } else {
+	done = boolean__true;
       }
     }
   }

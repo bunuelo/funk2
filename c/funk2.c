9579145634d2484e2c1ad292d5ca39900b306127
Bo Morgan <bo@mit.edu>
2012-03-05 13:50:16 -0800

    working on defragmenter

--- c/funk2.c
+++ c/funk2.c
@@ -23,6 +23,13 @@
 
 // funk2
 
+void f2__pre_reinitialize_global_vars() {
+  f2__primobjects__reinitialize_globalvars();
+  f2__cause__reinitialize_globalvars();
+  f2__primobject__ptypehash__reinitialize_globalvars(); 
+  f2__primobject_frame__reinitialize_globalvars();
+}
+
 void f2__initialize() {
   f2__module_registration__initialize();
   f2__redblacktree__initialize();
@@ -41,12 +48,8 @@ void f2__initialize() {
   // **   3. all packages besides global environment are "initialized" (including global environment prerequisite components, such as ptypehash, frame, and environment primobjects)
   // ** 
   {
-    {
-      f2__primobjects__reinitialize_globalvars();
-      f2__cause__reinitialize_globalvars();
-      f2__primobject__ptypehash__reinitialize_globalvars(); 
-      f2__primobject_frame__reinitialize_globalvars();
-    }
+    f2__pre_reinitialize_global_vars();
+    
     f2__primobject_environment__initialize();
     f2__globalenv__initialize();
     f2__primobject__ptypehash__initialize();

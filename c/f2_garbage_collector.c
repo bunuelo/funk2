aaea364e489ca3bc9af11307f6930fb6743fbc1e
Bo Morgan <bo@mit.edu>
2012-03-09 10:34:11 -0800

    working on defragmenter (moving fixing memory pointers to modeule_registration)

--- c/f2_garbage_collector.c
+++ c/f2_garbage_collector.c
@@ -415,10 +415,10 @@ void funk2_garbage_collector__defragment__fix_pointers(funk2_garbage_collector_t
   {
     s64 pool_index;
     for (pool_index = 0; pool_index < memory_pool_num; pool_index ++) {
-      funk2_garbage_collector_pool__defragment__fix_pointers(&(this->gc_pool[pool_index]), defragmenter);
+      funk2_garbage_collector_pool__defragment__fix_pointers(&(this->gc_pool[pool_index]));
     }
   }
-  funk2_never_delete_list__defragment__fix_pointers(&(this->never_delete_list), defragmenter);
+  funk2_never_delete_list__defragment__fix_pointers(&(this->never_delete_list));
 }
 
 // **

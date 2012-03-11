e8416cb0a2b054f3b25b4b4b66ea6907db0c5b95
Bo Morgan <bo@mit.edu>
2012-03-11 11:42:27 -0700

    working on defragment2 (debugging fix pointers)

--- c/f2_bytecodes.c
+++ c/f2_bytecodes.c
@@ -429,8 +429,13 @@ int f2__fiber__bytecode_helper__jump_funk__no_increment_pc_reg(f2ptr fiber, f2pt
       return 1;
     }
   }
-  status("f2__fiber__bytecode_helper__jump_funk__no_increment_pc_reg bug: cannot_funk_unfunkable_funktion.");
-  f2__print(cause, funktion);
+  if (raw__chunk__is_type(cause, funktion)) {
+    status("f2__fiber__bytecode_helper__jump_funk__no_increment_pc_reg bug: cannot_funk_unfunkable_funktion (saving chunk to 'larva_18.chunk'.");
+    raw__chunk__save(cause, funktion, new__string(cause, "larva_18.chunk"));
+  } else {
+    status("f2__fiber__bytecode_helper__jump_funk__no_increment_pc_reg bug: cannot_funk_unfunkable_funktion.");
+    f2__print(cause, funktion);
+  }
   f2ptr bug_frame = f2__frame__new(cause, nil);
   f2__frame__add_var_value(cause, bug_frame, new__symbol(cause, "bug_type"), new__symbol(cause, "cannot_funk_unfunkable_funktion"));
   f2__frame__add_var_value(cause, bug_frame, new__symbol(cause, "funktion"), funktion);

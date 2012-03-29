9fe2ee7e0e0156dd86aba6af0e302395b83ffc64
Bo Morgan <bo@mit.edu>
2012-03-29 12:10:22 -0700

    working on defragment2 (debugging fix pointers)

--- c/f2_primobject__traced_cmutex.c
+++ c/f2_primobject__traced_cmutex.c
@@ -168,12 +168,6 @@ f2ptr f2traced_cmutex__primobject_type__new_aux(f2ptr cause) {
 
 // **
 
-void f2__primobject__traced_cmutex__reinitialize_globalvars() {
-  f2ptr cause = initial_cause();
-  
-  reinitialize_primobject(traced_cmutex);
-}
-
 void f2__primobject__traced_cmutex__defragment__fix_pointers() {
   // -- reinitialize --
   
@@ -203,16 +197,9 @@ void f2__primobject__traced_cmutex__defragment__fix_pointers() {
   defragment__fix_pointer(__funk2.globalenv.object_type.primobject.primobject_type_traced_cmutex.terminal_print_with_frame__symbol);
   f2__primcfunk__init__defragment__fix_pointers(traced_cmutex__terminal_print_with_frame);
   defragment__fix_pointer(__funk2.globalenv.object_type.primobject.primobject_type_traced_cmutex.terminal_print_with_frame__funk);
-  
 }
 
-void f2__primobject__traced_cmutex__initialize() {
-  funk2_module_registration__add_module(&(__funk2.module_registration), "primobject-traced_cmutex", "", &f2__primobject__traced_cmutex__reinitialize_globalvars, &f2__primobject__traced_cmutex__defragment__fix_pointers);
-  
-  f2__primobject__traced_cmutex__reinitialize_globalvars();
-  
-  environment__add_var_value(initial_cause(), global_environment(), __traced_cmutex__symbol, nil);
-  
+void f2__primobject__traced_cmutex__reinitialize_globalvars() {
   f2ptr cause = initial_cause();
   
   // traced_cmutex
@@ -229,6 +216,11 @@ void f2__primobject__traced_cmutex__initialize() {
   {f2__primcfunk__init__with_c_cfunk_var__1_arg(traced_cmutex__lock_stack_trace, this, cfunk); __funk2.globalenv.object_type.primobject.primobject_type_traced_cmutex.lock_stack_trace__funk = never_gc(cfunk);}
   {char* symbol_str = "terminal_print_with_frame"; __funk2.globalenv.object_type.primobject.primobject_type_traced_cmutex.terminal_print_with_frame__symbol = new__symbol(cause, symbol_str);}
   {f2__primcfunk__init__with_c_cfunk_var__2_arg(traced_cmutex__terminal_print_with_frame, this, terminal_print_frame, cfunk); __funk2.globalenv.object_type.primobject.primobject_type_traced_cmutex.terminal_print_with_frame__funk = never_gc(cfunk);}
+}
+
+void f2__primobject__traced_cmutex__initialize() {
+  funk2_module_registration__add_module(&(__funk2.module_registration), "primobject-traced_cmutex", "", &f2__primobject__traced_cmutex__reinitialize_globalvars, &f2__primobject__traced_cmutex__defragment__fix_pointers);
   
+  f2__primobject__traced_cmutex__reinitialize_globalvars();
 }
 

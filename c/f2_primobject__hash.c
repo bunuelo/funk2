931e7d2f68eb6b2cd4c0cb81f8cdebdc9248b59b
Bo Morgan <bo@mit.edu>
2012-03-29 15:00:53 -0700

    working on defragment2 (debugging fix pointers)

--- c/f2_primobject__hash.c
+++ c/f2_primobject__hash.c
@@ -415,16 +415,6 @@ void f2__primobject_hash__defragment__fix_pointers() {
 void f2__primobject_hash__reinitialize_globalvars() {
   f2ptr cause = initial_cause();
   
-  reinitialize_primobject(hash);
-}
-
-void f2__primobject_hash__initialize() {
-  funk2_module_registration__add_module(&(__funk2.module_registration), "primobject-hash", "", &f2__primobject_hash__reinitialize_globalvars, &f2__primobject_hash__defragment__fix_pointers);
-  
-  f2__primobject_hash__reinitialize_globalvars();
-  
-  f2ptr cause = initial_cause();
-  
   // hash
   
   initialize_primobject_6_slot(hash, write_cmutex, key_count, bin_num_power, bin_array, hash_value_funk, equals_funk);
@@ -447,5 +437,11 @@ void f2__primobject_hash__initialize() {
   {f2__primcfunk__init__with_c_cfunk_var__2_arg(hash__as__frame, this, slot_name, cfunk); __funk2.globalenv.object_type.primobject.primobject_type_hash.as__frame__funk = never_gc(cfunk);}
   
   f2__primcfunk__init__0(hash);
+}
+
+void f2__primobject_hash__initialize() {
+  funk2_module_registration__add_module(&(__funk2.module_registration), "primobject-hash", "", &f2__primobject_hash__reinitialize_globalvars, &f2__primobject_hash__defragment__fix_pointers);
+  
+  f2__primobject_hash__reinitialize_globalvars();
 }
 

1113f01151acca36c1c2c5faba8f2f3884e2b75e
Bo Morgan <bo@mit.edu>
2012-03-29 16:31:16 -0700

    working on defragment2 (debugging fix pointers)

--- c/f2_core_extension.c
+++ c/f2_core_extension.c
@@ -469,7 +469,9 @@ void f2__core_extension__initialize_module() {
   funk2_module_registration__add_module(&(__funk2.module_registration), "core_extension", "", &f2__core_extension__reinitialize_globalvars, &f2__core_extension__defragment__fix_pointers);
   
   f2__core_extension__reinitialize_globalvars();
-
+  
+  f2ptr cause = initial_cause();
+  
   environment__add_var_value(cause, global_environment(), new__symbol(cause, "-core_extension_handler-"), f2__core_extension_handler__new(cause));
 }
 

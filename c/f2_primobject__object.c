c79db0376ad582b8c7b402a4998dfd57ffd7ebe8
Bo Morgan <bo@mit.edu>
2012-03-29 11:14:56 -0700

    working on defragment2 (debugging fix pointers)

--- c/f2_primobject__object.c
+++ c/f2_primobject__object.c
@@ -100,7 +100,7 @@ f2ptr object__type_var_value__set(f2ptr cause, f2ptr this, f2ptr type, f2ptr var
 // end of object
 
 void f2__primobject_object__reinitialize_globalvars() {
-  __object__symbol = new__symbol(initial_cause(), "object");
+  f2__primcfunk__init(object__new);
 }
 
 void f2__primobject_object__defragment__fix_pointers() {
@@ -119,8 +119,6 @@ void f2__primobject_object__initialize() {
   funk2_module_registration__add_module(&(__funk2.module_registration), "primobject_object", "", &f2__primobject_object__reinitialize_globalvars, &f2__primobject_object__defragment__fix_pointers);
   
   f2__primobject_object__reinitialize_globalvars();
-  
-  f2__primcfunk__init(object__new);
   
   environment__add_var_value(initial_cause(), global_environment(), __object__symbol, nil);
 }

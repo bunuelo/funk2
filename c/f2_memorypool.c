f59679ccc99287b52f39f8a8c52eed440c819260
Bo Morgan <bo@mit.edu>
2012-03-30 10:48:45 -0700

    working on defragment2 (debugging fix pointers)

--- c/f2_memorypool.c
+++ c/f2_memorypool.c
@@ -313,6 +313,9 @@ void funk2_memorypool__shrink_last_free_block(funk2_memorypool_t* this, f2size_t
   if (byte_num < memblock__minimum_size) {
     error(nil, "funk2_memorypool__shrink_last_free_block fatal error: cannot attempt to resize block to size smmaller than memblock__minimum_size.");
   }
+  if (byte_num < f2ptr_block__max_value) {
+    byte_num = f2ptr_block__max_value;
+  }
   funk2_memblock_t* old_end_of_blocks       = funk2_memorypool__end_of_blocks(this);
   u64               old_last_block_byte_num = this->last_block_byte_num; 
   if (old_last_block_byte_num > byte_num) {

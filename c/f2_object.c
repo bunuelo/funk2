591c62d6aca38a8eb1d7b2d0681b6375d3f03578
Bo Morgan <bo@mit.edu>
2012-03-30 11:33:10 -0700

    working on defragment2 (debugging fix pointers)

--- c/f2_object.c
+++ c/f2_object.c
@@ -21,8 +21,6 @@
 
 #include "funk2.h"
 
-//#define F2__DEBUG__OBJECT__TYPE
-
 #if defined(F2__DEBUG__OBJECT__TYPE)
 #  define object_type_status(msg, rest...) status("[object_type] " msg, ## rest)
 #else

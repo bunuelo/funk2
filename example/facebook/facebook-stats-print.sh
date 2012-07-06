d19b4512679d79ecad9dc34512494cf02ff54777
Bo Morgan <bo@mit.edu>
2012-07-05 18:26:00 -0700

    working on facebook

--- example/facebook/facebook-stats-print.sh
+++ example/facebook/facebook-stats-print.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-ls -1 /home/neptune/Desktop/facebook/profile/ | wc; ls -1 /home/neptune/Desktop/facebook/profile/*/public_webpage.html | wc; ls -1 /home/neptune/Desktop/facebook/profile/*/login_webpage.html | wc; ls -1 /home/neptune/Desktop/facebook/profile/*/all_friends_webpage.html | wc; du -sk /home/neptune/Desktop/facebook/profile/
+echo -n "Know of Profile        : "; ls -1 /home/neptune/Desktop/facebook/profile/ | wc -l
+echo -n "Downloaded Public Page : "; ls -1 /home/neptune/Desktop/facebook/profile/*/public_webpage.html | wc -l
+echo -n "Downloaded Login Page  : "; ls -1 /home/neptune/Desktop/facebook/profile/*/login_webpage.html | wc -l
+echo -n "Downloaded Friends Page: "; ls -1 /home/neptune/Desktop/facebook/profile/*/all_friends_webpage.html | wc -l
+echo -n "Megabytes of HD Space  : "; du -sm /home/neptune/Desktop/facebook/profile/
 

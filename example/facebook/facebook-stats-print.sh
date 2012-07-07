82ee0a65830b424ac110bb8c3420a739d1e19213
Bo Morgan <bo@mit.edu>
2012-07-07 11:39:56 -0700

    working on facebook

--- example/facebook/facebook-stats-print.sh
+++ example/facebook/facebook-stats-print.sh
@@ -2,11 +2,12 @@
 
 cd /home/neptune/Desktop/facebook/
 
-echo -n "All Profile              : "; ls -1 know_of/ tried_downloading/ | wc -l
+echo -n "All Profile              : "; ls -1 know_of/ tried_downloading/ tried_expanding/ | wc -l
 echo -n "Know of Profile          : "; ls -1 know_of/ | wc -l
 echo -n "Tried Downloading Profile: "; ls -1 tried_downloading/ | wc -l
-echo -n "Downloaded Public Page   : "; ls -1 tried_downloading/*/public_webpage.html | wc -l
-echo -n "Downloaded Login Page    : "; ls -1 tried_downloading/*/login_webpage.html | wc -l
-echo -n "Downloaded Friends Page  : "; ls -1 tried_downloading/*/all_friends_webpage.html | wc -l
+echo -n "Tried Expanding Profile  : "; ls -1 tried_expanding/ | wc -l
+echo -n "Downloaded Public Page   : "; ls -1 */*/public_webpage.html | wc -l
+echo -n "Downloaded Login Page    : "; ls -1 */*/login_webpage.html | wc -l
+echo -n "Downloaded Friends Page  : "; ls -1 */*/all_friends_webpage.html | wc -l
 echo -n "Megabytes of HD Space    : "; du -sm .
 

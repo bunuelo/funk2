5535f551daa6b7ed45bb4f77feb40b1506406948
Bo Morgan <bo@mit.edu>
2012-07-08 09:05:05 -0700

    working on facebook

--- example/facebook/facebook-stats-print.sh
+++ example/facebook/facebook-stats-print.sh
@@ -2,10 +2,10 @@
 
 cd /home/neptune/Desktop/facebook/
 
-echo -n "All Profile              : "; find -maxdepth 1 know-of/ tried-downloading/ tried-expanding/ | wc -l
-echo -n "Know of Profile          : "; find -maxdepth 1 know-of/ | wc -l
-echo -n "Tried Downloading Profile: "; find -maxdepth 1 tried-downloading/ | wc -l
-echo -n "Tried Expanding Profile  : "; find -maxdepth 1 tried-expanding/ | wc -l
+echo -n "All Profile              : "; find know-of/ tried-downloading/ tried-expanding/ -maxdepth 1 | wc -l
+echo -n "Know of Profile          : "; find know-of/ -maxdepth 1 | wc -l
+echo -n "Tried Downloading Profile: "; find tried-downloading/ -maxdepth 1 | wc -l
+echo -n "Tried Expanding Profile  : "; find tried-expanding/ -maxdepth 1 | wc -l
 echo -n "Downloaded Public Page   : "; find . | grep /public_webpage.html | wc -l
 echo -n "Downloaded Login Page    : "; find . | grep /login_webpage.html | wc -l
 echo -n "Downloaded Friends Page  : "; find . | grep /all_friends_webpage.html | wc -l

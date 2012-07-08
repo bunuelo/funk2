3f8ba808ec402a1b6cf5340cb1fa5b0678ec1aae
Bo Morgan <bo@mit.edu>
2012-07-08 09:04:21 -0700

    working on facebook

--- example/facebook/facebook-stats-print.sh
+++ example/facebook/facebook-stats-print.sh
@@ -2,12 +2,12 @@
 
 cd /home/neptune/Desktop/facebook/
 
-echo -n "All Profile              : "; ls -1 know-of/ tried-downloading/ tried-expanding/ | wc -l
-echo -n "Know of Profile          : "; ls -1 know-of/ | wc -l
-echo -n "Tried Downloading Profile: "; ls -1 tried-downloading/ | wc -l
-echo -n "Tried Expanding Profile  : "; ls -1 tried-expanding/ | wc -l
-echo -n "Downloaded Public Page   : "; ls -1 */*/public_webpage.html | wc -l
-echo -n "Downloaded Login Page    : "; ls -1 */*/login_webpage.html | wc -l
-echo -n "Downloaded Friends Page  : "; ls -1 */*/all_friends_webpage.html | wc -l
+echo -n "All Profile              : "; find -maxdepth 1 know-of/ tried-downloading/ tried-expanding/ | wc -l
+echo -n "Know of Profile          : "; find -maxdepth 1 know-of/ | wc -l
+echo -n "Tried Downloading Profile: "; find -maxdepth 1 tried-downloading/ | wc -l
+echo -n "Tried Expanding Profile  : "; find -maxdepth 1 tried-expanding/ | wc -l
+echo -n "Downloaded Public Page   : "; find . | grep /public_webpage.html | wc -l
+echo -n "Downloaded Login Page    : "; find . | grep /login_webpage.html | wc -l
+echo -n "Downloaded Friends Page  : "; find . | grep /all_friends_webpage.html | wc -l
 echo -n "Megabytes of HD Space    : "; du -sm .
 

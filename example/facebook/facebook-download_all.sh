806672578f022286578462ab6471c0ac6f0522a6
Bo Morgan <bo@mit.edu>
2012-07-08 11:01:04 -0700

    adding facebook-expand_all and facebook-download_all

--- example/facebook/facebook-download_all.sh
+++ example/facebook/facebook-download_all.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH="lib/.libs:/opt/intel/Compiler/11.1/038/lib/intel64/"; export DYLD_LIBRARY_PATH=lib/.libs; export PATH="lib/.libs:$PATH"; bin/funk2 -i img/bootstrap-facebook-download_all.img
+export LD_LIBRARY_PATH="lib/.libs:/opt/intel/Compiler/11.1/038/lib/intel64/"
+export DYLD_LIBRARY_PATH=lib/.libs
+export PATH="lib/.libs:$PATH"
+
+while sleep 10; do bin/funk2 -i img/bootstrap-facebook-download_all.img; done
+
+
 
--- example/facebook/facebook-expand_all.sh
+++ example/facebook/facebook-expand_all.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH="lib/.libs:/opt/intel/Compiler/11.1/038/lib/intel64/"; export DYLD_LIBRARY_PATH=lib/.libs; export PATH="lib/.libs:$PATH"; bin/funk2 -i img/bootstrap-facebook-expand_all.img
+export LD_LIBRARY_PATH="lib/.libs:/opt/intel/Compiler/11.1/038/lib/intel64/"
+export DYLD_LIBRARY_PATH=lib/.libs
+export PATH="lib/.libs:$PATH"
+
+while sleep 10; do bin/funk2 -i img/bootstrap-facebook-expand_all.img; done
+
+
+
 

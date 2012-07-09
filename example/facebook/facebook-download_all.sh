1fdc550b1a09f9b459ba9902c16659490b2ec6bd
Bo Morgan <bo@mit.edu>
2012-07-09 13:03:02 -0700

    working on facebook

--- example/facebook/facebook-download_all.sh
+++ example/facebook/facebook-download_all.sh
@@ -4,7 +4,7 @@ export LD_LIBRARY_PATH="lib/.libs:/opt/intel/Compiler/11.1/038/lib/intel64/"
 export DYLD_LIBRARY_PATH=lib/.libs
 export PATH="lib/.libs:$PATH"
 
-while sleep 10; do bin/funk2 -i img/bootstrap-facebook-download_all.img; done
+while sleep 1; do bin/funk2 -i img/bootstrap-facebook-download_all.img; done
 
 
 

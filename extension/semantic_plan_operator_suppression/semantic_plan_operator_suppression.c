b10954dfaf0160a90ce54cbbbc8d2b19f0d51905
Bo Morgan <bo@mit.edu>
2012-07-29 18:34:31 -0700

    working on semantic knowledge

--- extension/semantic_plan_operator_suppression/semantic_plan_operator_suppression.c
+++ extension/semantic_plan_operator_suppression/semantic_plan_operator_suppression.c
@@ -30,8 +30,8 @@ f2ptr raw__semantic_plan_operator_suppression__type_create(f2ptr cause, f2ptr th
   }
   assert_value(raw__semantic_object__type_create(cause, this, semantic_realm));
   // avoids redefining in cases of multiple inheritance.
-  if (raw__semantic_frame__lookup_set(cause, this, new__symbol(cause, "property"), new__symbol(cause, "physical_object_type")) == nil) {
-    raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "physical_object_type"), nil);
+  if (raw__semantic_frame__lookup_set(cause, this, new__symbol(cause, "property"), new__symbol(cause, "resource")) == nil) {
+    raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "resource"), nil);
   }
   return this;
 }
@@ -82,87 +82,36 @@ f2ptr f2__semantic_plan_operator_suppression__type(f2ptr cause, f2ptr this) {
 export_cefunk1(semantic_plan_operator_suppression__type, thing, 0, "Returns the specific type of object that this semantic_plan_operator_suppression is.");
 
 
-f2ptr raw__semantic_plan_operator_suppression__physical_object_type(f2ptr cause, f2ptr this) {
-  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "physical_object_type"));
+f2ptr raw__semantic_plan_operator_suppression__resource(f2ptr cause, f2ptr this) {
+  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "resource"));
 }
 
-f2ptr f2__semantic_plan_operator_suppression__physical_object_type(f2ptr cause, f2ptr this) {
+f2ptr f2__semantic_plan_operator_suppression__resource(f2ptr cause, f2ptr this) {
   assert_argument_type(semantic_plan_operator_suppression, this);
-  return raw__semantic_plan_operator_suppression__physical_object_type(cause, this);
+  return raw__semantic_plan_operator_suppression__resource(cause, this);
 }
-export_cefunk1(semantic_plan_operator_suppression__physical_object_type, this, 0, "");
+export_cefunk1(semantic_plan_operator_suppression__resource, this, 0, "");
 
 
-f2ptr raw__semantic_plan_operator_suppression__physical_object_type__set(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "physical_object_type"), that);
+f2ptr raw__semantic_plan_operator_suppression__resource__set(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "resource"), that);
 }
 
-f2ptr f2__semantic_plan_operator_suppression__physical_object_type__set(f2ptr cause, f2ptr this, f2ptr that) {
+f2ptr f2__semantic_plan_operator_suppression__resource__set(f2ptr cause, f2ptr this, f2ptr that) {
   assert_argument_type(semantic_plan_operator_suppression, this);
-  return raw__semantic_plan_operator_suppression__physical_object_type__set(cause, this, that);
+  return raw__semantic_plan_operator_suppression__resource__set(cause, this, that);
 }
-export_cefunk2(semantic_plan_operator_suppression__physical_object_type__set, this, that, 0, "");
-
-
-f2ptr raw__semantic_plan_operator_suppression__preposition__on__add(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__add(cause, this, new__symbol(cause, "preposition"), new__symbol(cause, "on"), that);
-}
-
-f2ptr f2__semantic_plan_operator_suppression__preposition__on__add(f2ptr cause, f2ptr this, f2ptr that) {
-  assert_argument_type(semantic_plan_operator_suppression, this);
-  assert_argument_type(semantic_plan_operator_suppression, that);
-  return raw__semantic_plan_operator_suppression__preposition__on__add(cause, this, that);
-}
-export_cefunk2(semantic_plan_operator_suppression__preposition__on__add, this, that, 0, "");
-
-
-f2ptr raw__semantic_plan_operator_suppression__preposition__on__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__remove(cause, this, new__symbol(cause, "preposition"), new__symbol(cause, "on"), that);
-}
-
-f2ptr f2__semantic_plan_operator_suppression__preposition__on__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  assert_argument_type(semantic_plan_operator_suppression, this);
-  assert_argument_type(semantic_plan_operator_suppression, that);
-  return raw__semantic_plan_operator_suppression__preposition__on__remove(cause, this, that);
-}
-export_cefunk2(semantic_plan_operator_suppression__preposition__on__remove, this, that, 0, "");
-
-
-f2ptr raw__semantic_plan_operator_suppression__preposition__in__add(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__add(cause, this, new__symbol(cause, "preposition"), new__symbol(cause, "in"), that);
-}
-
-f2ptr f2__semantic_plan_operator_suppression__preposition__in__add(f2ptr cause, f2ptr this, f2ptr that) {
-  assert_argument_type(semantic_plan_operator_suppression, this);
-  assert_argument_type(semantic_plan_operator_suppression, that);
-  return raw__semantic_plan_operator_suppression__preposition__in__add(cause, this, that);
-}
-export_cefunk2(semantic_plan_operator_suppression__preposition__in__add, this, that, 0, "");
-
-
-f2ptr raw__semantic_plan_operator_suppression__preposition__in__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__remove(cause, this, new__symbol(cause, "preposition"), new__symbol(cause, "in"), that);
-}
-
-f2ptr f2__semantic_plan_operator_suppression__preposition__in__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  assert_argument_type(semantic_plan_operator_suppression, this);
-  assert_argument_type(semantic_plan_operator_suppression, that);
-  return raw__semantic_plan_operator_suppression__preposition__in__remove(cause, this, that);
-}
-export_cefunk2(semantic_plan_operator_suppression__preposition__in__remove, this, that, 0, "");
+export_cefunk2(semantic_plan_operator_suppression__resource__set, this, that, 0, "");
 
 
 f2ptr f2__semantic_plan_operator_suppression__primobject_type__new(f2ptr cause) {
   f2ptr this = f2__primobject_type__new(cause, f2list1__new(cause, new__symbol(cause, "semantic_object")));
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"),             new__symbol(cause, "new"),                  f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_suppression"), new__symbol(cause, "semantic_plan_operator_suppression__new")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"),             new__symbol(cause, "is_type"),              f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_suppression"), new__symbol(cause, "semantic_plan_operator_suppression__is_type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),                 new__symbol(cause, "type"),                 f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_suppression"), new__symbol(cause, "semantic_plan_operator_suppression__type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),                 new__symbol(cause, "physical_object_type"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_suppression"), new__symbol(cause, "semantic_plan_operator_suppression__physical_object_type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),                 new__symbol(cause, "physical_object_type"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_suppression"), new__symbol(cause, "semantic_plan_operator_suppression__physical_object_type__set")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-add"),        new__symbol(cause, "preposition-in"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_suppression"), new__symbol(cause, "semantic_plan_operator_suppression__preposition__in__add")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-remove"),     new__symbol(cause, "preposition-in"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_suppression"), new__symbol(cause, "semantic_plan_operator_suppression__preposition__in__remove")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-add"),        new__symbol(cause, "preposition-on"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_suppression"), new__symbol(cause, "semantic_plan_operator_suppression__preposition__on__add")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-remove"),     new__symbol(cause, "preposition-on"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_suppression"), new__symbol(cause, "semantic_plan_operator_suppression__preposition__on__remove")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "new"),      f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_suppression"), new__symbol(cause, "semantic_plan_operator_suppression__new")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "is_type"),  f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_suppression"), new__symbol(cause, "semantic_plan_operator_suppression__is_type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "type"),     f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_suppression"), new__symbol(cause, "semantic_plan_operator_suppression__type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "resource"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_suppression"), new__symbol(cause, "semantic_plan_operator_suppression__resource")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "resource"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_suppression"), new__symbol(cause, "semantic_plan_operator_suppression__resource__set")));}
+);}
   return this;
 }
 

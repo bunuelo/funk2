e28171245c6a97d167d935b1ab3695e78d54e046
Bo Morgan <bo@mit.edu>
2012-07-29 17:47:41 -0700

    working on semantic knowledge

--- extension/semantic_plan_operator_sequence/semantic_plan_operator_sequence.c
+++ extension/semantic_plan_operator_sequence/semantic_plan_operator_sequence.c
@@ -19,41 +19,42 @@
 // rights to redistribute these changes.
 // 
 
-#include "semantic_physical_object.h"
+#include "semantic_plan_operator_sequence.h"
 
 
-// semantic_physical_object
+// semantic_plan_operator_sequence
 
-f2ptr raw__semantic_physical_object__type_create(f2ptr cause, f2ptr this, f2ptr semantic_realm) {
+f2ptr raw__semantic_plan_operator_sequence__type_create(f2ptr cause, f2ptr this, f2ptr semantic_realm) {
   if (! raw__frame__contains_var(cause, this, new__symbol(cause, "type"))) {
-    raw__frame__add_var_value(cause, this, new__symbol(cause, "type"), new__symbol(cause, "semantic_physical_object"));
+    raw__frame__add_var_value(cause, this, new__symbol(cause, "type"), new__symbol(cause, "semantic_plan_operator_sequence"));
   }
   assert_value(raw__semantic_object__type_create(cause, this, semantic_realm));
   // avoids redefining in cases of multiple inheritance.
-  if (raw__semantic_frame__lookup_set(cause, this, new__symbol(cause, "property"), new__symbol(cause, "physical_object_type")) == nil) {
-    raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "physical_object_type"), nil);
+  if (raw__semantic_frame__lookup_set(cause, this, new__symbol(cause, "relation"), new__symbol(cause, "past_operator")) == nil) {
+    raw__semantic_frame__add(cause, this, new__symbol(cause, "relation"), new__symbol(cause, "past_operator"), nil);
+    raw__semantic_frame__add(cause, this, new__symbol(cause, "relation"), new__symbol(cause, "future_operator"), nil);
   }
   return this;
 }
 
-f2ptr raw__semantic_physical_object__new(f2ptr cause, f2ptr semantic_realm) {
+f2ptr raw__semantic_plan_operator_sequence__new(f2ptr cause, f2ptr semantic_realm) {
   f2ptr this = assert_value(f2__frame__new(cause, nil));
-  assert_value(raw__semantic_physical_object__type_create(cause, this, semantic_realm));
+  assert_value(raw__semantic_plan_operator_sequence__type_create(cause, this, semantic_realm));
   return this;
 }
 
-f2ptr f2__semantic_physical_object__new(f2ptr cause, f2ptr semantic_realm) {
+f2ptr f2__semantic_plan_operator_sequence__new(f2ptr cause, f2ptr semantic_realm) {
   assert_argument_type(semantic_realm, semantic_realm);
-  return raw__semantic_physical_object__new(cause, semantic_realm);
+  return raw__semantic_plan_operator_sequence__new(cause, semantic_realm);
 }
-export_cefunk1(semantic_physical_object__new, semantic_realm, 0, "Returns a new semantic_physical_object object.");
+export_cefunk1(semantic_plan_operator_sequence__new, semantic_realm, 0, "Returns a new semantic_plan_operator_sequence object.");
 
 
-boolean_t raw__semantic_physical_object__is_type(f2ptr cause, f2ptr thing) {
+boolean_t raw__semantic_plan_operator_sequence__is_type(f2ptr cause, f2ptr thing) {
   if (! raw__frame__is_type(cause, thing)) {
     return boolean__false;
   }
-  f2ptr this_type_name_symbol = new__symbol(cause, "semantic_physical_object");
+  f2ptr this_type_name_symbol = new__symbol(cause, "semantic_plan_operator_sequence");
   f2ptr thing_type_name       = f2__frame__lookup_var_value(cause, thing, new__symbol(cause, "type"), nil);
   if (raw__eq(cause, this_type_name_symbol, thing_type_name)) {
     return boolean__true;
@@ -65,104 +66,76 @@ boolean_t raw__semantic_physical_object__is_type(f2ptr cause, f2ptr thing) {
   return boolean__false;
 }
 
-f2ptr f2__semantic_physical_object__is_type(f2ptr cause, f2ptr thing) {
-  return f2bool__new(raw__semantic_physical_object__is_type(cause, thing));
+f2ptr f2__semantic_plan_operator_sequence__is_type(f2ptr cause, f2ptr thing) {
+  return f2bool__new(raw__semantic_plan_operator_sequence__is_type(cause, thing));
 }
-export_cefunk1(semantic_physical_object__is_type, thing, 0, "Returns whether or not thing is of type semantic_physical_object.");
+export_cefunk1(semantic_plan_operator_sequence__is_type, thing, 0, "Returns whether or not thing is of type semantic_plan_operator_sequence.");
 
 
-f2ptr raw__semantic_physical_object__type(f2ptr cause, f2ptr this) {
+f2ptr raw__semantic_plan_operator_sequence__type(f2ptr cause, f2ptr this) {
   return f2__object__type(cause, this);
 }
 
-f2ptr f2__semantic_physical_object__type(f2ptr cause, f2ptr this) {
-  assert_argument_type(semantic_physical_object, this);
-  return raw__semantic_physical_object__type(cause, this);
+f2ptr f2__semantic_plan_operator_sequence__type(f2ptr cause, f2ptr this) {
+  assert_argument_type(semantic_plan_operator_sequence, this);
+  return raw__semantic_plan_operator_sequence__type(cause, this);
 }
-export_cefunk1(semantic_physical_object__type, thing, 0, "Returns the specific type of object that this semantic_physical_object is.");
+export_cefunk1(semantic_plan_operator_sequence__type, thing, 0, "Returns the specific type of object that this semantic_plan_operator_sequence is.");
 
 
-f2ptr raw__semantic_physical_object__physical_object_type(f2ptr cause, f2ptr this) {
-  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "physical_object_type"));
-}
-
-f2ptr f2__semantic_physical_object__physical_object_type(f2ptr cause, f2ptr this) {
-  assert_argument_type(semantic_physical_object, this);
-  return raw__semantic_physical_object__physical_object_type(cause, this);
-}
-export_cefunk1(semantic_physical_object__physical_object_type, this, 0, "");
-
-
-f2ptr raw__semantic_physical_object__physical_object_type__set(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "physical_object_type"), that);
+f2ptr raw__semantic_plan_operator_sequence__past_operator(f2ptr cause, f2ptr this) {
+  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "relation"), new__symbol(cause, "past_operator"));
 }
 
-f2ptr f2__semantic_physical_object__physical_object_type__set(f2ptr cause, f2ptr this, f2ptr that) {
-  assert_argument_type(semantic_physical_object, this);
-  return raw__semantic_physical_object__physical_object_type__set(cause, this, that);
+f2ptr f2__semantic_plan_operator_sequence__past_operator(f2ptr cause, f2ptr this) {
+  assert_argument_type(semantic_plan_operator_sequence, this);
+  return raw__semantic_plan_operator_sequence__past_operator(cause, this);
 }
-export_cefunk2(semantic_physical_object__physical_object_type__set, this, that, 0, "");
+export_cefunk1(semantic_plan_operator_sequence__past_operator, this, 0, "");
 
 
-f2ptr raw__semantic_physical_object__preposition__on__add(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__add(cause, this, new__symbol(cause, "preposition"), new__symbol(cause, "on"), that);
-}
-
-f2ptr f2__semantic_physical_object__preposition__on__add(f2ptr cause, f2ptr this, f2ptr that) {
-  assert_argument_type(semantic_physical_object, this);
-  assert_argument_type(semantic_physical_object, that);
-  return raw__semantic_physical_object__preposition__on__add(cause, this, that);
-}
-export_cefunk2(semantic_physical_object__preposition__on__add, this, that, 0, "");
-
-
-f2ptr raw__semantic_physical_object__preposition__on__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__remove(cause, this, new__symbol(cause, "preposition"), new__symbol(cause, "on"), that);
+f2ptr raw__semantic_plan_operator_sequence__past_operator__set(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "relation"), new__symbol(cause, "past_operator"), that);
 }
 
-f2ptr f2__semantic_physical_object__preposition__on__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  assert_argument_type(semantic_physical_object, this);
-  assert_argument_type(semantic_physical_object, that);
-  return raw__semantic_physical_object__preposition__on__remove(cause, this, that);
+f2ptr f2__semantic_plan_operator_sequence__past_operator__set(f2ptr cause, f2ptr this, f2ptr that) {
+  assert_argument_type(semantic_plan_operator_sequence, this);
+  return raw__semantic_plan_operator_sequence__past_operator__set(cause, this, that);
 }
-export_cefunk2(semantic_physical_object__preposition__on__remove, this, that, 0, "");
+export_cefunk2(semantic_plan_operator_sequence__past_operator__set, this, that, 0, "");
 
 
-f2ptr raw__semantic_physical_object__preposition__in__add(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__add(cause, this, new__symbol(cause, "preposition"), new__symbol(cause, "in"), that);
-}
-
-f2ptr f2__semantic_physical_object__preposition__in__add(f2ptr cause, f2ptr this, f2ptr that) {
-  assert_argument_type(semantic_physical_object, this);
-  assert_argument_type(semantic_physical_object, that);
-  return raw__semantic_physical_object__preposition__in__add(cause, this, that);
-}
-export_cefunk2(semantic_physical_object__preposition__in__add, this, that, 0, "");
-
-
-f2ptr raw__semantic_physical_object__preposition__in__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  return raw__semantic_frame__remove(cause, this, new__symbol(cause, "preposition"), new__symbol(cause, "in"), that);
+f2ptr raw__semantic_plan_operator_sequence__future_operator(f2ptr cause, f2ptr this) {
+  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "relation"), new__symbol(cause, "future_operator"));
 }
 
-f2ptr f2__semantic_physical_object__preposition__in__remove(f2ptr cause, f2ptr this, f2ptr that) {
-  assert_argument_type(semantic_physical_object, this);
-  assert_argument_type(semantic_physical_object, that);
-  return raw__semantic_physical_object__preposition__in__remove(cause, this, that);
+f2ptr f2__semantic_plan_operator_sequence__future_operator(f2ptr cause, f2ptr this) {
+  assert_argument_type(semantic_plan_operator_sequence, this);
+  return raw__semantic_plan_operator_sequence__future_operator(cause, this);
 }
-export_cefunk2(semantic_physical_object__preposition__in__remove, this, that, 0, "");
+export_cefunk1(semantic_plan_operator_sequence__future_operator, this, 0, "");
 
 
-f2ptr f2__semantic_physical_object__primobject_type__new(f2ptr cause) {
+f2ptr raw__semantic_plan_operator_sequence__future_operator__set(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "relation"), new__symbol(cause, "future_operator"), that);
+}
+
+f2ptr f2__semantic_plan_operator_sequence__future_operator__set(f2ptr cause, f2ptr this, f2ptr that) {
+  assert_argument_type(semantic_plan_operator_sequence, this);
+  return raw__semantic_plan_operator_sequence__future_operator__set(cause, this, that);
+}
+export_cefunk2(semantic_plan_operator_sequence__future_operator__set, this, that, 0, "");
+
+
+f2ptr f2__semantic_plan_operator_sequence__primobject_type__new(f2ptr cause) {
   f2ptr this = f2__primobject_type__new(cause, f2list1__new(cause, new__symbol(cause, "semantic_object")));
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"),             new__symbol(cause, "new"),                  f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_physical_object"), new__symbol(cause, "semantic_physical_object__new")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"),             new__symbol(cause, "is_type"),              f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_physical_object"), new__symbol(cause, "semantic_physical_object__is_type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),                 new__symbol(cause, "type"),                 f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_physical_object"), new__symbol(cause, "semantic_physical_object__type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),                 new__symbol(cause, "physical_object_type"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_physical_object"), new__symbol(cause, "semantic_physical_object__physical_object_type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),                 new__symbol(cause, "physical_object_type"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_physical_object"), new__symbol(cause, "semantic_physical_object__physical_object_type__set")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-add"),        new__symbol(cause, "preposition-in"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_physical_object"), new__symbol(cause, "semantic_physical_object__preposition__in__add")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-remove"),     new__symbol(cause, "preposition-in"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_physical_object"), new__symbol(cause, "semantic_physical_object__preposition__in__remove")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-add"),        new__symbol(cause, "preposition-on"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_physical_object"), new__symbol(cause, "semantic_physical_object__preposition__on__add")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-remove"),     new__symbol(cause, "preposition-on"),       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_physical_object"), new__symbol(cause, "semantic_physical_object__preposition__on__remove")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "new"),             f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_sequence"), new__symbol(cause, "semantic_plan_operator_sequence__new")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "is_type"),         f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_sequence"), new__symbol(cause, "semantic_plan_operator_sequence__is_type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "type"),            f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_sequence"), new__symbol(cause, "semantic_plan_operator_sequence__type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "past_operator"),   f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_sequence"), new__symbol(cause, "semantic_plan_operator_sequence__past_operator")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "past_operator"),   f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_sequence"), new__symbol(cause, "semantic_plan_operator_sequence__past_operator__set")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "future_operator"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_sequence"), new__symbol(cause, "semantic_plan_operator_sequence__future_operator")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "future_operator"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_plan_operator_sequence"), new__symbol(cause, "semantic_plan_operator_sequence__future_operator__set")));}
   return this;
 }
 
@@ -172,32 +145,32 @@ f2ptr f2__semantic_physical_object__primobject_type__new(f2ptr cause) {
 
 // **
 
-f2ptr f2__semantic_physical_object__core_extension__ping(f2ptr cause) {
+f2ptr f2__semantic_plan_operator_sequence__core_extension__ping(f2ptr cause) {
   return nil;
 }
-export_cefunk0(semantic_physical_object__core_extension__ping, 0, "");
+export_cefunk0(semantic_plan_operator_sequence__core_extension__ping, 0, "");
 
 
-f2ptr f2__semantic_physical_object__core_extension__initialize(f2ptr cause) {
-  core_extension__ping(semantic_physical_object, semantic_object);
-  status("semantic_physical_object initialized.");
+f2ptr f2__semantic_plan_operator_sequence__core_extension__initialize(f2ptr cause) {
+  core_extension__ping(semantic_plan_operator_sequence, semantic_object);
+  status("semantic_plan_operator_sequence initialized.");
   return nil;
 }
-export_cefunk0(semantic_physical_object__core_extension__initialize, 0, "");
+export_cefunk0(semantic_plan_operator_sequence__core_extension__initialize, 0, "");
 
 
-f2ptr f2__semantic_physical_object__core_extension__define_types(f2ptr cause) {
-  f2__add_type(cause, new__symbol(cause, "semantic_physical_object"), f2__semantic_physical_object__primobject_type__new(cause));
-  status("semantic_physical_object define types.");
+f2ptr f2__semantic_plan_operator_sequence__core_extension__define_types(f2ptr cause) {
+  f2__add_type(cause, new__symbol(cause, "semantic_plan_operator_sequence"), f2__semantic_plan_operator_sequence__primobject_type__new(cause));
+  status("semantic_plan_operator_sequence define types.");
   return nil;
 }
-export_cefunk0(semantic_physical_object__core_extension__define_types, 0, "");
+export_cefunk0(semantic_plan_operator_sequence__core_extension__define_types, 0, "");
 
 
-f2ptr f2__semantic_physical_object__core_extension__destroy(f2ptr cause) {
-  status("semantic_physical_object destroyed.");
+f2ptr f2__semantic_plan_operator_sequence__core_extension__destroy(f2ptr cause) {
+  status("semantic_plan_operator_sequence destroyed.");
   return nil;
 }
-export_cefunk0(semantic_physical_object__core_extension__destroy, 0, "");
+export_cefunk0(semantic_plan_operator_sequence__core_extension__destroy, 0, "");
 
 

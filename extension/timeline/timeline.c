4e4750d081c47b5f4d18cd57febee98c47882b3d
Bo Morgan <bo@mit.edu>
2012-08-11 14:49:12 -0400

    working on precondition hypothesis space

--- extension/timeline/timeline.c
+++ extension/timeline/timeline.c
@@ -1026,29 +1026,29 @@ f2ptr raw__timeline__calculate_positions(f2ptr cause, f2ptr this) {
     }
   }
   // calculate minimum_x for each connected part
-  //{
-  //  f2ptr connected_part_iter = connected_parts;
-  //  while (connected_part_iter != nil) {
-  //    f2ptr connected_part = f2__cons__car(cause, connected_part_iter);
-  //    {
-  //	f2ptr minimum_x     = nil;
-  //	f2ptr connected_set = raw__timeline_connected_part__event_set(cause, connected_part);
-  //	set__iteration(cause, connected_set, connected_event,
-  //		       f2ptr start_time = assert_value(f2__timeline_event__start_time(cause, connected_event));
-  //		       if ((minimum_x == nil) ||
-  //			   (assert_value(f2__is_less_than(cause, start_time, minimum_x)) != nil)) {
-  //			 minimum_x = start_time;
-  //		       }
-  //		       );
-  //	f2__timeline_connected_part__event_set__set(cause, connected_part, minimum_x);
-  //    }
-  //    connected_part_iter = assert_value(f2__cons__cdr(cause, connected_part_iter));
-  //  }
-  //}
+  {
+    f2ptr connected_part_iter = connected_parts;
+    while (connected_part_iter != nil) {
+      f2ptr connected_part = f2__cons__car(cause, connected_part_iter);
+      {
+  	f2ptr minimum_x     = nil;
+  	f2ptr connected_set = raw__timeline_connected_part__event_set(cause, connected_part);
+  	set__iteration(cause, connected_set, connected_event,
+  		       f2ptr start_time = assert_value(f2__timeline_event__start_time(cause, connected_event));
+  		       if ((minimum_x == nil) ||
+  			   (assert_value(f2__is_less_than(cause, start_time, minimum_x)) != nil)) {
+  			 minimum_x = start_time;
+  		       }
+  		       );
+  	f2__timeline_connected_part__minimum_x__set(cause, connected_part, minimum_x);
+      }
+      connected_part_iter = assert_value(f2__cons__cdr(cause, connected_part_iter));
+    }
+  }
   // sort connected parts by minimum_x
-  //{
-  //  
-  //}
+  {
+    
+  }
   // 
   double y_event_distance = f2double__d(raw__timeline__y_event_distance(cause, this), cause);
   {

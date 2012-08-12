9d1a6a4edc4096f2a56e5e7aeff0c24068330582
Bo Morgan <bo@mit.edu>
2012-08-12 13:48:11 -0400

    working on deliberative1 goals

--- extension/semantic_planner/semantic_planner.c
+++ extension/semantic_planner/semantic_planner.c
@@ -259,6 +259,39 @@ f2ptr f2__semantic_planner__negative_goal__remove(f2ptr cause, f2ptr this, f2ptr
 export_cefunk2(semantic_planner__negative_goal__remove, this, that, 0, "");
 
 
+f2ptr raw__semantic_planner__bug_plan__lookup_set(f2ptr cause, f2ptr this) {
+  return raw__semantic_frame__lookup_set(cause, this, new__symbol(cause, "relation"), new__symbol(cause, "bug_plan"));
+}
+
+f2ptr f2__semantic_planner__bug_plan__lookup_set(f2ptr cause, f2ptr this) {
+  assert_argument_type(semantic_planner, this);
+  return raw__semantic_planner__bug_plan__lookup_set(cause, this);
+}
+export_cefunk1(semantic_planner__bug_plan__lookup_set, this, 0, "Returns the set of relation-ons.");
+
+
+f2ptr raw__semantic_planner__bug_plan__add(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__add(cause, this, new__symbol(cause, "relation"), new__symbol(cause, "bug_plan"), that);
+}
+
+f2ptr f2__semantic_planner__bug_plan__add(f2ptr cause, f2ptr this, f2ptr that) {
+  assert_argument_type(semantic_planner, this);
+  return raw__semantic_planner__bug_plan__add(cause, this, that);
+}
+export_cefunk2(semantic_planner__bug_plan__add, this, that, 0, "");
+
+
+f2ptr raw__semantic_planner__bug_plan__remove(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__remove(cause, this, new__symbol(cause, "relation"), new__symbol(cause, "bug_plan"), that);
+}
+
+f2ptr f2__semantic_planner__bug_plan__remove(f2ptr cause, f2ptr this, f2ptr that) {
+  assert_argument_type(semantic_planner, this);
+  return raw__semantic_planner__bug_plan__remove(cause, this, that);
+}
+export_cefunk2(semantic_planner__bug_plan__remove, this, that, 0, "");
+
+
 f2ptr f2__semantic_planner__primobject_type__new(f2ptr cause) {
   f2ptr this = f2__primobject_type__new(cause, f2list1__new(cause, new__symbol(cause, "semantic_object")));
   {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"),             new__symbol(cause, "new"),             f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_planner"), new__symbol(cause, "semantic_planner__new")));}
@@ -280,6 +313,9 @@ f2ptr f2__semantic_planner__primobject_type__new(f2ptr cause) {
   {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-lookup_set"), new__symbol(cause, "negative_goal"),   f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_planner"), new__symbol(cause, "semantic_planner__negative_goal__lookup_set")));}
   {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-add"),        new__symbol(cause, "negative_goal"),   f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_planner"), new__symbol(cause, "semantic_planner__negative_goal__add")));}
   {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-remove"),     new__symbol(cause, "negative_goal"),   f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_planner"), new__symbol(cause, "semantic_planner__negative_goal__remove")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-lookup_set"), new__symbol(cause, "bug_plan"),        f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_planner"), new__symbol(cause, "semantic_planner__bug_plan__lookup_set")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-add"),        new__symbol(cause, "bug_plan"),        f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_planner"), new__symbol(cause, "semantic_planner__bug_plan__add")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "semantic-remove"),     new__symbol(cause, "bug_plan"),        f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_planner"), new__symbol(cause, "semantic_planner__bug_plan__remove")));}
   return this;
 }
 

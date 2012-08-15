c0078a6cf4f69f05db8dd673861652f7859747f2
Bo Morgan <bo@mit.edu>
2012-08-15 12:25:26 -0400

    working on evaluation metrics

--- c/f2_cause.c
+++ c/f2_cause.c
@@ -181,7 +181,7 @@ f2ptr raw__cause_group__terminal_print_with_frame(f2ptr cause, f2ptr this, f2ptr
   f2ptr print_as_frame_hash = raw__terminal_print_frame__print_as_frame_hash(cause, terminal_print_frame);
   f2ptr frame               = raw__ptypehash__lookup(cause, print_as_frame_hash, this);
   if (frame == nil) {
-    frame = f2__frame__new(cause, f2list6__new(cause,
+    frame = f2__frame__new(cause, f2list8__new(cause,
 					       new__symbol(cause, "print_object_type"),     new__symbol(cause, "cause_group"),
 					       new__symbol(cause, "bytecode_count"),        f2__cause_group__bytecode_count(cause, this),
 					       new__symbol(cause, "execution_nanoseconds"), f2__cause_group__execution_nanoseconds(cause, this),

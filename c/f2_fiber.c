a9499ce12226e080b7a9b380f591115c0134d619
Bo Morgan <bo@mit.edu>
2012-08-15 13:40:14 -0400

    working on evaluation metrics

--- c/f2_fiber.c
+++ c/f2_fiber.c
@@ -556,7 +556,7 @@ void raw__fiber__handle_exit_virtual_processor(f2ptr cause, f2ptr this) {
     if (increase_execution_nanoseconds != 0) {
       u64 execution_nanoseconds = raw__fiber__execution_nanoseconds(cause, this);
       raw__fiber__execution_nanoseconds__set(cause, this, execution_nanoseconds + increase_execution_nanoseconds);
-      f2ptr cause_reg = f2fiber__cause_ref(this, cause);
+      f2ptr cause_reg = f2fiber__cause_reg(this, cause);
       if (cause_reg != nil) {
 	f2ptr cause_groups = f2cause__cause_groups(cause_reg, cause);
 	{
@@ -577,7 +577,7 @@ void raw__fiber__handle_exit_virtual_processor(f2ptr cause, f2ptr this) {
     if (increase_bytes_allocated_count != 0) {
       u64 bytes_allocated_count = raw__fiber__bytes_allocated_count(cause, this);
       raw__fiber__bytes_allocated_count__set(cause, this, bytes_allocated_count + increase_bytes_allocated_count);
-      f2ptr cause_reg = f2fiber__cause_ref(this, cause);
+      f2ptr cause_reg = f2fiber__cause_reg(this, cause);
       if (cause_reg != nil) {
 	f2ptr cause_groups = f2cause__cause_groups(cause_reg, cause);
 	{

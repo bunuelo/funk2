7dd75a953ebf7a377fe014b03d8146bb477f0a02
Bo Morgan <bo@mit.edu>
2012-09-11 18:15:37 -0400

    working on plots

--- funk2.sh
+++ funk2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 export UBUNTU_MENUPROXY=0
 export LD_LIBRARY_PATH="lib/.libs:/opt/intel/Compiler/11.1/038/lib/intel64/"
-export DYLD_LIBRARY_PATH=lib/.libs
+export DYLD_LIBRARY_PATH="lib/.libs"
 export PATH="lib/.libs:$PATH"
 bin/funk2 $@
-#bin/funk2-static

2626e24cc69e73b590a595c7ab6a0e4b6885f617
Bo Morgan <bo@mit.edu>
2012-09-14 18:44:59 -0400

    working on faster faster

--- c/f2_virtual_processor_thread.c
+++ c/f2_virtual_processor_thread.c
@@ -132,7 +132,7 @@ void* funk2_virtual_processor_thread__start_function(void* args) {
 	//
 	// We could unassign virtual_processor_thread from virtual_processor here, but we don't want to very often.
 	//
-	if ((spinning_virtual_processor_thread_count > 2) &&
+	if ((spinning_virtual_processor_thread_count > 32) &&
 	    (line_length == (spinning_virtual_processor_thread_count - 1))) {
 	  funk2_virtual_processor__know_of_one_less_spinning_virtual_processor_thread(virtual_processor);
 	  funk2_virtual_processor_thread__unassign_from_virtual_processor(this);

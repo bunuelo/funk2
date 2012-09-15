9bfd5801e4b26f0cca2ea3ace2653052fa6e0d9a
Bo Morgan <bo@mit.edu>
2012-09-15 01:59:00 -0400

    adding debug_funk2.sh

---
+++ debug_funk2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export UBUNTU_MENUPROXY=0
+export LD_LIBRARY_PATH="lib/.libs:/opt/intel/Compiler/11.1/038/lib/intel64/"
+export DYLD_LIBRARY_PATH="lib/.libs"
+export PATH="lib/.libs:$PATH"
+echo "run" | gdb bin/funk2 $@

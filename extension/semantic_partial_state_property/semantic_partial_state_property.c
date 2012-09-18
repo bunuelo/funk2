421b66adaf44dcceba277282308dc13eef52d7f2
Bo Morgan <bo@mit.edu>
2012-09-18 14:19:41 -0400

    adding semantic_partial_state_property and semantic_partial_state_property_relation

--- extension/semantic_partial_state_property/semantic_partial_state_property.c
+++ extension/semantic_partial_state_property/semantic_partial_state_property.c
@@ -24,25 +24,74 @@
 
 // semantic_partial_state_property
 
-f2ptr raw__semantic_partial_state_property__type_create(f2ptr cause, f2ptr this, f2ptr semantic_realm) {
+f2ptr raw__semantic_partial_state_property__type_create(f2ptr cause, f2ptr this, f2ptr semantic_realm,
+							f2ptr source_type,
+							f2ptr source_key_type,
+							f2ptr source_key,
+							f2ptr source_value,
+							f2ptr key_type,
+							f2ptr key,
+							f2ptr target_type) {
   if (! raw__frame__contains_var(cause, this, new__symbol(cause, "type"))) {
     raw__frame__add_var_value(cause, this, new__symbol(cause, "type"), new__symbol(cause, "semantic_partial_state_property"));
   }
   assert_value(raw__semantic_partial_state__type_create(cause, this, semantic_realm));
+  raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "source_type"),     source_type);
+  raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "source_key_type"), source_key_type);
+  raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "source_key"),      source_key);
+  raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "source_value"),    source_value);
+  raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "key_type"),        key_type);
+  raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "key"),             key);
+  raw__semantic_frame__add(cause, this, new__symbol(cause, "property"), new__symbol(cause, "target_type"),     target_type);
   return this;
 }
 
-f2ptr raw__semantic_partial_state_property__new(f2ptr cause, f2ptr semantic_realm) {
+f2ptr raw__semantic_partial_state_property__new(f2ptr cause, f2ptr semantic_realm,
+						f2ptr source_type,
+						f2ptr source_key_type,
+						f2ptr source_key,
+						f2ptr source_value,
+						f2ptr key_type,
+						f2ptr key,
+						f2ptr target_type) {
   f2ptr this = assert_value(f2__frame__new(cause, nil));
-  assert_value(raw__semantic_partial_state_property__type_create(cause, this, semantic_realm));
+  assert_value(raw__semantic_partial_state_property__type_create(cause, this, semantic_realm,
+								 source_type,
+								 source_key_type,
+								 source_key,
+								 source_value,
+								 key_type,
+								 key,
+								 target_type));
   return this;
 }
 
-f2ptr f2__semantic_partial_state_property__new(f2ptr cause, f2ptr semantic_realm) {
+f2ptr f2__semantic_partial_state_property__new(f2ptr cause, f2ptr semantic_realm,
+					       f2ptr source_type,
+					       f2ptr source_key_type,
+					       f2ptr source_key,
+					       f2ptr source_value,
+					       f2ptr key_type,
+					       f2ptr key,
+					       f2ptr target_type) {
   assert_argument_type(semantic_realm, semantic_realm);
-  return raw__semantic_partial_state_property__new(cause, semantic_realm);
+  return raw__semantic_partial_state_property__new(cause, semantic_realm,
+						   source_type,
+						   source_key_type,
+						   source_key,
+						   source_value,
+						   key_type,
+						   key,
+						   target_type);
 }
-export_cefunk1(semantic_partial_state_property__new, semantic_realm, 0, "Returns a new semantic_partial_state_property object.");
+export_cefunk8(semantic_partial_state_property__new, semantic_realm,
+	       source_type,
+	       source_key_type,
+	       source_key,
+	       source_value,
+	       key_type,
+	       key,
+	       target_type, 0, "Returns a new semantic_partial_state_property object.");
 
 
 boolean_t raw__semantic_partial_state_property__is_type(f2ptr cause, f2ptr thing) {
@@ -78,11 +127,179 @@ f2ptr f2__semantic_partial_state_property__type(f2ptr cause, f2ptr this) {
 export_cefunk1(semantic_partial_state_property__type, thing, 0, "Returns the specific type of object that this semantic_partial_state_property is.");
 
 
+f2ptr raw__semantic_partial_state_property__source_type(f2ptr cause, f2ptr this) {
+  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "source_type"));
+}
+
+f2ptr f2__semantic_partial_state_property__source_type(f2ptr cause, f2ptr this) {
+  assert_argument_type(semantic_partial_state_property, this);
+  return raw__semantic_partial_state_property__source_type(cause, this);
+}
+export_cefunk1(semantic_partial_state_property__source_type, this, 0, "");
+
+
+f2ptr raw__semantic_partial_state_property__source_type__set(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "source_type"), that);
+}
+
+f2ptr f2__semantic_partial_state_property__source_type__set(f2ptr cause, f2ptr this, f2ptr that) {
+  assert_argument_type(semantic_partial_state_property, this);
+  return raw__semantic_partial_state_property__source_type__set(cause, this, that);
+}
+export_cefunk2(semantic_partial_state_property__source_type__set, this, that, 0, "");
+
+
+f2ptr raw__semantic_partial_state_property__source_key_type(f2ptr cause, f2ptr this) {
+  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "source_key_type"));
+}
+
+f2ptr f2__semantic_partial_state_property__source_key_type(f2ptr cause, f2ptr this) {
+  assert_argument_type(semantic_partial_state_property, this);
+  return raw__semantic_partial_state_property__source_key_type(cause, this);
+}
+export_cefunk1(semantic_partial_state_property__source_key_type, this, 0, "");
+
+
+f2ptr raw__semantic_partial_state_property__source_key_type__set(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "source_key_type"), that);
+}
+
+f2ptr f2__semantic_partial_state_property__source_key_type__set(f2ptr cause, f2ptr this, f2ptr that) {
+  assert_argument_type(semantic_partial_state_property, this);
+  return raw__semantic_partial_state_property__source_key_type__set(cause, this, that);
+}
+export_cefunk2(semantic_partial_state_property__source_key_type__set, this, that, 0, "");
+
+
+f2ptr raw__semantic_partial_state_property__source_key(f2ptr cause, f2ptr this) {
+  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "source_key"));
+}
+
+f2ptr f2__semantic_partial_state_property__source_key(f2ptr cause, f2ptr this) {
+  assert_argument_type(semantic_partial_state_property, this);
+  return raw__semantic_partial_state_property__source_key(cause, this);
+}
+export_cefunk1(semantic_partial_state_property__source_key, this, 0, "");
+
+
+f2ptr raw__semantic_partial_state_property__source_key__set(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "source_key"), that);
+}
+
+f2ptr f2__semantic_partial_state_property__source_key__set(f2ptr cause, f2ptr this, f2ptr that) {
+  assert_argument_type(semantic_partial_state_property, this);
+  return raw__semantic_partial_state_property__source_key__set(cause, this, that);
+}
+export_cefunk2(semantic_partial_state_property__source_key__set, this, that, 0, "");
+
+
+f2ptr raw__semantic_partial_state_property__source_value(f2ptr cause, f2ptr this) {
+  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "source_value"));
+}
+
+f2ptr f2__semantic_partial_state_property__source_value(f2ptr cause, f2ptr this) {
+  assert_argument_type(semantic_partial_state_property, this);
+  return raw__semantic_partial_state_property__source_value(cause, this);
+}
+export_cefunk1(semantic_partial_state_property__source_value, this, 0, "");
+
+
+f2ptr raw__semantic_partial_state_property__source_value__set(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "source_value"), that);
+}
+
+f2ptr f2__semantic_partial_state_property__source_value__set(f2ptr cause, f2ptr this, f2ptr that) {
+  assert_argument_type(semantic_partial_state_property, this);
+  return raw__semantic_partial_state_property__source_value__set(cause, this, that);
+}
+export_cefunk2(semantic_partial_state_property__source_value__set, this, that, 0, "");
+
+
+f2ptr raw__semantic_partial_state_property__key_type(f2ptr cause, f2ptr this) {
+  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "key_type"));
+}
+
+f2ptr f2__semantic_partial_state_property__key_type(f2ptr cause, f2ptr this) {
+  assert_argument_type(semantic_partial_state_property, this);
+  return raw__semantic_partial_state_property__key_type(cause, this);
+}
+export_cefunk1(semantic_partial_state_property__key_type, this, 0, "");
+
+
+f2ptr raw__semantic_partial_state_property__key_type__set(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "key_type"), that);
+}
+
+f2ptr f2__semantic_partial_state_property__key_type__set(f2ptr cause, f2ptr this, f2ptr that) {
+  assert_argument_type(semantic_partial_state_property, this);
+  return raw__semantic_partial_state_property__key_type__set(cause, this, that);
+}
+export_cefunk2(semantic_partial_state_property__key_type__set, this, that, 0, "");
+
+
+f2ptr raw__semantic_partial_state_property__key(f2ptr cause, f2ptr this) {
+  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "key"));
+}
+
+f2ptr f2__semantic_partial_state_property__key(f2ptr cause, f2ptr this) {
+  assert_argument_type(semantic_partial_state_property, this);
+  return raw__semantic_partial_state_property__key(cause, this);
+}
+export_cefunk1(semantic_partial_state_property__key, this, 0, "");
+
+
+f2ptr raw__semantic_partial_state_property__key__set(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "key"), that);
+}
+
+f2ptr f2__semantic_partial_state_property__key__set(f2ptr cause, f2ptr this, f2ptr that) {
+  assert_argument_type(semantic_partial_state_property, this);
+  return raw__semantic_partial_state_property__key__set(cause, this, that);
+}
+export_cefunk2(semantic_partial_state_property__key__set, this, that, 0, "");
+
+
+f2ptr raw__semantic_partial_state_property__target_type(f2ptr cause, f2ptr this) {
+  return raw__semantic_frame__lookup_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "target_type"));
+}
+
+f2ptr f2__semantic_partial_state_property__target_type(f2ptr cause, f2ptr this) {
+  assert_argument_type(semantic_partial_state_property, this);
+  return raw__semantic_partial_state_property__target_type(cause, this);
+}
+export_cefunk1(semantic_partial_state_property__target_type, this, 0, "");
+
+
+f2ptr raw__semantic_partial_state_property__target_type__set(f2ptr cause, f2ptr this, f2ptr that) {
+  return raw__semantic_frame__replace_type_var_value(cause, this, new__symbol(cause, "property"), new__symbol(cause, "target_type"), that);
+}
+
+f2ptr f2__semantic_partial_state_property__target_type__set(f2ptr cause, f2ptr this, f2ptr that) {
+  assert_argument_type(semantic_partial_state_property, this);
+  return raw__semantic_partial_state_property__target_type__set(cause, this, that);
+}
+export_cefunk2(semantic_partial_state_property__target_type__set, this, that, 0, "");
+
+
 f2ptr f2__semantic_partial_state_property__primobject_type__new(f2ptr cause) {
   f2ptr this = f2__primobject_type__new(cause, f2list1__new(cause, new__symbol(cause, "semantic_partial_state")));
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "new"),     f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__new")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "is_type"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__is_type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "type"),    f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "new"),             f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__new")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "is_type"),         f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__is_type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "type"),            f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "source_type"),     f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__source_type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "source_type"),     f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__source_type__set")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "source_key_type"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__source_key_type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "source_key_type"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__source_key_type__set")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "source_key"),      f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__source_key")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "source_key"),      f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__source_key__set")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "source_value"),    f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__source_value")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "source_value"),    f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__source_value__set")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "key_type"),        f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__key_type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "key_type"),        f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__key_type__set")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "key"),             f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__key")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "key"),             f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__key__set")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "target_type"),     f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__target_type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "set"),     new__symbol(cause, "target_type"),     f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_partial_state_property"), new__symbol(cause, "semantic_partial_state_property__target_type__set")));}
   return this;
 }
 

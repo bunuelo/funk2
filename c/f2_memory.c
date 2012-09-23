f78c84223b0f6d6e18b075e248351e617117dfbb
Bo Morgan <bo@mit.edu>
2012-09-23 13:25:14 -0400

    adding semantic_cons and semantic_thought

--- c/f2_memory.c
+++ c/f2_memory.c
@@ -287,7 +287,7 @@ f2ptr funk2_memory__funk2_memblock_f2ptr__try_new(funk2_memory_t* this, int pool
   
   this->pool[pool_index].total_free_memory                    -= funk2_memblock__byte_num(block);
   this->pool[pool_index].total_allocated_memory_since_last_gc += funk2_memblock__byte_num(block);
-  if (__funk2.memory.pool[pool_index].total_allocated_memory_since_last_gc >= (__funk2.memory.pool[pool_index].total_free_memory >> 8)) {
+  if (__funk2.memory.pool[pool_index].total_allocated_memory_since_last_gc >= (__funk2.memory.pool[pool_index].total_free_memory >> 1)) {
     __funk2.garbage_collector.gc_pool[pool_index].should_run_gc = boolean__true;
   }
   block->gc.tricolor = funk2_tricolor__white; // we can change the gc.tricolor of block as long as it is unused, otherwise we need to go through garbage_collector_pool functions for changing color.

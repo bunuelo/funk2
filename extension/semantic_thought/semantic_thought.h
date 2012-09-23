196770b4d2a9a7871c172cced3c9aad0e0d805fc
Bo Morgan <bo@mit.edu>
2012-09-23 19:35:29 -0400

    adding variable lookup to nonsemantic_plan

--- extension/semantic_thought/semantic_thought.h
+++ extension/semantic_thought/semantic_thought.h
@@ -23,7 +23,7 @@
 #define F2__EXTENSION__SEMANTIC_THOUGHT__H
 
 #include "../semantic_object/semantic_object.h"
-#include "../semantic_object/semantic_environment.h"
+#include "../semantic_environment/semantic_environment.h"
 
 
 // **

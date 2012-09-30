62a23ea6bfe923ce72c9806f0ef1778cf2f71f4e
Bo Morgan <bo@mit.edu>
2012-09-30 18:16:38 -0400

    adding mutable_array_pointer

--- c/f2_ptypes.h
+++ c/f2_ptypes.h
@@ -548,52 +548,18 @@ typedef enum larva_type_e {
   larva_type__invalid_type,
 } larva_type_t;
 
-f2ptr pfunk2__f2larva__new(f2ptr cause, u32 larva_type, f2ptr bug);
+f2ptr pfunk2__f2larva__new       (f2ptr cause, u32 larva_type, f2ptr bug);
 u32   pfunk2__f2larva__larva_type(f2ptr this, f2ptr cause);
-f2ptr pfunk2__f2larva__bug(f2ptr this, f2ptr cause);
-
-u32   raw__larva__larva_type(f2ptr cause, f2ptr this);
-f2ptr  f2__larva__larva_type(f2ptr cause, f2ptr this);
-
-f2ptr raw__larva__bug(f2ptr cause, f2ptr this);
-f2ptr  f2__larva__bug(f2ptr cause, f2ptr this);
-
-boolean_t raw__larva__is_type(f2ptr cause, f2ptr exp);
-f2ptr      f2__larva__is_type(f2ptr cause, f2ptr exp);
-
-boolean_t raw__larva__eq(f2ptr cause, f2ptr this, f2ptr that);
-f2ptr      f2__larva__eq(f2ptr cause, f2ptr this, f2ptr that);
-
-u64   raw__larva__eq_hash_value(f2ptr cause, f2ptr this);
-f2ptr  f2__larva__eq_hash_value(f2ptr cause, f2ptr this);
-
-boolean_t raw__larva__equals(f2ptr cause, f2ptr this, f2ptr that);
-f2ptr      f2__larva__equals(f2ptr cause, f2ptr this, f2ptr that);
+f2ptr pfunk2__f2larva__bug       (f2ptr this, f2ptr cause);
 
 
 // mutable_array_pointer
 
-f2ptr pfunk2__f2mutable_array_pointer__new(f2ptr cause, u64 index, f2ptr array);
-u64   pfunk2__f2mutable_array_pointer__index(f2ptr this, f2ptr cause);
-f2ptr pfunk2__f2mutable_array_pointer__array(f2ptr this, f2ptr cause);
-
-u64   raw__mutable_array_pointer__index(f2ptr cause, f2ptr this);
-f2ptr  f2__mutable_array_pointer__index(f2ptr cause, f2ptr this);
-
-f2ptr raw__mutable_array_pointer__array(f2ptr cause, f2ptr this);
-f2ptr  f2__mutable_array_pointer__array(f2ptr cause, f2ptr this);
-
-boolean_t raw__mutable_array_pointer__is_type(f2ptr cause, f2ptr exp);
-f2ptr      f2__mutable_array_pointer__is_type(f2ptr cause, f2ptr exp);
-
-boolean_t raw__mutable_array_pointer__eq(f2ptr cause, f2ptr this, f2ptr that);
-f2ptr      f2__mutable_array_pointer__eq(f2ptr cause, f2ptr this, f2ptr that);
-
-u64   raw__mutable_array_pointer__eq_hash_value(f2ptr cause, f2ptr this);
-f2ptr  f2__mutable_array_pointer__eq_hash_value(f2ptr cause, f2ptr this);
-
-boolean_t raw__mutable_array_pointer__equals(f2ptr cause, f2ptr this, f2ptr that);
-f2ptr      f2__mutable_array_pointer__equals(f2ptr cause, f2ptr this, f2ptr that);
+f2ptr pfunk2__f2mutable_array_pointer__new       (f2ptr cause, u64 index, f2ptr array);
+f2ptr pfunk2__f2mutable_array_pointer__array     (f2ptr this, f2ptr cause);
+void  pfunk2__f2mutable_array_pointer__array__set(f2ptr this, f2ptr cause, f2ptr value);
+u64   pfunk2__f2mutable_array_pointer__index     (f2ptr this, f2ptr cause);
+void  pfunk2__f2mutable_array_pointer__index__set(f2ptr this, f2ptr cause, u64 value);
 
 
 // gensym

2f06c975af00e6b1f2f107e1242dfd732082bd7e
Bo Morgan <bo@mit.edu>
2012-10-06 17:11:57 -0400

    changing semantic_resource_event_knowledge_base to forgetful_semantic_resource_event_knowledge_base

--- extension/forgetful_semantic_resource_event_knowledge_base/forgetful_semantic_resource_event_knowledge_base.c
+++ extension/forgetful_semantic_resource_event_knowledge_base/forgetful_semantic_resource_event_knowledge_base.c
@@ -19,37 +19,37 @@
 // rights to redistribute these changes.
 // 
 
-#include "semantic_resource_event_knowledge_base.h"
+#include "forgetful_semantic_resource_event_knowledge_base.h"
 
 
-// semantic_resource_event_knowledge_base
+// forgetful_semantic_resource_event_knowledge_base
 
-f2ptr raw__semantic_resource_event_knowledge_base__type_create(f2ptr cause, f2ptr this, f2ptr name, f2ptr semantic_realm) {
+f2ptr raw__forgetful_semantic_resource_event_knowledge_base__type_create(f2ptr cause, f2ptr this, f2ptr name, f2ptr semantic_realm) {
   if (! raw__frame__contains_var(cause, this, new__symbol(cause, "type"))) {
-    raw__frame__add_var_value(cause, this, new__symbol(cause, "type"), new__symbol(cause, "semantic_resource_event_knowledge_base"));
+    raw__frame__add_var_value(cause, this, new__symbol(cause, "type"), new__symbol(cause, "forgetful_semantic_resource_event_knowledge_base"));
   }
   assert_value(raw__forgetful_semantic_event_knowledge_base__type_create(cause, this, name, semantic_realm));
   return this;
 }
 
-f2ptr raw__semantic_resource_event_knowledge_base__new(f2ptr cause, f2ptr name, f2ptr semantic_realm) {
+f2ptr raw__forgetful_semantic_resource_event_knowledge_base__new(f2ptr cause, f2ptr name, f2ptr semantic_realm) {
   f2ptr this = assert_value(f2__frame__new(cause, nil));
-  assert_value(raw__semantic_resource_event_knowledge_base__type_create(cause, this, name, semantic_realm));
+  assert_value(raw__forgetful_semantic_resource_event_knowledge_base__type_create(cause, this, name, semantic_realm));
   return this;
 }
 
-f2ptr f2__semantic_resource_event_knowledge_base__new(f2ptr cause, f2ptr name, f2ptr semantic_realm) {
+f2ptr f2__forgetful_semantic_resource_event_knowledge_base__new(f2ptr cause, f2ptr name, f2ptr semantic_realm) {
   assert_argument_type(semantic_realm, semantic_realm);
-  return raw__semantic_resource_event_knowledge_base__new(cause, name, semantic_realm);
+  return raw__forgetful_semantic_resource_event_knowledge_base__new(cause, name, semantic_realm);
 }
-export_cefunk2(semantic_resource_event_knowledge_base__new, name, semantic_realm, 0, "Returns a new semantic_resource_event_knowledge_base object.");
+export_cefunk2(forgetful_semantic_resource_event_knowledge_base__new, name, semantic_realm, 0, "Returns a new forgetful_semantic_resource_event_knowledge_base object.");
 
 
-boolean_t raw__semantic_resource_event_knowledge_base__is_type(f2ptr cause, f2ptr thing) {
+boolean_t raw__forgetful_semantic_resource_event_knowledge_base__is_type(f2ptr cause, f2ptr thing) {
   if (! raw__frame__is_type(cause, thing)) {
     return boolean__false;
   }
-  f2ptr this_type_name_symbol = new__symbol(cause, "semantic_resource_event_knowledge_base");
+  f2ptr this_type_name_symbol = new__symbol(cause, "forgetful_semantic_resource_event_knowledge_base");
   f2ptr thing_type_name       = f2__frame__lookup_var_value(cause, thing, new__symbol(cause, "type"), nil);
   if (raw__eq(cause, this_type_name_symbol, thing_type_name)) {
     return boolean__true;
@@ -61,25 +61,25 @@ boolean_t raw__semantic_resource_event_knowledge_base__is_type(f2ptr cause, f2pt
   return boolean__false;
 }
 
-f2ptr f2__semantic_resource_event_knowledge_base__is_type(f2ptr cause, f2ptr thing) {
-  return f2bool__new(raw__semantic_resource_event_knowledge_base__is_type(cause, thing));
+f2ptr f2__forgetful_semantic_resource_event_knowledge_base__is_type(f2ptr cause, f2ptr thing) {
+  return f2bool__new(raw__forgetful_semantic_resource_event_knowledge_base__is_type(cause, thing));
 }
-export_cefunk1(semantic_resource_event_knowledge_base__is_type, thing, 0, "Returns whether or not thing is of type semantic_resource_event_knowledge_base.");
+export_cefunk1(forgetful_semantic_resource_event_knowledge_base__is_type, thing, 0, "Returns whether or not thing is of type forgetful_semantic_resource_event_knowledge_base.");
 
 
-f2ptr raw__semantic_resource_event_knowledge_base__type(f2ptr cause, f2ptr this) {
+f2ptr raw__forgetful_semantic_resource_event_knowledge_base__type(f2ptr cause, f2ptr this) {
   return f2__object__type(cause, this);
 }
 
-f2ptr f2__semantic_resource_event_knowledge_base__type(f2ptr cause, f2ptr this) {
-  assert_argument_type(semantic_resource_event_knowledge_base, this);
-  return raw__semantic_resource_event_knowledge_base__type(cause, this);
+f2ptr f2__forgetful_semantic_resource_event_knowledge_base__type(f2ptr cause, f2ptr this) {
+  assert_argument_type(forgetful_semantic_resource_event_knowledge_base, this);
+  return raw__forgetful_semantic_resource_event_knowledge_base__type(cause, this);
 }
-export_cefunk1(semantic_resource_event_knowledge_base__type, thing, 0,
-	       "Returns the specific type of object that this semantic_resource_event_knowledge_base is.");
+export_cefunk1(forgetful_semantic_resource_event_knowledge_base__type, thing, 0,
+	       "Returns the specific type of object that this forgetful_semantic_resource_event_knowledge_base is.");
 
 
-f2ptr raw__semantic_resource_event_knowledge_base__resource_targeting_events_containing_time(f2ptr cause, f2ptr this, f2ptr semantic_resource, f2ptr semantic_time) {
+f2ptr raw__forgetful_semantic_resource_event_knowledge_base__resource_targeting_events_containing_time(f2ptr cause, f2ptr this, f2ptr semantic_resource, f2ptr semantic_time) {
   f2ptr events_containing_time    = assert_value(f2__semantic_event_knowledge_base__events_containing_time(cause, this, semantic_time));
   f2ptr resource_targeting_events = nil;
   {
@@ -103,18 +103,18 @@ f2ptr raw__semantic_resource_event_knowledge_base__resource_targeting_events_con
   return resource_targeting_events;
 }
 
-f2ptr f2__semantic_resource_event_knowledge_base__resource_targeting_events_containing_time(f2ptr cause, f2ptr this, f2ptr semantic_resource, f2ptr semantic_time) {
-  assert_argument_type(semantic_resource_event_knowledge_base, this);
+f2ptr f2__forgetful_semantic_resource_event_knowledge_base__resource_targeting_events_containing_time(f2ptr cause, f2ptr this, f2ptr semantic_resource, f2ptr semantic_time) {
+  assert_argument_type(forgetful_semantic_resource_event_knowledge_base, this);
   assert_argument_type(semantic_resource,                      semantic_resource);
   assert_argument_type(semantic_time,                          semantic_time);
-  return raw__semantic_resource_event_knowledge_base__resource_targeting_events_containing_time(cause, this, semantic_resource, semantic_time);
+  return raw__forgetful_semantic_resource_event_knowledge_base__resource_targeting_events_containing_time(cause, this, semantic_resource, semantic_time);
 }
-export_cefunk3(semantic_resource_event_knowledge_base__resource_targeting_events_containing_time, this, semantic_resource, semantic_time, 0,
+export_cefunk3(forgetful_semantic_resource_event_knowledge_base__resource_targeting_events_containing_time, this, semantic_resource, semantic_time, 0,
 	       "Returns all of semantic_know_of_relationship_events containing the given semantic_time and targeting the given semantic_resource.");
 
 
 
-f2ptr raw__semantic_resource_event_knowledge_base__most_recent_targeting_events__filter_funk(f2ptr cause, f2ptr target_resource, f2ptr semantic_event) {
+f2ptr raw__forgetful_semantic_resource_event_knowledge_base__most_recent_targeting_events__filter_funk(f2ptr cause, f2ptr target_resource, f2ptr semantic_event) {
   if (raw__semantic_know_of_relationship_event__is_type(cause, semantic_event)) {
     f2ptr meta_relationship = f2__semantic_know_of_relationship_event__meta_relationship(cause, semantic_event);
     if (raw__relationship_meta_semantic_object__is_type(cause, meta_relationship)) {
@@ -127,37 +127,37 @@ f2ptr raw__semantic_resource_event_knowledge_base__most_recent_targeting_events_
   return f2bool__new(boolean__false);
 }
 
-f2ptr f2__semantic_resource_event_knowledge_base__most_recent_targeting_events__filter_funk(f2ptr cause, f2ptr target_resource, f2ptr semantic_event) {
+f2ptr f2__forgetful_semantic_resource_event_knowledge_base__most_recent_targeting_events__filter_funk(f2ptr cause, f2ptr target_resource, f2ptr semantic_event) {
   assert_argument_type(semantic_resource, target_resource);
   assert_argument_type(semantic_event,    semantic_event);
-  return raw__semantic_resource_event_knowledge_base__most_recent_targeting_events__filter_funk(cause, target_resource, semantic_event);
+  return raw__forgetful_semantic_resource_event_knowledge_base__most_recent_targeting_events__filter_funk(cause, target_resource, semantic_event);
 }
-export_cefunk2(semantic_resource_event_knowledge_base__most_recent_targeting_events__filter_funk, target_resource, semantic_event, 0,
+export_cefunk2(forgetful_semantic_resource_event_knowledge_base__most_recent_targeting_events__filter_funk, target_resource, semantic_event, 0,
 	       "Returns whether or not the given semantic_event targets the given target_resource.");
 
 
-f2ptr raw__semantic_resource_event_knowledge_base__most_recent_targeting_events(f2ptr cause, f2ptr this, f2ptr target_resource, f2ptr semantic_time) {
-  f2ptr filter_funk = f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_resource_event_knowledge_base"), new__symbol(cause, "semantic_resource_event_knowledge_base__most_recent_targeting_events__filter_funk"));
+f2ptr raw__forgetful_semantic_resource_event_knowledge_base__most_recent_targeting_events(f2ptr cause, f2ptr this, f2ptr target_resource, f2ptr semantic_time) {
+  f2ptr filter_funk = f2__core_extension_funk__new(cause, new__symbol(cause, "forgetful_semantic_resource_event_knowledge_base"), new__symbol(cause, "forgetful_semantic_resource_event_knowledge_base__most_recent_targeting_events__filter_funk"));
   return f2__semantic_event_knowledge_base__most_recent_filtered_events(cause, this, filter_funk, target_resource, semantic_time);
 }
 
-f2ptr f2__semantic_resource_event_knowledge_base__most_recent_targeting_events(f2ptr cause, f2ptr this, f2ptr target_resource, f2ptr semantic_time) {
-  assert_argument_type(semantic_resource_event_knowledge_base, this);
+f2ptr f2__forgetful_semantic_resource_event_knowledge_base__most_recent_targeting_events(f2ptr cause, f2ptr this, f2ptr target_resource, f2ptr semantic_time) {
+  assert_argument_type(forgetful_semantic_resource_event_knowledge_base, this);
   assert_argument_type(semantic_resource,                      target_resource);
   assert_argument_type(semantic_time,                          semantic_time);
-  return raw__semantic_resource_event_knowledge_base__most_recent_targeting_events(cause, this, target_resource, semantic_time);
+  return raw__forgetful_semantic_resource_event_knowledge_base__most_recent_targeting_events(cause, this, target_resource, semantic_time);
 }
-export_cefunk3(semantic_resource_event_knowledge_base__most_recent_targeting_events, this, target_resource, semantic_time, 0,
+export_cefunk3(forgetful_semantic_resource_event_knowledge_base__most_recent_targeting_events, this, target_resource, semantic_time, 0,
 	       "Returns all of semantic_know_of_relationship_events containing the given semantic_time and targeting the given semantic_resource.");
 
 
-f2ptr f2__semantic_resource_event_knowledge_base__primobject_type__new(f2ptr cause) {
+f2ptr f2__forgetful_semantic_resource_event_knowledge_base__primobject_type__new(f2ptr cause) {
   f2ptr this = f2__primobject_type__new(cause, f2list1__new(cause, new__symbol(cause, "forgetful_semantic_event_knowledge_base")));
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "new"),                                       f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_resource_event_knowledge_base"), new__symbol(cause, "semantic_resource_event_knowledge_base__new")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "is_type"),                                   f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_resource_event_knowledge_base"), new__symbol(cause, "semantic_resource_event_knowledge_base__is_type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "type"),                                      f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_resource_event_knowledge_base"), new__symbol(cause, "semantic_resource_event_knowledge_base__type")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "resource_targeting_events_containing_time"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_resource_event_knowledge_base"), new__symbol(cause, "semantic_resource_event_knowledge_base__resource_targeting_events_containing_time")));}
-  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "most_recent_targeting_events"),              f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_resource_event_knowledge_base"), new__symbol(cause, "semantic_resource_event_knowledge_base__most_recent_targeting_events")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "new"),                                       f2__core_extension_funk__new(cause, new__symbol(cause, "forgetful_semantic_resource_event_knowledge_base"), new__symbol(cause, "forgetful_semantic_resource_event_knowledge_base__new")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "execute"), new__symbol(cause, "is_type"),                                   f2__core_extension_funk__new(cause, new__symbol(cause, "forgetful_semantic_resource_event_knowledge_base"), new__symbol(cause, "forgetful_semantic_resource_event_knowledge_base__is_type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "type"),                                      f2__core_extension_funk__new(cause, new__symbol(cause, "forgetful_semantic_resource_event_knowledge_base"), new__symbol(cause, "forgetful_semantic_resource_event_knowledge_base__type")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "resource_targeting_events_containing_time"), f2__core_extension_funk__new(cause, new__symbol(cause, "forgetful_semantic_resource_event_knowledge_base"), new__symbol(cause, "forgetful_semantic_resource_event_knowledge_base__resource_targeting_events_containing_time")));}
+  {f2__primobject_type__add_slot_type(cause, this, new__symbol(cause, "get"),     new__symbol(cause, "most_recent_targeting_events"),              f2__core_extension_funk__new(cause, new__symbol(cause, "forgetful_semantic_resource_event_knowledge_base"), new__symbol(cause, "forgetful_semantic_resource_event_knowledge_base__most_recent_targeting_events")));}
   return this;
 }
 
@@ -167,31 +167,31 @@ f2ptr f2__semantic_resource_event_knowledge_base__primobject_type__new(f2ptr cau
 
 // **
 
-f2ptr f2__semantic_resource_event_knowledge_base__core_extension__ping(f2ptr cause) {
+f2ptr f2__forgetful_semantic_resource_event_knowledge_base__core_extension__ping(f2ptr cause) {
   return nil;
 }
-export_cefunk0(semantic_resource_event_knowledge_base__core_extension__ping, 0, "");
+export_cefunk0(forgetful_semantic_resource_event_knowledge_base__core_extension__ping, 0, "");
 
-f2ptr f2__semantic_resource_event_knowledge_base__core_extension__initialize(f2ptr cause) {
-  core_extension__ping(semantic_resource_event_knowledge_base, forgetful_semantic_event_knowledge_base);
-  core_extension__ping(semantic_resource_event_knowledge_base, semantic_know_of_relationship_event);
-  core_extension__ping(semantic_resource_event_knowledge_base, semantic_resource);
+f2ptr f2__forgetful_semantic_resource_event_knowledge_base__core_extension__initialize(f2ptr cause) {
+  core_extension__ping(forgetful_semantic_resource_event_knowledge_base, forgetful_semantic_event_knowledge_base);
+  core_extension__ping(forgetful_semantic_resource_event_knowledge_base, semantic_know_of_relationship_event);
+  core_extension__ping(forgetful_semantic_resource_event_knowledge_base, semantic_resource);
   return nil;
 }
-export_cefunk0(semantic_resource_event_knowledge_base__core_extension__initialize, 0, "");
+export_cefunk0(forgetful_semantic_resource_event_knowledge_base__core_extension__initialize, 0, "");
 
-f2ptr f2__semantic_resource_event_knowledge_base__core_extension__define_types(f2ptr cause) {
-  f2__add_type(cause, new__symbol(cause, "semantic_resource_event_knowledge_base"), f2__semantic_resource_event_knowledge_base__primobject_type__new(cause));
-  status("semantic_resource_event_knowledge_base defined types.");
+f2ptr f2__forgetful_semantic_resource_event_knowledge_base__core_extension__define_types(f2ptr cause) {
+  f2__add_type(cause, new__symbol(cause, "forgetful_semantic_resource_event_knowledge_base"), f2__forgetful_semantic_resource_event_knowledge_base__primobject_type__new(cause));
+  status("forgetful_semantic_resource_event_knowledge_base defined types.");
   return nil;
 }
-export_cefunk0(semantic_resource_event_knowledge_base__core_extension__define_types, 0, "");
+export_cefunk0(forgetful_semantic_resource_event_knowledge_base__core_extension__define_types, 0, "");
 
-f2ptr f2__semantic_resource_event_knowledge_base__core_extension__destroy(f2ptr cause) {
-  status("semantic_resource_event_knowledge_base destroyed.");
+f2ptr f2__forgetful_semantic_resource_event_knowledge_base__core_extension__destroy(f2ptr cause) {
+  status("forgetful_semantic_resource_event_knowledge_base destroyed.");
   return nil;
 }
-export_cefunk0(semantic_resource_event_knowledge_base__core_extension__destroy, 0, "");
+export_cefunk0(forgetful_semantic_resource_event_knowledge_base__core_extension__destroy, 0, "");
 
 
 
--- extension/forgetful_semantic_resource_event_knowledge_base/forgetful_semantic_resource_event_knowledge_base.h
+++ extension/forgetful_semantic_resource_event_knowledge_base/forgetful_semantic_resource_event_knowledge_base.h
@@ -19,33 +19,33 @@
 // rights to redistribute these changes.
 // 
 
-#ifndef F2__EXTENSION__SEMANTIC_RESOURCE_EVENT_KNOWLEDGE_BASE__H
-#define F2__EXTENSION__SEMANTIC_RESOURCE_EVENT_KNOWLEDGE_BASE__H
+#ifndef F2__EXTENSION__FORGETFUL_SEMANTIC_RESOURCE_EVENT_KNOWLEDGE_BASE__H
+#define F2__EXTENSION__FORGETFUL_SEMANTIC_RESOURCE_EVENT_KNOWLEDGE_BASE__H
 
 #include "../../c/funk2.h"
 #include "../forgetful_semantic_event_knowledge_base/forgetful_semantic_event_knowledge_base.h"
 #include "../semantic_know_of_relationship_event/semantic_know_of_relationship_event.h"
 #include "../semantic_resource/semantic_resource.h"
 
-// semantic_resource_event_knowledge_base
+// forgetful_semantic_resource_event_knowledge_base
 
-f2ptr     raw__semantic_resource_event_knowledge_base__type_create                 (f2ptr cause, f2ptr this, f2ptr name, f2ptr semantic_realm);
-f2ptr     raw__semantic_resource_event_knowledge_base__new                         (f2ptr cause, f2ptr name, f2ptr semantic_realm);
-f2ptr      f2__semantic_resource_event_knowledge_base__new                         (f2ptr cause, f2ptr name, f2ptr semantic_realm);
-boolean_t raw__semantic_resource_event_knowledge_base__is_type                     (f2ptr cause, f2ptr thing);
-f2ptr      f2__semantic_resource_event_knowledge_base__is_type                     (f2ptr cause, f2ptr thing);
-f2ptr     raw__semantic_resource_event_knowledge_base__type                        (f2ptr cause, f2ptr this);
-f2ptr      f2__semantic_resource_event_knowledge_base__type                        (f2ptr cause, f2ptr this);
-f2ptr     raw__semantic_resource_event_knowledge_base__most_recent_targeting_events(f2ptr cause, f2ptr this, f2ptr target_resource, f2ptr semantic_time);
-f2ptr      f2__semantic_resource_event_knowledge_base__most_recent_targeting_events(f2ptr cause, f2ptr this, f2ptr target_resource, f2ptr semantic_time);
+f2ptr     raw__forgetful_semantic_resource_event_knowledge_base__type_create                 (f2ptr cause, f2ptr this, f2ptr name, f2ptr semantic_realm);
+f2ptr     raw__forgetful_semantic_resource_event_knowledge_base__new                         (f2ptr cause, f2ptr name, f2ptr semantic_realm);
+f2ptr      f2__forgetful_semantic_resource_event_knowledge_base__new                         (f2ptr cause, f2ptr name, f2ptr semantic_realm);
+boolean_t raw__forgetful_semantic_resource_event_knowledge_base__is_type                     (f2ptr cause, f2ptr thing);
+f2ptr      f2__forgetful_semantic_resource_event_knowledge_base__is_type                     (f2ptr cause, f2ptr thing);
+f2ptr     raw__forgetful_semantic_resource_event_knowledge_base__type                        (f2ptr cause, f2ptr this);
+f2ptr      f2__forgetful_semantic_resource_event_knowledge_base__type                        (f2ptr cause, f2ptr this);
+f2ptr     raw__forgetful_semantic_resource_event_knowledge_base__most_recent_targeting_events(f2ptr cause, f2ptr this, f2ptr target_resource, f2ptr semantic_time);
+f2ptr      f2__forgetful_semantic_resource_event_knowledge_base__most_recent_targeting_events(f2ptr cause, f2ptr this, f2ptr target_resource, f2ptr semantic_time);
 
-f2ptr f2__semantic_resource_event_knowledge_base__primobject_type__new(f2ptr cause);
+f2ptr f2__forgetful_semantic_resource_event_knowledge_base__primobject_type__new(f2ptr cause);
 
 // **
 
-f2ptr f2__semantic_resource_event_knowledge_base__core_extension__ping      (f2ptr cause);
-f2ptr f2__semantic_resource_event_knowledge_base__core_extension__initialize(f2ptr cause);
-f2ptr f2__semantic_resource_event_knowledge_base__core_extension__destroy   (f2ptr cause);
+f2ptr f2__forgetful_semantic_resource_event_knowledge_base__core_extension__ping      (f2ptr cause);
+f2ptr f2__forgetful_semantic_resource_event_knowledge_base__core_extension__initialize(f2ptr cause);
+f2ptr f2__forgetful_semantic_resource_event_knowledge_base__core_extension__destroy   (f2ptr cause);
 
-#endif // F2__EXTENSION__SEMANTIC_RESOURCE_EVENT_KNOWLEDGE_BASE__H
+#endif // F2__EXTENSION__FORGETFUL_SEMANTIC_RESOURCE_EVENT_KNOWLEDGE_BASE__H
 

8706ff9c4ba7db88be1c2314d445bcf4431913ff
Bo Morgan <bo@mit.edu>
2012-10-09 17:22:17 -0400

    debugging remove semantic_frame slot bugs (not adding correctly?)

--- extension/semantic_knowledge_base/semantic_knowledge_base.c
+++ extension/semantic_knowledge_base/semantic_knowledge_base.c
@@ -120,13 +120,13 @@ export_cefunk2(semantic_knowledge_base_event__semantic_frame__set, thing, semant
 
 f2ptr f2__semantic_knowledge_base_event_type__new(f2ptr cause) {
   f2ptr this = f2__primobject_type__new(cause, f2list1__new(cause, new__symbol(cause, "event_stream_event")));
-  {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.execute__symbol, new__symbol(cause, "new"),            f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_knowledge_base"), new__symbol(cause, "semantic_knowledge_base_event__new")));}
-  {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.execute__symbol, new__symbol(cause, "is_type"),        f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_knowledge_base"), new__symbol(cause, "semantic_knowledge_base_event__is_type")));}
-  {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.get__symbol,     new__symbol(cause, "type"),           f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_knowledge_base"), new__symbol(cause, "semantic_knowledge_base_event__type")));}
-  {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.get__symbol,     new__symbol(cause, "event_type"),     f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_knowledge_base"), new__symbol(cause, "semantic_knowledge_base_event__event_type")));}
-  {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.set__symbol,     new__symbol(cause, "event_type"),     f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_knowledge_base"), new__symbol(cause, "semantic_knowledge_base_event__event_type__set")));}
-  {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.get__symbol,     new__symbol(cause, "semantic_frame"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_knowledge_base"), new__symbol(cause, "semantic_knowledge_base_event__semantic_frame")));}
-  {f2__primobject_type__add_slot_type(cause, this, __funk2.globalenv.set__symbol,     new__symbol(cause, "semantic_frame"), f2__core_extension_funk__new(cause, new__symbol(cause, "semantic_knowledge_base"), new__symbol(cause, "semantic_knowledge_base_event__semantic_frame__set")));}
+  core_extension__primobject_type__add_funk("semantic_knowledge_base", "semantic_knowledge_base_event", "execute", "new",            "new");
+  core_extension__primobject_type__add_funk("semantic_knowledge_base", "semantic_knowledge_base_event", "execute", "is_type",        "is_type");
+  core_extension__primobject_type__add_funk("semantic_knowledge_base", "semantic_knowledge_base_event", "get",     "type",           "type");
+  core_extension__primobject_type__add_funk("semantic_knowledge_base", "semantic_knowledge_base_event", "get",     "event_type",     "event_type");
+  core_extension__primobject_type__add_funk("semantic_knowledge_base", "semantic_knowledge_base_event", "set",     "event_type",     "event_type__set");
+  core_extension__primobject_type__add_funk("semantic_knowledge_base", "semantic_knowledge_base_event", "get",     "semantic_frame", "semantic_frame");
+  core_extension__primobject_type__add_funk("semantic_knowledge_base", "semantic_knowledge_base_event", "set",     "semantic_frame", "semantic_frame__set");
   return this;
 }
 

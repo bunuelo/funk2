11f6496a0260c04e4995ce04216d9c58e7be93f1
Bo Morgan <bo@mit.edu>
2012-10-10 17:00:30 -0400

    adding view_strings.sh

--- view_strings.sh
+++ view_strings.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-strings lib/.libs/libfunk2.a | sed 's/-/\n/g' | sed 's/_/\n/g' | sed 's/ /\n/g' | sort | uniq | less
+strings lib/.libs/libfunk2.a | sed 's/-/\n/g' | sed 's/_/\n/g' | sed 's/ /\n/g' | uniq | sort | less

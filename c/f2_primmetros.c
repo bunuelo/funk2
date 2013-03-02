b7792a054dce34e833de92387d29a2aa7df48467
Bo Morgan <bo@mit.edu>
2013-03-02 11:55:03 -0500

    polishing

--- c/f2_primmetros.c
+++ c/f2_primmetros.c
@@ -126,7 +126,8 @@ f2ptr raw__primmetro__let(f2ptr cause, f2ptr variable_definitions, f2ptr body_ex
 					    environment);
     return f2list3__new(cause,
 			new__symbol(cause, "funk-local_apply"),
-			primmetro_new_funk,
+			compiled_funk,
+			//primmetro_new_funk,
 			f2cons__new(cause,
 				    new__symbol(cause, "immutable_conslist"),
 				    definitions));

c78c5f8e06021ee564cf75f7a68eafad3934de5c
Bo Morgan <bo@mit.edu>
2013-03-02 11:37:48 -0500

    polishing

--- c/f2_primmetros.c
+++ c/f2_primmetros.c
@@ -118,11 +118,10 @@ f2ptr raw__primmetro__let(f2ptr cause, f2ptr variable_definitions, f2ptr body_ex
 							      nil);
     f2ptr fiber                                = assert_value(f2__this__fiber(cause));
     f2ptr fiber__environment                   = assert_value(f2__fiber__env(cause, fiber));
-    f2__print(cause, fiber__environment);
     f2ptr body_expressions__demetropolize_full = assert_value(f2__exps_demetropolize_full(cause, fiber, environment, body_expressions));
     f2ptr compiled_funk                        = assert_value(f2__funk__new(cause, fiber, fiber__environment, new__symbol(cause, "let"), variables, body_expressions__demetropolize_full, body_expressions, nil, nil, nil));
     return f2list3__new(cause,
-			new__symbol(cause, "funk-local_apply"),
+			new__symbol(cause, "funk-apply"),
 			compiled_funk,
 			f2cons__new(cause,
 				    new__symbol(cause, "immutable_conslist"),

4d7232dc436d64a4d68e5c2c61bdbfdd0c8b1e77
Bo Morgan <bo@mit.edu>
2013-03-06 10:46:54 -0500

    polishing

--- c/f2_primobjects.c
+++ c/f2_primobjects.c
@@ -1173,12 +1173,24 @@ f2ptr raw__bytecode__terminal_print_with_frame(f2ptr cause, f2ptr this, f2ptr te
   f2ptr print_as_frame_hash = raw__terminal_print_frame__print_as_frame_hash(cause, terminal_print_frame);
   f2ptr frame               = raw__ptypehash__lookup(cause, print_as_frame_hash, this);
   if (frame == nil) {
+    f2ptr print_arg0 = f2__bytecode__arg0(cause, this);
+    if (raw__cons__is_type(cause, print_arg0)) {
+      print_arg0 = new__symbol(cause, "*");
+    }
+    f2ptr print_arg1 = f2__bytecode__arg1(cause, this);
+    if (raw__cons__is_type(cause, print_arg1)) {
+      print_arg1 = new__symbol(cause, "*");
+    }
+    f2ptr print_arg2 = f2__bytecode__arg2(cause, this);
+    if (raw__cons__is_type(cause, print_arg2)) {
+      print_arg2 = new__symbol(cause, "*");
+    }
     frame = f2__frame__new(cause, f2list10__new(cause,
 						new__symbol(cause, "print_object_type"), new__symbol(cause, "bytecode"),
 						new__symbol(cause, "command"), f2__bytecode__command(cause, this),
-						new__symbol(cause, "arg0"),    f2__bytecode__arg0(   cause, this),
-						new__symbol(cause, "arg1"),    f2__bytecode__arg1(   cause, this),
-						new__symbol(cause, "arg2"),    f2__bytecode__arg2(   cause, this)));
+						new__symbol(cause, "arg0"),    print_arg0,
+						new__symbol(cause, "arg1"),    print_arg1,
+						new__symbol(cause, "arg2"),    print_arg2));
     f2__ptypehash__add(cause, print_as_frame_hash, this, frame);
   }
   return raw__frame__terminal_print_with_frame(cause, frame, terminal_print_frame);

7eaee9db52d268b40f818b4571393d1ffc015d10
Bo Morgan <bo@mit.edu>
2013-03-16 16:13:56 -0400

    adding machine_code_chunk_list-concat

--- c/f2_compile_x86.h
+++ c/f2_compile_x86.h
@@ -39,7 +39,7 @@ declare_object_type_3_slot(machine_code_jump, index, command, label,
 // machine_code_chunk
 
 typedef struct funk2_object_type__machine_code_chunk__slot_s funk2_object_type__machine_code_chunk__slot_t;
-declare_object_type_3_slot(machine_code_chunk, chunk, index_label_ptypehash, relative_jump_expressions,
+declare_object_type_3_slot(machine_code_chunk, chunk, index_label_ptypehash, jumps,
 			   f2ptr terminal_print_with_frame__symbol;
 			   f2ptr terminal_print_with_frame__funk;
 			   );

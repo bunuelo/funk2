32b337ff9327b3b2c1882a8abbfea32c03b9a4d2
Bo Morgan <bo@mit.edu>
2013-03-16 15:22:19 -0400

    adding machine_code_chunk_list-concat

--- c/f2_compile_x86.h
+++ c/f2_compile_x86.h
@@ -89,6 +89,8 @@ declare_primobject_3_slot(machine_code_jump, chunk, index_label_ptypehash, relat
 f2ptr raw__machine_code_jump__new(f2ptr cause, f2ptr chunk, f2ptr index_label_ptypehash, f2ptr relative_jump_expressions);
 f2ptr  f2__machine_code_jump__new(f2ptr cause, f2ptr chunk);
 
+f2ptr f2machine_code_jump__primobject_type__new_aux(f2ptr cause);
+
 
 // machine_code_chunk
 
@@ -97,12 +99,12 @@ declare_primobject_3_slot(machine_code_chunk, chunk, index_label_ptypehash, rela
 f2ptr raw__machine_code_chunk__new(f2ptr cause, f2ptr chunk, f2ptr index_label_ptypehash, f2ptr relative_jump_expressions);
 f2ptr  f2__machine_code_chunk__new(f2ptr cause, f2ptr chunk);
 
+f2ptr f2machine_code_chunk__primobject_type__new_aux(f2ptr cause);
 
 
 f2ptr f2chunk__new_compiled_from_funk(f2ptr cause, f2ptr funk);
 f2ptr f2chunk__new_compiled_from_metro(f2ptr cause, f2ptr metro);
 
-f2ptr f2machine_code_chunk__primobject_type__new_aux(f2ptr cause);
 
 f2ptr raw__expression__compile_x86(f2ptr cause, f2ptr expression);
 f2ptr  f2__expression__compile_x86(f2ptr cause, f2ptr expression);

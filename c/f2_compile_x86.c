7db50b13a1ea6c9658b2e8a09485e01aaaba2f58
Bo Morgan <bo@mit.edu>
2013-03-20 16:50:23 -0700

    working on machine_code_jump

--- c/f2_compile_x86.c
+++ c/f2_compile_x86.c
@@ -1252,12 +1252,12 @@ f2ptr raw__machine_code_jump__terminal_print_with_frame(f2ptr cause, f2ptr this,
   f2ptr print_as_frame_hash = raw__terminal_print_frame__print_as_frame_hash(cause, terminal_print_frame);
   f2ptr frame               = raw__ptypehash__lookup(cause, print_as_frame_hash, this);
   if (frame == nil) {
-    frame = f2__frame__new(cause, f2list8__new(cause,
-					       new__symbol(cause, "print_object_type"), new__symbol(cause, "machine_code_jump"),
-					       new__symbol(cause, "index"),             f2__machine_code_jump__index(    cause, this),
-					       new__symbol(cause, "command"),           f2__machine_code_jump__command(  cause, this),
-					       new__symbol(cause, "label"),             f2__machine_code_jump__label(    cause, this),
-					       new__symbol(cause, "arguments"),         f2__machine_code_jump__arguments(cause, this)));
+    frame = f2__frame__new(cause, f2list10__new(cause,
+						new__symbol(cause, "print_object_type"), new__symbol(cause, "machine_code_jump"),
+						new__symbol(cause, "index"),             f2__machine_code_jump__index(    cause, this),
+						new__symbol(cause, "command"),           f2__machine_code_jump__command(  cause, this),
+						new__symbol(cause, "label"),             f2__machine_code_jump__label(    cause, this),
+						new__symbol(cause, "arguments"),         f2__machine_code_jump__arguments(cause, this)));
     f2__ptypehash__add(cause, print_as_frame_hash, this, frame);
   }
   return raw__frame__terminal_print_with_frame(cause, frame, terminal_print_frame);

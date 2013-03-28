2a2b78fefb0ec15f5a2738761b426c235ec1b47d
Bo Morgan <bo@mit.edu>
2013-03-28 15:44:56 -0700

    working on thread specific SIGSEGV handler.

--- c/f2_signal.c
+++ c/f2_signal.c
@@ -143,6 +143,7 @@ void funk2_receive_signal(int sig, siginfo_t *info, void *arg) {
     status_backtrace();
     funk2_signal_segv_handler(sig, info, arg);
     status("funk2 warning: thread received segmentation fault (SIGSEGV) at address %p.  calling f2__this__fiber__exit.", info->si_addr);
+    f2ptr cause = nil;
     f2__this__fiber__exit(nil, new__error(f2list2__new(cause,
 						       new__symbol(cause, "bug_name"), new__symbol(cause, "SIGSEGV"))));
     status("funk2 warning: thread received segmentation fault (SIGSEGV) at address %p.  should not get here.  calling exit", info->si_addr);

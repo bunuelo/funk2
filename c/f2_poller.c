e58642e6aa204f2e4a8ad7965616def73601a7a2
Bo Morgan <bo@mit.edu>
2013-04-02 10:34:20 -0700

    working on variable processor speed

--- c/f2_poller.c
+++ c/f2_poller.c
@@ -89,12 +89,12 @@ void funk2_poller__sleep(funk2_poller_t* this) {
     this->average_cpu_usage = 1.0;
   }
   if (this->average_cpu_usage < (this->target_cpu_usage / 2)) {
-    this->sleep_nanoseconds /= 2;
+    this->sleep_nanoseconds /= pow(2, 1.0 / this->average_count);
     if (this->sleep_nanoseconds == 0) {
       this->sleep_nanoseconds = 1;
     }
   } else if (this->average_cpu_usage > (this->target_cpu_usage * 2)) {
-    this->sleep_nanoseconds *= 2;
+    this->sleep_nanoseconds *= pow(2, 1.0 / this->average_count);
   }
   raw__nanosleep(this->sleep_nanoseconds);
   this->last_real_nanoseconds      = real_nanoseconds;

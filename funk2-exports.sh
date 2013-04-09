c5d99c38be489ba4f73539fb322a9e18cdbed81b
Bo Morgan <bo@mit.edu>
2013-04-09 07:29:03 -0700

    fixing compile for cygwin (mingw-64)

--- funk2-exports.sh
+++ funk2-exports.sh
@@ -2,4 +2,4 @@
 export UBUNTU_MENUPROXY=0
 export LD_LIBRARY_PATH="lib/.libs:/opt/intel/Compiler/11.1/038/lib/intel64/"
 export DYLD_LIBRARY_PATH="lib/.libs"
-export PATH="lib/.libs:$PATH"
+export PATH="lib/.libs:/usr/x86_64-w64-mingw32/sys-root/mingw/bin/:$PATH"

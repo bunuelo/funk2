d679ef81daa9753e3004efb75d758ca01b099df6
Bo Morgan <bo@mit.edu>
2013-04-11 11:19:13 -0700

    debugging SIGSEGV in Windows bootstrapping.

--- c/f2_poller.c
+++ c/f2_poller.c
@@ -117,13 +117,15 @@ void funk2_poller__sleep(funk2_poller_t* this) {
   this->total_sleep_cycle_count ++;
   if ((real_nanoseconds - this->last_print_debug_nanoseconds_since_1970) >= 10 * nanoseconds_per_second) {
     this->last_print_debug_nanoseconds_since_1970 = real_nanoseconds;
-    status("funk2_poller vvv *** debug info *** vvv");
-    status("funk2_poller total_sleep_cycle_count=" u64__fstr, this->total_sleep_cycle_count);
-    status("funk2_poller last_reset_seconds     =%g", ((double)(real_nanoseconds - this->last_reset_nanoseconds_since_1970)) / (double)nanoseconds_per_second);
-    status("funk2_poller sleep_seconds          =%g", (double)(this->sleep_nanoseconds) / (double)nanoseconds_per_second);
-    status("funk2_poller average_cpu_usage      =%g", (double)(this->average_cpu_usage));
-    status_backtrace();
-    status("funk2_poller ^^^ *** debug info *** ^^^");
+    if (this->average_cpu_usage > (10 * this->target_cpu_usage)) {
+      status("funk2_poller vvv *** debug info *** vvv");
+      status("funk2_poller total_sleep_cycle_count=" u64__fstr, this->total_sleep_cycle_count);
+      status("funk2_poller last_reset_seconds     =%g", ((double)(real_nanoseconds - this->last_reset_nanoseconds_since_1970)) / (double)nanoseconds_per_second);
+      status("funk2_poller sleep_seconds          =%g", (double)(this->sleep_nanoseconds) / (double)nanoseconds_per_second);
+      status("funk2_poller average_cpu_usage      =%g", (double)(this->average_cpu_usage));
+      status_backtrace();
+      status("funk2_poller ^^^ *** debug info *** ^^^");
+    }
   }
 }
 

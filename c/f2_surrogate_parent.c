b238644787f8392e06e2754e12010aa5139ce6ef
Bo Morgan <bo@mit.edu>
2013-04-12 12:24:41 -0700

    debugging wasted CPU cycles on single processor hardware.  (adding f2_processor_conditionlock.[ch])

--- c/f2_surrogate_parent.c
+++ c/f2_surrogate_parent.c
@@ -140,7 +140,7 @@ void funk2_surrogate_parent__init(funk2_surrogate_parent_t* this) {
 	    exit(0);
 	  }
 	  if (bytes_in_buffer == 0) {
-	    __funk2__nanosleep(1000000);
+	    __funk2__nanosleep(10000000);
 	  }
 	}
 	memcpy(&fiber, f2ptr_buffer, sizeof(f2ptr));
@@ -160,7 +160,7 @@ void funk2_surrogate_parent__init(funk2_surrogate_parent_t* this) {
 		exit(0);
 	      }
 	      if (bytes_in_buffer == 0) {
-		__funk2__nanosleep(1000000);
+		__funk2__nanosleep(10000000);
 	      }
 	    }
 	    memcpy(&ch, ch_buffer, sizeof(u8));
